e3ded3bc6a64d6ca2a22a422ae5d5a8b492da25e
Alessio Rendina <asilcomando@gmail.com>
2018-03-14 23:19:28 +0100

    COMMERCE-31 Added new commerce wish list portlet keys

--- modules/dxp/apps/commerce/commerce-wish-list-api/src/main/java/com/liferay/commerce/wish/list/constants/CommerceWishListPortletKeys.java
+++ modules/dxp/apps/commerce/commerce-wish-list-api/src/main/java/com/liferay/commerce/wish/list/constants/CommerceWishListPortletKeys.java
@@ -16,6 +16,7 @@ package com.liferay.commerce.wish.list.constants;
 
 /**
  * @author Andrea Di Giorgi
+ * @author Alessio Antonio Rendina
  */
 public class CommerceWishListPortletKeys {
 
@@ -26,5 +27,9 @@ public class CommerceWishListPortletKeys {
 	public static final String COMMERCE_WISH_LIST_CONTENT =
 		"com_liferay_commerce_wish_list_web_internal_portlet_" +
 			"CommerceWishListContentPortlet";
+
+	public static final String MY_COMMERCE_WISH_LISTS =
+		"com_liferay_commerce_wish_list_web_internal_portlet_" +
+			"MyCommerceWishListsPortlet";
 
 }
