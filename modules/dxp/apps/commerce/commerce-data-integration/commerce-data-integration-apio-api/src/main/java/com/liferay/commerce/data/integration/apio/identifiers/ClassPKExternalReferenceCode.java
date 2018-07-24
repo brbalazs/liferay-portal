a49ad4f06bc4e3e6b6243f82eaab8ea75a4f257c
Rodrigo Guedes de Souza <rodrigo.guedes@liferay.com>
2018-07-24 13:10:23 -0300

    COMMERCE-562 Add ClassPKExternalReferenceCode

---
+++ modules/dxp/apps/commerce/commerce-data-integration/commerce-data-integration-apio-api/src/main/java/com/liferay/commerce/data/integration/apio/identifiers/ClassPKExternalReferenceCode.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.data.integration.apio.identifiers;
+
+import aQute.bnd.annotation.ProviderType;
+
+/**
+ * @author Rodrigo Guedes de Souza
+ */
+@ProviderType
+public interface ClassPKExternalReferenceCode {
+
+	public static ClassPKExternalReferenceCode create(
+		long classPK, String externalReferenceCode) {
+
+		if ((classPK == 0) || (externalReferenceCode == null)) {
+			return null;
+		}
+
+		return new ClassPKExternalReferenceCode() {
+
+			@Override
+			public long getClassPK() {
+				return classPK;
+			}
+
+			@Override
+			public String getExternalReferenceCode() {
+				return externalReferenceCode;
+			}
+
+		};
+	}
+
+	public long getClassPK();
+
+	public String getExternalReferenceCode();
+
+}
