a7a30694e7980aa4c0fd8f9baf24e16241ae9cdb
Sergio Gonzalez <sergio.gonzalez@liferay.com>
2018-10-04 16:22:13 +0200

    LPS-85998 We don't need to set groupPermissions and guestPermissions when cloning because we are already setting modelPermissions. Deprecate setGuestPermissions setGroupPermissions getGroupPermissions and getGuestPermissions.

--- portal-kernel/src/com/liferay/portal/kernel/service/ServiceContext.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/ServiceContext.java
@@ -113,8 +113,6 @@ public class ServiceContext implements Cloneable, Serializable {
 		serviceContext.setCurrentURL(getCurrentURL());
 		serviceContext.setExpandoBridgeAttributes(getExpandoBridgeAttributes());
 		serviceContext.setFailOnPortalException(isFailOnPortalException());
-		serviceContext.setGroupPermissions(getGroupPermissions());
-		serviceContext.setGuestPermissions(getGuestPermissions());
 
 		if (_headers != null) {
 			serviceContext.setHeaders(_headers);
@@ -209,12 +207,20 @@ public class ServiceContext implements Cloneable, Serializable {
 		}
 
 		String[] groupPermissions = groupPermissionsList.toArray(new String[0]);
-
-		setGroupPermissions(groupPermissions);
-
 		String[] guestPermissions = guestPermissionsList.toArray(new String[0]);
 
-		setGuestPermissions(guestPermissions);
+		ModelPermissions modelPermissions = getModelPermissions();
+
+		if (modelPermissions == null) {
+			modelPermissions = new ModelPermissions();
+		}
+
+		modelPermissions.addRolePermissions(
+			RoleConstants.PLACEHOLDER_DEFAULT_GROUP_ROLE, groupPermissions);
+		modelPermissions.addRolePermissions(
+			RoleConstants.GUEST, guestPermissions);
+
+		setModelPermissions(modelPermissions);
 	}
 
 	/**
@@ -384,7 +390,9 @@ public class ServiceContext implements Cloneable, Serializable {
 	 * resource.
 	 *
 	 * @return the specific group permissions
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
 	 */
+	@Deprecated
 	public String[] getGroupPermissions() {
 		if (_modelPermissions == null) {
 			return null;
@@ -424,7 +432,9 @@ public class ServiceContext implements Cloneable, Serializable {
 	 * resource.
 	 *
 	 * @return the specific guest permissions
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
 	 */
+	@Deprecated
 	public String[] getGuestPermissions() {
 		if (_modelPermissions == null) {
 			return null;
@@ -1010,14 +1020,6 @@ public class ServiceContext implements Cloneable, Serializable {
 
 		setFailOnPortalException(serviceContext.isFailOnPortalException());
 
-		if (serviceContext.getGroupPermissions() != null) {
-			setGroupPermissions(serviceContext.getGroupPermissions());
-		}
-
-		if (serviceContext.getGuestPermissions() != null) {
-			setGuestPermissions(serviceContext.getGuestPermissions());
-		}
-
 		if (serviceContext._headers != null) {
 			setHeaders(serviceContext._headers);
 		}
@@ -1031,6 +1033,10 @@ public class ServiceContext implements Cloneable, Serializable {
 
 		if (Validator.isNotNull(serviceContext.getLayoutURL())) {
 			setLayoutURL(serviceContext.getLayoutURL());
+		}
+
+		if (serviceContext.getModelPermissions() != null) {
+			setModelPermissions(serviceContext.getModelPermissions());
 		}
 
 		if (serviceContext.getModifiedDate() != null) {
@@ -1321,7 +1327,9 @@ public class ServiceContext implements Cloneable, Serializable {
 	 * manipulates the resource.
 	 *
 	 * @param groupPermissions the permissions (optionally <code>null</code>)
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
 	 */
+	@Deprecated
 	public void setGroupPermissions(String[] groupPermissions) {
 		if (_modelPermissions == null) {
 			_modelPermissions = new ModelPermissions();
@@ -1338,7 +1346,9 @@ public class ServiceContext implements Cloneable, Serializable {
 	 *
 	 * @param guestPermissions the guest permissions (optionally
 	 *        <code>null</code>)
+	 * @deprecated As of Judson (7.1.x), with no direct replacement
 	 */
+	@Deprecated
 	public void setGuestPermissions(String[] guestPermissions) {
 		if (_modelPermissions == null) {
 			_modelPermissions = new ModelPermissions();
