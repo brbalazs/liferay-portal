53bb7e859d74ac93ff0ddbe817d14fc50b6cb680
Hugo Huijser <hugo.huijser@liferay.com>
2018-10-09 10:03:16 +0800

    LPS-84119 SF

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-forms-web/src/main/java/com/liferay/portal/workflow/kaleo/forms/web/internal/portlet/KaleoFormsAdminPortlet.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-forms-web/src/main/java/com/liferay/portal/workflow/kaleo/forms/web/internal/portlet/KaleoFormsAdminPortlet.java
@@ -479,6 +479,7 @@ public class KaleoFormsAdminPortlet extends MVCPortlet {
 			actionRequest, "workflowDefinitionName");
 		int workflowDefinitionVersion = ParamUtil.getInteger(
 			actionRequest, "workflowDefinitionVersion");
+
 		String kaleoTaskFormPairsData = ParamUtil.getString(
 			actionRequest, "kaleoTaskFormPairsData");
 
