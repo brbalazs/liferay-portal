f09625589bc7fbc9b3e5bee4838e267f3b30c787
Marco Leo <marco.leo@liferay.com>
2019-01-08 11:29:52 +0100

    COMMERCE-774 Change table action variants for minium

---
+++ modules/dxp/apps/commerce/commerce-theme-minium/commerce-theme-minium-impl/src/main/java/com/liferay/commerce/theme/minium/impl/internal/table/contributor/CommerceAccountClayTableContextContributor.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.theme.minium.impl.internal.table.contributor;
+
+import com.liferay.commerce.frontend.ClayTable;
+import com.liferay.commerce.frontend.ClayTableContextContributor;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ */
+@Component(
+	immediate = true, property = "commerce.table.name=commerceAccounts",
+	service = ClayTableContextContributor.class
+)
+public class CommerceAccountClayTableContextContributor
+	implements ClayTableContextContributor {
+
+	@Override
+	public void contribute(
+		ClayTable clayTable, Map<String, Object> context,
+		HttpServletRequest httpServletRequest) {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		if (!_MINIUM_THEME_ID.equals(themeDisplay.getThemeId())) {
+			return;
+		}
+
+		context.put("actionsMenuVariant", "miniumActionsMenuVariant");
+	}
+
+	private static final String _MINIUM_THEME_ID = "minium_WAR_miniumtheme";
+
+}
