f5e0db78c2c2028be0855672a61048d979b48765
Alessio Rendina <asilcomando@gmail.com>
2019-01-14 20:29:29 +0100

    COMMERCE-778 Added new display context

---
+++ modules/dxp/apps/commerce/commerce-account-web/src/main/java/com/liferay/commerce/account/web/internal/display/context/CommerceAccountUserDisplayContext.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.account.web.internal.display.context;
+
+import com.liferay.commerce.account.service.CommerceAccountService;
+import com.liferay.commerce.account.util.CommerceAccountHelper;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.users.admin.configuration.UserFileUploadsConfiguration;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceAccountUserDisplayContext
+	extends BaseCommerceAccountDisplayContext {
+
+	public CommerceAccountUserDisplayContext(
+		CommerceAccountHelper commerceAccountHelper,
+		CommerceAccountService commerceAccountService,
+		HttpServletRequest httpServletRequest, Portal portal,
+		UserFileUploadsConfiguration userFileUploadsConfiguration,
+		UserLocalService userLocalService) {
+
+		super(
+			commerceAccountHelper, commerceAccountService, httpServletRequest,
+			portal);
+
+		_userLocalService = userLocalService;
+		_userFileUploadsConfiguration = userFileUploadsConfiguration;
+	}
+
+	public User getSelectedUser() throws PortalException {
+		long userId = ParamUtil.getLong(
+			commerceAccountRequestHelper.getRequest(), "userId");
+
+		if (userId > 0) {
+			return _userLocalService.getUser(userId);
+		}
+
+		return portal.getUser(commerceAccountRequestHelper.getRequest());
+	}
+
+	public UserFileUploadsConfiguration getUserFileUploadsConfiguration() {
+		return _userFileUploadsConfiguration;
+	}
+
+	private final UserFileUploadsConfiguration _userFileUploadsConfiguration;
+	private final UserLocalService _userLocalService;
+
+}
