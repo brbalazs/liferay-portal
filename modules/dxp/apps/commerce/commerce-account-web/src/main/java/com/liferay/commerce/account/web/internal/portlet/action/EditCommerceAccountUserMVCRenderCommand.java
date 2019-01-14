1c4f3ada58b09a53c3d7b32a280767c00b2fa476
Alessio Rendina <asilcomando@gmail.com>
2019-01-14 20:30:16 +0100

    COMMERCE-778 Added edit account user render and action command

---
+++ modules/dxp/apps/commerce/commerce-account-web/src/main/java/com/liferay/commerce/account/web/internal/portlet/action/EditCommerceAccountUserMVCRenderCommand.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.account.web.internal.portlet.action;
+
+import com.liferay.commerce.account.constants.CommerceAccountPortletKeys;
+import com.liferay.commerce.account.service.CommerceAccountService;
+import com.liferay.commerce.account.util.CommerceAccountHelper;
+import com.liferay.commerce.account.web.internal.display.context.CommerceAccountUserDisplayContext;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.users.admin.configuration.UserFileUploadsConfiguration;
+
+import java.util.Map;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	configurationPid = "com.liferay.user.admin.configuration.UserFileUploadsConfiguration",
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CommerceAccountPortletKeys.COMMERCE_ACCOUNT,
+		"mvc.command.name=editCommerceAccountUser"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditCommerceAccountUserMVCRenderCommand
+	implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			renderRequest);
+
+		CommerceAccountUserDisplayContext commerceAccountUserDisplayContext =
+			new CommerceAccountUserDisplayContext(
+				_commerceAccountHelper, _commerceAccountService,
+				httpServletRequest, _portal, _userFileUploadsConfiguration,
+				_userLocalService);
+
+		renderRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT, commerceAccountUserDisplayContext);
+
+		return "/edit_user.jsp";
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_userFileUploadsConfiguration = ConfigurableUtil.createConfigurable(
+			UserFileUploadsConfiguration.class, properties);
+	}
+
+	@Reference
+	private CommerceAccountHelper _commerceAccountHelper;
+
+	@Reference
+	private CommerceAccountService _commerceAccountService;
+
+	@Reference
+	private Portal _portal;
+
+	private volatile UserFileUploadsConfiguration _userFileUploadsConfiguration;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
