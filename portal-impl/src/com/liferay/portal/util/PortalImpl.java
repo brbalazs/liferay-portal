2cf37a6887af2bda636b09b4287c5918cb17ea59
ricardocousolr <ricardo.couso@liferay.com>
2019-01-31 11:39:43 +0100

    LPS-89966 Include layoutId and privateLayout when calculating the layout's actual url

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -2828,9 +2828,16 @@ public class PortalImpl implements Portal {
 		variables.put(
 			"liferay:groupId", String.valueOf(browsableLayout.getGroupId()));
 
+		variables.put(
+			"liferay:layoutId", String.valueOf(browsableLayout.getLayoutId()));
+
 		variables.put("liferay:mainPath", mainPath);
 		variables.put(
 			"liferay:plid", String.valueOf(browsableLayout.getPlid()));
+
+		variables.put(
+			"liferay:privateLayout",
+			String.valueOf(browsableLayout.isPrivateLayout()));
 
 		if (browsableLayout instanceof VirtualLayout) {
 			variables.put(
