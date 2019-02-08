f65be2a38599aae469a27a7d5015fa0155f9d728
Pavel Savinov <pavel.savinov@liferay.com>
2019-02-08 16:23:33 +0100

    LPS-88949 Remove duplicate check

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
@@ -988,9 +988,7 @@ public class LayoutsAdminDisplayContext {
 
 		Group selGroup = getSelGroup();
 
-		if (selGroup.isLayoutSetPrototype() ||
-			selGroup.isLayoutSetPrototype()) {
-
+		if (selGroup.isLayoutSetPrototype()) {
 			_privateLayout = true;
 
 			return _privateLayout;
