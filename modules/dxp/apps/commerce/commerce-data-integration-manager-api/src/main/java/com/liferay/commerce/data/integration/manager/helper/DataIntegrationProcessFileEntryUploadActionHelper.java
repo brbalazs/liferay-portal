2fd6b43130e958f4e30efed8f4ecddd07924cae7
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2019-02-23 11:28:09 +0100

    COMMERCE-1039 commerce-data-integration-api Add helper classes interfaces

---
+++ modules/dxp/apps/commerce/commerce-data-integration-manager-api/src/main/java/com/liferay/commerce/data/integration/manager/helper/DataIntegrationProcessFileEntryUploadActionHelper.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.data.integration.manager.helper;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.io.IOException;
+
+import javax.portlet.ActionRequest;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@ProviderType
+public interface DataIntegrationProcessFileEntryUploadActionHelper {
+
+	public DLFileEntry upload(
+			ActionRequest actionRequest, String fileNameParameter)
+		throws IOException, PortalException;
+
+}
---
+++ modules/dxp/apps/commerce/commerce-data-integration-manager-api/src/main/java/com/liferay/commerce/data/integration/manager/helper/DataIntegrationProcessWebKeys.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.data.integration.manager.helper;
+
+/**
+ * @author Riccardo Ferrari
+ */
+public class DataIntegrationProcessWebKeys {
+
+	public static final String DI_PROCESS = "DI_PROCESS";
+
+	public static final String DI_PROCESS_ACTION_HELPER =
+		"DI_PROCESS_ACTION_HELPER";
+
+}
