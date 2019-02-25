cc81f696a9592868215c9ed61bc7a05e4e8197bf
Ethan Bustad <ethan.bustad@liferay.com>
2019-02-25 09:39:31 -0800

    COMMERCE-872 Update JS module versions

--- modules/dxp/apps/commerce/commerce-theme-minium/commerce-theme-minium-impl/src/main/java/com/liferay/commerce/theme/minium/impl/internal/product/renderer/list/entry/MiniumCPContentListEntryRenderer.java
+++ modules/dxp/apps/commerce/commerce-theme-minium/commerce-theme-minium-impl/src/main/java/com/liferay/commerce/theme/minium/impl/internal/product/renderer/list/entry/MiniumCPContentListEntryRenderer.java
@@ -195,16 +195,16 @@ public class MiniumCPContentListEntryRenderer
 			themeDisplay.getPathThemeImages() + "/commerce-icons.svg");
 
 		String module =
-			"commerce-theme-minium-impl@1.0.7/product_card/ProductCard.es";
+			"commerce-theme-minium-impl@1.0.8/product_card/ProductCard.es";
 
 		Set<String> dependencies = new HashSet<>();
 
 		dependencies.add(
-			"commerce-frontend-taglib@1.1.0/add_to_cart/AddToCartButton.es");
+			"commerce-frontend-taglib@1.1.1/add_to_cart/AddToCartButton.es");
 		dependencies.add(
-			"commerce-frontend-taglib@1.1.0/compare_checkbox" +
+			"commerce-frontend-taglib@1.1.1/compare_checkbox" +
 				"/CompareCheckbox.es");
-		dependencies.add("commerce-frontend-taglib@1.1.0/price/Price.es");
+		dependencies.add("commerce-frontend-taglib@1.1.1/price/Price.es");
 
 		ComponentDescriptor componentDescriptor = new ComponentDescriptor(
 			"ProductCard.render", module, null, dependencies);
