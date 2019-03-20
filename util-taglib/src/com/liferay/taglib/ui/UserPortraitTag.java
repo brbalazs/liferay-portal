dffcbd21830927e4e17b17abe31059719f5207b9
Preston-Crary <preston.crary@liferay.com>
2019-03-20 15:21:46 -0700

    LPS-92095 SF.

--- util-taglib/src/com/liferay/taglib/ui/UserPortraitTag.java
+++ util-taglib/src/com/liferay/taglib/ui/UserPortraitTag.java
@@ -100,11 +100,11 @@ public class UserPortraitTag extends IncludeTag {
 	}
 
 	public long getUserId() {
-		if (_user != null) {
-			return _user.getUserId();
+		if (_user == null) {
+			return 0;
 		}
 
-		return 0;
+		return _user.getUserId();
 	}
 
 	@Override
