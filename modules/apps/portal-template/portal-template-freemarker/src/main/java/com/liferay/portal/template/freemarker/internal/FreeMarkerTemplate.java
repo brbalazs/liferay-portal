2fab5e59ebd95196612efc59ed2d6412d6339ead
tina.tian <tina.tian@liferay.com>
2019-03-29 14:27:58 -0700

    LPS-91938 Remove unused inner class

--- modules/apps/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/internal/FreeMarkerTemplate.java
+++ modules/apps/portal-template/portal-template-freemarker/src/main/java/com/liferay/portal/template/freemarker/internal/FreeMarkerTemplate.java
@@ -47,8 +47,6 @@ import freemarker.template.utility.ObjectWrapperWithAPISupport;
 import java.io.Serializable;
 import java.io.Writer;
 
-import java.security.PrivilegedExceptionAction;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -265,26 +263,6 @@ public class FreeMarkerTemplate extends AbstractSingleResourceTemplate {
 		private final Map<String, Object> _map;
 		private final ObjectWrapper _objectWrapper;
 		private final Map<String, TemplateModel> _wrappedValueMap;
-
-	}
-
-	private class TemplatePrivilegedExceptionAction
-		implements PrivilegedExceptionAction<Template> {
-
-		public TemplatePrivilegedExceptionAction(
-			TemplateResource templateResource) {
-
-			_templateResource = templateResource;
-		}
-
-		@Override
-		public Template run() throws Exception {
-			return _configuration.getTemplate(
-				getTemplateResourceUUID(_templateResource),
-				TemplateConstants.DEFAUT_ENCODING);
-		}
-
-		private final TemplateResource _templateResource;
 
 	}
 
