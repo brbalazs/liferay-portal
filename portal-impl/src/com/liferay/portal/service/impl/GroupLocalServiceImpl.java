ce09a5f778d2043973de98c48d7a26b12e5f0f8a
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2019-04-08 17:40:35 +0200

    LPS-93527 Use default group language to assign groupKey.

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -3763,9 +3763,12 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 		}
 
 		if ((nameMap != null) &&
-			Validator.isNotNull(nameMap.get(LocaleUtil.getDefault()))) {
+			Validator.isNotNull(
+				nameMap.get(
+					LocaleUtil.fromLanguageId(group.getDefaultLanguageId())))) {
 
-			groupKey = nameMap.get(LocaleUtil.getDefault());
+			groupKey = nameMap.get(
+				LocaleUtil.fromLanguageId(group.getDefaultLanguageId()));
 		}
 
 		friendlyURL = getFriendlyURL(
