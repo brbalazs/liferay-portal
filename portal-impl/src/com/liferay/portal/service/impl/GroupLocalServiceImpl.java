85fdad15831d38b90b71b4cfc8c368dd86ecd459
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2019-04-11 10:02:35 +0200

    LPS-93527 SF

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -3949,7 +3949,29 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 
 			validateLanguageIds(defaultLanguageId, newLanguageIds);
 
-			_updateGroupLocaleDependentFields(group, defaultLanguageId);
+			if (!Objects.equals(
+					group.getDefaultLanguageId(), defaultLanguageId)) {
+
+				Locale defaultLocale = LocaleUtil.fromLanguageId(
+					defaultLanguageId);
+
+				Map<Locale, String> oldNameMap = group.getNameMap();
+
+				group.setNameMap(oldNameMap, defaultLocale);
+
+				Map<Locale, String> oldDecriptionMap =
+					group.getDescriptionMap();
+
+				group.setDescriptionMap(oldDecriptionMap, defaultLocale);
+
+				Map<Locale, String> nameMap = group.getNameMap();
+
+				if ((nameMap != null) &&
+					Validator.isNotNull(nameMap.get(defaultLocale))) {
+
+					group.setGroupKey(nameMap.get(defaultLocale));
+				}
+			}
 
 			if (!Objects.equals(oldLanguageIds, newLanguageIds)) {
 				LanguageUtil.resetAvailableGroupLocales(groupId);
@@ -5334,34 +5356,6 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 	}
 
 	protected File publicLARFile;
-
-	private void _updateGroupLocaleDependentFields(
-		Group group, String defaultLanguageId) {
-
-		if (StringUtil.equals(
-				group.getDefaultLanguageId(), defaultLanguageId)) {
-
-			return;
-		}
-
-		Locale defaultLocale = LocaleUtil.fromLanguageId(defaultLanguageId);
-
-		Map<Locale, String> oldNameMap = group.getNameMap();
-
-		group.setNameMap(oldNameMap, defaultLocale);
-
-		Map<Locale, String> oldDecriptionMap = group.getDescriptionMap();
-
-		group.setDescriptionMap(oldDecriptionMap, defaultLocale);
-
-		Map<Locale, String> nameMap = group.getNameMap();
-
-		if ((nameMap != null) &&
-			Validator.isNotNull(nameMap.get(defaultLocale))) {
-
-			group.setGroupKey(nameMap.get(defaultLocale));
-		}
-	}
 
 	private static final Class<?>[]
 		_CHECK_REMOTE_STAGING_GROUP_PARAMETER_TYPES = new Class<?>[] {
