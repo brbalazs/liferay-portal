05165aaae9a177325a6a7d77d60c728fb2296712
Victor Galan <victor.galan@liferay.com>
2019-05-13 15:13:16 +0200

    LPS-92493 Do not update translation when changing locale just copy the default locale value if there is not translation available

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/main.js
@@ -345,9 +345,12 @@ AUI.add(
 							if (inputSelectedValue === '') {
 								var inputDefaultValue = inputComponent.getValue(defaultLanguageId);
 
-								inputComponent.updateInputLanguage(inputDefaultValue);
 
 								inputComponent.selectFlag(selectedLanguageId);
+
+								inputComponent.updateInput(inputDefaultValue);
+
+								eventHandle.detach();
 							}
 						}
 					},
