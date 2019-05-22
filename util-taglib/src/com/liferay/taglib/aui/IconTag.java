9d78fea5e401201d1c0650db5b9ba297f6206cab
Brian Chan <brian.chan@liferay.com>
2019-05-22 10:47:31 -0700

    LPS-119124 Backport of LPS-95648: Fix accessibility's issues Original commit message: LPS-95648 faster 001dbd0ee4ddb8d26c8dd0585e183583a72e1cfb

--- util-taglib/src/com/liferay/taglib/aui/IconTag.java
+++ util-taglib/src/com/liferay/taglib/aui/IconTag.java
@@ -161,8 +161,7 @@ public class IconTag extends BaseIconTag {
 				jspWriter.write("\" focusable=\"false\" ");
 				jspWriter.write(
 					InlineUtil.buildDynamicAttributes(getDynamicAttributes()));
-				jspWriter.write(StringPool.GREATER_THAN);
-				jspWriter.write("<use data-href=\"");
+				jspWriter.write("><use data-href=\"");
 
 				String src = getSrc();
 
