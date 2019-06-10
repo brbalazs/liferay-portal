adb614379d50b8fc3c6689ca332beb545e2e64e8
Cristina González Castellano <cristina.gonzalez@liferay.com>
2019-06-10 10:19:53 +0200

    LPS-96469 Rating entry are always saved as Discussion, even when the classPK is a MBMessage one.

--- modules/apps/ratings/ratings-service/src/main/java/com/liferay/ratings/internal/exportimport/data/handler/RatingsEntryStagedModelDataHandler.java
+++ modules/apps/ratings/ratings-service/src/main/java/com/liferay/ratings/internal/exportimport/data/handler/RatingsEntryStagedModelDataHandler.java
@@ -18,6 +18,8 @@ import com.liferay.exportimport.data.handler.base.BaseStagedModelDataHandler;
 import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
+import com.liferay.portal.kernel.comment.Discussion;
+import com.liferay.portal.kernel.exception.NoSuchModelException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -37,6 +39,7 @@ import com.liferay.ratings.kernel.service.RatingsEntryLocalService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -139,7 +142,27 @@ public class RatingsEntryStagedModelDataHandler
 				}
 			}
 			else {
-				persistedModelLocalService.getPersistedModel(newClassPK);
+				try {
+					persistedModelLocalService.getPersistedModel(newClassPK);
+				}
+				catch (NoSuchModelException nsme) {
+					if (Objects.equals(
+							entry.getClassName(), Discussion.class.getName())) {
+
+						PersistedModelLocalService
+							mbMessagePersistedModelLocalService =
+								PersistedModelLocalServiceRegistryUtil.
+									getPersistedModelLocalService(
+										"com.liferay.message.boards.model." +
+											"MBMessage");
+
+						mbMessagePersistedModelLocalService.getPersistedModel(
+							newClassPK);
+					}
+					else {
+						throw nsme;
+					}
+				}
 			}
 		}
 		catch (PortalException pe) {
