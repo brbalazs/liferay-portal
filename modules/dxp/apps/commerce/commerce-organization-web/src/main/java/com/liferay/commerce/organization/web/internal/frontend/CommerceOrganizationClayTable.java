5b239f04856508393df5db1d58bb4a3b1d3beeff
Alessio Rendina <asilcomando@gmail.com>
2019-06-11 17:53:39 +0200

    COMMERCE-1447 Added commerce organization clay table

---
+++ modules/dxp/apps/commerce/commerce-organization-web/src/main/java/com/liferay/commerce/organization/web/internal/frontend/CommerceOrganizationClayTable.java
@@ -0,0 +1,205 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.organization.web.internal.frontend;
+
+import com.liferay.commerce.frontend.ClayTable;
+import com.liferay.commerce.frontend.ClayTableAction;
+import com.liferay.commerce.frontend.ClayTableActionProvider;
+import com.liferay.commerce.frontend.ClayTableSchema;
+import com.liferay.commerce.frontend.ClayTableSchemaBuilder;
+import com.liferay.commerce.frontend.ClayTableSchemaBuilderFactory;
+import com.liferay.commerce.frontend.CommerceDataSetDataProvider;
+import com.liferay.commerce.frontend.Filter;
+import com.liferay.commerce.frontend.Pagination;
+import com.liferay.commerce.organization.web.internal.model.Organization;
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.OrganizationService;
+import com.liferay.portal.kernel.service.permission.OrganizationPermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"commerce.data.provider.key=" + CommerceOrganizationClayTable.NAME,
+		"commerce.table.name=" + CommerceOrganizationClayTable.NAME
+	},
+	service = {
+		ClayTable.class, ClayTableActionProvider.class,
+		CommerceDataSetDataProvider.class
+	}
+)
+public class CommerceOrganizationClayTable
+	implements ClayTable, ClayTableActionProvider,
+			   CommerceDataSetDataProvider<Organization> {
+
+	public static final String NAME = "commerceOrganizations";
+
+	@Override
+	public List<ClayTableAction> clayTableActions(
+			HttpServletRequest httpServletRequest, long groupId, Object model)
+		throws PortalException {
+
+		List<ClayTableAction> clayTableActions = new ArrayList<>();
+
+		Organization organization = (Organization)model;
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		long organizationId = ParamUtil.getLong(
+			httpServletRequest, "organizationId");
+
+		if (OrganizationPermissionUtil.contains(
+				themeDisplay.getPermissionChecker(), organizationId,
+				ActionKeys.DELETE)) {
+
+			StringBundler sb = new StringBundler(7);
+
+			sb.append("javascript:deleteCommerceOrganization");
+			sb.append(StringPool.OPEN_PARENTHESIS);
+			sb.append(StringPool.APOSTROPHE);
+			sb.append(organization.getOrganizationId());
+			sb.append(StringPool.APOSTROPHE);
+			sb.append(StringPool.CLOSE_PARENTHESIS);
+			sb.append(StringPool.SEMICOLON);
+
+			ClayTableAction deleteClayTableAction = new ClayTableAction(
+				sb.toString(), StringPool.BLANK,
+				LanguageUtil.get(httpServletRequest, "delete"), false, false);
+
+			clayTableActions.add(deleteClayTableAction);
+		}
+
+		return clayTableActions;
+	}
+
+	@Override
+	public int countItems(HttpServletRequest httpServletRequest, Filter filter)
+		throws PortalException {
+
+		OrganizationFilterImpl organizationFilter =
+			(OrganizationFilterImpl)filter;
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return _organizationService.getOrganizationsCount(
+			themeDisplay.getCompanyId(),
+			organizationFilter.getOrganizationId());
+	}
+
+	@Override
+	public ClayTableSchema getClayTableSchema() {
+		ClayTableSchemaBuilder clayTableSchemaBuilder =
+			_clayTableSchemaBuilderFactory.clayTableSchemaBuilder();
+
+		clayTableSchemaBuilder.addField("name", "name");
+		clayTableSchemaBuilder.addField("path", "path");
+
+		return clayTableSchemaBuilder.build();
+	}
+
+	@Override
+	public String getId() {
+		return NAME;
+	}
+
+	@Override
+	public List<Organization> getItems(
+			HttpServletRequest httpServletRequest, Filter filter,
+			Pagination pagination, Sort sort)
+		throws PortalException {
+
+		OrganizationFilterImpl organizationFilter =
+			(OrganizationFilterImpl)filter;
+
+		List<Organization> organizations = new ArrayList<>();
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		List<com.liferay.portal.kernel.model.Organization> organizationList =
+			_organizationService.getOrganizations(
+				themeDisplay.getCompanyId(),
+				organizationFilter.getOrganizationId(),
+				pagination.getStartPosition(), pagination.getEndPosition());
+
+		for (com.liferay.portal.kernel.model.Organization organization :
+				organizationList) {
+
+			organizations.add(
+				new Organization(
+					organization.getOrganizationId(), organization.getName(),
+					getPath(organization.getTreePath())));
+		}
+
+		return organizations;
+	}
+
+	@Override
+	public boolean isShowActionsMenu() {
+		return true;
+	}
+
+	protected String getPath(String treePath) throws PortalException {
+		String[] organizationIds = StringUtil.split(
+			treePath, CharPool.FORWARD_SLASH);
+
+		StringBundler sb = new StringBundler(organizationIds.length * 2);
+
+		for (int i = 1; i < organizationIds.length; i++) {
+			sb.append(CharPool.FORWARD_SLASH);
+
+			com.liferay.portal.kernel.model.Organization organization =
+				_organizationService.getOrganization(
+					GetterUtil.getLong(organizationIds[i]));
+
+			sb.append(organization.getName());
+		}
+
+		return sb.toString();
+	}
+
+	@Reference
+	private ClayTableSchemaBuilderFactory _clayTableSchemaBuilderFactory;
+
+	@Reference
+	private OrganizationService _organizationService;
+
+}
