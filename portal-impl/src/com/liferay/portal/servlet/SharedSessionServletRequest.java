cf4da6de05103cf848cd571d02d56771b3eecabc
Victor Ware <victor.ware@liferay.com>
2019-06-17 14:37:10 -0700

    LPS-19090 Partial backport of LRQA-49613
    Original commit: 62ba67df003cd1f876c0e057d72062438f43a580
    Remove support for Jetty

--- portal-impl/src/com/liferay/portal/servlet/SharedSessionServletRequest.java
+++ portal-impl/src/com/liferay/portal/servlet/SharedSessionServletRequest.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.servlet;
 
-import com.liferay.portal.kernel.util.ServerDetector;
 import com.liferay.portal.resiliency.spi.agent.SPIAgentRequest;
 
 import javax.servlet.http.HttpServletRequest;
@@ -79,10 +78,6 @@ public class SharedSessionServletRequest extends HttpServletRequestWrapper {
 
 	protected HttpSession getSharedSessionWrapper(
 		HttpSession portalSession, HttpSession portletSession) {
-
-		if (ServerDetector.isJetty()) {
-			return new JettySharedSessionWrapper(portalSession, portletSession);
-		}
 
 		return new SharedSessionWrapper(portalSession, portletSession);
 	}
