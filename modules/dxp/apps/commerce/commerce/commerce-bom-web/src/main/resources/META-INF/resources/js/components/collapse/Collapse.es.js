31578788d18c92c1623c37a3e0ba108d42a453f1
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-06-20 11:34:30 +0200

    COMMERCE-1471 collapse component styled and refactored

--- modules/dxp/apps/commerce/commerce/commerce-bom-web/src/main/resources/META-INF/resources/js/components/collapse/Collapse.es.js
+++ modules/dxp/apps/commerce/commerce/commerce-bom-web/src/main/resources/META-INF/resources/js/components/collapse/Collapse.es.js
@@ -1,100 +1,74 @@
-import React, {
-    useState,
-    useRef,
-    useEffect
-} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 export default function Dropdown(props) {
+	const [state, setState] = useState(props.open ? 'expanded' : 'collapsed');
+	const bodyRef = useRef(null);
 
-    const [ collapseState, setCollapseState ] = useState('collapse');
-    const [ initialized, setInitialized ] = useState(false);
-    const bodyRef = useRef(null);
-
-
-    function toggle() {
-        switch (collapseState) {
-            case 'collapse':
-                return open()
-            case 'show':
-                return close()
-            default:
-                break;
-        }
-    }
-
-    function open() {
-        setCollapseState('showing');
-    }
-    
-    function close() {
-        setCollapseState('collapsing');
-    }
-
-    function finaliseTransition() {
-        if(collapseState === 'collapsing') {
-            setCollapseState('collapse')
-        }
-        
-        if(collapseState === 'showing') {
-            setCollapseState('show')
-        }
-
+	function toggle() {
+		switch (true) {
+			case state === 'expanded':
+                setState('collapsing');
+                bodyRef.current.style.maxHeight = bodyRef.current.scrollHeight + 'px';
+                bodyRef.current.addEventListener(
+                    'transitionend',
+                    () => setState('collapsed'),
+                    { once: true }
+                );
+				break;
+			case state === 'collapsed':
+                setState('expanding');
+                bodyRef.current.style.maxHeight = '0px';
+                bodyRef.current.addEventListener(
+					'transitionend',
+					() => setState('expanded'),
+					{ once: true }
+                );
+				break;
+			default:
+				break;
+		}
     }
 
     useEffect(() => {
-        if(props.open && !initialized && bodyRef.current) {
-            open()
-            setInitialized(true)        
+        switch (true) {
+            case state === 'expanding':
+                bodyRef.current.style.maxHeight = bodyRef.current.scrollHeight + 'px';
+                break;
+            case state === 'expanded':
+                bodyRef.current.style.maxHeight = '';
+                break;
+            case state === 'collapsing':
+                bodyRef.current.style.maxHeight = '0px';
+                break;
+            case state === 'collapsed':
+                bodyRef.current.style.maxHeight = '';
+                break;
+            default:
+                break;
         }
+    }, [state])
 
-        if(collapseState === 'showing') {
-            bodyRef.current.addEventListener('transitionend', finaliseTransition, {once: true});
-            bodyRef.current.style.height = bodyRef.current.scrollHeight + 'px';
-        }
-
-        if(collapseState === 'collapsing') {
-            bodyRef.current.addEventListener('transitionend', finaliseTransition, {once: true});
-            bodyRef.current.style.height = '0px';
-        }
-    })
-
-    const simplifiedTitleStateMap = {
-        collapsing: 'collapsed', 
-        collapse: 'collapsed', 
-        showing: '', 
-        show: '', 
-    }
-
-    const simplifiedBodyStateMap = {
-        collapsing: 'collapsing', 
-        collapse: 'collapse', 
-        showing: 'collapsing', 
-        show: 'collapse show', 
-    }
-
-    return (
-        <div className={`commerce-collapse${props.additionalWrapperClasses ? ` ${props.additionalWrapperClasses}` :``}`}>
-            <span 
-                aria-expanded={props.open}
-                className={`collapse-icon sheet-subtitle ${simplifiedTitleStateMap[collapseState]}`}
-                role="tab"
-                onClick={toggle}
-            >
-                <span>{props.title}</span>
-                <span className="collapse-icon-closed">
-                    {props.closedIcon}
-                </span>
-                <span className="collapse-icon-open">
-                    {props.openIcon}
-                </span>
-            </span>
-            <div 
-                className={`panel-collapse ${simplifiedBodyStateMap[collapseState]}`} 
-                role="tabpanel"
-                ref={bodyRef}
-            >
-                {props.content}
-            </div>
-        </div>
-    )
+	return (
+		<div className={`panel commerce-collapse`}>
+			<button
+				aria-controls="collapseTwo"
+				className={`commerce-collapse__header commerce-collapse__header--${state}`}
+				role="tab"
+				type="button"
+				onClick={toggle}
+				aria-expanded={state === 'expanded'}
+			>
+				<span className="collapse-title">{props.title}</span>
+				<span className="collapse-icon-closed">{props.closedIcon}</span>
+				<span className="collapse-icon-open">{props.openIcon}</span>
+			</button>
+			<div
+				className={`commerce-collapse__content commerce-collapse__content--${state}`}
+				role="tabpanel"
+				ref={bodyRef}
+			>
+				{props.content}
+			</div>
+		</div>
+	);
 }
