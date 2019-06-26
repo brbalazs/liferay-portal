71b376ea4f8134df7ff720eecf2181a103a12950
balazssk <balazs.safrany-kovalik@liferay.com>
2019-06-26 11:49:38 -0700

    LPS-85535 Upgrade journal content: remove unused chars from radio fields

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/JournalServiceUpgrade.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/JournalServiceUpgrade.java
@@ -52,6 +52,7 @@ import com.liferay.journal.internal.upgrade.v1_1_4.UpgradeUrlTitle;
 import com.liferay.journal.internal.upgrade.v1_1_5.UpgradeContentImages;
 import com.liferay.journal.internal.upgrade.v1_1_6.UpgradeAssetDisplayPageEntry;
 import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.internal.upgrade.v1_1_9.UpgradeContentRadioFields;
 import com.liferay.portal.configuration.upgrade.PrefsPropsToConfigurationUpgradeHelper;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBManagerUtil;
@@ -190,6 +191,8 @@ public class JournalServiceUpgrade implements UpgradeStepRegistrator {
 			new UpgradeDiscussionSubscriptionClassName(
 				_subscriptionLocalService, JournalArticle.class.getName(),
 				UpgradeDiscussionSubscriptionClassName.DeletionMode.ADD_NEW));
+
+		registry.register("1.1.8", "1.1.9", new UpgradeContentRadioFields());
 	}
 
 	protected void deleteTempImages() throws Exception {
