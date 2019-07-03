b1278896d436a7d01889f84e8f49882abddad39f
tina.tian <tina.tian@liferay.com>
2019-07-03 09:49:51 -0700

    LPS-93315 SF, inline

--- portal-impl/src/com/liferay/portal/service/impl/LayoutServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutServiceImpl.java
@@ -1576,11 +1576,11 @@ public class LayoutServiceImpl extends LayoutServiceBaseImpl {
 		GroupPermissionUtil.check(
 			getPermissionChecker(), targetGroupId, ActionKeys.PUBLISH_STAGING);
 
-		String timeZoneId = MapUtil.getString(parameterMap, "timeZoneId");
-
 		Trigger trigger = TriggerFactoryUtil.createTrigger(
 			PortalUUIDUtil.generate(), groupName, schedulerStartDate,
-			schedulerEndDate, cronText, TimeZone.getTimeZone(timeZoneId));
+			schedulerEndDate, cronText,
+			TimeZone.getTimeZone(
+				MapUtil.getString(parameterMap, "timeZoneId")));
 
 		User user = userPersistence.findByPrimaryKey(getUserId());
 
@@ -1692,11 +1692,11 @@ public class LayoutServiceImpl extends LayoutServiceBaseImpl {
 		GroupPermissionUtil.check(
 			getPermissionChecker(), sourceGroupId, ActionKeys.PUBLISH_STAGING);
 
-		String timeZoneId = MapUtil.getString(parameterMap, "timeZoneId");
-
 		Trigger trigger = TriggerFactoryUtil.createTrigger(
 			PortalUUIDUtil.generate(), groupName, schedulerStartDate,
-			schedulerEndDate, cronText, TimeZone.getTimeZone(timeZoneId));
+			schedulerEndDate, cronText,
+			TimeZone.getTimeZone(
+				MapUtil.getString(parameterMap, "timeZoneId")));
 
 		User user = userPersistence.findByPrimaryKey(getUserId());
 
