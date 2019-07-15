92d5dddfde05a7f64e68d16d12b4c904ed9e1fe4
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2019-07-15 17:35:41 +0200

    LPS-123152 LPS-67565 Save only if portlet does support the mode

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -7416,12 +7416,10 @@ public class PortalImpl implements Portal {
 		Portlet portlet = PortletLocalServiceUtil.getPortletById(
 			getCompanyId(request), portletId);
 
-		if (isCustomPortletMode(portletMode)) {
-			Set<String> allPortletModes = portlet.getAllPortletModes();
+		Set<String> allPortletModes = portlet.getAllPortletModes();
 
-			if (!allPortletModes.contains(portletMode.toString())) {
-				return PortletMode.VIEW;
-			}
+		if (!allPortletModes.contains(portletMode.toString())) {
+			return PortletMode.VIEW;
 		}
 
 		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
