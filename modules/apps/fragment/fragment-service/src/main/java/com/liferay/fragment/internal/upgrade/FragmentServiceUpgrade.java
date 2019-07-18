3e29888765e46c610b1f470ada16ce5424b5d73f
ricardocousolr <ricardo.couso@liferay.com>
2019-07-18 10:10:45 +0200

    LPS-97133 Upgrade portlet preferences to point to the group's control panel layout

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/upgrade/FragmentServiceUpgrade.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/upgrade/FragmentServiceUpgrade.java
@@ -15,6 +15,7 @@
 package com.liferay.fragment.internal.upgrade;
 
 import com.liferay.fragment.internal.upgrade.v1_0_1.UpgradeFragmentEntry;
+import com.liferay.fragment.internal.upgrade.v1_0_2.UpgradePortletPreferences;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
@@ -28,6 +29,8 @@ public class FragmentServiceUpgrade implements UpgradeStepRegistrator {
 	@Override
 	public void register(Registry registry) {
 		registry.register("1.0.0", "1.0.1", new UpgradeFragmentEntry());
+
+		registry.register("1.0.1", "1.0.2", new UpgradePortletPreferences());
 	}
 
 }
