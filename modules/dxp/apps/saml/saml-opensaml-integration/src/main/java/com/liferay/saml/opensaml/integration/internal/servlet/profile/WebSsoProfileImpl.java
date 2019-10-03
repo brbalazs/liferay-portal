45d172c5ee2536b6619435843a9396f788a72749
Brian Chan <brian.chan@liferay.com>
2019-10-03 15:15:46 -0700

    LPS-102359 sort

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/servlet/profile/WebSsoProfileImpl.java
@@ -1516,10 +1516,9 @@ public class WebSsoProfileImpl extends BaseProfile implements WebSsoProfile {
 		MessageContext<Response> outboundMessageContext =
 			inOutOperationContext.getOutboundMessageContext();
 
-		outboundMessageContext.setMessage(samlResponse);
-
 		outboundMessageContext.addSubcontext(
 			messageContext.getSubcontext(SAMLBindingContext.class, true));
+		outboundMessageContext.setMessage(samlResponse);
 
 		SecurityParametersContext securityParametersContext =
 			outboundMessageContext.getSubcontext(
