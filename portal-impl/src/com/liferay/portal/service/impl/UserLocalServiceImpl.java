83ea46d2c59b5c1e546513e682d166a1f1f8feb2
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2019-10-09 17:37:12 +0200

    LPS-102980 Do not use control panel plid, if not it will redirect to login page. Use same logic as in sendEmailAddressVerification.

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -6390,12 +6390,26 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 					user.getCompanyId(), User.class.getName(), user.getUserId(),
 					TicketConstants.TYPE_PASSWORD, null, null, serviceContext);
 
-				String plid = String.valueOf(serviceContext.getPlid());
+				String updatePasswordURL = "/portal/update_password?";
+
+				long plid = serviceContext.getPlid();
+
+				if (plid > 0) {
+					Layout layout = layoutLocalService.fetchLayout(plid);
+
+					if (layout != null) {
+						Group group = layout.getGroup();
+
+						if (!layout.isPrivateLayout() && !group.isUser()) {
+							updatePasswordURL +=
+								"p_l_id=" + serviceContext.getPlid() + "&";
+						}
+					}
+				}
 
 				passwordResetURL = StringBundler.concat(
 					serviceContext.getPortalURL(), serviceContext.getPathMain(),
-					"/portal/update_password?p_l_id=", plid, "&ticketKey=",
-					ticket.getKey());
+					updatePasswordURL, "ticketKey=", ticket.getKey());
 
 				localizedBodyMap = LocalizationUtil.getLocalizationMap(
 					companyPortletPreferences,
