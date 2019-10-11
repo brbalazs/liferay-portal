b61b794ccdb5466503c52c8f4138e3d45e13f3b0
Javier Gamarra <nhpatt@gmail.com>
2019-10-11 08:28:49 +0200

    LPS-98387 expose actions for messages, wiki and structured content

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/StructuredContentResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/StructuredContentResourceImpl.java
@@ -76,6 +76,7 @@ import com.liferay.portal.kernel.servlet.DummyHttpServletResponse;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -634,6 +635,54 @@ public class StructuredContentResourceImpl
 		}
 	}
 
+	private Map<String, Map<String, String>> _getActions(
+		JournalArticle journalArticle) {
+
+		return HashMapBuilder.<String, Map<String, String>>put(
+			"delete",
+			addAction(
+				"DELETE", journalArticle.getResourcePrimKey(),
+				"deleteStructuredContent", JournalArticle.class.getName(),
+				journalArticle.getGroupId())
+		).put(
+			"get",
+			addAction(
+				"VIEW", journalArticle.getResourcePrimKey(),
+				"getStructuredContent", JournalArticle.class.getName(),
+				journalArticle.getGroupId())
+		).put(
+			"get-template",
+			addAction(
+				"VIEW", journalArticle.getResourcePrimKey(),
+				"getStructuredContentRenderedContentTemplate",
+				JournalArticle.class.getName(), journalArticle.getGroupId())
+		).put(
+			"replace",
+			addAction(
+				"UPDATE", journalArticle.getResourcePrimKey(),
+				"putStructuredContent", JournalArticle.class.getName(),
+				journalArticle.getGroupId())
+		).put(
+			"subscribe",
+			addAction(
+				"SUBSCRIBE", journalArticle.getResourcePrimKey(),
+				"putStructuredContentSubscribe", JournalArticle.class.getName(),
+				journalArticle.getGroupId())
+		).put(
+			"unsubscribe",
+			addAction(
+				"SUBSCRIBE", journalArticle.getResourcePrimKey(),
+				"putStructuredContentUnsubscribe",
+				JournalArticle.class.getName(), journalArticle.getGroupId())
+		).put(
+			"update",
+			addAction(
+				"UPDATE", journalArticle.getResourcePrimKey(),
+				"patchStructuredContent", JournalArticle.class.getName(),
+				journalArticle.getGroupId())
+		).build();
+	}
+
 	private DDMFormField _getDDMFormField(
 		DDMStructure ddmStructure, String name) {
 
@@ -668,6 +717,20 @@ public class StructuredContentResourceImpl
 			JournalArticle.class.getName(), contextCompany.getCompanyId(),
 			structuredContent.getCustomFields(),
 			contextAcceptLanguage.getPreferredLocale());
+	}
+
+	private Map<String, Map<String, String>> _getListActions(Long siteId) {
+		return HashMapBuilder.<String, Map<String, String>>put(
+			"create",
+			addAction(
+				"ADD_ARTICLE", "postSiteStructuredContent",
+				"com.liferay.journal", siteId)
+		).put(
+			"get",
+			addAction(
+				"VIEW", "getSiteStructuredContentsPage", "com.liferay.journal",
+				siteId)
+		).build();
 	}
 
 	private List<DDMFormField> _getRootDDMFormFields(
@@ -730,7 +793,7 @@ public class StructuredContentResourceImpl
 					document.get(
 						com.liferay.portal.kernel.search.Field.ARTICLE_ID),
 					WorkflowConstants.STATUS_APPROVED)),
-			sorts);
+			sorts, (Map)_getListActions(siteId));
 	}
 
 	private ThemeDisplay _getThemeDisplay(JournalArticle journalArticle)
@@ -865,7 +928,8 @@ public class StructuredContentResourceImpl
 				GetterUtil.getBoolean(
 					contextHttpServletRequest.getHeader(
 						"X-Accept-All-Languages")),
-				_dtoConverterRegistry, journalArticle.getResourcePrimKey(),
+				(Map)_getActions(journalArticle), _dtoConverterRegistry,
+				journalArticle.getResourcePrimKey(),
 				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
 				contextUser));
 	}
