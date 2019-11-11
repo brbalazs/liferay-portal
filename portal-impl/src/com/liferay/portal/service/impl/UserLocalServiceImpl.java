9311bc8f2380c5c41c73d485402dab5887eac6fc
Roberto Díaz <roberto.diaz@liferay.com>
2019-11-11 18:18:00 +0100

    LPS-103098 null check
    (cherry picked from commit 7310b551478b80d859ee4154e0c3968b8d3a4f6e)

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -4273,7 +4273,9 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		for (Long userId : userIds) {
 			User user = userLocalService.fetchUser(userId);
 
-			users.add(user);
+			if (user != null) {
+				users.add(user);
+			}
 		}
 
 		unsetRoleUsers(roleId, users);
