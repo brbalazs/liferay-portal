563d19a2a583e5718db80d98ad71b7271a996669
IstvanD <istvan.dezsi@liferay.com>
2019-11-15 13:28:46 +0100

    LPS-102554 Use contentURL that was passed as a parameter to the tag

--- modules/apps/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/soy/FlagsTag.java
+++ modules/apps/flags/flags-taglib/src/main/java/com/liferay/flags/taglib/servlet/taglib/soy/FlagsTag.java
@@ -36,6 +36,7 @@ import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.HashMap;
@@ -200,11 +201,17 @@ public class FlagsTag extends ComponentRendererTag {
 
 		JSONObject dataJSONObject = JSONFactoryUtil.createJSONObject();
 
+		Object contentURL = context.get("contentURL");
+
+		if (Validator.isNull(contentURL)) {
+			contentURL = _getCurrentURL();
+		}
+
 		dataJSONObject.put(namespace + "className", context.get("className"));
 		dataJSONObject.put(namespace + "classPK", context.get("classPK"));
 		dataJSONObject.put(
 			namespace + "contentTitle", context.get("contentTitle"));
-		dataJSONObject.put(namespace + "contentURL", _getCurrentURL());
+		dataJSONObject.put(namespace + "contentURL", contentURL);
 		dataJSONObject.put(
 			namespace + "reportedUserId", context.get("reportedUserId"));
 
