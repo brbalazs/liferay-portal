aee43fcca49389d59a562153f76715ac3f8af4d7
antonio-ortega <60252917@liferay.com>
2019-12-05 11:52:56 +0100

    LPS-105059 Attach callback only when it exists
    (cherry picked from commit bbd782bc256dd62b7fac2a872252111c2a175252)

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
@@ -1402,7 +1402,13 @@
 				config.dialog = dialogConfig;
 			}
 
-			var eventHandles = [Liferay.once(config.eventName, callback)];
+			var eventHandles = []
+			
+			if (callback) {
+				eventHandles.push(
+					Liferay.once(config.eventName, callback)
+				);
+			}
 
 			var detachSelectionOnHideFn = function(event) {
 				Liferay.fire(config.eventName);
