5f24a3e6a3692507d86f3d8f286f02ddabc12d37
Jorge Díaz <jorge.diaz@liferay.com>
2019-12-05 09:35:04 +0100

    LPS-105242 Partial backport of commit c45831877bdfd7d8afd276c7c217618d1886a66c from LPS-44217 (Feature request ticket) in order to maintain same login than master branch

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -9096,7 +9096,7 @@ public class PortalImpl implements Portal {
 		int pos = portalDomain.indexOf(CharPool.COLON);
 
 		if (pos > 0) {
-			return virtualHostName.regionMatches(0, portalDomain, 0, pos);
+			portalDomain = portalDomain.substring(0, pos);
 		}
 
 		if (virtualHostName.equals(portalDomain) ||
