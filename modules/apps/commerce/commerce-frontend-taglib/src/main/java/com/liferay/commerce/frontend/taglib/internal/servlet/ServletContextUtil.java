b0ab11b4c9d340e201be31ab3df4f1a100913ce7
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-12-09 15:45:53 +0100

    COMMERCE-2514 js components moved into root folder in order to avoid conflicts

--- modules/apps/commerce/commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/internal/servlet/ServletContextUtil.java
+++ modules/apps/commerce/commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/internal/servlet/ServletContextUtil.java
@@ -23,7 +23,6 @@ import com.liferay.commerce.frontend.FilterFactoryRegistry;
 import com.liferay.commerce.frontend.util.ProductHelper;
 import com.liferay.commerce.order.CommerceOrderHttpHelper;
 import com.liferay.commerce.product.content.util.CPContentHelper;
-import com.liferay.commerce.product.util.CPSubscriptionTypeRegistry;
 import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 
@@ -36,7 +35,6 @@ import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Marco Leo
- * @author Alessio Antonio Rendina
  */
 @Component(immediate = true, service = ServletContextUtil.class)
 public class ServletContextUtil {
@@ -75,12 +73,6 @@ public class ServletContextUtil {
 
 	public static final CPContentHelper getCPContentHelper() {
 		return _servletContextUtil._getCPContentHelper();
-	}
-
-	public static final CPSubscriptionTypeRegistry
-		getCPSubscriptionTypeRegistry() {
-
-		return _servletContextUtil._getCPSubscriptionTypeRegistry();
 	}
 
 	public static final FilterFactoryRegistry getFilterFactoryRegistry() {
@@ -163,12 +155,6 @@ public class ServletContextUtil {
 		_cpContentHelper = cpContentHelper;
 	}
 
-	protected void setCPSubscriptionTypeRegistry(
-		CPSubscriptionTypeRegistry cpSubscriptionTypeRegistry) {
-
-		_cpSubscriptionTypeRegistry = cpSubscriptionTypeRegistry;
-	}
-
 	@Reference(unbind = "-")
 	protected void setFilterFactoryRegistry(
 		FilterFactoryRegistry filterFactoryRegistry) {
@@ -228,10 +214,6 @@ public class ServletContextUtil {
 		return _cpContentHelper;
 	}
 
-	private CPSubscriptionTypeRegistry _getCPSubscriptionTypeRegistry() {
-		return _cpSubscriptionTypeRegistry;
-	}
-
 	private FilterFactoryRegistry _getFilterFactoryRegistry() {
 		return _filterFactoryRegistry;
 	}
@@ -259,7 +241,6 @@ public class ServletContextUtil {
 	private CommerceOrderHttpHelper _commerceOrderHttpHelper;
 	private ConfigurationProvider _configurationProvider;
 	private CPContentHelper _cpContentHelper;
-	private CPSubscriptionTypeRegistry _cpSubscriptionTypeRegistry;
 	private FilterFactoryRegistry _filterFactoryRegistry;
 	private NPMResolver _npmResolver;
 	private ProductHelper _productHelper;
