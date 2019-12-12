8c394abdd1d3c4e4b536743ff12c52cac3bd7048
Renato Rego <renato.rego@liferay.com>
2019-12-12 18:52:09 +0100

    LPS-105059 SF
    (cherry picked from commit e0d9f64d85a522884ddf1b8e831d69e7042a994e)

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
@@ -1402,12 +1402,10 @@
 				config.dialog = dialogConfig;
 			}
 
-			var eventHandles = []
-			
+			var eventHandles = [];
+
 			if (callback) {
-				eventHandles.push(
-					Liferay.once(config.eventName, callback)
-				);
+				eventHandles.push(Liferay.once(config.eventName, callback));
 			}
 
 			var detachSelectionOnHideFn = function(event) {
