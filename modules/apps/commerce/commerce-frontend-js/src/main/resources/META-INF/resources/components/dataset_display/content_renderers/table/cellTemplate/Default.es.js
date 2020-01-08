3725ccec8ee3a23287177f57cf305953af878b80
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2020-01-08 16:27:31 +0100

    COMMERCE-2514 added new cell renderers + fixed bug on side panel bulk actions

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/content_renderers/table/cellTemplate/Default.es.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/content_renderers/table/cellTemplate/Default.es.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import ClayIcon from '@clayui/icon';
 
 function Default(props) {
-	return <React.Fragment>{props.value}</React.Fragment>;
+	switch (true) {
+		case !!props.value.icon:
+			return <ClayIcon symbol={props.value.icon} />
+		case !!props.value.pictureUrl:
+			return (<img alt={props.value.label} src={props.value.pictureUrl} />)
+		case !!props.value.label:
+			return (<React.Fragment>{props.value.label}</React.Fragment>)
+		default:
+			return <React.Fragment>{props.value}</React.Fragment>
+	}
 }
 
 export default Default;
