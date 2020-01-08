e3baf975f6fdd85dd573e435606e377bc40d6c9d
Christopher Kian <christopher.kian@liferay.com>
2020-01-08 14:54:56 +0100

    LPS-105571 If we already have an entry in the localizationMap for the locale, then we should be able to set it to any value - including the default value, since it was manually entered.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -853,7 +853,8 @@ AUI.add(
 
 							if (
 								locale === defaultLocale ||
-								value !== localizationMap[defaultLocale]
+								value !== localizationMap[defaultLocale] ||
+								localizationMap[locale]
 							) {
 								localizationMap[locale] = value;
 							}
