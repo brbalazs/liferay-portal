637edba527fb6855436ad145a3f8cf981c408677
Alec Sloan <Alec.sloan@Liferay.com>
2020-01-10 10:35:22 +0100

    COMMERCE-2321 Add configurations for Price Range Facet portlet

---
+++ modules/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/constants/CPPriceRangeFacetsConstants.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.content.search.web.internal.constants;
+
+/**
+ * @author Alec Sloan
+ */
+public class CPPriceRangeFacetsConstants {
+
+	public static final String DEFAULT_PRICE_RANGES_JSON_ARRAY =
+		"[{'range': '[0 TO 50]'}\\, {'range': '[50 TO 100]'}\\, {'range': " +
+			"'[100 TO 200]'}\\, {'range': '[200 TO 500]'}\\, {'range': '[500 " +
+				"TO 1000]'}\\, {'range': '[1000 TO *]'}]";
+
+}
