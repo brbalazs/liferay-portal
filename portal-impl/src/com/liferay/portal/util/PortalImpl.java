4a9f8ff5ad9154b1b6d617918f4801167f223ca0
tina.tian <tina.tian@liferay.com>
2020-01-13 11:17:02 -0800

    LPS-57105 Revert "LPS-57105 Improve", revert to simplify the logic so that can avoid recursive calls and unnecessary regex matching, when the inner recursive call returns "true", the outer Matcher.match will still be executed.

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -6855,10 +6855,8 @@ public class PortalImpl implements Portal {
 			return true;
 		}
 
-		if (resourceId.contains("%") &&
-			!isValidResourceId(HttpUtil.decodePath(resourceId))) {
-
-			return false;
+		if (resourceId.contains("%")) {
+			resourceId = HttpUtil.decodePath(resourceId);
 		}
 
 		Matcher matcher = _bannedResourceIdPattern.matcher(resourceId);
