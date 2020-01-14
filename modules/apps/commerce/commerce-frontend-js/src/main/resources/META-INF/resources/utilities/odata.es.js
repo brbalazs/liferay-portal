c0a1c9daab9f21a6f743484ae217ed7c5c5d2413
Ethan Bustad <ethan.bustad@liferay.com>
2020-01-13 17:12:20 -0800

    COMMERCE-X Whitespace: Fix indentation

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/utilities/odata.es.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/utilities/odata.es.js
@@ -15,15 +15,17 @@ export function createOdataFilterStrings(filters) {
 	const oDataFilterStrings = filters.map(filter => {
 		if (filter.value instanceof Array) {
 			return filter.value.map(
-                value => `(${createOdataFilterString(filter.id, filter.operator, value)})`).join(' or ')
+				value => `(${createOdataFilterString(filter.id, filter.operator, value)})`).join(' or ')
 		}
+
 		if (filter.main) {
 			return `(startwith(${filter.id}, '${filter.value}') eq true)`
 		}
+
 		return createOdataFilterString(filter.id, filter.operator, filter.value)
-    })
-        .map(filterString => `(${filterString})`)
-        .join(' and ');
+	})
+		.map(filterString => `(${filterString})`)
+		.join(' and ');
 
 	const oDataFilters = oDataFilterStrings.length ? `$filter=${oDataFilterStrings}` : '';
 	return oDataFilters
