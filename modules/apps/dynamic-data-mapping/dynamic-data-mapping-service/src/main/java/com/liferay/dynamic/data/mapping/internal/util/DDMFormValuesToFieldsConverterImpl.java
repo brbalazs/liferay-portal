ad65792e173a4521cc02a399ae322269258e6397
Lino Alves <lino.alves@liferay.com>
2020-01-17 14:17:51 -0300

    LPS-102643 getting serializable number by type

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/util/DDMFormValuesToFieldsConverterImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/util/DDMFormValuesToFieldsConverterImpl.java
@@ -190,9 +190,6 @@ public class DDMFormValuesToFieldsConverterImpl
 			if (type.equals(FieldConstants.NUMBER) ||
 				type.equals(FieldConstants.DOUBLE)) {
 
-				String stringValue = GetterUtil.getString(
-					value.getString(availableLocales));
-
 				NumberFormat numberFormat = NumberFormat.getInstance(
 					availableLocales);
 
@@ -201,7 +198,12 @@ public class DDMFormValuesToFieldsConverterImpl
 				}
 
 				try {
-					serializable = numberFormat.parse(stringValue);
+					Number number = numberFormat.parse(
+						GetterUtil.getString(
+							value.getString(availableLocales)));
+
+					serializable = FieldConstants.getSerializable(
+						type, number.toString());
 				}
 				catch (ParseException pe) {
 					serializable = FieldConstants.getSerializable(
