2bc54c55406a64351030e14f0f9d4e107d2d5e6a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2020-01-20 16:26:59 +0100

    LPS-107358 Uses reverse order to modified date

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -978,8 +978,15 @@ public class JournalDisplayContext {
 		_orderByType = ParamUtil.getString(_request, "orderByType");
 
 		if (Validator.isNull(_orderByType)) {
+			String defaultOrderByType = "asc";
+
+			if (Objects.equals(getOrderByCol(), "modified-date")) {
+				defaultOrderByType = "desc";
+			}
+
 			_orderByType = _portalPreferences.getValue(
-				JournalPortletKeys.JOURNAL, "order-by-type", "asc");
+				JournalPortletKeys.JOURNAL, "order-by-type",
+				defaultOrderByType);
 		}
 		else {
 			boolean saveOrderBy = ParamUtil.getBoolean(_request, "saveOrderBy");
