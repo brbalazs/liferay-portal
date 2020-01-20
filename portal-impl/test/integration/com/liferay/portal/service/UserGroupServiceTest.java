e7e788646eda86f67018344d10cf6fa13bab1100
Istvan Sajtos <istvan.sajtos@liferay.com>
2020-01-20 17:27:36 +0100

    LPS-92932 SF

--- portal-impl/test/integration/com/liferay/portal/service/UserGroupServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/UserGroupServiceTest.java
@@ -187,11 +187,10 @@ public class UserGroupServiceTest {
 	}
 
 	protected void assertSearch(User user, int expected) throws Exception {
-		LinkedHashMap<String, Object> userGroupParams = new LinkedHashMap<>();
-
-		userGroupParams.put(
-			UserGroupFinderConstants.PARAM_KEY_USER_GROUPS_USERS,
-			user.getUserId());
+		LinkedHashMap<String, Object> userGroupParams =
+			LinkedHashMapBuilder.<String, Object>put(
+				UserGroupFinderConstants.PARAM_KEY_USER_GROUPS_USERS,
+				Long.valueOf(user.getUserId()));
 
 		List<UserGroup> userGroups = UserGroupServiceUtil.search(
 			user.getCompanyId(), null, userGroupParams, QueryUtil.ALL_POS,
