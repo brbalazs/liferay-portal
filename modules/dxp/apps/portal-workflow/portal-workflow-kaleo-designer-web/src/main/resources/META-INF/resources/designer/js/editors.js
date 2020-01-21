00f765d9790aa3f2978008803b5b1fbf272b9ec7
Marcell Gyöpös <marcell.gyopos@liferay.com>
2020-01-21 10:40:25 +0100

    LPS-105533 Add default value for assignment

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/editors.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/editors.js
@@ -2,6 +2,7 @@ AUI.add(
 	'liferay-kaleo-designer-editors',
 	function(A) {
 		var AArray = A.Array;
+		var AObject = A.Object;
 		var getClassName = A.getClassName;
 		var Lang = A.Lang;
 		var Template = A.Template;
@@ -1716,6 +1717,12 @@ AUI.add(
 					var recipients = instance.get('recipients');
 
 					var value = recipients[index];
+
+					if (value && AObject.isEmpty(value)) {
+						value.assignmentType = 'taskAssignees';
+						value.receptionType = ['to'];
+						value.taskAssignees = [''];
+					}
 
 					instance.showEditorForm(
 						NotificationRecipientsEditorForm,
