5e3bc58d2a389775ae893f39a63c0bfc305be901
Javier Gamarra <nhpatt@gmail.com>
2020-01-23 04:00:13 +0100

    LPS-105235 fix bug setting the ScopeChecker (Object applies to everything)

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/graphql/servlet/GraphQLServletExtender.java
@@ -743,6 +743,16 @@ public class GraphQLServletExtender {
 
 			Class<?> fieldClass = field.getType();
 
+			if (fieldClass.equals(Object.class) &&
+				Objects.equals(field.getName(), "contextScopeChecker")) {
+
+				field.setAccessible(true);
+
+				field.set(instance, _getScopeChecker());
+
+				continue;
+			}
+
 			if (fieldClass.isAssignableFrom(AcceptLanguage.class)) {
 				field.setAccessible(true);
 
@@ -849,11 +859,6 @@ public class GraphQLServletExtender {
 					};
 
 				field.set(instance, sortsBiFunction);
-			}
-			else if (Objects.equals(field.getName(), "contextScopeChecker")) {
-				field.setAccessible(true);
-
-				field.set(instance, _getScopeChecker());
 			}
 		}
 
