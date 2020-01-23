f12926a1f3d4a9982e51328c56350573695bc363
Jorge García Jiménez <jorge.garcia@liferay.com>
2020-01-23 09:19:02 +0100

    LPS-93628 add table plugins to removeplugins to match alloyeditor configuration

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/editor/configuration/FragmentEntryLinkRichTextEditorConfigContributor.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/editor/configuration/FragmentEntryLinkRichTextEditorConfigContributor.java
@@ -127,7 +127,7 @@ public class FragmentEntryLinkRichTextEditorConfigContributor
 
 	protected String getRemovePluginsLists() {
 		return "contextmenu,elementspath,floatingspace,image,link,liststyle," +
-			"magicline,resize,tabletools,toolbar,ae_embed";
+			"magicline,resize,table,tabletools,toolbar,ae_embed";
 	}
 
 	protected JSONObject getToolbarsJSONObject() {
