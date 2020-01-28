8f5a337a986437a3984ccd3c35806ef48c7461d8
Brian Chan <brian.chan@liferay.com>
2020-01-28 11:36:42 -0800

    LPS-94690 SF
    (cherry picked from commit 443e6b422cb552643cdbbec28f9c15c05a6e0b99)

--- modules/apps/staging/staging-configuration-web/src/main/java/com/liferay/staging/configuration/web/internal/portlet/StagingConfigurationPortlet.java
+++ modules/apps/staging/staging-configuration-web/src/main/java/com/liferay/staging/configuration/web/internal/portlet/StagingConfigurationPortlet.java
@@ -153,10 +153,6 @@ public class StagingConfigurationPortlet extends MVCPortlet {
 				actionRequest, "secureConnection");
 			long remoteGroupId = ParamUtil.getLong(
 				actionRequest, "remoteGroupId");
-			boolean overrideRemoteSiteURL = ParamUtil.getBoolean(
-				actionRequest, "overrideRemoteSiteURL");
-			String remoteSiteURL = ParamUtil.getString(
-				actionRequest, "remoteSiteURL");
 
 			stagedGroup = liveGroup.isStagedRemotely();
 
@@ -170,6 +166,11 @@ public class StagingConfigurationPortlet extends MVCPortlet {
 					branchingPrivate, remoteAddress, remotePort,
 					remotePathContext, secureConnection, remoteGroupId,
 					serviceContext);
+
+				boolean overrideRemoteSiteURL = ParamUtil.getBoolean(
+					actionRequest, "overrideRemoteSiteURL");
+				String remoteSiteURL = ParamUtil.getString(
+					actionRequest, "remoteSiteURL");
 
 				_staging.setRemoteSiteURL(
 					liveGroup, overrideRemoteSiteURL, remoteSiteURL);
