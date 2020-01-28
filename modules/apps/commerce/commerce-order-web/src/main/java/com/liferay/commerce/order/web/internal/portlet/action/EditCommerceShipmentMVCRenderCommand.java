liferay/com-liferay-commerce

COMMERCE-2232 Added create shipment modal

commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/action/EditCommerceShipmentMVCRenderCommand.java
@@ -1,0 +1,126 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.web.internal.portlet.action;
+
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.exception.NoSuchOrderException;
+import com.liferay.commerce.notification.service.CommerceNotificationQueueEntryService;
+import com.liferay.commerce.order.web.internal.display.context.CommerceOrderEditDisplayContext;
+import com.liferay.commerce.payment.service.CommercePaymentMethodGroupRelService;
+import com.liferay.commerce.price.CommerceOrderPriceCalculation;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.commerce.service.CommerceAddressService;
+import com.liferay.commerce.service.CommerceOrderItemService;
+import com.liferay.commerce.service.CommerceOrderNoteService;
+import com.liferay.commerce.service.CommerceOrderPaymentLocalService;
+import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.commerce.service.CommerceShipmentService;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + CommercePortletKeys.COMMERCE_ORDER,
+		"mvc.command.name=editCommerceShipment"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditCommerceShipmentMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			CommerceOrderEditDisplayContext commerceOrderEditDisplayContext =
+				new CommerceOrderEditDisplayContext(
+					_commerceAddressService, _commerceChannelLocalService,
+					_commerceNotificationQueueEntryService,
+					_commerceOrderService, _commerceOrderItemService,
+					_commerceOrderNoteService,
+					_commerceOrderPaymentLocalService,
+					_commercePaymentMethodGroupRelService,
+					_commerceOrderPriceCalculation, _commerceShipmentService,
+					_itemSelector, renderRequest);
+
+			renderRequest.setAttribute(
+				WebKeys.PORTLET_DISPLAY_CONTEXT,
+				commerceOrderEditDisplayContext);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchOrderException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return "/error.jsp";
+			}
+
+			throw new PortletException(e);
+		}
+
+		return "/order/add_shipment.jsp";
+	}
+
+	@Reference
+	private CommerceAddressService _commerceAddressService;
+
+	@Reference
+	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@Reference
+	private CommerceNotificationQueueEntryService
+		_commerceNotificationQueueEntryService;
+
+	@Reference
+	private CommerceOrderItemService _commerceOrderItemService;
+
+	@Reference
+	private CommerceOrderNoteService _commerceOrderNoteService;
+
+	@Reference
+	private CommerceOrderPaymentLocalService _commerceOrderPaymentLocalService;
+
+	@Reference
+	private CommerceOrderPriceCalculation _commerceOrderPriceCalculation;
+
+	@Reference
+	private CommerceOrderService _commerceOrderService;
+
+	@Reference
+	private CommercePaymentMethodGroupRelService
+		_commercePaymentMethodGroupRelService;
+
+	@Reference
+	private CommerceShipmentService _commerceShipmentService;
+
+	@Reference
+	private ItemSelector _itemSelector;
+
+}

brbalazs/liferay-portal

modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/action/EditCommerceShipmentMVCRenderCommand.java
@@ -1,0 +1,136 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.order.web.internal.portlet.action;
+
+import com.liferay.commerce.constants.CommercePortletKeys;
+import com.liferay.commerce.exception.NoSuchOrderException;
+import com.liferay.commerce.notification.service.CommerceNotificationQueueEntryLocalService;
+import com.liferay.commerce.notification.service.CommerceNotificationTemplateService;
+import com.liferay.commerce.order.web.internal.display.context.CommerceOrderEditDisplayContext;
+import com.liferay.commerce.payment.service.CommercePaymentMethodGroupRelService;
+import com.liferay.commerce.price.CommerceOrderPriceCalculation;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.commerce.service.CommerceAddressService;
+import com.liferay.commerce.service.CommerceOrderItemService;
+import com.liferay.commerce.service.CommerceOrderNoteService;
+import com.liferay.commerce.service.CommerceOrderPaymentLocalService;
+import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.commerce.service.CommerceShipmentService;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + CommercePortletKeys.COMMERCE_ORDER,
+		"mvc.command.name=editCommerceShipment"
+	},
+	service = MVCRenderCommand.class
+)
+public class EditCommerceShipmentMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		try {
+			CommerceOrderEditDisplayContext commerceOrderEditDisplayContext =
+				new CommerceOrderEditDisplayContext(
+					_commerceAddressService, _commerceChannelLocalService,
+					_commerceNotificationTemplateService,
+					_commerceNotificationQueueEntryLocalService,
+					_commerceOrderService, _commerceOrderItemService,
+					_commerceOrderNoteService,
+					_commerceOrderPaymentLocalService,
+					_commercePaymentMethodGroupRelService,
+					_commerceOrderPriceCalculation, _commerceShipmentService,
+					_itemSelector, renderRequest, _userLocalService);
+
+			renderRequest.setAttribute(
+				WebKeys.PORTLET_DISPLAY_CONTEXT,
+				commerceOrderEditDisplayContext);
+		}
+		catch (Exception e) {
+			if (e instanceof NoSuchOrderException ||
+				e instanceof PrincipalException) {
+
+				SessionErrors.add(renderRequest, e.getClass());
+
+				return "/error.jsp";
+			}
+
+			throw new PortletException(e);
+		}
+
+		return "/order/add_shipment.jsp";
+	}
+
+	@Reference
+	private CommerceAddressService _commerceAddressService;
+
+	@Reference
+	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@Reference
+	private CommerceNotificationQueueEntryLocalService
+		_commerceNotificationQueueEntryLocalService;
+
+	@Reference
+	private CommerceNotificationTemplateService
+		_commerceNotificationTemplateService;
+
+	@Reference
+	private CommerceOrderItemService _commerceOrderItemService;
+
+	@Reference
+	private CommerceOrderNoteService _commerceOrderNoteService;
+
+	@Reference
+	private CommerceOrderPaymentLocalService _commerceOrderPaymentLocalService;
+
+	@Reference
+	private CommerceOrderPriceCalculation _commerceOrderPriceCalculation;
+
+	@Reference
+	private CommerceOrderService _commerceOrderService;
+
+	@Reference
+	private CommercePaymentMethodGroupRelService
+		_commercePaymentMethodGroupRelService;
+
+	@Reference
+	private CommerceShipmentService _commerceShipmentService;
+
+	@Reference
+	private ItemSelector _itemSelector;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}