1caf3e5676a78ae5e39b0d14898454c163af1473
Vendel Toreki <vendel.toreki@liferay.com>
2020-01-28 14:48:24 +0100

    LPS-94690 Renaming variable 'setRemoteSiteURL' to 'overrideRemoteSiteURL'
    (cherry picked from commit cf8d36b6bfc488bcc3efa43fae06cc61110e7c5d)

--- modules/apps/staging/staging-configuration-web/src/main/java/com/liferay/staging/configuration/web/internal/portlet/StagingConfigurationPortlet.java
+++ modules/apps/staging/staging-configuration-web/src/main/java/com/liferay/staging/configuration/web/internal/portlet/StagingConfigurationPortlet.java
@@ -153,8 +153,8 @@ public class StagingConfigurationPortlet extends MVCPortlet {
 				actionRequest, "secureConnection");
 			long remoteGroupId = ParamUtil.getLong(
 				actionRequest, "remoteGroupId");
-			boolean setRemoteSiteURL = ParamUtil.getBoolean(
-				actionRequest, "setRemoteSiteURL");
+			boolean overrideRemoteSiteURL = ParamUtil.getBoolean(
+				actionRequest, "overrideRemoteSiteURL");
 			String remoteSiteURL = ParamUtil.getString(
 				actionRequest, "remoteSiteURL");
 
@@ -172,7 +172,7 @@ public class StagingConfigurationPortlet extends MVCPortlet {
 					serviceContext);
 
 				_staging.setRemoteSiteURL(
-					liveGroup, setRemoteSiteURL, remoteSiteURL);
+					liveGroup, overrideRemoteSiteURL, remoteSiteURL);
 			}
 			catch (Exception e) {
 				SessionErrors.add(actionRequest, Exception.class, e);
