8063cf31bac798de63903f67c5ec1bc5ddf18715
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2020-01-31 16:12:51 +0100

    COMMERCE-2726 date range filters now handle min and max now dynamically handle mix and max values

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/management_bar/components/filters/DateRange.es.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/management_bar/components/filters/DateRange.es.js
@@ -55,7 +55,7 @@ function DateFilter(props) {
 				</label>
 				<input
 					className="form-control"
-					max={props.max && formatDateObject(props.max)}
+					max={toValue || (props.max && formatDateObject(props.max))}
 					min={props.min && formatDateObject(props.min)}
 					onChange={e => setFromValue(e.target.value)}
 					pattern="\d{4}-\d{2}-\d{2}"
@@ -69,7 +69,7 @@ function DateFilter(props) {
 				<input
 					className="form-control"
 					max={props.max && formatDateObject(props.max)}
-					min={props.min && formatDateObject(props.min)}
+					min={fromValue || (props.min && formatDateObject(props.min))}
 					onChange={e => setToValue(e.target.value)}
 					pattern="\d{4}-\d{2}-\d{2}"
 					placeholder={props.placeholder || 'yyyy-mm-dd'}
