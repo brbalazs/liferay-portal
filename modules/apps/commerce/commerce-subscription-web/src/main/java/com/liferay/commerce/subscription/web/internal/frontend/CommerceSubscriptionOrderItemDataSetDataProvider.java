dfc9436ecd842af2ce8cbd6a233b880b7c799b64
Luca Pellizzon <luca.pellizzon@liferay.com>
2020-01-31 17:33:48 +0100

    COMMERCE-2661 subscriptions order items dataset

---
+++ modules/apps/commerce/commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/frontend/CommerceSubscriptionOrderItemDataSetDataProvider.java
@@ -0,0 +1,361 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.subscription.web.internal.frontend;
+
+import com.liferay.commerce.context.CommerceContext;
+import com.liferay.commerce.context.CommerceContextFactory;
+import com.liferay.commerce.currency.model.CommerceMoney;
+import com.liferay.commerce.discount.CommerceDiscountValue;
+import com.liferay.commerce.frontend.CommerceDataSetDataProvider;
+import com.liferay.commerce.frontend.Filter;
+import com.liferay.commerce.frontend.Pagination;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.model.CommerceOrderItem;
+import com.liferay.commerce.model.CommerceSubscriptionEntry;
+import com.liferay.commerce.price.CommerceProductPrice;
+import com.liferay.commerce.price.CommerceProductPriceCalculation;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.model.CPSubscriptionInfo;
+import com.liferay.commerce.product.util.CPSubscriptionType;
+import com.liferay.commerce.product.util.CPSubscriptionTypeRegistry;
+import com.liferay.commerce.service.CommerceOrderItemLocalService;
+import com.liferay.commerce.service.CommerceSubscriptionEntryLocalService;
+import com.liferay.commerce.subscription.web.internal.model.OrderItem;
+import com.liferay.commerce.subscription.web.internal.model.Sku;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.TextFormatter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+import javax.portlet.WindowStateException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	immediate = true,
+	property = "commerce.data.provider.key=" + CommerceSubscriptionDataSetConstants.COMMERCE_DATA_SET_KEY_SUBSCRIPTION_ORDER_ITEMS,
+	service = CommerceDataSetDataProvider.class
+)
+public class CommerceSubscriptionOrderItemDataSetDataProvider
+	implements CommerceDataSetDataProvider<OrderItem> {
+
+	@Override
+	public int countItems(HttpServletRequest httpServletRequest, Filter filter)
+		throws PortalException {
+
+		return 1;
+	}
+
+	@Override
+	public List<OrderItem> getItems(
+			HttpServletRequest httpServletRequest, Filter filter,
+			Pagination pagination, Sort sort)
+		throws PortalException {
+
+		List<OrderItem> orderItems = new ArrayList<>();
+
+		long commerceSubscriptionEntryId = ParamUtil.getLong(
+			httpServletRequest, "commerceSubscriptionEntryId");
+
+		long companyId = _portal.getCompanyId(httpServletRequest);
+
+		long controlPanelPlid = _portal.getControlPanelPlid(companyId);
+
+		Locale locale = _portal.getLocale(httpServletRequest);
+
+		CommerceContext commerceContext = _commerceContextFactory.create(
+			companyId, _portal.getScopeGroupId(controlPanelPlid),
+			_portal.getUserId(httpServletRequest), 0, 0);
+
+		CommerceSubscriptionEntry commerceSubscriptionEntry =
+			_commerceSubscriptionEntryLocalService.getCommerceSubscriptionEntry(
+				commerceSubscriptionEntryId);
+
+		CommerceOrderItem commerceOrderItem =
+			_commerceOrderItemLocalService.getCommerceOrderItem(
+				commerceSubscriptionEntry.getCommerceOrderItemId());
+
+		String price = StringPool.BLANK;
+		String total = StringPool.BLANK;
+		String discount = StringPool.BLANK;
+
+		CommerceProductPrice commerceProductPrice =
+			_commerceProductPriceCalculation.getCommerceProductPrice(
+				commerceOrderItem.getCPInstanceId(),
+				commerceOrderItem.getQuantity(), commerceContext);
+
+		if (commerceProductPrice != null) {
+			CommerceMoney unitPrice = commerceProductPrice.getUnitPrice();
+			CommerceMoney finalPrice = commerceProductPrice.getFinalPrice();
+
+			price = HtmlUtil.escape(unitPrice.format(locale));
+			total = HtmlUtil.escape(finalPrice.format(locale));
+
+			CommerceDiscountValue discountValue =
+				commerceProductPrice.getDiscountValue();
+
+			if (discountValue != null) {
+				CommerceMoney discountAmount =
+					discountValue.getDiscountAmount();
+
+				discount = HtmlUtil.escape(discountAmount.format(locale));
+			}
+		}
+
+		orderItems.add(
+			new OrderItem(
+				commerceOrderItem.getCommerceOrderItemId(),
+				commerceOrderItem.getCommerceOrderId(),
+				new Sku(
+					commerceOrderItem.getSku(),
+					_getOrderItemPanelURL(
+						commerceOrderItem.getCommerceOrderItemId(),
+						httpServletRequest)),
+				commerceOrderItem.getName(locale), price,
+				_getSubscriptionDuration(
+					commerceOrderItem, locale, httpServletRequest),
+				_getSubscriptionPeriod(
+					commerceOrderItem, locale, httpServletRequest),
+				discount, commerceOrderItem.getQuantity(), total));
+
+		return orderItems;
+	}
+
+	private String _getOrderItemPanelURL(
+			long commerceOrderItemId, HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		PortletURL portletURL = PortletProviderUtil.getPortletURL(
+			httpServletRequest, CommerceOrder.class.getName(),
+			PortletProvider.Action.MANAGE);
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "editCommerceOrderItem");
+		portletURL.setParameter(
+			"redirect", _portal.getCurrentURL(httpServletRequest));
+
+		long commerceOrderId = ParamUtil.getLong(
+			httpServletRequest, "commerceOrderId");
+
+		portletURL.setParameter(
+			"commerceOrderId", String.valueOf(commerceOrderId));
+
+		portletURL.setParameter(
+			"commerceOrderItemId", String.valueOf(commerceOrderItemId));
+
+		try {
+			portletURL.setWindowState(LiferayWindowState.POP_UP);
+		}
+		catch (WindowStateException wse) {
+			_log.error(wse, wse);
+		}
+
+		return portletURL.toString();
+	}
+
+	private String _getPeriodKey(
+		String period, boolean plural, HttpServletRequest httpServletRequest) {
+
+		if (plural) {
+			return LanguageUtil.get(
+				httpServletRequest,
+				TextFormatter.formatPlural(StringUtil.toLowerCase(period)));
+		}
+
+		return LanguageUtil.get(httpServletRequest, period);
+	}
+
+	private String _getSubscriptionDuration(
+			CommerceOrderItem commerceOrderItem, Locale locale,
+			HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		String subscriptionDuration = StringPool.BLANK;
+
+		if (commerceOrderItem.isSubscription()) {
+			CommerceOrder commerceOrder = commerceOrderItem.getCommerceOrder();
+
+			if (commerceOrder.isOpen()) {
+				CPInstance cpInstance = commerceOrderItem.getCPInstance();
+
+				CPSubscriptionInfo cpSubscriptionInfo =
+					cpInstance.getCPSubscriptionInfo();
+
+				if (cpSubscriptionInfo == null) {
+					return subscriptionDuration;
+				}
+
+				String period = StringPool.BLANK;
+
+				CPSubscriptionType cpSubscriptionType =
+					_cpSubscriptionTypeRegistry.getCPSubscriptionType(
+						cpSubscriptionInfo.getSubscriptionType());
+
+				if (cpSubscriptionType != null) {
+					period = cpSubscriptionType.getLabel(locale);
+				}
+
+				long duration = cpSubscriptionInfo.getMaxSubscriptionCycles();
+
+				if (duration > 0) {
+					subscriptionDuration = LanguageUtil.format(
+						httpServletRequest, "duration-x-x",
+						new Object[] {
+							duration,
+							_getPeriodKey(
+								period, duration != 1, httpServletRequest)
+						});
+				}
+			}
+			else {
+				CommerceSubscriptionEntry commerceSubscriptionEntry =
+					_commerceSubscriptionEntryLocalService.
+						fetchCommerceSubscriptionEntry(
+							commerceOrderItem.getCommerceOrderItemId());
+
+				if (commerceSubscriptionEntry == null) {
+					return subscriptionDuration;
+				}
+
+				String period = StringPool.BLANK;
+
+				long duration =
+					commerceSubscriptionEntry.getMaxSubscriptionCycles();
+
+				subscriptionDuration = LanguageUtil.format(
+					httpServletRequest, "duration-x-x",
+					new Object[] {
+						duration,
+						_getPeriodKey(period, duration != 1, httpServletRequest)
+					});
+			}
+		}
+
+		return subscriptionDuration;
+	}
+
+	private String _getSubscriptionPeriod(
+			CommerceOrderItem commerceOrderItem, Locale locale,
+			HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		String subscriptionPeriod = StringPool.BLANK;
+
+		if (commerceOrderItem.isSubscription()) {
+			CommerceOrder commerceOrder = commerceOrderItem.getCommerceOrder();
+
+			if (commerceOrder.isOpen()) {
+				CPInstance cpInstance = commerceOrderItem.getCPInstance();
+
+				CPSubscriptionInfo cpSubscriptionInfo =
+					cpInstance.getCPSubscriptionInfo();
+
+				if (cpSubscriptionInfo == null) {
+					return subscriptionPeriod;
+				}
+
+				String period = StringPool.BLANK;
+
+				CPSubscriptionType cpSubscriptionType =
+					_cpSubscriptionTypeRegistry.getCPSubscriptionType(
+						cpSubscriptionInfo.getSubscriptionType());
+
+				if (cpSubscriptionType != null) {
+					period = cpSubscriptionType.getLabel(locale);
+				}
+
+				int subscriptionLength =
+					cpSubscriptionInfo.getSubscriptionLength();
+
+				subscriptionPeriod = LanguageUtil.format(
+					httpServletRequest, "every-x-x",
+					new Object[] {
+						subscriptionLength,
+						_getPeriodKey(
+							period, subscriptionLength != 1, httpServletRequest)
+					});
+			}
+			else {
+				CommerceSubscriptionEntry commerceSubscriptionEntry =
+					_commerceSubscriptionEntryLocalService.
+						fetchCommerceSubscriptionEntry(
+							commerceOrderItem.getCommerceOrderItemId());
+
+				if (commerceSubscriptionEntry == null) {
+					return subscriptionPeriod;
+				}
+
+				String period = StringPool.BLANK;
+
+				int subscriptionLength =
+					commerceSubscriptionEntry.getSubscriptionLength();
+
+				subscriptionPeriod = LanguageUtil.format(
+					httpServletRequest, "every-x-x",
+					new Object[] {
+						subscriptionLength,
+						_getPeriodKey(
+							period, subscriptionLength != 1, httpServletRequest)
+					});
+			}
+		}
+
+		return subscriptionPeriod;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceSubscriptionOrderItemDataSetDataProvider.class);
+
+	@Reference
+	private CommerceContextFactory _commerceContextFactory;
+
+	@Reference
+	private CommerceOrderItemLocalService _commerceOrderItemLocalService;
+
+	@Reference
+	private CommerceProductPriceCalculation _commerceProductPriceCalculation;
+
+	@Reference
+	private CommerceSubscriptionEntryLocalService
+		_commerceSubscriptionEntryLocalService;
+
+	@Reference
+	private CPSubscriptionTypeRegistry _cpSubscriptionTypeRegistry;
+
+	@Reference
+	private Portal _portal;
+
+}
