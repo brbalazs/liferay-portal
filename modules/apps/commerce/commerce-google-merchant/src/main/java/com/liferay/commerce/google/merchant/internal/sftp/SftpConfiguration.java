755e8a2ca32fcbc8d0e3524a9c6723158909f4dc
Eric Chin <eric.chin@liferay.com>
2020-01-31 10:36:19 -0800

    COMMERCE-2685 Refactor sftp configuration

---
+++ modules/apps/commerce/commerce-google-merchant/src/main/java/com/liferay/commerce/google/merchant/internal/sftp/SftpConfiguration.java
@@ -0,0 +1,41 @@
+package com.liferay.commerce.google.merchant.internal.sftp;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.commerce.google.merchant.constants.GoogleMerchantConstants;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Thomas Stewart
+ */
+@ExtendedObjectClassDefinition(category = "google-merchant")
+@Meta.OCD(
+	id = "com.liferay.commerce.google.merchant.internal.sftp.SftpConfiguration",
+	name = "sftp-upload-configuration"
+)
+public interface SftpConfiguration {
+
+	@Meta.AD(
+		deflt = GoogleMerchantConstants.GOOGLE_PARTNER_UPLOAD_URL,
+		name = "host",
+		required = false
+	)
+	public String host();
+
+	@Meta.AD(name = "feed-username", required = false)
+	public String username();
+
+	@Meta.AD(name = "feed-password", required = false)
+	public String password();
+
+	@Meta.AD(
+		deflt = "19321",
+		name = "port",
+		required = false
+	)
+	public int port();
+
+	@Meta.AD(name = "fingerprint", required = false)
+	public String fingerprint();
+
+}
