e81230acd7f90692b8ad2b1600889933e4541c90
Roland Pákai <roland.pakai@liferay.com>
2020-02-06 11:05:02 +0100

    LPS-106893 Implement nestedMoveField for handling nested field move

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -3471,6 +3471,12 @@ AUI.add(
 						fields.splice(newIndex, 0, fields.splice(oldIndex, 1)[0]);
 
 						var field = fields[newIndex];
+						
+						instance.nestedMoveField(field);
+					},
+
+					nestedMoveField: function(field) {
+						var instance = this;
 
 						var fieldDefinition = field.getFieldDefinition();
 
@@ -3479,6 +3485,12 @@ AUI.add(
 
 							if (type === 'ddm-text-html') {
 								instance.recreateEditor(field);
+							}
+
+							for (var i = 0; i < field.get('fields').length; i++) {
+								var nestedField = field.get('fields')[i];
+
+								instance.nestedMoveField(nestedField);
 							}
 						}
 					},
