06f6fc39915c30269b6052632abe7866c7ed4031
Brian Chan <brian.chan@liferay.com>
2020-02-07 10:29:43 -0800

    LPS-107620 In throws, keep it generic

--- modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMGIFImageScaler.java
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMGIFImageScaler.java
@@ -23,7 +23,6 @@ import com.liferay.adaptive.media.image.scaler.AMImageScaledImage;
 import com.liferay.adaptive.media.image.scaler.AMImageScaler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
-import com.liferay.portal.kernel.exception.ImageResolutionException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.process.CollectorOutputProcessor;
@@ -102,7 +101,7 @@ public class AMGIFImageScaler implements AMImageScaler {
 	}
 
 	private Tuple<Integer, Integer> _getDimension(byte[] bytes)
-		throws ImageResolutionException, IOException {
+		throws IOException, PortalException {
 
 		try (InputStream inputStream = new UnsyncByteArrayInputStream(bytes)) {
 			RenderedImage renderedImage = RenderedImageUtil.readImage(
