7791c77042ac52661776364a3d30c665ee0b7b39
Matthew Tambara <matthew.tambara@liferay.com>
2020-02-07 09:57:50 -0800

    LPS-108250 Jars use slash as file separator on all platforms

--- modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspJavaFileObjectResolver.java
+++ modules/apps/static/portal-osgi-web/portal-osgi-web-servlet-jsp-compiler/src/main/java/com/liferay/portal/osgi/web/servlet/jsp/compiler/internal/JspJavaFileObjectResolver.java
@@ -251,9 +251,7 @@ public class JspJavaFileObjectResolver implements JavaFileObjectResolver {
 
 							String entryPathString = entryPath.toString();
 
-							if (entryPathString.charAt(0) ==
-									File.separatorChar) {
-
+							if (entryPathString.charAt(0) == CharPool.SLASH) {
 								entryPathString = entryPathString.substring(1);
 							}
 
