30aa0473f82758666e4a9ef2141abaed7791e093
Marco Leo <marco.leo@liferay.com>
2020-02-07 16:07:44 +0100

    COMMERCE-2702 Include credentials in fetch

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/DatasetDisplay.es.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/DatasetDisplay.es.js
@@ -45,7 +45,13 @@ function loadData(apiUrl, filters, delta, page = 1, sorting = []) {
 
 	const url = `${apiUrl}${authString}${pagination}${sortingString}${filterString}`;
 
-	return fetch(url).then(response => response.json());
+	return fetch(
+		url,
+		{
+			credentials: 'include',
+			headers: new Headers({'x-csrf-token': Liferay.authToken}),
+			method: 'GET'
+		}).then(response => response.json());
 }
 
 function DatasetDisplay(props) {
