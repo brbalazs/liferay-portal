23d7733cc4dd9adb88f094d6d5ebb45144a05e4c
IstvanD <istvan.dezsi@liferay.com>
2020-02-10 17:40:20 +0100

    LPS-108205 Adding custom range rule for integer fields

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -11,6 +11,10 @@ AUI.add(
 
 		var INSTANCE_ID_PREFIX = '_INSTANCE_';
 
+		var INTEGER_MIN_VALUE = 0;
+
+		var INTEGER_MAX_VALUE = 2147483647;
+
 		var SELECTOR_REPEAT_BUTTONS = '.lfr-ddm-repeatable-add-button, .lfr-ddm-repeatable-delete-button';
 
 		var TPL_ICON_CARET = '<span class="collapse-icon-closed"><span class="icon-caret-right"></span></span>';
@@ -447,6 +451,45 @@ AUI.add(
 						instance.eventHandlers = null;
 
 						instance.get('container').remove();
+					},
+
+					addIntegerRangeRule() {
+						var instance = this;
+
+						if (instance) {
+							var dataType = instance.get('dataType');
+
+							if (dataType && dataType === 'integer') {
+								var liferayForm = instance.get('liferayForm');
+
+								if (liferayForm) {
+									var node = instance.getInputNode();
+
+									var fieldName = node.get('name');
+
+									var errorMessage = Liferay.Util.sub(
+										Liferay.Language.get(
+											'please-enter-a-valid-integer-value-between-x-and-x'
+										),
+										INTEGER_MIN_VALUE,
+										INTEGER_MAX_VALUE
+									);
+
+									liferayForm.addRule(
+										fieldName,
+										'integerRange_custom',
+										errorMessage,
+										val => {
+											return (
+												val >= INTEGER_MIN_VALUE &&
+												val <= INTEGER_MAX_VALUE
+											);
+										},
+										true
+									);
+								}
+							}
+						}
 					},
 
 					createField: function(fieldTemplate) {
@@ -936,6 +979,8 @@ AUI.add(
 						if (event.formName === formNode.attr('name')) {
 							instance.set('liferayForm', event.form);
 						}
+
+						instance.addIntegerRangeRule();
 					},
 
 					_getLocalizable: function() {
