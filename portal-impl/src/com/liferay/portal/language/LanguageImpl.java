673133eb23250b13c48dcbeab36fc1c938d73bb6
shuyangzhou <shuyang.zhou@liferay.com>
2020-02-11 10:22:59 -0800

    LPS-107255 Redundant set, if we can get to the gb branch, we must already have been in the mb branch.

--- portal-impl/src/com/liferay/portal/language/LanguageImpl.java
+++ portal-impl/src/com/liferay/portal/language/LanguageImpl.java
@@ -816,8 +816,6 @@ public class LanguageImpl implements Language, Serializable {
 			suffix = "storage.size.suffix.gb";
 
 			size = size / _STORAGE_SIZE_DENOMINATOR;
-
-			numberFormat.setMaximumFractionDigits(1);
 		}
 
 		suffix = get(locale, suffix);
