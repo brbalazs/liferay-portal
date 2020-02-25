d10b083c0b2daea8c5777a966016b6db6386a430
Igor Beslic <igor.beslic@liferay.com>
2020-02-25 17:11:12 +0100

    COMMERCE-2895 commerce-product-service - CPOptionLocalService performs type validation

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPOptionLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPOptionLocalServiceImpl.java
@@ -14,6 +14,8 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.configuration.CPOptionConfiguration;
+import com.liferay.commerce.product.constants.CPConstants;
 import com.liferay.commerce.product.constants.CPField;
 import com.liferay.commerce.product.exception.CPOptionKeyException;
 import com.liferay.commerce.product.model.CPOption;
@@ -22,6 +24,8 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
@@ -35,11 +39,14 @@ import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.settings.SystemSettingsLocator;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.spring.extender.service.ServiceReference;
 
 import java.io.Serializable;
 
@@ -52,6 +59,7 @@ import java.util.Map;
 
 /**
  * @author Marco Leo
+ * @author Igor Beslic
  */
 public class CPOptionLocalServiceImpl extends CPOptionLocalServiceBaseImpl {
 
@@ -64,6 +72,8 @@ public class CPOptionLocalServiceImpl extends CPOptionLocalServiceBaseImpl {
 			String key, String externalReferenceCode,
 			ServiceContext serviceContext)
 		throws PortalException {
+
+		validateDDMFormFieldTypeName(ddmFormFieldTypeName, skuContributor);
 
 		User user = userLocalService.getUser(userId);
 
@@ -203,6 +213,8 @@ public class CPOptionLocalServiceImpl extends CPOptionLocalServiceBaseImpl {
 			boolean facetable, boolean required, boolean skuContributor,
 			String key, ServiceContext serviceContext)
 		throws PortalException {
+
+		validateDDMFormFieldTypeName(ddmFormFieldTypeName, skuContributor);
 
 		CPOption cpOption = cpOptionPersistence.findByPrimaryKey(cpOptionId);
 
@@ -353,8 +365,50 @@ public class CPOptionLocalServiceImpl extends CPOptionLocalServiceBaseImpl {
 		}
 	}
 
+	protected void validateDDMFormFieldTypeName(
+			String ddmFormFieldTypeName, boolean skuContributor)
+		throws ConfigurationException {
+
+		if (Validator.isNull(ddmFormFieldTypeName)) {
+			throw new IllegalArgumentException("Illegal DDM form field type");
+		}
+
+		CPOptionConfiguration cpOptionConfiguration =
+			_getCPOptionConfiguration();
+
+		String[] ddmFormFieldTypesAllowed =
+			cpOptionConfiguration.ddmFormFieldTypesAllowed();
+
+		if (skuContributor) {
+			ddmFormFieldTypesAllowed =
+				cpOptionConfiguration.skuContributorDDMFormFieldTypesAllowed();
+		}
+
+		if (ArrayUtil.contains(
+				ddmFormFieldTypesAllowed, ddmFormFieldTypeName)) {
+
+			return;
+		}
+
+		throw new IllegalArgumentException(
+			String.format(
+				"Illegal DDM form field type {%s} for SKU contributor {%s}",
+				ddmFormFieldTypeName, skuContributor));
+	}
+
+	private CPOptionConfiguration _getCPOptionConfiguration()
+		throws ConfigurationException {
+
+		return _configurationProvider.getConfiguration(
+			CPOptionConfiguration.class,
+			new SystemSettingsLocator(CPConstants.CP_OPTION_SERVICE_NAME));
+	}
+
 	private static final String[] _SELECTED_FIELD_NAMES = {
 		Field.ENTRY_CLASS_PK, Field.COMPANY_ID, Field.UID
 	};
+
+	@ServiceReference(type = ConfigurationProvider.class)
+	private ConfigurationProvider _configurationProvider;
 
 }
