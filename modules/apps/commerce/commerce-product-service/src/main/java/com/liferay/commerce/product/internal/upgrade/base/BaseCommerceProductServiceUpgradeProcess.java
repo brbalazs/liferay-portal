1cffb88597bb82e6355d67db28fee6cb39fe7a06
Ethan Bustad <ethan.bustad@liferay.com>
2020-02-28 12:45:55 -0800

    COMMERCE-2952 Add upgrade processes to fix column name

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/base/BaseCommerceProductServiceUpgradeProcess.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/base/BaseCommerceProductServiceUpgradeProcess.java
@@ -114,6 +114,32 @@ public abstract class BaseCommerceProductServiceUpgradeProcess
 		}
 	}
 
+	protected void renameColumn(
+			Class<?> tableClass, String tableName, String oldColumnName,
+			String newColumnName)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Renaming column %s to table %s", oldColumnName,
+					tableName));
+		}
+
+		if (!hasColumn(tableName, newColumnName)) {
+			alter(
+				tableClass, new AlterColumnName(oldColumnName, newColumnName));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", newColumnName,
+						tableName));
+			}
+		}
+	}
+
 	protected boolean tableHasIndex(String tableName, String indexName)
 		throws Exception {
 
