2710d9fb47ccf06bd9797269a93da895ae3f3d20
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-03-02 15:33:35 -0800

    LRAC-5266 move isCustomField to base

--- modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/BaseEntityModelListener.java
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/BaseEntityModelListener.java
@@ -21,6 +21,10 @@ import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
 import com.liferay.analytics.settings.configuration.AnalyticsConfigurationTracker;
 import com.liferay.analytics.settings.security.constants.AnalyticsSecurityConstants;
 import com.liferay.expando.kernel.model.ExpandoBridge;
+import com.liferay.expando.kernel.model.ExpandoRow;
+import com.liferay.expando.kernel.model.ExpandoTable;
+import com.liferay.expando.kernel.model.ExpandoTableConstants;
+import com.liferay.expando.kernel.service.ExpandoTableLocalService;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
@@ -36,6 +40,7 @@ import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ShardedModel;
 import com.liferay.portal.kernel.model.TreeModel;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.CompanyService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.ArrayUtil;
@@ -195,6 +200,29 @@ public abstract class BaseEntityModelListener<T extends BaseModel<T>>
 
 	protected List<String> getUserAttributeNames() {
 		return _userAttributeNames;
+	}
+
+	protected boolean isCustomField(String className, long tableId) {
+		long classNameId = classNameLocalService.getClassNameId(className);
+
+		try {
+			ExpandoTable expandoTable = expandoTableLocalService.getTable(
+				tableId);
+
+			if (ExpandoTableConstants.DEFAULT_TABLE_NAME.equals(
+					expandoTable.getName()) &&
+				(expandoTable.getClassNameId() == classNameId)) {
+
+				return true;
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isInfoEnabled()) {
+				_log.info("Unable to get expando table " + tableId);
+			}
+		}
+
+		return false;
 	}
 
 	protected boolean isExcluded(T model) {
@@ -370,7 +398,13 @@ public abstract class BaseEntityModelListener<T extends BaseModel<T>>
 	protected AnalyticsMessageLocalService analyticsMessageLocalService;
 
 	@Reference
+	protected ClassNameLocalService classNameLocalService;
+
+	@Reference
 	protected CompanyService companyService;
+
+	@Reference
+	protected ExpandoTableLocalService expandoTableLocalService;
 
 	@Reference
 	protected UserLocalService userLocalService;
