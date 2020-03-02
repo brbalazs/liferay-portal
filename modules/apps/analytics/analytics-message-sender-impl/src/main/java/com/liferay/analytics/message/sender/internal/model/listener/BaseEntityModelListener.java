4cc4aea3442cebae7f0e92a44c19afe8bbb7b0eb
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-03-02 15:25:02 -0800

    LRAC-5266 move to base

--- modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/BaseEntityModelListener.java
+++ modules/apps/analytics/analytics-message-sender-impl/src/main/java/com/liferay/analytics/message/sender/internal/model/listener/BaseEntityModelListener.java
@@ -187,7 +187,15 @@ public abstract class BaseEntityModelListener<T extends BaseModel<T>>
 
 	protected abstract T getModel(long id) throws Exception;
 
+	protected List<String> getOrganizationAttributeNames() {
+		return _organizationAttributeNames;
+	}
+
 	protected abstract String getPrimaryKeyName();
+
+	protected List<String> getUserAttributeNames() {
+		return _userAttributeNames;
+	}
 
 	protected boolean isExcluded(T model) {
 		return false;
@@ -549,5 +557,17 @@ public abstract class BaseEntityModelListener<T extends BaseModel<T>>
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		BaseEntityModelListener.class);
+
+	private static final List<String> _organizationAttributeNames =
+		Arrays.asList(
+			"expando", "modifiedDate", "name", "parentOrganizationId",
+			"treePath", "type");
+	private static final List<String> _userAttributeNames = Arrays.asList(
+		"agreedToTermsOfUse", "comments", "companyId", "contactId",
+		"createDate", "defaultUser", "emailAddress", "emailAddressVerified",
+		"expando", "externalReferenceCode", "facebookId", "firstName",
+		"googleUserId", "greeting", "jobTitle", "languageId", "lastName",
+		"ldapServerId", "memberships", "middleName", "modifiedDate", "openId",
+		"portraitId", "screenName", "status", "timeZoneId", "uuid");
 
 }
