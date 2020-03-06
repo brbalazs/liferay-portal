8d8e0978818a021cee44b0866f90d8cadb7c3b14
Alessio Rendina <asilcomando@gmail.com>
2020-03-06 17:55:04 +0100

    COMMERCE-2702 Use correct method

--- modules/apps/commerce/commerce-tax-engine-fixed-service/src/main/java/com/liferay/commerce/tax/engine/fixed/internal/engine/ByAddressCommerceTaxEngine.java
+++ modules/apps/commerce/commerce-tax-engine-fixed-service/src/main/java/com/liferay/commerce/tax/engine/fixed/internal/engine/ByAddressCommerceTaxEngine.java
@@ -66,7 +66,7 @@ public class ByAddressCommerceTaxEngine implements CommerceTaxEngine {
 			commerceTaxCalculateRequest.getCommerceBillingAddressId();
 
 		if (isTaxAppliedToShippingAddress(
-				commerceTaxCalculateRequest.getSiteGroupId())) {
+				commerceTaxCalculateRequest.getChannelGroupId())) {
 
 			commerceAddressId =
 				commerceTaxCalculateRequest.getCommerceShippingAddressId();
