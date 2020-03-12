bac94bcb878f58b274e03d2efc96441969fe4cad
Alessio Rendina <asilcomando@gmail.com>
2020-03-12 16:04:27 +0100

    COMMERCE-2702 Added category display layout clay table

---
+++ modules/apps/commerce/commerce-product-asset-categories-web/src/main/java/com/liferay/commerce/product/asset/categories/web/internal/frontend/CommerceCategoryDisplayPageClayTable.java
@@ -0,0 +1,350 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.asset.categories.web.internal.frontend;
+
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.commerce.frontend.CommerceDataSetDataProvider;
+import com.liferay.commerce.frontend.Filter;
+import com.liferay.commerce.frontend.Pagination;
+import com.liferay.commerce.frontend.clay.data.set.ClayDataSetAction;
+import com.liferay.commerce.frontend.clay.data.set.ClayDataSetActionProvider;
+import com.liferay.commerce.frontend.clay.data.set.ClayDataSetDisplayView;
+import com.liferay.commerce.frontend.clay.table.ClayTableDataSetDisplayView;
+import com.liferay.commerce.frontend.clay.table.ClayTableSchema;
+import com.liferay.commerce.frontend.clay.table.ClayTableSchemaBuilder;
+import com.liferay.commerce.frontend.clay.table.ClayTableSchemaBuilderFactory;
+import com.liferay.commerce.frontend.clay.table.ClayTableSchemaField;
+import com.liferay.commerce.product.asset.categories.web.internal.model.CategoryDisplayPage;
+import com.liferay.commerce.product.constants.CPPortletKeys;
+import com.liferay.commerce.product.model.CPDisplayLayout;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CPDisplayLayoutService;
+import com.liferay.commerce.product.service.CommerceChannelService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.PortletProvider;
+import com.liferay.portal.kernel.portlet.PortletProviderUtil;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.QueryConfig;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.Serializable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"commerce.data.provider.key=" + CommerceCategoryDisplayPageClayTable.NAME,
+		"commerce.data.set.display.name=" + CommerceCategoryDisplayPageClayTable.NAME
+	},
+	service = {
+		ClayDataSetActionProvider.class, ClayDataSetDisplayView.class,
+		CommerceDataSetDataProvider.class
+	}
+)
+public class CommerceCategoryDisplayPageClayTable
+	extends ClayTableDataSetDisplayView
+	implements ClayDataSetActionProvider,
+			   CommerceDataSetDataProvider<CategoryDisplayPage> {
+
+	public static final String NAME = "category-display-pages";
+
+	@Override
+	public List<ClayDataSetAction> clayDataSetActions(
+			HttpServletRequest httpServletRequest, long groupId, Object model)
+		throws PortalException {
+
+		List<ClayDataSetAction> clayTableActions = new ArrayList<>();
+
+		try {
+			CategoryDisplayPage categoryDisplayPage =
+				(CategoryDisplayPage)model;
+
+			ClayDataSetAction editClayDataSetAction = new ClayDataSetAction(
+				StringPool.BLANK,
+				_getCategoryDisplayPageEditURL(
+					httpServletRequest,
+					categoryDisplayPage.getCategoryDisplayPageId()),
+				StringPool.BLANK, LanguageUtil.get(httpServletRequest, "edit"),
+				null, false, false);
+
+			editClayDataSetAction.setTarget("sidePanel");
+
+			clayTableActions.add(editClayDataSetAction);
+
+			ClayDataSetAction deleteClayDataSetAction = new ClayDataSetAction(
+				StringPool.BLANK,
+				_getCategoryDisplayPageDeleteURL(
+					httpServletRequest,
+					categoryDisplayPage.getCategoryDisplayPageId()),
+				StringPool.BLANK,
+				LanguageUtil.get(httpServletRequest, "delete"), null, false,
+				false);
+
+			clayTableActions.add(deleteClayDataSetAction);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return clayTableActions;
+	}
+
+	@Override
+	public int countItems(HttpServletRequest httpServletRequest, Filter filter)
+		throws PortalException {
+
+		long commerceChannelId = ParamUtil.getLong(
+			httpServletRequest, "commerceChannelId");
+
+		CommerceChannel commerceChannel =
+			_commerceChannelService.getCommerceChannel(commerceChannelId);
+
+		Indexer<CPDisplayLayout> indexer =
+			IndexerRegistryUtil.nullSafeGetIndexer(CPDisplayLayout.class);
+
+		return (int)indexer.searchCount(
+			_buildSearchContext(
+				commerceChannel, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null));
+	}
+
+	@Override
+	public ClayTableSchema getClayTableSchema() {
+		ClayTableSchemaBuilder clayTableSchemaBuilder =
+			_clayTableSchemaBuilderFactory.clayTableSchemaBuilder();
+
+		ClayTableSchemaField nameField = clayTableSchemaBuilder.addField(
+			"categoryName", "category-name");
+
+		nameField.setContentRenderer("actionLink");
+
+		clayTableSchemaBuilder.addField("layout", "layout");
+
+		return clayTableSchemaBuilder.build();
+	}
+
+	@Override
+	public List<CategoryDisplayPage> getItems(
+			HttpServletRequest httpServletRequest, Filter filter,
+			Pagination pagination, Sort sort)
+		throws PortalException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		long commerceChannelId = ParamUtil.getLong(
+			httpServletRequest, "commerceChannelId");
+
+		CommerceChannel commerceChannel =
+			_commerceChannelService.getCommerceChannel(commerceChannelId);
+
+		List<CategoryDisplayPage> categoryDisplayPages = new ArrayList<>();
+
+		List<CPDisplayLayout> cpDisplayLayouts = _getCPDisplayLayouts(
+			commerceChannel, pagination.getStartPosition(),
+			pagination.getEndPosition(), sort);
+
+		for (CPDisplayLayout cpDisplayLayout : cpDisplayLayouts) {
+			categoryDisplayPages.add(
+				new CategoryDisplayPage(
+					cpDisplayLayout.getCPDisplayLayoutId(),
+					_getCategoryName(cpDisplayLayout),
+					_getLayout(cpDisplayLayout, themeDisplay.getLanguageId())));
+		}
+
+		return categoryDisplayPages;
+	}
+
+	private SearchContext _buildSearchContext(
+		CommerceChannel commerceChannel, int start, int end, Sort sort) {
+
+		SearchContext searchContext = new SearchContext();
+
+		Map<String, Serializable> attributes = new HashMap<>();
+
+		attributes.put("entryModelClassName", AssetCategory.class.getName());
+		attributes.put("searchFilterEnabled", true);
+
+		searchContext.setAttributes(attributes);
+
+		searchContext.setCompanyId(commerceChannel.getCompanyId());
+		searchContext.setGroupIds(
+			new long[] {commerceChannel.getSiteGroupId()});
+		searchContext.setStart(start);
+		searchContext.setEnd(end);
+
+		if (sort != null) {
+			searchContext.setSorts(sort);
+		}
+
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		queryConfig.setHighlightEnabled(false);
+		queryConfig.setScoreEnabled(false);
+
+		return searchContext;
+	}
+
+	private String _getCategoryDisplayPageDeleteURL(
+		HttpServletRequest httpServletRequest, long categoryDisplayPageId) {
+
+		PortletURL portletURL = _portal.getControlPanelPortletURL(
+			httpServletRequest, CPPortletKeys.COMMERCE_CHANNELS,
+			PortletRequest.ACTION_PHASE);
+
+		String redirect = ParamUtil.getString(
+			httpServletRequest, "currentUrl",
+			_portal.getCurrentURL(httpServletRequest));
+
+		portletURL.setParameter(
+			ActionRequest.ACTION_NAME, "editCategoryDisplayLayout");
+		portletURL.setParameter(Constants.CMD, Constants.DELETE);
+		portletURL.setParameter("redirect", redirect);
+		portletURL.setParameter(
+			"cpDisplayLayoutId", String.valueOf(categoryDisplayPageId));
+
+		return portletURL.toString();
+	}
+
+	private String _getCategoryDisplayPageEditURL(
+			HttpServletRequest httpServletRequest, long categoryDisplayPageId)
+		throws Exception {
+
+		PortletURL portletURL = PortletProviderUtil.getPortletURL(
+			httpServletRequest, CommerceChannel.class.getName(),
+			PortletProvider.Action.MANAGE);
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "editCategoryDisplayLayout");
+
+		long commerceChannelId = ParamUtil.getLong(
+			httpServletRequest, "commerceChannelId");
+
+		portletURL.setParameter(
+			"commerceChannelId", String.valueOf(commerceChannelId));
+
+		portletURL.setParameter(
+			"cpDisplayLayoutId", String.valueOf(categoryDisplayPageId));
+
+		portletURL.setWindowState(LiferayWindowState.POP_UP);
+
+		return portletURL.toString();
+	}
+
+	private String _getCategoryName(CPDisplayLayout cpDisplayLayout) {
+		AssetCategory assetCategory = cpDisplayLayout.fetchAssetCategory();
+
+		if (assetCategory == null) {
+			return StringPool.BLANK;
+		}
+
+		return assetCategory.getName();
+	}
+
+	private List<CPDisplayLayout> _getCPDisplayLayouts(
+			CommerceChannel commerceChannel, int start, int end, Sort sort)
+		throws PortalException {
+
+		Indexer<CPDisplayLayout> indexer =
+			IndexerRegistryUtil.nullSafeGetIndexer(CPDisplayLayout.class);
+
+		Hits hits = indexer.search(
+			_buildSearchContext(commerceChannel, start, end, sort));
+
+		List<Document> documents = hits.toList();
+
+		List<CPDisplayLayout> cpDisplayLayouts = new ArrayList<>(
+			documents.size());
+
+		for (Document document : documents) {
+			long cpDisplayLayoutId = GetterUtil.getLong(
+				document.get(Field.ENTRY_CLASS_PK));
+
+			CPDisplayLayout cpDisplayLayout =
+				_cpDisplayLayoutService.fetchCPDisplayLayout(cpDisplayLayoutId);
+
+			if (cpDisplayLayout == null) {
+				cpDisplayLayouts = null;
+
+				long companyId = GetterUtil.getLong(
+					document.get(Field.COMPANY_ID));
+
+				indexer.delete(companyId, document.getUID());
+			}
+			else if (cpDisplayLayouts != null) {
+				cpDisplayLayouts.add(cpDisplayLayout);
+			}
+		}
+
+		return cpDisplayLayouts;
+	}
+
+	private String _getLayout(
+		CPDisplayLayout cpDisplayLayout, String languageId) {
+
+		Layout layout = cpDisplayLayout.fetchLayout();
+
+		if (layout == null) {
+			return StringPool.BLANK;
+		}
+
+		return layout.getName(languageId);
+	}
+
+	@Reference
+	private ClayTableSchemaBuilderFactory _clayTableSchemaBuilderFactory;
+
+	@Reference
+	private CommerceChannelService _commerceChannelService;
+
+	@Reference
+	private CPDisplayLayoutService _cpDisplayLayoutService;
+
+	@Reference
+	private Portal _portal;
+
+}
