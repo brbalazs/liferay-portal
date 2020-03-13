2f9f22e6772e137a00dfe682f68d467868b3caf6
Michael Prigge <michael.prigge@liferay.com>
2020-03-13 14:44:06 -0700

    LPS-110213 Add back across repository moving support with the current store api.
    Revert "LPS-109947 - partial backport of LPS-103419: removal of method call causes a SystemException to not occur.
    
    Original commit: LPS-103419 Remove Store.updateFile(long, long, long, String) 323446e141d38c1e6490d809520c8b7f4800661f"
    
    This reverts commit 18de10e56fa245cd9ab0c834e093c6d69fa18732.

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLFileEntryLocalServiceImpl.java
@@ -2390,6 +2390,8 @@ public class DLFileEntryLocalServiceImpl
 		DLFileEntry dlFileEntry = dlFileEntryPersistence.findByPrimaryKey(
 			fileEntryId);
 
+		long oldDataRepositoryId = dlFileEntry.getDataRepositoryId();
+
 		validateFile(
 			dlFileEntry.getGroupId(), newFolderId, dlFileEntry.getFileEntryId(),
 			dlFileEntry.getFileName(), dlFileEntry.getTitle());
@@ -2417,6 +2419,12 @@ public class DLFileEntryLocalServiceImpl
 
 		dlFolderLocalService.updateLastPostDate(
 			newFolderId, serviceContext.getModifiedDate(null));
+
+		// File
+
+		DLStoreUtil.updateFile(
+			user.getCompanyId(), oldDataRepositoryId,
+			dlFileEntry.getDataRepositoryId(), dlFileEntry.getName());
 
 		return dlFileEntry;
 	}
