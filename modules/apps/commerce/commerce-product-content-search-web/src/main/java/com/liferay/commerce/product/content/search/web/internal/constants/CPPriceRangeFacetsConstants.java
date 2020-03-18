877063ea94475214bb8f5684d6627e95c0c478e9
Alec Sloan <Alec.sloan@Liferay.com>
2020-03-18 11:25:10 -0700

    COMMERCE-3206 Fix price range facet default buckets

--- modules/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/constants/CPPriceRangeFacetsConstants.java
+++ modules/apps/commerce/commerce-product-content-search-web/src/main/java/com/liferay/commerce/product/content/search/web/internal/constants/CPPriceRangeFacetsConstants.java
@@ -20,8 +20,8 @@ package com.liferay.commerce.product.content.search.web.internal.constants;
 public class CPPriceRangeFacetsConstants {
 
 	public static final String DEFAULT_PRICE_RANGES_JSON_ARRAY =
-		"[{'range': '[0 TO 50]'}\\, {'range': '[50 TO 100]'}\\, {'range': " +
-			"'[100 TO 200]'}\\, {'range': '[200 TO 500]'}\\, {'range': '[500 " +
-				"TO 1000]'}\\, {'range': '[1000 TO *]'}]";
+		"[{'range': '[0 TO 49.99]'}\\, {'range': '[50 TO 99.99]'}\\, " +
+			"{'range': '[100 TO 199.99]'}\\, {'range': '[200 TO 499.99]'}\\, " +
+				"{'range': '[500 TO 999.99]'}\\, {'range': '[1000 TO *]'}]";
 
 }
