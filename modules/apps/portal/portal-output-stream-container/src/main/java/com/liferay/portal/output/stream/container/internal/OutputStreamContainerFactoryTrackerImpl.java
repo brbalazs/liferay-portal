b6769288aea089e2d9306e350400448faac6304d
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-03-19 09:00:27 +0100

    LPS-98866 Apply DUMMY_OUTPUT_STREAM_CONTAINER_FACTORY_NAME.

--- modules/apps/portal/portal-output-stream-container/src/main/java/com/liferay/portal/output/stream/container/internal/OutputStreamContainerFactoryTrackerImpl.java
+++ modules/apps/portal/portal-output-stream-container/src/main/java/com/liferay/portal/output/stream/container/internal/OutputStreamContainerFactoryTrackerImpl.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.util.HashMapDictionary;
 import com.liferay.portal.output.stream.container.OutputStreamContainer;
 import com.liferay.portal.output.stream.container.OutputStreamContainerFactory;
 import com.liferay.portal.output.stream.container.OutputStreamContainerFactoryTracker;
+import com.liferay.portal.output.stream.container.constants.OutputStreamContainerConstants;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -190,7 +191,10 @@ public class OutputStreamContainerFactoryTrackerImpl
 				OutputStreamContainerFactory.class,
 				new ConsoleOutputStreamContainerFactory(), dictionary));
 
-		dictionary.put("name", "dummy");
+		dictionary.put(
+			"name",
+			OutputStreamContainerConstants.
+				DUMMY_OUTPUT_STREAM_CONTAINER_FACTORY_NAME);
 		dictionary.put("service.ranking:Integer", "-100");
 
 		_serviceRegistrations.add(
