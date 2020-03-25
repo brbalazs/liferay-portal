7a07c63fce14a41aaa314f26ccafd89d45c020a3
Daniel Couso <daniel.couso@liferay.com>
2020-03-25 12:42:05 +0100

    LPS-109848 Return template as is since wildcards can be processed

--- portal-impl/src/com/liferay/portal/util/RobotsUtil.java
+++ portal-impl/src/com/liferay/portal/util/RobotsUtil.java
@@ -40,11 +40,9 @@ public class RobotsUtil {
 		String virtualHost, boolean secure, int port) {
 
 		if (Validator.isNotNull(virtualHost)) {
-			String content = ContentUtil.get(
+			return ContentUtil.get(
 				RobotsUtil.class.getClassLoader(),
 				PropsValues.ROBOTS_TXT_WITH_SITEMAP);
-
-			return _replaceWildcards(content, virtualHostname, secure, port);
 		}
 
 		return ContentUtil.get(
