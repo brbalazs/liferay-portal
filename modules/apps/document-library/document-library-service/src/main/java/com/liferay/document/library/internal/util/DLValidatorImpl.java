dc137f6179e1bc7746df01d4839e9252f2576ba7
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-03-26 11:55:58 +0100

    LPS-106508 Check that extensions are symmetrically equal wrt case

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLValidatorImpl.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/util/DLValidatorImpl.java
@@ -39,6 +39,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import java.util.Map;
+import java.util.Objects;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -143,7 +144,7 @@ public final class DLValidatorImpl implements DLValidator {
 
 		for (String fileExtension : fileExtensions) {
 			if (StringPool.STAR.equals(fileExtension) ||
-				StringUtil.endsWith(fileName, fileExtension)) {
+				_endsWith(fileName, fileExtension)) {
 
 				validFileExtension = true;
 
@@ -310,6 +311,24 @@ public final class DLValidatorImpl implements DLValidator {
 
 	protected void setDLConfiguration(DLConfiguration dlConfiguration) {
 		_dlConfiguration = dlConfiguration;
+	}
+
+	private boolean _endsWith(String s, String end) {
+		if (s.length() < end.length()) {
+			return false;
+		}
+
+		String tail = s.substring(s.length() - end.length());
+
+		if (Objects.equals(
+				StringUtil.toLowerCase(tail), StringUtil.toLowerCase(end)) &&
+			Objects.equals(
+				StringUtil.toUpperCase(tail), StringUtil.toUpperCase(end))) {
+
+			return true;
+		}
+
+		return false;
 	}
 
 	private volatile DLConfiguration _dlConfiguration;
