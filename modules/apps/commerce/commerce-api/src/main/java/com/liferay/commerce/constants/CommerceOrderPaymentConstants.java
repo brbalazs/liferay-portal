f4dc2522725405d11107ef0e3d862c1627a7391f
Marco Leo <marco.leo@liferay.com>
2020-03-29 00:42:07 +0100

    COMMERCE-3382 Delete Any Status

--- modules/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/constants/CommerceOrderPaymentConstants.java
+++ modules/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/constants/CommerceOrderPaymentConstants.java
@@ -23,8 +23,6 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
  */
 public class CommerceOrderPaymentConstants {
 
-	public static final int STATUS_ANY = WorkflowConstants.STATUS_ANY;
-
 	public static final int STATUS_AUTHORIZED = WorkflowConstants.STATUS_DRAFT;
 
 	public static final int STATUS_CANCELLED =
@@ -38,14 +36,12 @@ public class CommerceOrderPaymentConstants {
 	public static final int STATUS_PENDING = WorkflowConstants.STATUS_PENDING;
 
 	public static final int[] STATUSES = {
-		STATUS_ANY, STATUS_AUTHORIZED, STATUS_CANCELLED, STATUS_COMPLETED,
-		STATUS_FAILED, STATUS_PENDING
+		STATUS_AUTHORIZED, STATUS_CANCELLED, STATUS_COMPLETED, STATUS_FAILED,
+		STATUS_PENDING
 	};
 
 	public static String getOrderPaymentLabelStyle(int orderPaymentStatus) {
-		if ((orderPaymentStatus == STATUS_ANY) ||
-			(orderPaymentStatus == STATUS_AUTHORIZED)) {
-
+		if (orderPaymentStatus == STATUS_AUTHORIZED) {
 			return "info";
 		}
 		else if (orderPaymentStatus == STATUS_COMPLETED) {
@@ -64,10 +60,7 @@ public class CommerceOrderPaymentConstants {
 	}
 
 	public static String getOrderPaymentStatusLabel(int orderPaymentStatus) {
-		if (orderPaymentStatus == STATUS_ANY) {
-			return WorkflowConstants.LABEL_ANY;
-		}
-		else if (orderPaymentStatus == STATUS_AUTHORIZED) {
+		if (orderPaymentStatus == STATUS_AUTHORIZED) {
 			return "authorized";
 		}
 		else if (orderPaymentStatus == STATUS_CANCELLED) {
