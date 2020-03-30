a1aaa5558aa4ebf541ebf5a8440582e5d4de4a36
Ethan Bustad <ethan.bustad@liferay.com>
2020-03-30 12:37:19 -0700

    COMMERCE-3406 Remove stale license boilerplate

--- modules/dxp/apps/commerce/commerce-dashboard-web/src/main/resources/META-INF/resources/js/utils/test/loadData.js
+++ modules/dxp/apps/commerce/commerce-dashboard-web/src/main/resources/META-INF/resources/js/utils/test/loadData.js
@@ -12,12 +12,6 @@
  * details.
  */
 
-/**
- * © 2019 Liferay, Inc. <https://liferay.com>
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 import {
 	formatCategoriesForChart,
 	formatPointForForecast,
