73c4c90e024b0e09e969f84436230526b432fa94
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2020-04-02 17:03:16 +0200

    LPS-106585 Add queryString parameters to redirect URL in case of Link to URL layout is being processed.

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
@@ -308,6 +308,26 @@ public class FriendlyURLServlet extends HttpServlet {
 			}
 		}
 
+		Layout layout = (Layout)request.getAttribute(WebKeys.LAYOUT);
+
+		if (layout != null) {
+			String layoutType = layout.getType();
+
+			if (layoutType.equals(LayoutConstants.TYPE_URL)) {
+				Map<String, String[]> parameterMap =
+					request.getParameterMap();
+
+				if (parameterMap != null) {
+					for (Map.Entry<String, String[]> entry :
+							parameterMap.entrySet()) {
+
+						actualURL = HttpUtil.setParameter(
+							actualURL, entry.getKey(), entry.getValue()[0]);
+					}
+				}
+			}
+		}
+
 		return new Redirect(actualURL);
 	}
 
