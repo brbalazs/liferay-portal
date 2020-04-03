9b8377a3eceb56081f17ca81cf51689cd9f98673
Brian Chan <brian.chan@liferay.com>
2020-04-03 12:23:32 -0700

    LPS-110552 SF

--- portal-impl/src/com/liferay/portlet/sites/util/SitesImpl.java
+++ portal-impl/src/com/liferay/portlet/sites/util/SitesImpl.java
@@ -1102,7 +1102,6 @@ public class SitesImpl implements Sites {
 
 		long lastMergeTime = GetterUtil.getLong(
 			settingsProperties.getProperty(LAST_MERGE_TIME));
-
 		long lastMergeVersion = GetterUtil.getLong(
 			settingsProperties.getProperty(LAST_MERGE_VERSION));
 
