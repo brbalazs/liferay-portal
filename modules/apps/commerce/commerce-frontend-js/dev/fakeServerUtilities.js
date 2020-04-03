6636baf50811941c1f916d40e4230c694761051d
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2020-04-03 09:54:12 +0200

    LPS-108049 Remove faker dependency

--- modules/apps/commerce/commerce-frontend-js/dev/fakeServerUtilities.js
+++ modules/apps/commerce/commerce-frontend-js/dev/fakeServerUtilities.js
@@ -1,10 +1,23 @@
-/* eslint-disable require-jsdoc */
-const faker = require('faker');
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+
 
 function defineServerResponses(app) {
 	app.get('/test-endpoint', (_, res) => {
 		res.json({
-			randomId: faker.random.uuid(),
+			message: 'test-message',
 		});
 	});
 }
