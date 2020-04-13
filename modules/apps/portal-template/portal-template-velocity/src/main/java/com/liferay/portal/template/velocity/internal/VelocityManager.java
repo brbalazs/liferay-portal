34cb0aab4c0cc8c5c2e3be47a333510015b8fc4c
Dante Wang <dante.wang@liferay.com>
2020-04-13 13:14:54 +0800

    LPS-110849 partial backport LPS-84119 Rename parameters (3279bb9f)

--- modules/apps/portal-template/portal-template-velocity/src/main/java/com/liferay/portal/template/velocity/internal/VelocityManager.java
+++ modules/apps/portal-template/portal-template-velocity/src/main/java/com/liferay/portal/template/velocity/internal/VelocityManager.java
@@ -64,10 +64,12 @@ public class VelocityManager extends BaseSingleTemplateManager {
 	@Override
 	public void addTaglibTheme(
 		Map<String, Object> contextObjects, String themeName,
-		HttpServletRequest request, HttpServletResponse response) {
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
 
 		VelocityTaglib velocityTaglib = new VelocityTaglibImpl(
-			request.getServletContext(), request, response, contextObjects);
+			httpServletRequest.getServletContext(), httpServletRequest,
+			httpServletResponse, contextObjects);
 
 		contextObjects.put(themeName, velocityTaglib);
 
