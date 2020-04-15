2e94f2c261af06fc1930afc837f5e9e48120e668
Jonathan McCann <jonathan.mccann@liferay.com>
2020-04-15 09:23:16 -0700

    LPS-111679 Reset the languageId when using the default language options inherited from the instance

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
@@ -44,6 +44,7 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Layout;
@@ -51,6 +52,7 @@ import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.LayoutSetPrototype;
 import com.liferay.portal.kernel.model.MembershipRequest;
 import com.liferay.portal.kernel.model.MembershipRequestConstants;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.PortalPreferences;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
@@ -900,6 +902,15 @@ public class SiteAdminPortlet extends MVCPortlet {
 		}
 
 		if (inheritLocales) {
+			Company company = themeDisplay.getCompany();
+
+			User user = company.getDefaultUser();
+
+			Locale defaultLocale = user.getLocale();
+
+			formTypeSettingsProperties.setProperty(
+				"languageId", LocaleUtil.toLanguageId(defaultLocale));
+
 			formTypeSettingsProperties.setProperty(
 				PropsKeys.LOCALES,
 				StringUtil.merge(
