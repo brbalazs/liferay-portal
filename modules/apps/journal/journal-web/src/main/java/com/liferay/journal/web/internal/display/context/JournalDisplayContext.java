7494c73a6211805407786413f2a501fb3f108643
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-04-15 16:08:41 +0200

    LPS-111762 Create factory method to avoid generating duplicate objects and repeating same database queries, as in JournalContentDisplayContext.

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -142,24 +142,25 @@ import javax.servlet.http.HttpServletRequest;
  */
 public class JournalDisplayContext {
 
-	public JournalDisplayContext(
+	public static JournalDisplayContext create(
 		HttpServletRequest request, LiferayPortletRequest liferayPortletRequest,
 		LiferayPortletResponse liferayPortletResponse,
 		PortletPreferences portletPreferences, TrashHelper trashHelper) {
 
-		_request = request;
-		_liferayPortletRequest = liferayPortletRequest;
-		_liferayPortletResponse = liferayPortletResponse;
-		_portletPreferences = portletPreferences;
-		_trashHelper = trashHelper;
+		JournalDisplayContext journalDisplayContext =
+			(JournalDisplayContext)liferayPortletRequest.getAttribute(
+				_JOURNAL_DISPLAY_CONTEXT);
 
-		_journalWebConfiguration =
-			(JournalWebConfiguration)_request.getAttribute(
-				JournalWebConfiguration.class.getName());
-		_portalPreferences = PortletPreferencesFactoryUtil.getPortalPreferences(
-			_request);
-		_themeDisplay = (ThemeDisplay)_request.getAttribute(
-			WebKeys.THEME_DISPLAY);
+		if (journalDisplayContext == null) {
+			journalDisplayContext = new JournalDisplayContext(
+				request, liferayPortletRequest, liferayPortletResponse,
+				portletPreferences, trashHelper);
+
+			liferayPortletRequest.setAttribute(
+				_JOURNAL_DISPLAY_CONTEXT, journalDisplayContext);
+		}
+
+		return journalDisplayContext;
 	}
 
 	public List<DropdownItem> getActionDropdownItems() throws Exception {
@@ -1810,6 +1811,26 @@ public class JournalDisplayContext {
 		return searchContext;
 	}
 
+	private JournalDisplayContext(
+		HttpServletRequest request, LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		PortletPreferences portletPreferences, TrashHelper trashHelper) {
+
+		_request = request;
+		_liferayPortletRequest = liferayPortletRequest;
+		_liferayPortletResponse = liferayPortletResponse;
+		_portletPreferences = portletPreferences;
+		_trashHelper = trashHelper;
+
+		_journalWebConfiguration =
+			(JournalWebConfiguration)_request.getAttribute(
+				JournalWebConfiguration.class.getName());
+		_portalPreferences = PortletPreferencesFactoryUtil.getPortalPreferences(
+			_request);
+		_themeDisplay = (ThemeDisplay)_request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
 	private String _getFeedsURL() {
 		PortletURL portletURL = _liferayPortletResponse.createRenderURL();
 
@@ -2077,6 +2098,9 @@ public class JournalDisplayContext {
 			return false;
 		}
 	}
+
+	private static final String _JOURNAL_DISPLAY_CONTEXT =
+		"JOURNAL_DISPLAY_CONTEXT";
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		JournalDisplayContext.class);
