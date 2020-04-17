25acbce799b5f77ea694dfe01d86564bc2616cdb
Javier Gamarra <nhpatt@gmail.com>
2020-04-17 16:45:48 +0200

    LPS-112101 graphQL tests

--- modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/BlogPostingImageResourceTest.java
+++ modules/apps/headless/headless-delivery/headless-delivery-test/src/testIntegration/java/com/liferay/headless/delivery/resource/v1_0/test/BlogPostingImageResourceTest.java
@@ -30,7 +30,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -40,24 +39,6 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class BlogPostingImageResourceTest
 	extends BaseBlogPostingImageResourceTestCase {
-
-	@Ignore
-	@Override
-	@Test
-	public void testGraphQLDeleteBlogPostingImage() {
-	}
-
-	@Ignore
-	@Override
-	@Test
-	public void testGraphQLGetBlogPostingImage() {
-	}
-
-	@Ignore
-	@Override
-	@Test
-	public void testGraphQLGetSiteBlogPostingImagesPage() {
-	}
 
 	@Test
 	public void testPostSiteBlogPostingImageRollback() throws Exception {
@@ -99,8 +80,13 @@ public class BlogPostingImageResourceTest
 	}
 
 	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"title"};
+	}
+
+	@Override
 	protected String[] getIgnoredEntityFieldNames() {
-		return new String[] {"fileExtension"};
+		return new String[] {"fileExtension", "sizeInBytes"};
 	}
 
 	@Override
@@ -112,6 +98,13 @@ public class BlogPostingImageResourceTest
 		files.put("file", FileUtil.createTempFile(randomString.getBytes()));
 
 		return files;
+	}
+
+	@Override
+	protected BlogPostingImage testGraphQLBlogPostingImage_addBlogPostingImage()
+		throws Exception {
+
+		return testDeleteBlogPostingImage_addBlogPostingImage();
 	}
 
 	private String _read(String url) throws Exception {
