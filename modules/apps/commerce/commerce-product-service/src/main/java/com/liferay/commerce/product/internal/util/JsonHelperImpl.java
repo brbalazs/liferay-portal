bf8b641e812e63d9480567ed5421fe69eb49fa67
Igor Beslic <igor.beslic@liferay.com>
2020-04-17 15:11:16 +0200

    COMMERCE-3555 commerce-product-service - move JSON relatedd operations to JsonHelper

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/util/JsonHelperImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/util/JsonHelperImpl.java
@@ -17,19 +17,45 @@ package com.liferay.commerce.product.internal.util;
 import com.liferay.commerce.product.util.JsonHelper;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.util.Validator;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Igor Beslic
  */
 @Component(immediate = true, service = JsonHelper.class)
 public class JsonHelperImpl implements JsonHelper {
+
+	public JsonHelperImpl() {
+	}
+
+	public JsonHelperImpl(JSONFactory jsonFactory) {
+		_jsonFactory = jsonFactory;
+	}
+
+	public boolean equals(String jsonArrayString1, String jsonArrayString2) {
+		JSONArray jsonArray1 = _toJSONArray(jsonArrayString1);
+		JSONArray jsonArray2 = _toJSONArray(jsonArrayString2);
+
+		if (jsonArray1.length() != jsonArray2.length()) {
+			return false;
+		}
+
+		Map<String, String> map1 = _toMap(jsonArray1);
+		Map<String, String> map2 = _toMap(jsonArray2);
+
+		return map1.equals(map2);
+	}
 
 	public String getFirstElementStringValue(String jsonArrayString) {
 		if (!isArray(jsonArrayString)) {
@@ -58,7 +84,7 @@ public class JsonHelperImpl implements JsonHelper {
 			return valueJSONArray;
 		}
 
-		valueJSONArray = JSONFactoryUtil.createJSONArray();
+		valueJSONArray = _jsonFactory.createJSONArray();
 
 		String valueString = jsonObject.getString(key);
 
@@ -96,5 +122,67 @@ public class JsonHelperImpl implements JsonHelper {
 
 		return false;
 	}
+
+	@Override
+	public JSONArray toJSONArray(Map<String, List<String>> keyValues) {
+		JSONArray jsonArray = _jsonFactory.createJSONArray();
+
+		for (Map.Entry<String, List<String>> keyValuesEntry :
+				keyValues.entrySet()) {
+
+			String key = keyValuesEntry.getKey();
+
+			JSONObject arrayEntryJSONObject = _jsonFactory.createJSONObject();
+
+			arrayEntryJSONObject.put("key", key);
+
+			List<String> values = keyValuesEntry.getValue();
+
+			JSONArray valuesJSONArray = _jsonFactory.createJSONArray();
+
+			for (String value : values) {
+				valuesJSONArray.put(value);
+			}
+
+			arrayEntryJSONObject.put("value", valuesJSONArray);
+
+			jsonArray.put(arrayEntryJSONObject);
+		}
+
+		return jsonArray;
+	}
+
+	private JSONArray _toJSONArray(String jsonArrayString) {
+		if (!isArray(jsonArrayString)) {
+			throw new IllegalArgumentException(
+				jsonArrayString + " is not valid JSON array");
+		}
+
+		try {
+			return _jsonFactory.createJSONArray(jsonArrayString);
+		}
+		catch (JSONException jsonException) {
+			throw new IllegalArgumentException(
+				jsonArrayString + " is not valid JSON array");
+		}
+	}
+
+	private Map<String, String> _toMap(JSONArray jsonArray) {
+		Map<String, String> map = new HashMap<>();
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			String key = jsonObject.getString("key");
+			String value = jsonObject.getString("value");
+
+			map.put(key, value);
+		}
+
+		return map;
+	}
+
+	@Reference
+	private JSONFactory _jsonFactory;
 
 }
