2dbc225dd9f944a366906c50db0b9b95420f2949
Luca Pellizzon <luca.pellizzon@liferay.com>
2020-04-23 17:26:21 +0200

    COMMERCE-3248 sort order list by creation date

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderLocalServiceImpl.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderLocalServiceImpl.java
@@ -67,6 +67,8 @@ import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.SortFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -1406,6 +1408,11 @@ public class CommerceOrderLocalServiceImpl
 		searchContext.setKeywords(keywords);
 		searchContext.setStart(start);
 		searchContext.setEnd(end);
+
+		Sort sort = SortFactoryUtil.getSort(
+			CommerceOrder.class, Sort.LONG_TYPE, Field.CREATE_DATE, "DESC");
+
+		searchContext.setSorts(sort);
 
 		QueryConfig queryConfig = searchContext.getQueryConfig();
 
