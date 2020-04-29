16fc97930904836b829adc20fb670e379c694f78
Igor Beslic <igor.beslic@liferay.com>
2020-04-29 18:25:02 +0200

    COMMERCE-3559 product-test - Reorganize - Remove code that is present in UTIL class and test proper method

--- modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/option/test/CommerceOptionValueHelperTest.java
+++ modules/apps/commerce/commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/option/test/CommerceOptionValueHelperTest.java
@@ -15,32 +15,20 @@
 package com.liferay.commerce.product.option.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.commerce.product.constants.CPConstants;
 import com.liferay.commerce.product.model.CPDefinition;
-import com.liferay.commerce.product.model.CPDefinitionOptionRel;
-import com.liferay.commerce.product.model.CPDefinitionOptionValueRel;
-import com.liferay.commerce.product.model.CPInstance;
-import com.liferay.commerce.product.model.CPOption;
 import com.liferay.commerce.product.model.CommerceCatalog;
 import com.liferay.commerce.product.option.CommerceOptionValue;
 import com.liferay.commerce.product.option.CommerceOptionValueHelper;
 import com.liferay.commerce.product.service.CPDefinitionLocalService;
-import com.liferay.commerce.product.service.CPDefinitionOptionRelLocalService;
-import com.liferay.commerce.product.service.CPDefinitionOptionRelLocalServiceUtil;
-import com.liferay.commerce.product.service.CPDefinitionOptionValueRelLocalServiceUtil;
-import com.liferay.commerce.product.service.CPInstanceLocalService;
-import com.liferay.commerce.product.service.CPInstanceLocalServiceUtil;
 import com.liferay.commerce.product.service.CommerceCatalogLocalService;
 import com.liferay.commerce.product.service.CommerceCatalogLocalServiceUtil;
 import com.liferay.commerce.product.test.util.CPTestUtil;
-import com.liferay.commerce.product.type.simple.constants.SimpleCPTypeConstants;
-import com.liferay.commerce.product.util.CPInstanceHelper;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
-import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -52,8 +40,6 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-
-import java.math.BigDecimal;
 
 import java.util.List;
 
@@ -103,12 +89,7 @@ public class CommerceOptionValueHelperTest {
 	}
 
 	@Test
-	public void test1() {
-		Assert.assertNotNull("I am here to fail");
-	}
-
-	@Test
-	public void testGetCPInstanceCommerceOptionValues() throws Exception {
+	public void testGetCPDefinitionCommerceOptionValues() throws Exception {
 		frutillaRule.scenario(
 			"Get CP instance commerce option values"
 		).given(
@@ -121,8 +102,8 @@ public class CommerceOptionValueHelperTest {
 		).and(
 			"Option_1_Value_2 points to product B"
 		).when(
-			"There are two CP instances that represents SKU bundle values " +
-				"combination Option_1_Value_1, Option_1_Value_2"
+			"There are two CP instances that represents product bundle " +
+				"values combination Option_1_Value_1, Option_1_Value_2"
 		).and(
 			"commerce option value is requested for SKU's cpIstanceId"
 		).then(
@@ -131,105 +112,17 @@ public class CommerceOptionValueHelperTest {
 
 		_setupPermissionChecker();
 
-		CPDefinition optionACPDefinition =
-			CPTestUtil.addCPDefinitionFromCatalog(
-				_commerceCatalog.getGroupId(), SimpleCPTypeConstants.NAME, true,
-				true);
-
-		CPInstance optionACPInstance = _cpInstanceHelper.getDefaultCPInstance(
-			optionACPDefinition.getCPDefinitionId());
-
-		CPDefinition optionBCPDefinition =
-			CPTestUtil.addCPDefinitionFromCatalog(
-				_commerceCatalog.getGroupId(), SimpleCPTypeConstants.NAME, true,
-				true);
-
-		CPInstance optionBCPInstance = _cpInstanceHelper.getDefaultCPInstance(
-			optionBCPDefinition.getCPDefinitionId());
-
-		CPDefinition bundleCPDefinition = CPTestUtil.addCPDefinitionFromCatalog(
-			_commerceCatalog.getGroupId(), SimpleCPTypeConstants.NAME, true,
-			true);
-
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(
+		CPDefinition bundleCPDefinition =
+			CPTestUtil.addCPDefinitionWithChildCPDefinitions(
 				_commerceCatalog.getGroupId());
 
-		CPOption productBundleOption = CPTestUtil.addCPOption(
-			_commerceCatalog.getGroupId(),
-			CPTestUtil.getDefaultDDMFormFieldType(true), true);
+		List<CommerceOptionValue> cpInstanceCommerceOptionValues =
+			_commerceOptionValueHelper.getCPDefinitionCommerceOptionValues(
+				bundleCPDefinition.getCPDefinitionId(), StringPool.BLANK);
 
-		CPDefinitionOptionRel cpDefinitionOptionRel =
-			CPDefinitionOptionRelLocalServiceUtil.addCPDefinitionOptionRel(
-				bundleCPDefinition.getCPDefinitionId(),
-				productBundleOption.getCPOptionId(),
-				RandomTestUtil.randomLocaleStringMap(),
-				RandomTestUtil.randomLocaleStringMap(),
-				CPTestUtil.getDefaultDDMFormFieldType(true), 0.2, false, false,
-				true, false, CPConstants.PRODUCT_OPTION_PRICE_TYPE_STATIC,
-				serviceContext);
-
-		CPDefinitionOptionValueRel optionACPDefinitionOptionValueRel =
-			CPDefinitionOptionValueRelLocalServiceUtil.
-				addCPDefinitionOptionValueRel(
-					cpDefinitionOptionRel.getCPDefinitionOptionRelId(),
-					RandomTestUtil.randomLocaleStringMap(), 0.4, "product-a",
-					serviceContext);
-
-		CPDefinitionOptionValueRelLocalServiceUtil.
-			updateCPDefinitionOptionValueRel(
-				optionACPDefinitionOptionValueRel.
-					getCPDefinitionOptionValueRelId(),
-				RandomTestUtil.randomLocaleStringMap(), 0.4, "product-a",
-				optionACPInstance.getCPInstanceId(), 2,
-				new BigDecimal("100.20"), serviceContext);
-
-		CPDefinitionOptionValueRel optionBCPDefinitionOptionValueRel =
-			CPDefinitionOptionValueRelLocalServiceUtil.
-				addCPDefinitionOptionValueRel(
-					cpDefinitionOptionRel.getCPDefinitionOptionRelId(),
-					RandomTestUtil.randomLocaleStringMap(), 0.2, "product-b",
-					serviceContext);
-
-		CPDefinitionOptionValueRelLocalServiceUtil.
-			updateCPDefinitionOptionValueRel(
-				optionBCPDefinitionOptionValueRel.
-					getCPDefinitionOptionValueRelId(),
-				RandomTestUtil.randomLocaleStringMap(), 0.2, "product-b",
-				optionBCPInstance.getCPInstanceId(), 2,
-				new BigDecimal("200.20"), serviceContext);
-
-		_cpInstanceLocalService.buildCPInstances(
-			bundleCPDefinition.getCPDefinitionId(), serviceContext);
-
-		List<CPInstance> bundleCPDefinitionApprovedCPInstances =
-			_cpInstanceLocalService.getCPDefinitionApprovedCPInstances(
-				bundleCPDefinition.getCPDefinitionId());
-
-		for (CPInstance bundleCPInstance :
-				bundleCPDefinitionApprovedCPInstances) {
-
-			List<CommerceOptionValue> cpInstanceCommerceOptionValues =
-				_commerceOptionValueHelper.getCPInstanceCommerceOptionValues(
-					bundleCPInstance.getCPInstanceId());
-
-			for (CommerceOptionValue cpInstanceCommerceOptionValue :
-					cpInstanceCommerceOptionValues) {
-
-				Assert.assertNotNull(
-					cpInstanceCommerceOptionValue.getCPInstanceId());
-				Assert.assertNotNull(cpInstanceCommerceOptionValue.getPrice());
-
-				CPInstance optionValueCPInstance =
-					CPInstanceLocalServiceUtil.getCPInstance(
-						cpInstanceCommerceOptionValue.getCPInstanceId());
-
-				Assert.assertNotEquals(
-					"Static price overrides CP instance price",
-					optionValueCPInstance.getPrice(),
-					cpInstanceCommerceOptionValue.getPrice());
-			}
-		}
+		Assert.assertTrue(
+			"No option values present if user did not select option",
+			cpInstanceCommerceOptionValues.isEmpty());
 	}
 
 	@Rule
@@ -255,15 +148,5 @@ public class CommerceOptionValueHelperTest {
 
 	@Inject
 	private CPDefinitionLocalService _cpDefinitionLocalService;
-
-	@Inject
-	private CPDefinitionOptionRelLocalService
-		_cpDefinitionOptionRelLocalService;
-
-	@Inject
-	private CPInstanceHelper _cpInstanceHelper;
-
-	@Inject
-	private CPInstanceLocalService _cpInstanceLocalService;
 
 }
