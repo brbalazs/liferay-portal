896182f6990018523a86d9a2c3a39afc460f3070
Francisco Dias <francisco.dias@liferay.com>
2020-04-30 19:02:37 -0300

    LRAC-5575 Create CheckExperimentLink HOC

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/index.js
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/index.js
@@ -3,6 +3,7 @@ export {compose} from 'redux';
 export {default as withPolling} from './WithPolling';
 
 export {default as checkProjectState} from './CheckProjectState';
+export {default as checkExperimentLink} from './CheckExperimentLink';
 export {default as optional} from './Optional';
 export {default as redirectIf} from './RedirectIf';
 export {default as withAccount} from './WithAccount';
