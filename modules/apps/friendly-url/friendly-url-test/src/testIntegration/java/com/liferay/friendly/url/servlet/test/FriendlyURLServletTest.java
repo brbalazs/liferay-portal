200185af8afb320c020ea0a5b5e3061be7016976
Brian Chan <brian.chan@liferay.com>
2020-05-07 09:32:53 -0700

    LPS-106585 Sort

--- modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/servlet/test/FriendlyURLServletTest.java
+++ modules/apps/friendly-url/friendly-url-test/src/testIntegration/java/com/liferay/friendly/url/servlet/test/FriendlyURLServletTest.java
@@ -300,6 +300,7 @@ public class FriendlyURLServletTest {
 
 		_layoutLocalService.updateLayout(redirectLayout);
 
+		mockHttpServletRequest.setParameter("param", "true");
 		mockHttpServletRequest.setPathInfo(StringPool.SLASH);
 
 		String requestURI =
@@ -307,8 +308,6 @@ public class FriendlyURLServletTest {
 				getPath(_group, redirectLayout);
 
 		mockHttpServletRequest.setRequestURI(requestURI);
-
-		mockHttpServletRequest.setParameter("param", "true");
 
 		MockHttpServletResponse mockHttpServletResponse =
 			new MockHttpServletResponse();
