09cc905489a40002638e998b19663b387d807853
Marco Leo <marco.leo@liferay.com>
2020-05-11 23:22:46 +0200

    Revert "COMMERCE-2789 DnD Functionality - Frontend"
    This reverts commit e75cee84f244ae1211810da43203f8356fcdb605.

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/DatasetDisplay.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/DatasetDisplay.js
@@ -17,11 +17,7 @@ import {ClayPaginationBarWithBasicItems} from '@clayui/pagination-bar';
 import PropTypes from 'prop-types';
 import React, {useState, useRef, useEffect} from 'react';
 
-import {closest} from '../../utilities/closest.es';
-import {
-	provideDragDrop
-} from "../../utilities/drag_drop/contextProvider.es";
-
+import {closest} from '../../utilities/closest';
 import {
 	DATASET_ACTION_PERFORMED,
 	DATASET_DISPLAY_UPDATED,
@@ -515,25 +511,4 @@ DatasetDisplay.defaultProps = {
 	style: 'default'
 };
 
-export default function DatasetDisplayEnhancer(props) {
-	const baseComponent = (props) => {
-		const {
-			apiUrl,
-			id,
-			spritemap,
-			views
-		} = props;
-
-		return (
-			<DatasetDisplay
-				{...props}
-				apiUrl={apiUrl}
-				id={id}
-				spritemap={spritemap}
-				views={views}
-			/>
-		);
-	};
-
-	return provideDragDrop(props, baseComponent);
-}
+export default DatasetDisplay;
