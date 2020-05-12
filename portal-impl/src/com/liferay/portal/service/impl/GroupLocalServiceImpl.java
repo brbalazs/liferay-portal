b7cec2a5c2da6575fdb534e78117a7eddecddfed
shuyangzhou <shuyang.zhou@liferay.com>
2020-05-12 10:03:05 -0700

    LPS-113225 SF

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -5130,15 +5130,15 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 		}
 	}
 
-	protected void validateLanguageIds( long groupId,
-			String defaultLanguageId, String languageIds)
+	protected void validateLanguageIds(
+			long groupId, String defaultLanguageId, String languageIds)
 		throws PortalException {
 
 		String[] languageIdsArray = StringUtil.split(languageIds);
 
 		for (String languageId : languageIdsArray) {
-			if (!LanguageUtil.isAvailableLocale( groupId,
-					LocaleUtil.fromLanguageId(languageId))) {
+			if (!LanguageUtil.isAvailableLocale(
+					groupId, LocaleUtil.fromLanguageId(languageId))) {
 
 				LocaleException le = new LocaleException(
 					LocaleException.TYPE_DISPLAY_SETTINGS);
