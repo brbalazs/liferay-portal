9858a020bf38f3b18c941cb0e31be2cad1b25513
Alessio Rendina <asilcomando@gmail.com>
2020-05-15 18:16:41 +0200

    COMMERCE-3896 Added commerce order statuses upgrade process

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/CommerceUpgradeStepRegistrator.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/CommerceUpgradeStepRegistrator.java
@@ -34,6 +34,7 @@ import com.liferay.commerce.internal.upgrade.v4_4_0.CommerceOrderManuallyAdjuste
 import com.liferay.commerce.internal.upgrade.v4_5_1.CommerceShippingMethodUpgradeProcess;
 import com.liferay.commerce.internal.upgrade.v4_6_0.ShipmentUpgradeProcess;
 import com.liferay.commerce.internal.upgrade.v4_6_0.SubscriptionUpgradeProcess;
+import com.liferay.commerce.internal.upgrade.v4_8_1.CommerceOrderStatusesUpgradeProcess;
 import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.CPInstanceLocalService;
 import com.liferay.portal.kernel.log.Log;
@@ -175,8 +176,9 @@ public class CommerceUpgradeStepRegistrator implements UpgradeStepRegistrator {
 			new DummyUpgradeProcess());
 
 		registry.register(
-			_SCHEMA_VERSION_4_7_0, _SCHEMA_VERSION_4_8_0,
-			new com.liferay.commerce.internal.upgrade.v4_8_0.
+			_SCHEMA_VERSION_4_7_0, _SCHEMA_VERSION_4_8_1,
+			new CommerceOrderStatusesUpgradeProcess());
+
 				CommerceOrderUpgradeProcess(),
 			new com.liferay.commerce.internal.upgrade.v4_8_0.
 				CommerceOrderItemUpgradeProcess());
@@ -227,6 +229,8 @@ public class CommerceUpgradeStepRegistrator implements UpgradeStepRegistrator {
 	private static final String _SCHEMA_VERSION_4_7_0 = "4.7.0";
 
 	private static final String _SCHEMA_VERSION_4_8_0 = "4.8.0";
+	private static final String _SCHEMA_VERSION_4_8_1 = "4.8.1";
+
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		CommerceUpgradeStepRegistrator.class);
