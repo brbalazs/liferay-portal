00691300c920353e8df85ce424150605efe4f95d
Kevin Lee <kevin.lee@liferay.com>
2020-05-20 15:21:49 -0700

    LPS-113604 Close must precede Open

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/xml_util.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/xml_util.js
@@ -103,11 +103,11 @@ AUI.add(
 
 						formatted += item + STR_CHAR_CRLF;
 
-						if (item.indexOf(STR_CDATA_OPEN) > -1) {
-							inCDATA = true;
-						}
-						else if (item.indexOf(STR_CDATA_CLOSE) > -1) {
+						if (item.indexOf(STR_CDATA_CLOSE) > -1) {
 							inCDATA = false;
+						}
+						else if (item.indexOf(STR_CDATA_OPEN) > -1) {
+							inCDATA = true;
 						}
 
 						pad += indent;
