4528609513c249252effb803f701efb630880564
Riccardo <riccardo.alberti@liferay.com>
2020-05-20 15:39:10 +0200

    COMMERCE-3012 discount product group resource, util and dto converter

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/DiscountProductGroupResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/DiscountProductGroupResourceImpl.java
@@ -14,13 +14,31 @@
 
 package com.liferay.headless.commerce.admin.pricing.internal.resource.v2_0;
 
+import com.liferay.commerce.discount.exception.NoSuchDiscountException;
+import com.liferay.commerce.discount.model.CommerceDiscount;
+import com.liferay.commerce.discount.model.CommerceDiscountRel;
+import com.liferay.commerce.discount.service.CommerceDiscountRelService;
+import com.liferay.commerce.discount.service.CommerceDiscountService;
+import com.liferay.commerce.pricing.model.CommercePricingClass;
+import com.liferay.commerce.pricing.service.CommercePricingClassService;
+import com.liferay.headless.commerce.admin.pricing.dto.v2_0.DiscountProductGroup;
+import com.liferay.headless.commerce.admin.pricing.internal.dto.v2_0.converter.DiscountProductGroupDTOConverter;
+import com.liferay.headless.commerce.admin.pricing.internal.util.v2_0.DiscountProductGroupUtil;
 import com.liferay.headless.commerce.admin.pricing.resource.v2_0.DiscountProductGroupResource;
+import com.liferay.headless.commerce.core.util.ServiceContextHelper;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
- * @author Zoltán Takács
+ * @author Riccardo Alberti
  */
 @Component(
 	properties = "OSGI-INF/liferay/rest/v2_0/discount-product-group.properties",
@@ -28,4 +46,146 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class DiscountProductGroupResourceImpl
 	extends BaseDiscountProductGroupResourceImpl {
+
+	@Override
+	public void deleteDiscountProductGroup(Long id) throws Exception {
+		_commerceDiscountRelService.deleteCommerceDiscountRel(id);
+	}
+
+	@Override
+	public Page<DiscountProductGroup>
+			getDiscountByExternalReferenceCodeDiscountProductGroupsPage(
+				String externalReferenceCode, Pagination pagination)
+		throws Exception {
+
+		CommerceDiscount commerceDiscount =
+			_commerceDiscountService.fetchByExternalReferenceCode(
+				contextCompany.getCompanyId(), externalReferenceCode);
+
+		if (commerceDiscount == null) {
+			throw new NoSuchDiscountException(
+				"Unable to find Discount with externalReferenceCode: " +
+					externalReferenceCode);
+		}
+
+		List<CommerceDiscountRel> commerceDiscountRels =
+			_commerceDiscountRelService.getCommerceDiscountRels(
+				commerceDiscount.getCommerceDiscountId(),
+				CommercePricingClass.class.getName(),
+				pagination.getStartPosition(), pagination.getEndPosition(),
+				null);
+
+		int totalItems =
+			_commerceDiscountRelService.getCommerceDiscountRelsCount(
+				commerceDiscount.getCommerceDiscountId(),
+				CommercePricingClass.class.getName());
+
+		return Page.of(
+			_toDiscountProductGroups(commerceDiscountRels), pagination,
+			totalItems);
+	}
+
+	@Override
+	public Page<DiscountProductGroup> getDiscountIdDiscountProductGroupsPage(
+			Long id, Pagination pagination)
+		throws Exception {
+
+		List<CommerceDiscountRel> commerceDiscountRels =
+			_commerceDiscountRelService.getCommerceDiscountRels(
+				id, CommercePricingClass.class.getName(),
+				pagination.getStartPosition(), pagination.getEndPosition(),
+				null);
+
+		int totalItems =
+			_commerceDiscountRelService.getCommerceDiscountRelsCount(
+				id, CommercePricingClass.class.getName());
+
+		return Page.of(
+			_toDiscountProductGroups(commerceDiscountRels), pagination,
+			totalItems);
+	}
+
+	@Override
+	public DiscountProductGroup
+			postDiscountByExternalReferenceCodeDiscountProductGroup(
+				String externalReferenceCode,
+				DiscountProductGroup discountProductGroup)
+		throws Exception {
+
+		CommerceDiscount commerceDiscount =
+			_commerceDiscountService.fetchByExternalReferenceCode(
+				contextCompany.getCompanyId(), externalReferenceCode);
+
+		if (commerceDiscount == null) {
+			throw new NoSuchDiscountException(
+				"Unable to find Discount with externalReferenceCode: " +
+					externalReferenceCode);
+		}
+
+		CommerceDiscountRel commerceDiscountRel =
+			DiscountProductGroupUtil.addCommerceDiscountRel(
+				_commercePricingClassService, _commerceDiscountRelService,
+				discountProductGroup, commerceDiscount,
+				_serviceContextHelper.getServiceContext());
+
+		return _toDiscountProductGroup(
+			commerceDiscountRel.getCommerceDiscountRelId());
+	}
+
+	@Override
+	public DiscountProductGroup postDiscountIdDiscountProductGroup(
+			Long id, DiscountProductGroup discountProductGroup)
+		throws Exception {
+
+		CommerceDiscountRel commerceDiscountRel =
+			DiscountProductGroupUtil.addCommerceDiscountRel(
+				_commercePricingClassService, _commerceDiscountRelService,
+				discountProductGroup,
+				_commerceDiscountService.getCommerceDiscount(id),
+				_serviceContextHelper.getServiceContext());
+
+		return _toDiscountProductGroup(
+			commerceDiscountRel.getCommerceDiscountRelId());
+	}
+
+	private DiscountProductGroup _toDiscountProductGroup(
+			Long commerceDiscountRelId)
+		throws Exception {
+
+		return _discountProductGroupDTOConverter.toDTO(
+			new DefaultDTOConverterContext(
+				commerceDiscountRelId,
+				contextAcceptLanguage.getPreferredLocale()));
+	}
+
+	private List<DiscountProductGroup> _toDiscountProductGroups(
+			List<CommerceDiscountRel> commerceDiscountRels)
+		throws Exception {
+
+		List<DiscountProductGroup> discountProductGroups = new ArrayList<>();
+
+		for (CommerceDiscountRel commerceDiscountRel : commerceDiscountRels) {
+			discountProductGroups.add(
+				_toDiscountProductGroup(
+					commerceDiscountRel.getCommerceDiscountRelId()));
+		}
+
+		return discountProductGroups;
+	}
+
+	@Reference
+	private CommerceDiscountRelService _commerceDiscountRelService;
+
+	@Reference
+	private CommerceDiscountService _commerceDiscountService;
+
+	@Reference
+	private CommercePricingClassService _commercePricingClassService;
+
+	@Reference
+	private DiscountProductGroupDTOConverter _discountProductGroupDTOConverter;
+
+	@Reference
+	private ServiceContextHelper _serviceContextHelper;
+
 }
