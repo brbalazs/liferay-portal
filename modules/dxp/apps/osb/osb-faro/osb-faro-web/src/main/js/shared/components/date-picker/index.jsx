6c14906cee11689b1ffd98fed060972061f8aa47
Kien Do <kien.do@liferay.com>
2020-05-20 11:47:55 -0700

    LRAC-5925 Use isSame over isAfter for isNextDisabled

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/date-picker/index.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/date-picker/index.jsx
@@ -141,7 +141,7 @@ export default class DatePicker extends React.Component {
 			state: {currentMonth}
 		} = this;
 
-		return !!maxDate && !maxDate.isAfter(currentMonth);
+		return !!maxDate && !maxDate.isSame(currentMonth);
 	}
 
 	@autobind
