7b69e9cb0c6762fbde5eb35de8a77f081f14a8f5
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-05-21 18:31:49 -0300

    LRAC-5941 Adjust test of UpgradeConnectionCard

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/data-source/__tests__/UpgradeConnectionCard.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/data-source/__tests__/UpgradeConnectionCard.jsx
@@ -5,27 +5,16 @@ import {noop} from 'lodash';
 
 jest.unmock('react-dom');
 
-const actionsMock = [
-	{
-		label: 'test',
-		onClick: () => noop
-	}
-];
-
+const actionMock = {
+	label: 'test',
+	onClick: () => noop
+};
 describe('UpgradeConnectionCard', () => {
 	afterEach(cleanup);
 
 	it('should render', () => {
-		const {container} = render(<UpgradeConnectionCard />);
-
-		expect(container.firstChild).toHaveClass(
-			'upgrade-connection-card-root'
-		);
-	});
-
-	it('should render with actions', () => {
 		const {container} = render(
-			<UpgradeConnectionCard actions={actionsMock} />
+			<UpgradeConnectionCard action={actionMock} />
 		);
 
 		expect(container).toHaveTextContent('test');
