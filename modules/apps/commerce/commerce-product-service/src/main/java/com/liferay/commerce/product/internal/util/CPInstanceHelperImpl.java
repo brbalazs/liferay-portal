c34c5b304df3770a9c877a8fefcff89d3a3adeec
Matija Petanjek <matija.petanjek@liferay.com>
2020-05-21 13:59:10 +0200

    COMMERCE-3917 commerce-prodcut-serivce - remove unused methods (they're moved to DDMHerlper)

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/util/CPInstanceHelperImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/util/CPInstanceHelperImpl.java
@@ -14,7 +14,6 @@
 
 package com.liferay.commerce.product.internal.util;
 
-import com.liferay.commerce.account.model.CommerceAccount;
 import com.liferay.commerce.account.util.CommerceAccountHelper;
 import com.liferay.commerce.media.CommerceMediaResolver;
 import com.liferay.commerce.product.catalog.CPCatalogEntry;
@@ -43,13 +42,6 @@ import com.liferay.commerce.product.util.JsonHelper;
 import com.liferay.commerce.product.util.comparator.CPDefinitionOptionValueRelPriorityComparator;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeServicesTracker;
 import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderer;
-import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderingContext;
-import com.liferay.dynamic.data.mapping.model.DDMForm;
-import com.liferay.dynamic.data.mapping.model.DDMFormField;
-import com.liferay.dynamic.data.mapping.model.DDMFormFieldOptions;
-import com.liferay.dynamic.data.mapping.model.DDMFormRule;
-import com.liferay.dynamic.data.mapping.model.LocalizedValue;
-import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -59,7 +51,6 @@ import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.util.KeyValuePair;
-import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -69,14 +60,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.portlet.RenderRequest;
-import javax.portlet.RenderResponse;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -178,14 +161,6 @@ public class CPInstanceHelperImpl implements CPInstanceHelper {
 
 		return _cpAttachmentFileEntryLocalService.getCPAttachmentFileEntries(
 			cpDefinitionId, serializedDDMFormValues, type, start, end);
-	}
-
-	@Override
-	public DDMForm getCPAttachmentFileEntryDDMForm(
-			long cpDefinitionId, Locale locale)
-		throws PortalException {
-
-		return _getDDMForm(cpDefinitionId, locale, false, true, true, false);
 	}
 
 	@Override
@@ -385,17 +360,6 @@ public class CPInstanceHelperImpl implements CPInstanceHelper {
 	}
 
 	@Override
-	public DDMForm getCPInstanceDDMForm(
-			long cpDefinitionId, Locale locale, boolean ignoreSKUCombinations,
-			boolean skuContributor)
-		throws PortalException {
-
-		return _getDDMForm(
-			cpDefinitionId, locale, ignoreSKUCombinations, skuContributor,
-			false, false);
-	}
-
-	@Override
 	public String getCPInstanceThumbnailSrc(long cpInstanceId)
 		throws Exception {
 
@@ -537,179 +501,6 @@ public class CPInstanceHelperImpl implements CPInstanceHelper {
 		return values;
 	}
 
-	@Override
-	public DDMForm getPublicStoreDDMForm(
-			long groupId, long commerceAccountId, long cpDefinitionId,
-			Locale locale, boolean ignoreSKUCombinations,
-			boolean skuContributor)
-		throws PortalException {
-
-		DDMForm ddmForm = _getDDMForm(
-			cpDefinitionId, locale, ignoreSKUCombinations, skuContributor,
-			false, true);
-
-		if (!ignoreSKUCombinations) {
-			ddmForm.addDDMFormRule(
-				createDDMFormRule(
-					ddmForm, groupId, commerceAccountId, cpDefinitionId));
-		}
-
-		return ddmForm;
-	}
-
-	@Override
-	public String renderCPAttachmentFileEntryOptions(
-			long cpDefinitionId, String json, RenderRequest renderRequest,
-			RenderResponse renderResponse)
-		throws PortalException {
-
-		Locale locale = _portal.getLocale(renderRequest);
-
-		DDMForm ddmForm = getCPAttachmentFileEntryDDMForm(
-			cpDefinitionId, locale);
-
-		return _render(
-			cpDefinitionId, locale, ddmForm, json, renderRequest,
-			renderResponse);
-	}
-
-	@Override
-	public String renderCPInstanceOptions(
-			long cpDefinitionId, String json, boolean ignoreSKUCombinations,
-			boolean skuContributor, RenderRequest renderRequest,
-			RenderResponse renderResponse)
-		throws PortalException {
-
-		Locale locale = _portal.getLocale(renderRequest);
-
-		DDMForm ddmForm = getCPInstanceDDMForm(
-			cpDefinitionId, locale, ignoreSKUCombinations, skuContributor);
-
-		return _render(
-			cpDefinitionId, locale, ddmForm, json, renderRequest,
-			renderResponse);
-	}
-
-	@Override
-	public String renderPublicStoreOptions(
-			long cpDefinitionId, String json, boolean ignoreSKUCombinations,
-			boolean skuContributor, RenderRequest renderRequest,
-			RenderResponse renderResponse)
-		throws PortalException {
-
-		Locale locale = _portal.getLocale(renderRequest);
-
-		CommerceAccount commerceAccount =
-			_commerceAccountHelper.getCurrentCommerceAccount(
-				_commerceChannelLocalService.
-					getCommerceChannelGroupIdBySiteGroupId(
-						_portal.getScopeGroupId(renderRequest)),
-				_portal.getHttpServletRequest(renderRequest));
-
-		long commerceAccountId = 0;
-
-		if (commerceAccount != null) {
-			commerceAccountId = commerceAccount.getCommerceAccountId();
-		}
-
-		DDMForm ddmForm = getPublicStoreDDMForm(
-			_portal.getScopeGroupId(renderRequest), commerceAccountId,
-			cpDefinitionId, locale, ignoreSKUCombinations, skuContributor);
-
-		return _render(
-			cpDefinitionId, locale, ddmForm, json, renderRequest,
-			renderResponse);
-	}
-
-	protected DDMFormRule createDDMFormRule(
-		DDMForm ddmForm, long groupId, long commerceAccountId,
-		long cpDefinitionId) {
-
-		String action = createDDMFormRuleAction(
-			ddmForm, groupId, commerceAccountId, cpDefinitionId);
-
-		return new DDMFormRule("TRUE", action);
-	}
-
-	/**
-	 * Create a DDM form rule action as a call function, e.g.
-	 * <pre>
-	 * call(
-	 * 	'getCPInstanceOptionsValues',
-	 * 	concat(
-	 * 		'cpDefinitionId=56698', ';', '56703=', getValue('56703'), ';',
-	 * 		'56706=', getValue('56706')),
-	 * 	'56703=color;56706=size')
-	 * </pre>
-	 */
-	protected String createDDMFormRuleAction(
-		DDMForm ddmForm, long groupId, long commerceAccountId,
-		long cpDefinitionId) {
-
-		String callFunctionStatement =
-			"call('getCPInstanceOptionsValues', concat(%s), '%s')";
-
-		return String.format(
-			callFunctionStatement,
-			createDDMFormRuleInputMapping(
-				ddmForm, groupId, commerceAccountId, cpDefinitionId),
-			createDDMFormRuleOutputMapping(ddmForm));
-	}
-
-	protected String createDDMFormRuleInputMapping(
-		DDMForm ddmForm, long groupId, long commerceAccountId,
-		long cpDefinitionId) {
-
-		// The input information will be transformed in parameter request of
-		// DDMDataProviderRequest class and it'll be accessible in the data
-		// provider implementation.
-
-		String inputMappingStatement = "'%s=', getValue('%s')";
-		String delimiter = ", ';',";
-
-		List<DDMFormField> ddmFormFields = ddmForm.getDDMFormFields();
-
-		Stream<DDMFormField> stream = ddmFormFields.stream();
-
-		Stream<String> inputMappingStatementStream = stream.map(
-			field -> String.format(
-				inputMappingStatement, field.getName(), field.getName()));
-
-		inputMappingStatementStream = Stream.concat(
-			Stream.of(
-				String.format(
-					"'cpDefinitionId=%s'", String.valueOf(cpDefinitionId))),
-			inputMappingStatementStream);
-
-		inputMappingStatementStream = Stream.concat(
-			Stream.of(String.format("'groupId=%s'", String.valueOf(groupId))),
-			inputMappingStatementStream);
-
-		inputMappingStatementStream = Stream.concat(
-			Stream.of(
-				String.format(
-					"'commerceAccountId=%s'",
-					String.valueOf(commerceAccountId))),
-			inputMappingStatementStream);
-
-		return inputMappingStatementStream.collect(
-			Collectors.joining(delimiter));
-	}
-
-	protected String createDDMFormRuleOutputMapping(DDMForm ddmForm) {
-		String outputMappingStatement = "%s=%s";
-
-		List<DDMFormField> ddmFormFields = ddmForm.getDDMFormFields();
-
-		Stream<DDMFormField> stream = ddmFormFields.stream();
-
-		Stream<String> stringStream = stream.map(
-			field -> String.format(
-				outputMappingStatement, field.getName(), field.getName()));
-
-		return stringStream.collect(Collectors.joining(StringPool.SEMICOLON));
-	}
-
 	private CPInstance _fetchCPInstanceBySKUContributors(
 			long cpDefinitionId, String json)
 		throws PortalException {
@@ -792,144 +583,6 @@ public class CPInstanceHelperImpl implements CPInstanceHelper {
 		return _cpInstanceLocalService.getCPInstance(cpInstanceId);
 	}
 
-	private DDMForm _getDDMForm(
-			long cpDefinitionId, Locale locale, boolean ignoreSKUCombinations,
-			boolean skuContributor, boolean optional, boolean publicStore)
-		throws PortalException {
-
-		List<CPDefinitionOptionRel> cpDefinitionOptionRels;
-
-		if (skuContributor) {
-			cpDefinitionOptionRels =
-				_cpDefinitionOptionRelLocalService.getCPDefinitionOptionRels(
-					cpDefinitionId, true);
-		}
-		else {
-			cpDefinitionOptionRels =
-				_cpDefinitionOptionRelLocalService.getCPDefinitionOptionRels(
-					cpDefinitionId);
-		}
-
-		if (cpDefinitionOptionRels.isEmpty()) {
-			return null;
-		}
-
-		DDMForm ddmForm = new DDMForm();
-
-		for (CPDefinitionOptionRel cpDefinitionOptionRel :
-				cpDefinitionOptionRels) {
-
-			if (Validator.isNull(
-					cpDefinitionOptionRel.getDDMFormFieldTypeName())) {
-
-				continue;
-			}
-
-			List<CPDefinitionOptionValueRel> cpDefinitionOptionValueRels = null;
-
-			if (cpDefinitionOptionRel.isSkuContributor() && publicStore) {
-				cpDefinitionOptionValueRels =
-					getCPInstanceCPDefinitionOptionValueRels(
-						cpDefinitionId,
-						cpDefinitionOptionRel.getCPDefinitionOptionRelId());
-			}
-			else {
-				cpDefinitionOptionValueRels =
-					cpDefinitionOptionRel.getCPDefinitionOptionValueRels();
-			}
-
-			DDMFormField ddmFormField = _getDDMFormField(
-				cpDefinitionOptionRel, cpDefinitionOptionValueRels, locale);
-
-			ddmFormField.setRequired(
-				_isDDMFormRequired(
-					cpDefinitionOptionRel, ignoreSKUCombinations, optional,
-					publicStore));
-
-			ddmForm.addDDMFormField(ddmFormField);
-		}
-
-		ddmForm.addAvailableLocale(locale);
-		ddmForm.setDefaultLocale(locale);
-
-		return ddmForm;
-	}
-
-	private DDMFormField _getDDMFormField(
-		CPDefinitionOptionRel cpDefinitionOptionRel,
-		List<CPDefinitionOptionValueRel> cpDefinitionOptionValueRels,
-		Locale locale) {
-
-		DDMFormField ddmFormField = new DDMFormField(
-			cpDefinitionOptionRel.getKey(),
-			cpDefinitionOptionRel.getDDMFormFieldTypeName());
-
-		LocalizedValue ddmFormFieldLabelLocalizedValue = new LocalizedValue(
-			locale);
-
-		ddmFormFieldLabelLocalizedValue.addString(
-			locale, cpDefinitionOptionRel.getName(locale));
-
-		ddmFormField.setLabel(ddmFormFieldLabelLocalizedValue);
-
-		if (cpDefinitionOptionValueRels.isEmpty()) {
-			return ddmFormField;
-		}
-
-		DDMFormFieldOptions ddmFormFieldOptions = _getDDMFormFieldOptions(
-			cpDefinitionOptionValueRels, locale);
-
-		ddmFormField.setDDMFormFieldOptions(ddmFormFieldOptions);
-
-		if (cpDefinitionOptionRel.isSkuContributor()) {
-			ddmFormField.setPredefinedValue(
-				_getDDMFormFieldPredefinedValue(ddmFormFieldOptions));
-		}
-
-		return ddmFormField;
-	}
-
-	private DDMFormFieldOptions _getDDMFormFieldOptions(
-		List<CPDefinitionOptionValueRel> cpDefinitionOptionValueRels,
-		Locale locale) {
-
-		DDMFormFieldOptions ddmFormFieldOptions = new DDMFormFieldOptions();
-
-		for (CPDefinitionOptionValueRel cpDefinitionOptionValueRel :
-				cpDefinitionOptionValueRels) {
-
-			ddmFormFieldOptions.addOptionLabel(
-				cpDefinitionOptionValueRel.getKey(), locale,
-				cpDefinitionOptionValueRel.getName(locale));
-		}
-
-		return ddmFormFieldOptions;
-	}
-
-	private LocalizedValue _getDDMFormFieldPredefinedValue(
-		DDMFormFieldOptions ddmFormFieldOptions) {
-
-		Map<String, LocalizedValue> options = ddmFormFieldOptions.getOptions();
-
-		if (options.isEmpty()) {
-			return new LocalizedValue(ddmFormFieldOptions.getDefaultLocale());
-		}
-
-		for (Map.Entry<String, LocalizedValue> entry : options.entrySet()) {
-			LocalizedValue localizedValue = new LocalizedValue();
-
-			LocalizedValue curLocalizedValue = entry.getValue();
-
-			localizedValue.addString(
-				curLocalizedValue.getDefaultLocale(), entry.getKey());
-
-			return localizedValue;
-		}
-
-		throw new IllegalArgumentException(
-			"Provided DDM field options miss valid field value");
-	}
-
 	private long _getTopId(Map<Long, Integer> idIdHits) {
 		long topId = 0;
 		int topIdHits = 0;
@@ -963,82 +616,6 @@ public class CPInstanceHelperImpl implements CPInstanceHelper {
 		}
 
 		return true;
-	}
-
-	private boolean _isDDMFormRequired(
-		CPDefinitionOptionRel cpDefinitionOptionRel,
-		boolean ignoreSKUCombinations, boolean optional, boolean publicStore) {
-
-		if (optional) {
-			return false;
-		}
-
-		Map<String, Object> properties =
-			_ddmFormFieldTypeServicesTracker.getDDMFormFieldTypeProperties(
-				cpDefinitionOptionRel.getDDMFormFieldTypeName());
-
-		String fieldTypeDataDomain = MapUtil.getString(
-			properties, "ddm.form.field.type.data.domain");
-
-		if (Validator.isNotNull(fieldTypeDataDomain) &&
-			fieldTypeDataDomain.equals("list") &&
-			!_cpDefinitionOptionValueRelLocalService.
-				hasCPDefinitionOptionValueRels(
-					cpDefinitionOptionRel.getCPDefinitionOptionRelId())) {
-
-			return false;
-		}
-
-		if (ignoreSKUCombinations) {
-			return cpDefinitionOptionRel.isRequired();
-		}
-
-		if (cpDefinitionOptionRel.isSkuContributor() ||
-			(publicStore && cpDefinitionOptionRel.isRequired())) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	private String _render(
-			long cpDefinitionId, Locale locale, DDMForm ddmForm, String json,
-			RenderRequest renderRequest, RenderResponse renderResponse)
-		throws PortalException {
-
-		if (ddmForm == null) {
-			return StringPool.BLANK;
-		}
-
-		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
-			renderRequest);
-
-		HttpServletResponse httpServletResponse =
-			_portal.getHttpServletResponse(renderResponse);
-
-		DDMFormRenderingContext ddmFormRenderingContext =
-			new DDMFormRenderingContext();
-
-		ddmFormRenderingContext.setContainerId(
-			"ProductOptions" + String.valueOf(cpDefinitionId));
-		ddmFormRenderingContext.setHttpServletRequest(httpServletRequest);
-		ddmFormRenderingContext.setHttpServletResponse(httpServletResponse);
-		ddmFormRenderingContext.setLocale(locale);
-		ddmFormRenderingContext.setPortletNamespace(
-			renderResponse.getNamespace());
-		ddmFormRenderingContext.setShowRequiredFieldsWarning(false);
-
-		if (Validator.isNotNull(json)) {
-			DDMFormValues ddmFormValues = _ddmFormValuesHelper.deserialize(
-				ddmForm, json, locale);
-
-			if (ddmFormValues != null) {
-				ddmFormRenderingContext.setDDMFormValues(ddmFormValues);
-			}
-		}
-
-		return _ddmFormRenderer.render(ddmForm, ddmFormRenderingContext);
 	}
 
 	@Reference
