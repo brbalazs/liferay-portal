d1d01a1d5fe53fe5f18f556d313c51245d50dc3e
Riccardo <riccardo.alberti@liferay.com>
2020-05-21 19:54:45 +0200

    COMMERCE-3012 discount rule resource, util and dto converter

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/DiscountRuleResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-pricing-impl/src/main/java/com/liferay/headless/commerce/admin/pricing/internal/resource/v2_0/DiscountRuleResourceImpl.java
@@ -14,9 +14,28 @@
 
 package com.liferay.headless.commerce.admin.pricing.internal.resource.v2_0;
 
+import com.liferay.commerce.discount.exception.NoSuchDiscountException;
+import com.liferay.commerce.discount.model.CommerceDiscount;
+import com.liferay.commerce.discount.model.CommerceDiscountRule;
+import com.liferay.commerce.discount.service.CommerceDiscountRuleService;
+import com.liferay.commerce.discount.service.CommerceDiscountService;
+import com.liferay.headless.commerce.admin.pricing.dto.v2_0.DiscountRule;
+import com.liferay.headless.commerce.admin.pricing.internal.dto.v2_0.converter.DiscountRuleDTOConverter;
+import com.liferay.headless.commerce.admin.pricing.internal.util.v2_0.DiscountRuleUtil;
 import com.liferay.headless.commerce.admin.pricing.resource.v2_0.DiscountRuleResource;
+import com.liferay.headless.commerce.core.util.ServiceContextHelper;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.core.Response;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +46,155 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = DiscountRuleResource.class
 )
 public class DiscountRuleResourceImpl extends BaseDiscountRuleResourceImpl {
+
+	@Override
+	public void deleteDiscountRule(Long id) throws Exception {
+		_commerceDiscountRuleService.deleteCommerceDiscountRule(id);
+	}
+
+	@Override
+	public Page<DiscountRule>
+			getDiscountByExternalReferenceCodeDiscountRulesPage(
+				String externalReferenceCode, Pagination pagination)
+		throws Exception {
+
+		CommerceDiscount commerceDiscount =
+			_commerceDiscountService.fetchByExternalReferenceCode(
+				contextCompany.getCompanyId(), externalReferenceCode);
+
+		if (commerceDiscount == null) {
+			throw new NoSuchDiscountException(
+				"Unable to find Discount with externalReferenceCode: " +
+					externalReferenceCode);
+		}
+
+		List<CommerceDiscountRule> commerceDiscountRules =
+			_commerceDiscountRuleService.getCommerceDiscountRules(
+				commerceDiscount.getCommerceDiscountId(),
+				pagination.getStartPosition(), pagination.getEndPosition(),
+				null);
+
+		int totalItems =
+			_commerceDiscountRuleService.getCommerceDiscountRulesCount(
+				commerceDiscount.getCommerceDiscountId());
+
+		return Page.of(
+			_toDiscountRules(commerceDiscountRules), pagination, totalItems);
+	}
+
+	@Override
+	public Page<DiscountRule> getDiscountIdDiscountRulesPage(
+			Long id, Pagination pagination)
+		throws Exception {
+
+		List<CommerceDiscountRule> commerceDiscountRules =
+			_commerceDiscountRuleService.getCommerceDiscountRules(
+				id, pagination.getStartPosition(), pagination.getEndPosition(),
+				null);
+
+		int totalItems =
+			_commerceDiscountRuleService.getCommerceDiscountRulesCount(id);
+
+		return Page.of(
+			_toDiscountRules(commerceDiscountRules), pagination, totalItems);
+	}
+
+	@Override
+	public DiscountRule getDiscountRule(Long id) throws Exception {
+		return _toDiscountRule(GetterUtil.getLong(id));
+	}
+
+	@Override
+	public Response patchDiscountRule(Long id, DiscountRule discountRule)
+		throws Exception {
+
+		CommerceDiscountRule commerceDiscountRule =
+			_commerceDiscountRuleService.getCommerceDiscountRule(id);
+
+		_commerceDiscountRuleService.updateCommerceDiscountRule(
+			commerceDiscountRule.getCommerceDiscountRuleId(),
+			discountRule.getType(),
+			GetterUtil.get(
+				discountRule.getTypeSettings(),
+				commerceDiscountRule.getTypeSettings()));
+
+		Response.ResponseBuilder responseBuilder = Response.ok();
+
+		return responseBuilder.build();
+	}
+
+	@Override
+	public DiscountRule postDiscountByExternalReferenceCodeDiscountRule(
+			String externalReferenceCode, DiscountRule discountRule)
+		throws Exception {
+
+		CommerceDiscount commerceDiscount =
+			_commerceDiscountService.fetchByExternalReferenceCode(
+				contextCompany.getCompanyId(), externalReferenceCode);
+
+		if (commerceDiscount == null) {
+			throw new NoSuchDiscountException(
+				"Unable to find Discount with externalReferenceCode: " +
+					externalReferenceCode);
+		}
+
+		CommerceDiscountRule commerceDiscountRule =
+			DiscountRuleUtil.addCommerceDiscountRule(
+				_commerceDiscountRuleService, discountRule, commerceDiscount,
+				_serviceContextHelper.getServiceContext());
+
+		return _toDiscountRule(
+			commerceDiscountRule.getCommerceDiscountRuleId());
+	}
+
+	@Override
+	public DiscountRule postDiscountIdDiscountRule(
+			Long id, DiscountRule discountRule)
+		throws Exception {
+
+		CommerceDiscountRule commerceDiscountRule =
+			DiscountRuleUtil.addCommerceDiscountRule(
+				_commerceDiscountRuleService, discountRule,
+				_commerceDiscountService.getCommerceDiscount(id),
+				_serviceContextHelper.getServiceContext());
+
+		return _toDiscountRule(
+			commerceDiscountRule.getCommerceDiscountRuleId());
+	}
+
+	private DiscountRule _toDiscountRule(Long discountId) throws Exception {
+		return _discountRuleDTOConverter.toDTO(
+			new DefaultDTOConverterContext(
+				discountId, contextAcceptLanguage.getPreferredLocale()));
+	}
+
+	private List<DiscountRule> _toDiscountRules(
+			List<CommerceDiscountRule> commerceDiscountRules)
+		throws Exception {
+
+		List<DiscountRule> discountRules = new ArrayList<>();
+
+		for (CommerceDiscountRule commerceDiscountRule :
+				commerceDiscountRules) {
+
+			discountRules.add(
+				_toDiscountRule(
+					commerceDiscountRule.getCommerceDiscountRuleId()));
+		}
+
+		return discountRules;
+	}
+
+	@Reference
+	private CommerceDiscountRuleService _commerceDiscountRuleService;
+
+	@Reference
+	private CommerceDiscountService _commerceDiscountService;
+
+	@Reference
+	private DiscountRuleDTOConverter _discountRuleDTOConverter;
+
+	@Reference
+	private ServiceContextHelper _serviceContextHelper;
+
 }
