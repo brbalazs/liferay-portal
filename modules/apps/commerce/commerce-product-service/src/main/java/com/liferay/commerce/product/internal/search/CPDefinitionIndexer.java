2bd57fb8e063ee076b6b825372d32de9a4c4a595
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2020-05-29 12:02:54 +0200

    COMMERCE-3817 SF

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/search/CPDefinitionIndexer.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/search/CPDefinitionIndexer.java
@@ -654,7 +654,8 @@ public class CPDefinitionIndexer extends BaseIndexer<CPDefinition> {
 			document.addKeyword(Field.HIDDEN, true);
 		}
 
-		document.addKeyword(CPField.CP_DEFINITION_STATUS, cpDefinition.getStatus());
+		document.addKeyword(
+			CPField.CP_DEFINITION_STATUS, cpDefinition.getStatus());
 
 		CommerceCatalog commerceCatalog = cpDefinition.getCommerceCatalog();
 
