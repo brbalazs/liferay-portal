fb85e8cb4281ea174a5803ee938f70b9b5a4bfa2
Norbert Nemeth <norbert.nemeth@liferay.com>
2020-05-29 13:49:19 +0200

    LPS-114001 partial revert of LPS-105572 commit 58377c91742faebb76ab540f3ea24e4235b6bda1 Do not use the current value as placeholder value when creating a new locale

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -856,10 +856,6 @@ AUI.add(
 							}
 
 							if (Lang.isUndefined(value)) {
-								value = instance.getValue();
-							}
-
-							if (Lang.isUndefined(value)) {
 								value = instance.getDefaultLocalization(
 									instance.get('displayLocale')
 								);
