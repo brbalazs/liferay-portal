ca68bec37124e8e61dcbe862144d38ff1a2b1144
Alec Sloan <Alec.sloan@Liferay.com>
2020-06-05 16:07:23 -0700

    COMMERCE-4043 Regenerate Service Files (gradle buildService)

--- modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPDefinitionLocalService.java
+++ modules/apps/commerce/commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPDefinitionLocalService.java
@@ -144,6 +144,9 @@ public interface CPDefinitionLocalService
 	public CPDefinition copyCPDefinition(long cpDefinitionId)
 		throws PortalException;
 
+	public CPDefinition copyCPDefinition(long cpDefinitionId, long groupId)
+		throws PortalException;
+
 	/**
 	 * Creates a new cp definition with the primary key. Does not add the cp definition to the database.
 	 *
