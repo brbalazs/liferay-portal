865060bae736188d214a2b6bd75b176420312ec3
Francisco Dias <francisco.dias@liferay.com>
2020-06-11 12:31:43 -0300

    LRAC-6100 SF

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/util/table-columns.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/util/table-columns.jsx
@@ -703,7 +703,7 @@ export const sitePagesListColumns = {
 			),
 			routeFn: ({data: {assetId, assetTitle}}) =>
 				setUriQueryValues(
-					pickBy({...rangeSelectors}),
+					pickBy(rangeSelectors),
 					toRoute(route, {
 						channelId,
 						groupId,
