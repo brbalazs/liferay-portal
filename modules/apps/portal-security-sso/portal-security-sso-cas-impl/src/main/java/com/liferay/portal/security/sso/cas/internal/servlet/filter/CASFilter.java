ffba1ee336491b882120e9f37a4135b27ead57e0
Istvan Sajtos <istvan.sajtos@liferay.com>
2020-06-16 15:42:57 +0200

    LPS-111354 portal-security-sso-cas-impl: Use the newer, non-deprecated method

--- modules/apps/portal-security-sso/portal-security-sso-cas-impl/src/main/java/com/liferay/portal/security/sso/cas/internal/servlet/filter/CASFilter.java
+++ modules/apps/portal-security-sso/portal-security-sso-cas-impl/src/main/java/com/liferay/portal/security/sso/cas/internal/servlet/filter/CASFilter.java
@@ -219,7 +219,8 @@ public class CASFilter extends BaseFilter {
 
 		if (Validator.isNull(serviceURL)) {
 			serviceURL = CommonUtils.constructServiceUrl(
-				request, response, serviceURL, serverName, "ticket", true);
+				request, response, serviceURL, serverName, "service", "ticket",
+				true);
 		}
 
 		String ticket = ParamUtil.getString(request, "ticket");
