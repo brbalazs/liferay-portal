4c81557c4ce9e8c54144fa91cba0b0fea37e6563
Brian Chan <brian.chan@liferay.com>
2020-06-16 19:21:41 -0700

    Revert "LPS-110474 Track all occurrences of include-and-override, assuming it can be an array spread over different files. It can be useful later as in PropsHelperUtil"
    This reverts commit 5f41bfd67a8eeb763834936988eab2b071b33626.

--- portal-impl/src/com/liferay/portal/configuration/easyconf/ClassLoaderAggregateProperties.java
+++ portal-impl/src/com/liferay/portal/configuration/easyconf/ClassLoaderAggregateProperties.java
@@ -34,7 +34,6 @@ import java.lang.reflect.Field;
 import java.net.URL;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -64,8 +63,6 @@ public class ClassLoaderAggregateProperties extends AggregatedProperties {
 
 		_prefixedSystemConfiguration = new SubsetConfiguration(
 			_systemConfiguration, _getPrefix(), null);
-
-		setProperty("include-and-override", _includeAndOverride);
 	}
 
 	@Override
@@ -232,8 +229,6 @@ public class ClassLoaderAggregateProperties extends AggregatedProperties {
 
 		String[] fileNames = tempCompositeConfiguration.getStringArray(
 			Conventions.INCLUDE_PROPERTY);
-
-		Collections.addAll(_includeAndOverride, fileNames);
 
 		ArrayUtil.reverse(fileNames);
 
@@ -449,7 +444,6 @@ public class ClassLoaderAggregateProperties extends AggregatedProperties {
 	private final String _componentName;
 	private final CompositeConfiguration _globalCompositeConfiguration =
 		new CompositeConfiguration();
-	private final List<String> _includeAndOverride = new ArrayList<>();
 	private final List<String> _loadedSources = new ArrayList<>();
 	private final Configuration _prefixedSystemConfiguration;
 	private final SystemConfiguration _systemConfiguration =
