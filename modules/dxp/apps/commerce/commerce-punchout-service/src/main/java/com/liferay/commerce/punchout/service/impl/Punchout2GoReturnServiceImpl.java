92a219f29703b4cd4e75e11c0cd45dd811999d12
Jaclyn Ong <jaclyn.ong@liferay.com>
2020-06-18 16:35:02 -0700

    COMMERCE-3810 Add logs to cart transfer

--- modules/dxp/apps/commerce/commerce-punchout-service/src/main/java/com/liferay/commerce/punchout/service/impl/Punchout2GoReturnServiceImpl.java
+++ modules/dxp/apps/commerce/commerce-punchout-service/src/main/java/com/liferay/commerce/punchout/service/impl/Punchout2GoReturnServiceImpl.java
@@ -28,6 +28,8 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 
@@ -55,155 +57,179 @@ public class Punchout2GoReturnServiceImpl implements PunchoutReturnService {
 			CommerceOrder commerceOrder, String url)
 		throws Exception {
 
-		URL urlObj = new URL(url);
+		try {
+			JSONObject cartJSONObject = _jsonFactory.createJSONObject();
 
-		HttpURLConnection httpURLConnection =
-			(HttpURLConnection)urlObj.openConnection();
+			cartJSONObject.put("contract_id", StringPool.BLANK);
+			cartJSONObject.put(
+				"discount", commerceOrder.getTotalDiscountAmount());
+			cartJSONObject.put("discount_description", StringPool.BLANK);
+			cartJSONObject.put("quote_id", StringPool.BLANK);
+			cartJSONObject.put("shipping", commerceOrder.getShippingAmount());
+			cartJSONObject.put(
+				"shipping_description", commerceOrder.getShippingOptionName());
+			cartJSONObject.put("tax", commerceOrder.getTaxAmount());
+			cartJSONObject.put("tax_description", StringPool.BLANK);
+			cartJSONObject.put("total", commerceOrder.getTotal());
 
-		httpURLConnection.setDoOutput(true);
-		httpURLConnection.setRequestMethod("POST");
-		httpURLConnection.setRequestProperty(
-			"Content-type", "application/json");
+			CommerceCurrency commerceCurrency =
+				commerceOrder.getCommerceCurrency();
 
-		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
-			httpURLConnection.getOutputStream());
+			cartJSONObject.put("currencyCode", commerceCurrency.getCode());
 
-		JSONObject cartJSONObject = _jsonFactory.createJSONObject();
+			JSONArray cartItemJSONArray = _jsonFactory.createJSONArray();
 
-		cartJSONObject.put("contract_id", StringPool.BLANK);
-		cartJSONObject.put("discount", commerceOrder.getTotalDiscountAmount());
-		cartJSONObject.put("discount_description", StringPool.BLANK);
-		cartJSONObject.put("quote_id", StringPool.BLANK);
-		cartJSONObject.put("shipping", commerceOrder.getShippingAmount());
-		cartJSONObject.put(
-			"shipping_description", commerceOrder.getShippingOptionName());
-		cartJSONObject.put("tax", commerceOrder.getTaxAmount());
-		cartJSONObject.put("tax_description", StringPool.BLANK);
-		cartJSONObject.put("total", commerceOrder.getTotal());
-
-		CommerceCurrency commerceCurrency = commerceOrder.getCommerceCurrency();
-
-		cartJSONObject.put("currencyCode", commerceCurrency.getCode());
-
-		JSONArray cartItemJSONArray = _jsonFactory.createJSONArray();
-
-		for (CommerceOrderItem commerceOrderItem :
+			for (CommerceOrderItem commerceOrderItem :
 				commerceOrder.getCommerceOrderItems()) {
 
-			JSONObject cartItemJSONObject = _jsonFactory.createJSONObject();
-
-			cartItemJSONObject.put(
-				"supplierauxid",
-				commerceOrder.getCommerceOrderId() + "/" +
-					commerceOrderItem.getCommerceOrderItemId());
-			cartItemJSONObject.put("supplierid", commerceOrderItem.getSku());
-
-			CPDefinition cpDefinition = commerceOrderItem.getCPDefinition();
-
-			cartItemJSONObject.put(
-				"description", cpDefinition.getDescription());
-
-			AssetEntry assetEntry = _assetEntryLocalService.getEntry(
-				CPDefinition.class.getName(), cpDefinition.getCPDefinitionId());
-
-			StringBundler assetCategorySB = new StringBundler();
-
-			for (AssetCategory assetCategory : assetEntry.getCategories()) {
-				assetCategorySB.append(assetCategory.getName());
-
-				assetCategorySB.append(StringPool.COMMA);
-			}
-
-			cartItemJSONObject.put(
-				"classification", assetCategorySB.toString());
-
-			cartItemJSONObject.put("uom", "EA");
-
-			cartItemJSONObject.put("quantity", commerceOrderItem.getQuantity());
-			cartItemJSONObject.put(
-				"unitprice", commerceOrderItem.getUnitPrice());
-
-			cartItemJSONObject.put(
-				"commercePriceListId",
-				commerceOrderItem.getCommercePriceListId());
-			cartItemJSONObject.put(
-				"deliveryGroup", commerceOrderItem.getDeliveryGroup());
-			cartItemJSONObject.put(
-				"discountAmount", commerceOrderItem.getDiscountAmount());
-			cartItemJSONObject.put(
-				"externalReferenceCode",
-				commerceOrderItem.getExternalReferenceCode());
-			cartItemJSONObject.put(
-				"finalPrice", commerceOrderItem.getFinalPrice());
-			cartItemJSONObject.put(
-				"parentCommerceOrderItemId",
-				commerceOrderItem.getParentCommerceOrderItemId());
-			cartItemJSONObject.put(
-				"printedNote", commerceOrderItem.getPrintedNote());
-			cartItemJSONObject.put(
-				"promoPrice", commerceOrderItem.getPromoPrice());
-			cartItemJSONObject.put(
-				"shippedQuantity", commerceOrderItem.getShippedQuantity());
-			cartItemJSONObject.put(
-				"shippingAddressId", commerceOrderItem.getShippingAddressId());
-
-			CPInstance cpInstance = _cpInstanceLocalService.fetchCPInstance(
-				commerceOrderItem.getCPInstanceId());
-
-			cartItemJSONObject.put("UNSPSC", cpInstance.getUNSPSC());
-
-			DateFormat dateFormat = new SimpleDateFormat(
-				"yyyy-MM-dd'T'HH:mm:ss'Z'");
-
-			if (commerceOrderItem.getCreateDate() != null) {
-				String createDateString = dateFormat.format(
-					commerceOrderItem.getCreateDate());
-
-				cartItemJSONObject.put("createDate", createDateString);
-			}
-
-			if (commerceOrderItem.getModifiedDate() != null) {
-				String modifiedDateString = dateFormat.format(
-					commerceOrderItem.getModifiedDate());
-
-				cartItemJSONObject.put("modifiedDate", modifiedDateString);
-			}
-
-			if (commerceOrderItem.getRequestedDeliveryDate() != null) {
-				String requestedDeliveryDateString = dateFormat.format(
-					commerceOrderItem.getRequestedDeliveryDate());
+				JSONObject cartItemJSONObject = _jsonFactory.createJSONObject();
 
 				cartItemJSONObject.put(
-					"requestedDeliveryDate", requestedDeliveryDateString);
+					"supplierauxid",
+					commerceOrder.getCommerceOrderId() + "/" +
+					commerceOrderItem.getCommerceOrderItemId());
+				cartItemJSONObject.put(
+					"supplierid", commerceOrderItem.getSku());
+
+				CPDefinition cpDefinition = commerceOrderItem.getCPDefinition();
+
+				cartItemJSONObject.put(
+					"description", cpDefinition.getDescription());
+
+				AssetEntry assetEntry = _assetEntryLocalService.getEntry(
+					CPDefinition.class.getName(),
+					cpDefinition.getCPDefinitionId());
+
+				StringBundler assetCategorySB = new StringBundler();
+
+				for (AssetCategory assetCategory : assetEntry.getCategories()) {
+					assetCategorySB.append(assetCategory.getName());
+
+					assetCategorySB.append(StringPool.COMMA);
+				}
+
+				cartItemJSONObject.put(
+					"classification", assetCategorySB.toString());
+
+				cartItemJSONObject.put("uom", "EA");
+
+				cartItemJSONObject.put(
+					"quantity", commerceOrderItem.getQuantity());
+				cartItemJSONObject.put(
+					"unitprice", commerceOrderItem.getUnitPrice());
+
+				cartItemJSONObject.put(
+					"commercePriceListId",
+					commerceOrderItem.getCommercePriceListId());
+				cartItemJSONObject.put(
+					"deliveryGroup", commerceOrderItem.getDeliveryGroup());
+				cartItemJSONObject.put(
+					"discountAmount", commerceOrderItem.getDiscountAmount());
+				cartItemJSONObject.put(
+					"externalReferenceCode",
+					commerceOrderItem.getExternalReferenceCode());
+				cartItemJSONObject.put(
+					"finalPrice", commerceOrderItem.getFinalPrice());
+				cartItemJSONObject.put(
+					"parentCommerceOrderItemId",
+					commerceOrderItem.getParentCommerceOrderItemId());
+				cartItemJSONObject.put(
+					"printedNote", commerceOrderItem.getPrintedNote());
+				cartItemJSONObject.put(
+					"promoPrice", commerceOrderItem.getPromoPrice());
+				cartItemJSONObject.put(
+					"shippedQuantity", commerceOrderItem.getShippedQuantity());
+				cartItemJSONObject.put(
+					"shippingAddressId",
+					commerceOrderItem.getShippingAddressId());
+
+				CPInstance cpInstance = _cpInstanceLocalService.fetchCPInstance(
+					commerceOrderItem.getCPInstanceId());
+
+				cartItemJSONObject.put("unspsc", cpInstance.getUNSPSC());
+
+				DateFormat dateFormat = new SimpleDateFormat(
+					"yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+				if (commerceOrderItem.getCreateDate() != null) {
+					String createDateString = dateFormat.format(
+						commerceOrderItem.getCreateDate());
+
+					cartItemJSONObject.put("createDate", createDateString);
+				}
+
+				if (commerceOrderItem.getModifiedDate() != null) {
+					String modifiedDateString = dateFormat.format(
+						commerceOrderItem.getModifiedDate());
+
+					cartItemJSONObject.put("modifiedDate", modifiedDateString);
+				}
+
+				if (commerceOrderItem.getRequestedDeliveryDate() != null) {
+					String requestedDeliveryDateString = dateFormat.format(
+						commerceOrderItem.getRequestedDeliveryDate());
+
+					cartItemJSONObject.put(
+						"requestedDeliveryDate", requestedDeliveryDateString);
+				}
+
+				cartItemJSONArray.put(cartItemJSONObject);
 			}
 
-			cartItemJSONArray.put(cartItemJSONObject);
+			cartJSONObject.put("items", cartItemJSONArray);
+
+			String cartJSON = cartJSONObject.toString();
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"Punchout2Go cart transfer request to " + url + "; cart JSON: " + cartJSON);
+			}
+
+			URL urlObj = new URL(url);
+
+			HttpURLConnection httpURLConnection =
+				(HttpURLConnection) urlObj.openConnection();
+
+			httpURLConnection.setDoOutput(true);
+			httpURLConnection.setRequestMethod("POST");
+			httpURLConnection.setRequestProperty(
+				"Content-type", "application/json");
+
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(
+				httpURLConnection.getOutputStream());
+
+			outputStreamWriter.write(cartJSON);
+
+			outputStreamWriter.flush();
+
+			int responseCode = httpURLConnection.getResponseCode();
+
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				String json = StringUtil.toLowerCase(
+					StringUtil.read(httpURLConnection.getInputStream()));
+
+				JSONArray jsonArray = _jsonFactory.createJSONArray(
+					StringPool.OPEN_BRACKET + json + StringPool.CLOSE_BRACKET);
+
+				JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+				return jsonObject.getString("redirecturl");
+			}
+
+			_log.error(
+				"Punchout2Go cart transfer response code: " + responseCode);
+
+			return null;
 		}
+		catch (Exception e) {
+			_log.error("Punchout2Go cart transfer failed", e);
 
-		cartJSONObject.put("items", cartItemJSONArray);
-
-		String cartJSON = cartJSONObject.toString();
-
-		outputStreamWriter.write(cartJSON);
-
-		outputStreamWriter.flush();
-
-		int responseCode = httpURLConnection.getResponseCode();
-
-		if (responseCode == HttpURLConnection.HTTP_OK) {
-			String json = StringUtil.toLowerCase(
-				StringUtil.read(httpURLConnection.getInputStream()));
-
-			JSONArray jsonArray = _jsonFactory.createJSONArray(
-				StringPool.OPEN_BRACKET + json + StringPool.CLOSE_BRACKET);
-
-			JSONObject jsonObject = jsonArray.getJSONObject(0);
-
-			return jsonObject.getString("redirecturl");
+			throw e;
 		}
-
-		return null;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		Punchout2GoReturnServiceImpl.class);
 
 	@Reference
 	private AssetEntryLocalService _assetEntryLocalService;
