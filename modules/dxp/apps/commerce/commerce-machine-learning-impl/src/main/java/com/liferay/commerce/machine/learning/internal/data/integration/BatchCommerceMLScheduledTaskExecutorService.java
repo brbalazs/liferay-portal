0b49366cbf269fbf159d2835fd80928aa80050d4
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2020-06-22 19:16:14 +0200

    COMMERCE-4185 commerce-machine-learning-impl Data Integration support

--- modules/dxp/apps/commerce/commerce-machine-learning-impl/src/main/java/com/liferay/commerce/machine/learning/internal/data/integration/BatchCommerceMLScheduledTaskExecutorService.java
+++ modules/dxp/apps/commerce/commerce-machine-learning-impl/src/main/java/com/liferay/commerce/machine/learning/internal/data/integration/BatchCommerceMLScheduledTaskExecutorService.java
@@ -14,24 +14,12 @@
 
 package com.liferay.commerce.machine.learning.internal.data.integration;
 
-import com.liferay.batch.engine.BatchEngineExportTaskExecutor;
-import com.liferay.batch.engine.BatchEngineImportTaskExecutor;
-import com.liferay.batch.engine.BatchEngineTaskContentType;
-import com.liferay.batch.engine.BatchEngineTaskExecuteStatus;
-import com.liferay.batch.engine.BatchEngineTaskOperation;
 import com.liferay.batch.engine.model.BatchEngineExportTask;
-import com.liferay.batch.engine.model.BatchEngineImportTask;
-import com.liferay.batch.engine.service.BatchEngineExportTaskLocalService;
-import com.liferay.batch.engine.service.BatchEngineImportTaskLocalService;
 import com.liferay.commerce.data.integration.model.CommerceDataIntegrationProcess;
 import com.liferay.commerce.data.integration.model.CommerceDataIntegrationProcessLog;
-import com.liferay.commerce.data.integration.service.CommerceDataIntegrationProcessLocalService;
-import com.liferay.commerce.data.integration.service.CommerceDataIntegrationProcessLogLocalService;
 import com.liferay.commerce.machine.learning.internal.gateway.CommerceMLGatewayClient;
 import com.liferay.commerce.machine.learning.internal.gateway.CommerceMLJobState;
 import com.liferay.commerce.machine.learning.internal.gateway.CommerceMLJobStateConstants;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.backgroundtask.BackgroundTaskConstants;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
@@ -41,13 +29,7 @@ import com.liferay.portal.kernel.util.UnicodeProperties;
 import java.io.File;
 import java.io.InputStream;
 
-import java.nio.file.Files;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
@@ -60,7 +42,8 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true,
 	service = BatchCommerceMLScheduledTaskExecutorService.class
 )
-public class BatchCommerceMLScheduledTaskExecutorService {
+public class BatchCommerceMLScheduledTaskExecutorService
+	extends BaseBatchCommerceMLScheduledTaskExecutorService {
 
 	public void executeScheduledTask(
 			long commerceDataIntegrationProcessId,
@@ -70,14 +53,14 @@ public class BatchCommerceMLScheduledTaskExecutorService {
 		throws PortalException {
 
 		CommerceDataIntegrationProcess commerceDataIntegrationProcess =
-			_commerceDataIntegrationProcessLocalService.
+			commerceDataIntegrationProcessLocalService.
 				getCommerceDataIntegrationProcess(
 					commerceDataIntegrationProcessId);
 
 		Date startDate = new Date();
 
 		CommerceDataIntegrationProcessLog commerceDataIntegrationProcessLog =
-			_commerceDataIntegrationProcessLogLocalService.
+			commerceDataIntegrationProcessLogLocalService.
 				addCommerceDataIntegrationProcessLog(
 					commerceDataIntegrationProcess.getUserId(),
 					commerceDataIntegrationProcess.
@@ -103,7 +86,7 @@ public class BatchCommerceMLScheduledTaskExecutorService {
 				_commerceMLGatewayClient.startCommerceMLJob(
 					typeSettingsProperties);
 
-			commerceDataIntegrationProcessLog = _appendToLogOutput(
+			commerceDataIntegrationProcessLog = appendToLogOutput(
 				commerceDataIntegrationProcessLog,
 				"Starting job: " + commerceMLJobState.getApplicationId());
 
@@ -112,7 +95,7 @@ public class BatchCommerceMLScheduledTaskExecutorService {
 					commerceMLJobState.getApplicationId(),
 					commerceDataIntegrationProcess.getTypeSettingsProperties());
 
-				commerceDataIntegrationProcessLog = _appendToLogOutput(
+				commerceDataIntegrationProcessLog = appendToLogOutput(
 					commerceDataIntegrationProcessLog,
 					"Completed job: " + commerceMLJobState.getApplicationId());
 
@@ -150,153 +133,25 @@ public class BatchCommerceMLScheduledTaskExecutorService {
 				BackgroundTaskConstants.STATUS_FAILED);
 		}
 
-		_commerceDataIntegrationProcessLogLocalService.
+		commerceDataIntegrationProcessLogLocalService.
 			updateCommerceDataIntegrationProcessLog(
 				commerceDataIntegrationProcessLog);
 	}
 
-	protected CommerceDataIntegrationProcessLog runExportTask(
-			CommerceDataIntegrationProcess commerceDataIntegrationProcess,
-			CommerceDataIntegrationProcessLog commerceDataIntegrationProcessLog,
-			BatchEngineTaskItemDelegateResourceMapper
-				batchEnginetaskItemDelegateResourceMapper)
+	protected void uploadExport(
+			BatchEngineExportTask batchEngineExportTask,
+			CommerceDataIntegrationProcess commerceDataIntegrationProcess)
 		throws Exception {
 
-		_appendToLogOutput(
-			commerceDataIntegrationProcessLog,
-			"Start exporting: " +
-				batchEnginetaskItemDelegateResourceMapper.getResourceName());
+		InputStream inputStream =
+			batchEngineExportTaskLocalService.openContentInputStream(
+				batchEngineExportTask.getBatchEngineExportTaskId());
 
-		BatchEngineExportTask batchEngineExportTask =
-			_batchEngineExportTaskLocalService.addBatchEngineExportTask(
-				commerceDataIntegrationProcess.getCompanyId(),
-				commerceDataIntegrationProcess.getUserId(), null,
-				batchEnginetaskItemDelegateResourceMapper.getResourceName(),
-				BatchEngineTaskContentType.JSONL.name(),
-				BatchEngineTaskExecuteStatus.INITIAL.name(), null,
-				new HashMap<>(),
-				batchEnginetaskItemDelegateResourceMapper.
-					getBatchEngineTaskItemDelegate());
+		_commerceMLGatewayClient.uploadCommerceMLJobResource(
+			batchEngineExportTask.getClassName(), inputStream,
+			commerceDataIntegrationProcess.getTypeSettingsProperties());
 
-		_batchEngineExportTaskExecutor.execute(batchEngineExportTask);
-
-		BatchEngineTaskExecuteStatus batchEngineTaskExecuteStatus =
-			BatchEngineTaskExecuteStatus.valueOf(
-				batchEngineExportTask.getExecuteStatus());
-
-		if (batchEngineTaskExecuteStatus.equals(
-				BatchEngineTaskExecuteStatus.COMPLETED)) {
-
-			if (_log.isDebugEnabled()) {
-				_log.debug(
-					"Batch Export process completed, uploading: " +
-						batchEngineExportTask.getClassName());
-			}
-
-			_appendToLogOutput(
-				commerceDataIntegrationProcessLog,
-				"Start uploading: " +
-					batchEnginetaskItemDelegateResourceMapper.
-						getResourceName());
-
-			_uploadExport(
-				batchEngineExportTask, commerceDataIntegrationProcess);
-
-			_batchEngineExportTaskLocalService.deleteBatchEngineExportTask(
-				batchEngineExportTask);
-		}
-		else {
-			throw new PortalException(
-				"Error exporting: " +
-					batchEnginetaskItemDelegateResourceMapper.
-						getResourceName());
-		}
-
-		return commerceDataIntegrationProcessLog;
-	}
-
-	protected CommerceDataIntegrationProcessLog runImportTask(
-			CommerceDataIntegrationProcess commerceDataIntegrationProcess,
-			CommerceDataIntegrationProcessLog commerceDataIntegrationProcessLog,
-			BatchEngineTaskItemDelegateResourceMapper
-				batchEnginetaskItemDelegateResourceMapper,
-			File resourceFile)
-		throws Exception {
-
-		_appendToLogOutput(
-			commerceDataIntegrationProcessLog,
-			"Start import task: " +
-				batchEnginetaskItemDelegateResourceMapper.getResourceName());
-
-		BatchEngineImportTask batchEngineImportTask =
-			_batchEngineImportTaskLocalService.addBatchEngineImportTask(
-				commerceDataIntegrationProcess.getCompanyId(),
-				commerceDataIntegrationProcess.getUserId(), 20, null,
-				batchEnginetaskItemDelegateResourceMapper.getResourceName(),
-				Files.readAllBytes(resourceFile.toPath()),
-				BatchEngineTaskContentType.JSONL.name(),
-				BatchEngineTaskExecuteStatus.INITIAL.name(),
-				batchEnginetaskItemDelegateResourceMapper.getFieldMapping(),
-				BatchEngineTaskOperation.CREATE.name(), null,
-				batchEnginetaskItemDelegateResourceMapper.
-					getBatchEngineTaskItemDelegate());
-
-		_batchEngineImportTaskExecutor.execute(batchEngineImportTask);
-
-		BatchEngineTaskExecuteStatus batchEngineTaskExecuteStatus =
-			BatchEngineTaskExecuteStatus.valueOf(
-				batchEngineImportTask.getExecuteStatus());
-
-		if (batchEngineTaskExecuteStatus.equals(
-				BatchEngineTaskExecuteStatus.COMPLETED)) {
-
-			if (_log.isDebugEnabled()) {
-				_log.debug(
-					"Batch Import process completed for entity: " +
-						batchEngineImportTask.getClassName());
-			}
-
-			_batchEngineImportTaskLocalService.deleteBatchEngineImportTask(
-				batchEngineImportTask);
-
-			_appendToLogOutput(
-				commerceDataIntegrationProcessLog,
-				"Completed import task: " +
-					batchEnginetaskItemDelegateResourceMapper.
-						getResourceName());
-		}
-		else {
-			throw new PortalException(
-				"Error importing resource: " +
-					batchEnginetaskItemDelegateResourceMapper.
-						getResourceName());
-		}
-
-		return commerceDataIntegrationProcessLog;
-	}
-
-	protected static final DateFormat dateFormat = new SimpleDateFormat(
-		"yyyy-MM-dd'T'HH:mm:ss.SSSZ");
-
-	private CommerceDataIntegrationProcessLog _appendToLogOutput(
-		CommerceDataIntegrationProcessLog commerceDataIntegrationProcessLog,
-		String message) {
-
-		StringBundler sb = new StringBundler(5);
-
-		sb.append(commerceDataIntegrationProcessLog.getOutput());
-		sb.append(dateFormat.format(new Date()));
-		sb.append(StringPool.SPACE);
-		sb.append(message);
-		sb.append(StringPool.NEW_LINE);
-
-		commerceDataIntegrationProcessLog.setOutput(sb.toString());
-
-		commerceDataIntegrationProcessLog.setEndDate(new Date());
-
-		return _commerceDataIntegrationProcessLogLocalService.
-			updateCommerceDataIntegrationProcessLog(
-				commerceDataIntegrationProcessLog);
+		inputStream.close();
 	}
 
 	private void _pollAndWait(
@@ -336,48 +191,10 @@ public class BatchCommerceMLScheduledTaskExecutorService {
 		throw new Exception("Timeout waiting for ML Job completion");
 	}
 
-	private void _uploadExport(
-			BatchEngineExportTask batchEngineExportTask,
-			CommerceDataIntegrationProcess commerceDataIntegrationProcess)
-		throws Exception {
-
-		InputStream inputStream =
-			_batchEngineExportTaskLocalService.openContentInputStream(
-				batchEngineExportTask.getBatchEngineExportTaskId());
-
-		_commerceMLGatewayClient.uploadCommerceMLJobResource(
-			batchEngineExportTask.getClassName(), inputStream,
-			commerceDataIntegrationProcess.getTypeSettingsProperties());
-
-		inputStream.close();
-	}
-
 	private static final int _MAX_POLL_COUNT = 180;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		BatchCommerceMLScheduledTaskExecutorService.class);
-
-	@Reference
-	private BatchEngineExportTaskExecutor _batchEngineExportTaskExecutor;
-
-	@Reference
-	private BatchEngineExportTaskLocalService
-		_batchEngineExportTaskLocalService;
-
-	@Reference
-	private BatchEngineImportTaskExecutor _batchEngineImportTaskExecutor;
-
-	@Reference
-	private BatchEngineImportTaskLocalService
-		_batchEngineImportTaskLocalService;
-
-	@Reference
-	private CommerceDataIntegrationProcessLocalService
-		_commerceDataIntegrationProcessLocalService;
-
-	@Reference
-	private CommerceDataIntegrationProcessLogLocalService
-		_commerceDataIntegrationProcessLogLocalService;
 
 	@Reference
 	private CommerceMLGatewayClient _commerceMLGatewayClient;
