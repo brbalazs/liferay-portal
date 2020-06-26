8223779e8a8284219a6f89c0b776d3e6eecdbebd
Jonathan McCann <jonathan.mccann@liferay.com>
2020-06-26 14:33:10 -0700

    LPS-116250 Do not update the password modified date otherwise the user will be logged out since their session is older than their password. Partially reverts the changes made by LPS-85179 since LPS-85179 is still reproducible even with those changes in place

--- modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImpl.java
+++ modules/apps/portal-security/portal-security-ldap-impl/src/main/java/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImpl.java
@@ -1685,11 +1685,9 @@ public class LDAPUserImporterImpl implements LDAPUserImporter, UserImporter {
 
 			if (passwordGenerated) {
 				user.setDigest(StringPool.BLANK);
+
+				_userLocalService.updateUser(user);
 			}
-
-			user.setPasswordModifiedDate(modifiedDate);
-
-			_userLocalService.updateUser(user);
 		}
 		finally {
 			ServiceContextThreadLocal.popServiceContext();
