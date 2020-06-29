5566bcd856886c7e1d17bc92f42085a7a45e1258
Kien Do <kien.do@liferay.com>
2020-06-29 16:03:21 -0700

    LRAC-6083 Add Delete function to Recommendation View Page

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/recommendations/pages/View.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/recommendations/pages/View.tsx
@@ -3,19 +3,44 @@ import OutputVersionsCard from '../components/OutputVersionsCard';
 import React from 'react';
 import TrainingItemsCard from '../components/TrainingItemsCard';
 import withRecommendation from 'shared/hoc/WithRecommendation';
+import {addAlert} from 'shared/actions/alerts';
+import {Alert, Modal, RouterType} from 'shared/types';
+import {close, modalTypes, open} from 'shared/actions/modals';
+import {compose} from 'redux';
+import {connect} from 'react-redux';
 import {Filter, Job} from '../utils/utils';
 import {get} from 'lodash';
 import {getRecommendations} from 'shared/util/breadcrumbs';
-import {RouterType} from 'shared/types';
+import {RECOMMENDATION_DELETE_MUTATION} from '../queries/RecommendationMutation';
 import {Routes, toRoute} from 'shared/util/router';
+import {sub} from 'shared/util/lang';
+import {useMutation} from '@apollo/react-hooks';
+import {withHistory} from 'shared/hoc';
 
 interface IViewProps {
+	addAlert: Alert.AddAlert;
+	close: Modal.close;
+	history: {
+		push: (string) => void;
+	};
 	job: Job;
+	open: Modal.open;
 	router: RouterType;
 }
 
-const View: React.FC<IViewProps> = ({job, router}) => {
+const View: React.FC<IViewProps> = ({
+	addAlert,
+	close,
+	history,
+	job,
+	open,
+	router
+}) => {
 	const {groupId, jobId} = router.params;
+
+	const [deleteRecommendationJobs] = useMutation(
+		RECOMMENDATION_DELETE_MUTATION
+	);
 
 	const itemFilters: Filter[] = get(job, 'parameters', []).filter(
 		({name}) => name !== 'includePreviousPeriod'
@@ -47,7 +72,78 @@ const View: React.FC<IViewProps> = ({job, router}) => {
 							label: Liferay.Language.get('edit')
 						},
 						{
-							label: Liferay.Language.get('delete')
+							label: Liferay.Language.get('delete'),
+							onClick: () => {
+								open(modalTypes.CONFIRMATION_MODAL, {
+									message: (
+										<div>
+											<h4 className='text-secondary'>
+												{sub(
+													Liferay.Language.get(
+														'delete-x-and-its-historical-training-output-data'
+													),
+													[name]
+												)}
+											</h4>
+
+											<p>
+												{Liferay.Language.get(
+													'components-using-this-model-will-need-to-be-reconfigured'
+												)}
+											</p>
+										</div>
+									),
+									modalVariant: 'modal-warning',
+									onClose: close,
+									onSubmit: () => {
+										deleteRecommendationJobs({
+											variables: {
+												jobIds: [jobId]
+											}
+										})
+											.then(() => {
+												addAlert({
+													alertType:
+														Alert.Types.SUCCESS,
+													message: sub(
+														Liferay.Language.get(
+															'x-has-been-deleted'
+														),
+														[name]
+													) as string
+												});
+
+												history.push(
+													toRoute(
+														Routes.SETTINGS_RECOMMENDATIONS,
+														{
+															groupId
+														}
+													)
+												);
+											})
+											.catch(() => {
+												addAlert({
+													alertType:
+														Alert.Types.ERROR,
+													message: Liferay.Language.get(
+														'there-was-an-error-processing-your-request.-please-try-again'
+													),
+													timeout: false
+												});
+											});
+									},
+									submitButtonDisplay: 'warning',
+									submitMessage: Liferay.Language.get(
+										'delete'
+									),
+									title: sub(
+										Liferay.Language.get('deleting-x'),
+										[name]
+									),
+									titleIcon: 'warning-full'
+								});
+							}
 						}
 					]}
 					pageActionsDisplayLimit={3}
@@ -64,4 +160,11 @@ const View: React.FC<IViewProps> = ({job, router}) => {
 	);
 };
 
-export default withRecommendation(View);
+export default compose<any>(
+	withRecommendation,
+	withHistory,
+	connect(
+		null,
+		{addAlert, close, open}
+	)
+)(View);
