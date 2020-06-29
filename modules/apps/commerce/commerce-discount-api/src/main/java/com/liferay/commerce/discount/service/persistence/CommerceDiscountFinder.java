f51eb3f596768c6b5ce5a43db6984108c8ba4bac
Riccardo <riccardo.alberti@liferay.com>
2020-06-29 21:14:57 +0200

    COMMERCE-4171 Autogenerated code (gw buildService) discount

--- modules/apps/commerce/commerce-discount-api/src/main/java/com/liferay/commerce/discount/service/persistence/CommerceDiscountFinder.java
+++ modules/apps/commerce/commerce-discount-api/src/main/java/com/liferay/commerce/discount/service/persistence/CommerceDiscountFinder.java
@@ -27,6 +27,10 @@ public interface CommerceDiscountFinder {
 		long commercePricingClassId, String title);
 
 	public java.util.List<com.liferay.commerce.discount.model.CommerceDiscount>
+		findByCommercePricingClassId(
+			long commercePricingClassId, String title, int start, int end);
+
+	public java.util.List<com.liferay.commerce.discount.model.CommerceDiscount>
 		findByUnqualifiedProduct(
 			long companyId, long cpDefinitionId, long[] assetCategoryIds,
 			long[] commercePricingClassIds);
@@ -52,10 +56,6 @@ public interface CommerceDiscountFinder {
 	public java.util.List<com.liferay.commerce.discount.model.CommerceDiscount>
 		findByAG_C_C_Order(
 			long[] commerceAccountGroupIds, String commerceDiscountTargetType);
-
-	public java.util.List<com.liferay.commerce.discount.model.CommerceDiscount>
-		findByCommercePricingClassId(
-			long commercePricingClassId, String title, int start, int end);
 
 	public java.util.List<com.liferay.commerce.discount.model.CommerceDiscount>
 		findByC_C_C_Product(
