79af06a07f6789df9903e4a8ae27f9a02ff2116c
Kien Do <kien.do@liferay.com>
2020-06-29 17:29:21 -0700

    LRAC-6083 Add delete recommendations function to RecommendationsList

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/recommendations/hocs/RecommendationList.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/recommendations/hocs/RecommendationList.tsx
@@ -10,7 +10,16 @@ import Label from 'shared/components/Label';
 import Nav from 'shared/components/Nav';
 import React from 'react';
 import RecommendationListQuery from '../queries/RecommendationListQuery';
+import {
+	ACTION_TYPES,
+	useSelectionContext,
+	withSelectionProvider
+} from 'shared/context/selection';
+import {addAlert} from 'shared/actions/alerts';
+import {Alert, Modal, RouterType} from 'shared/types';
+import {close, modalTypes, open} from 'shared/actions/modals';
 import {compose} from 'redux';
+import {connect} from 'react-redux';
 import {formatUTCDate} from 'shared/util/date';
 import {getFormattedTitle} from 'shared/components/NoResultsDisplay';
 import {
@@ -27,18 +36,28 @@ import {
 } from '../utils/utils';
 import {NAME} from 'shared/util/pagination';
 import {NameCell} from 'shared/components/table/cell-components';
-import {RouterType} from 'shared/types';
-import {Routes, toRoute} from 'shared/util/router';
+import {RECOMMENDATION_DELETE_MUTATION} from '../queries/RecommendationMutation';
+import {Routes, setUriQueryValues, toRoute} from 'shared/util/router';
+import {sub} from 'shared/util/lang';
+import {useMutation} from '@apollo/react-hooks';
 import {User} from 'shared/util/records';
-import {
-	useSelectionContext,
-	withSelectionProvider
-} from 'shared/context/selection';
-import {withCrossPageSelect, withCurrentUser} from 'shared/hoc';
+import {withCrossPageSelect, withCurrentUser, withHistory} from 'shared/hoc';
 
 const {
-	pagination: {orderDescending}
+	pagination: {cur: defaultPage, orderDescending}
 } = Constants;
+
+interface IRecommendationListProps {
+	addAlert: Alert.AddAlert;
+	close: Modal.close;
+	currentUser: User;
+	groupId: string;
+	history: {
+		push: (string) => void;
+	};
+	open: Modal.open;
+	router: RouterType;
+}
 
 const withData = () =>
 	graphql(RecommendationListQuery, {
@@ -51,6 +70,207 @@ const withData = () =>
 			total
 		}))
 	});
+
+const withQueryOptions = Component => ({
+	addAlert,
+	close,
+	currentUser,
+	history,
+	open,
+	refetch,
+	...otherProps
+}: IRecommendationListProps & {
+	delta: string;
+	groupId: string;
+	refetch: (options: {variables: {[key: string]: any}}) => Promise<any>;
+}) => {
+	const {selectedItems, selectionDispatch} = useSelectionContext();
+
+	const [deleteRecommendationJobs] = useMutation(
+		RECOMMENDATION_DELETE_MUTATION
+	);
+
+	const {delta, groupId} = otherProps;
+
+	const singleSelectedItem =
+		selectedItems.size === 1 ? selectedItems.first() : null;
+
+	const selectedItemsCount = selectedItems.size;
+
+	const confirmationMessage = singleSelectedItem
+		? sub(
+				Liferay.Language.get(
+					'delete-x-and-its-historical-training-output-data'
+				),
+				[singleSelectedItem.name]
+		  )
+		: sub(
+				Liferay.Language.get(
+					'delete-x-models-and-their-historical-training-output-data'
+				),
+				[selectedItemsCount]
+		  );
+
+	const handleSubmit = () => {
+		deleteRecommendationJobs({
+			variables: {
+				jobIds: selectedItems.map(({id}) => id).toArray()
+			}
+		})
+			.then(() => {
+				const successMessage = singleSelectedItem
+					? sub(Liferay.Language.get('x-has-been-deleted'), [
+							singleSelectedItem.name
+					  ])
+					: sub(Liferay.Language.get('x-models-have-been-deleted'), [
+							selectedItemsCount
+					  ]);
+
+				addAlert({
+					alertType: Alert.Types.SUCCESS,
+					message: successMessage as string
+				});
+
+				selectionDispatch({type: ACTION_TYPES.clearAll});
+
+				refetch({
+					variables: {
+						keywords: '',
+						size: delta,
+						sort: {
+							column: NAME,
+							type: orderDescending.toUpperCase()
+						},
+						start: 0
+					}
+				});
+
+				history.push(
+					setUriQueryValues(
+						{
+							keywords: '',
+							orderBy: orderDescending,
+							orderByField: NAME,
+							page: defaultPage
+						},
+						toRoute(Routes.SETTINGS_RECOMMENDATIONS, {
+							groupId
+						})
+					)
+				);
+			})
+			.catch(() => {
+				addAlert({
+					alertType: Alert.Types.ERROR,
+					message: Liferay.Language.get(
+						'there-was-an-error-processing-your-request.-please-try-again'
+					),
+					timeout: false
+				});
+			});
+	};
+
+	return (
+		<Component
+			{...otherProps}
+			renderNav={() => {
+				if (!currentUser.isAdmin()) {
+					return null;
+				}
+
+				if (selectedItemsCount) {
+					return (
+						<Nav>
+							<Nav.Item>
+								{
+									<Button
+										borderless
+										display='secondary'
+										onClick={() => {
+											open(
+												modalTypes.CONFIRMATION_MODAL,
+												{
+													message: (
+														<div>
+															<h4 className='text-secondary'>
+																{
+																	confirmationMessage
+																}
+															</h4>
+
+															<p>
+																{singleSelectedItem
+																	? Liferay.Language.get(
+																			'components-using-this-model-will-need-to-be-reconfigured'
+																	  )
+																	: Liferay.Language.get(
+																			'components-using-these-models-will-need-to-be-reconfigured'
+																	  )}
+															</p>
+														</div>
+													),
+													modalVariant:
+														'modal-warning',
+													onClose: close,
+													onSubmit: handleSubmit,
+													submitButtonDisplay:
+														'warning',
+													submitMessage: Liferay.Language.get(
+														'delete'
+													),
+													title: sub(
+														Liferay.Language.get(
+															'deleting-x'
+														),
+														[
+															singleSelectedItem
+																? singleSelectedItem.name
+																: sub(
+																		Liferay.Language.get(
+																			'x-models'
+																		),
+																		[
+																			selectedItemsCount
+																		]
+																  )
+														]
+													),
+													titleIcon: 'warning-full'
+												}
+											);
+										}}
+										outline
+									>
+										{Liferay.Language.get('delete')}
+									</Button>
+								}
+							</Nav.Item>
+						</Nav>
+					);
+				}
+
+				return (
+					<Nav>
+						<Nav.Item>
+							{
+								<Button
+									className='nav-btn'
+									display='primary'
+									href={toRoute(
+										Routes.SETTINGS_RECOMMENDATIONS_CREATE_ITEM_SIMILARITY_MODEL,
+										{groupId}
+									)}
+								>
+									{Liferay.Language.get('new-model')}
+								</Button>
+							}
+						</Nav.Item>
+					</Nav>
+				);
+			}}
+		/>
+	);
+};
 
 const RecommendationListWithData = withCrossPageSelect(withData, {
 	defaultOrderByField: NAME,
@@ -120,86 +340,34 @@ const RecommendationListWithData = withCrossPageSelect(withData, {
 	page: false,
 	primary: true,
 	rowIdentifier: 'id',
-	showDropdownRangeKey: false
+	showDropdownRangeKey: false,
+	withQueryOptions
 });
 
-interface IRecommendationListProps {
-	currentUser: User;
-	groupId: string;
-	router: RouterType;
-}
-
 const RecommendationList: React.FC<IRecommendationListProps> = ({
-	currentUser,
 	groupId,
 	router,
 	...otherProps
-}) => {
-	const {selectedItems} = useSelectionContext();
-
-	const renderNav = () => {
-		if (!currentUser.isAdmin()) {
-			return null;
-		}
-
-		if (selectedItems.size) {
-			return (
-				<Nav>
-					<Nav.Item>
-						{
-							<Button
-								borderless
-								display='secondary'
-								onClick={() => {
-									// TODO LRAC-6083 Add delete functionality
-								}}
-								outline
-							>
-								{Liferay.Language.get('delete')}
-							</Button>
-						}
-					</Nav.Item>
-				</Nav>
-			);
-		}
-
-		return (
-			<Nav>
-				<Nav.Item>
-					{
-						<Button
-							className='nav-btn'
-							display='primary'
-							href={toRoute(
-								Routes.SETTINGS_RECOMMENDATIONS_CREATE_ITEM_SIMILARITY_MODEL,
-								{groupId}
-							)}
-						>
-							{Liferay.Language.get('new-model')}
-						</Button>
-					}
-				</Nav.Item>
-			</Nav>
-		);
-	};
-
-	return (
-		<Card className='recommendations-list-root' pageDisplay>
-			<RecommendationListWithData
-				{...otherProps}
-				defaultOrderBy={orderDescending}
-				defaultOrderByField={NAME}
-				defaultSort={{field: NAME, sortOrder: orderDescending}}
-				entityLabel={Liferay.Language.get('recommendations')}
-				groupId={groupId}
-				renderNav={renderNav}
-				router={router}
-			/>
-		</Card>
-	);
-};
+}) => (
+	<Card className='recommendations-list-root' pageDisplay>
+		<RecommendationListWithData
+			{...otherProps}
+			defaultOrderBy={orderDescending}
+			defaultOrderByField={NAME}
+			defaultSort={{field: NAME, sortOrder: orderDescending}}
+			entityLabel={Liferay.Language.get('recommendations')}
+			groupId={groupId}
+			router={router}
+		/>
+	</Card>
+);
 
 export default compose<any>(
 	withCurrentUser,
-	withSelectionProvider
+	withSelectionProvider,
+	withHistory,
+	connect(
+		null,
+		{addAlert, close, open}
+	)
 )(RecommendationList);
