eb47a4c05e8cce29ab36c30f12f1476cb3eefe21
Kien Do <kien.do@liferay.com>
2020-07-01 12:18:10 -0700

    LRAC-6083 Remove unnecessary Provider

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/recommendations/components/__tests__/OutputVersionsCard.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/recommendations/components/__tests__/OutputVersionsCard.jsx
@@ -1,18 +1,16 @@
 import * as data from 'test/data';
-import mockStore from 'test/mock-store';
 import OutputVersionsCard from '../OutputVersionsCard';
 import React from 'react';
 import {jobTrainingFrequencies} from 'shared/util/constants';
 import {MockedProvider} from '@apollo/react-testing';
 import {mockRecommendationJobRunsReq} from 'test/graphql-data';
-import {Provider} from 'react-redux';
 import {render} from '@testing-library/react';
 import {waitForLoading} from 'test/helpers';
 
 jest.unmock('react-dom');
 
 describe('OutputVersionsCard', () => {
-	it('should render', async() => {
+	it('should render', async () => {
 		const {container} = render(
 			<MockedProvider
 				mocks={[
@@ -21,12 +19,10 @@ describe('OutputVersionsCard', () => {
 					])
 				]}
 			>
-				<Provider store={mockStore()}>
-					<OutputVersionsCard
-						router={{params: {jobId: '321'}}}
-						trainingFrequency={jobTrainingFrequencies.every14Days}
-					/>
-				</Provider>
+				<OutputVersionsCard
+					router={{params: {jobId: '321'}}}
+					trainingFrequency={jobTrainingFrequencies.every14Days}
+				/>
 			</MockedProvider>
 		);
 
