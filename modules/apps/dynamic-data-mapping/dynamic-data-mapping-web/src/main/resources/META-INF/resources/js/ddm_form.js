844a1ecb2b30947def34f6e9f1873f6040123f43
Laszlo Pap <laszlo.pap@liferay.com>
2020-07-06 13:56:57 +0200

    LPS-116001 Can't create web content if certain DDM fields' name contains diacritic characters

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -1358,7 +1358,7 @@ AUI.add(
 
 						var parsedValue = instance.getParsedValue(instance.getValue());
 
-						var titleNode = A.one('#' + instance.getInputName() + 'Title');
+						var titleNode = A.one('input[name=' + instance.getInputName() + 'Title]');
 
 						titleNode.val(parsedValue.title || '');
 
@@ -1488,7 +1488,7 @@ AUI.add(
 
 						clearButtonNode.attr('disabled', readOnly);
 
-						var altNode = container.one('#' + instance.getInputName() + 'Alt');
+						var altNode = container.one('input[name=' + instance.getInputName() + 'Alt]');
 
 						if (altNode) {
 							altNode.set('readOnly', readOnly);
@@ -1587,7 +1587,7 @@ AUI.add(
 
 						var parsedValue = instance.getParsedValue(instance.getValue());
 
-						var titleNode = A.one('#' + instance.getInputName() + 'Title');
+						var titleNode = A.one('input[name=' + instance.getInputName() + 'Title]');
 
 						var parsedTitleMap = instance.getParsedValue(
 							parsedValue.titleMap
@@ -1895,7 +1895,7 @@ AUI.add(
 
 						var inputName = instance.getInputName();
 
-						var layoutNameNode = container.one('#' + inputName + 'LayoutName');
+						var layoutNameNode = container.one('input[name=' + inputName + 'LayoutName]');
 
 						var parsedValue = instance.getParsedValue(value);
 
@@ -2839,11 +2839,11 @@ AUI.add(
 
 						var notEmpty = instance.isNotEmpty(parsedValue);
 
-						var altNode = A.one('#' + instance.getInputName() + 'Alt');
+						var altNode = A.one('input[name=' + instance.getInputName() + 'Alt]');
 
 						altNode.attr('disabled', !notEmpty);
 
-						var titleNode = A.one('#' + instance.getInputName() + 'Title');
+						var titleNode = A.one('input[name=' + instance.getInputName() + 'Title]');
 
 						if (notEmpty) {
 							altNode.val(parsedValue.alt || '');
@@ -2925,7 +2925,7 @@ AUI.add(
 						var parsedValue = instance.getParsedValue(ImageField.superclass.getValue.apply(instance, arguments));
 
 						if (instance.isNotEmpty(parsedValue)) {
-							var altNode = A.one('#' + instance.getInputName() + 'Alt');
+							var altNode = A.one('input[name=' + instance.getInputName() + 'Alt]');
 
 							parsedValue.alt = altNode.val();
 
@@ -2956,7 +2956,7 @@ AUI.add(
 								parsedValue.name = parsedValue.title;
 							}
 
-							var altNode = A.one('#' + instance.getInputName() + 'Alt');
+							var altNode = A.one('input[name=' + instance.getInputName() + 'Alt]');
 
 							altNode.val(parsedValue.alt);
 
@@ -3083,7 +3083,7 @@ AUI.add(
 							)
 						);
 
-						var locationNode = A.one('#' + inputName + 'Location');
+						var locationNode = A.one('input[name=' + inputName + 'Location]');
 
 						locationNode.html(event.newVal.address);
 					}
