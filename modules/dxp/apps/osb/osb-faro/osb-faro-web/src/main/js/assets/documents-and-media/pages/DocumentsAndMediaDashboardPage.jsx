9f8a7943ee4025fd3d72a455deced5d9b843df3a
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-07-08 18:28:05 -0300

    LRAC-6007 Update all usage of DropdownRangeKey under "Documents and Media" to use the new version.

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/assets/documents-and-media/pages/DocumentsAndMediaDashboardPage.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/assets/documents-and-media/pages/DocumentsAndMediaDashboardPage.jsx
@@ -17,6 +17,7 @@ export default function DocumentsAndMediaDashboardPage() {
 				<div className='col-sm-12'>
 					<MetricsCard
 						label={Liferay.Language.get('visitors-behavior')}
+						legacyDropdownRangeKey={false}
 					/>
 				</div>
 			</div>
@@ -24,6 +25,7 @@ export default function DocumentsAndMediaDashboardPage() {
 				<div className='col-sm-12'>
 					<AudienceReportCard
 						label={Liferay.Language.get('audience')}
+						legacyDropdownRangeKey={false}
 					/>
 				</div>
 			</div>
@@ -31,12 +33,14 @@ export default function DocumentsAndMediaDashboardPage() {
 				<div className='col-lg-6 col-md-12'>
 					<LocationsCard
 						label={Liferay.Language.get('downloads-by-location')}
+						legacyDropdownRangeKey={false}
 						metricLabel={Liferay.Language.get('downloads')}
 					/>
 				</div>
 				<div className='col-lg-6 col-md-12'>
 					<DevicesCard
 						label={Liferay.Language.get('downloads-by-technology')}
+						legacyDropdownRangeKey={false}
 						metricLabel={Liferay.Language.get('downloads')}
 					/>
 				</div>
@@ -46,6 +50,7 @@ export default function DocumentsAndMediaDashboardPage() {
 					<TouchpointsListCard
 						assetType={'DOCUMENT'}
 						label={Liferay.Language.get('asset-appears-on')}
+						legacyDropdownRangeKey={false}
 					/>
 				</div>
 			</div>
