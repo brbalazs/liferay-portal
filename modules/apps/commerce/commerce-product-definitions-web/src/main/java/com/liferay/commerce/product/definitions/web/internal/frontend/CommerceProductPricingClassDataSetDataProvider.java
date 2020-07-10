e066477d0f5ae843213082bd975a3120a77b1eeb
Riccardo <riccardo.alberti@liferay.com>
2020-07-10 15:16:52 +0200

    COMMERCE-4149 Use correct method names in data set provider

--- modules/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/frontend/CommerceProductPricingClassDataSetDataProvider.java
+++ modules/apps/commerce/commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/frontend/CommerceProductPricingClassDataSetDataProvider.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
+ * <p>
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- *
+ * <p>
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -81,9 +81,10 @@ public class CommerceProductPricingClassDataSetDataProvider
 			httpServletRequest, "cpDefinitionId");
 
 		List<CommercePricingClass> commercePricingClasses =
-			_commercePricingClassService.searchByCPDefinitionId(
-				cpDefinitionId, filter.getKeywords(),
-				pagination.getStartPosition(), pagination.getEndPosition());
+			_commercePricingClassService.
+				searchCommercePricingClassesByCPDefinitionId(
+					cpDefinitionId, filter.getKeywords(),
+					pagination.getStartPosition(), pagination.getEndPosition());
 
 		for (CommercePricingClass commercePricingClass :
 				commercePricingClasses) {
