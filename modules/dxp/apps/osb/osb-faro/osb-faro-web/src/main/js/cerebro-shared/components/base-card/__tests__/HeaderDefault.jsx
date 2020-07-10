e620fb473db93ab2489e169d3ba2dde9e863d42f
Francisco Dias <francisco.dias@liferay.com>
2020-07-10 12:23:46 -0300

    LRAC-5998 Convert HeaderDefault test to rtl

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/cerebro-shared/components/base-card/__tests__/HeaderDefault.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/cerebro-shared/components/base-card/__tests__/HeaderDefault.jsx
@@ -1,30 +1,39 @@
 import HeaderDefault from '../HeaderDefault';
 import React from 'react';
+import {fireEvent, render} from '@testing-library/react';
 import {INTERVAL_KEY_MAP} from 'shared/util/time';
-import {LAST_24_HOURS} from 'shared/util/constants';
-import {shallow} from 'enzyme';
+import {MockedProvider} from '@apollo/react-testing';
+import {mockTimeRangeReq} from 'test/graphql-data';
 
+jest.unmock('react-dom');
+
+const DefaultComponent = props => (
+	<MockedProvider mocks={[mockTimeRangeReq()]}>
+		<HeaderDefault label={'Title'} {...props} />
+	</MockedProvider>
+);
 describe('HeaderDefault', () => {
 	it('should render', () => {
-		const component = shallow(<HeaderDefault />);
+		const {container} = render(<DefaultComponent />);
 
-		expect(component).toMatchSnapshot();
+		jest.runAllTimers();
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should call the onChangeInterval prop fn with "day" if the rangekey is changed to an hourly value', () => {
 		const spy = jest.fn();
-		const component = shallow(
-			<HeaderDefault
+		const {getByText} = render(
+			<DefaultComponent
 				onChangeInterval={spy}
 				rangeSelectors={{rangeKey: '30'}}
 				showInterval
 			/>
 		);
 
-		component
-			.find('Apollo(Component)')
-			.props()
-			.onChange(LAST_24_HOURS);
+		jest.runAllTimers();
+
+		fireEvent.click(getByText('Last 24 hours'));
 
 		expect(spy).toHaveBeenCalledWith(INTERVAL_KEY_MAP.day);
 	});
