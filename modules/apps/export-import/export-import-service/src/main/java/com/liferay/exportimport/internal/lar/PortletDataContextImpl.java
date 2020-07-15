3a12d868143fcb4e1fe59b6c672744c29267dfbe
shuyangzhou <shuyang.zhou@liferay.com>
2020-07-15 16:34:15 -0700

    LPS-116452 Simplify

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/lar/PortletDataContextImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/lar/PortletDataContextImpl.java
@@ -454,14 +454,8 @@ public class PortletDataContextImpl implements PortletDataContext {
 
 	@Override
 	public boolean addPrimaryKey(Class<?> clazz, String primaryKey) {
-		boolean value = hasPrimaryKey(clazz, primaryKey);
-
-		if (!value) {
-			_primaryKeys.add(
-				getPrimaryKeyString(clazz, (Serializable)primaryKey));
-		}
-
-		return value;
+		return !_primaryKeys.add(
+			getPrimaryKeyString(clazz, (Serializable)primaryKey));
 	}
 
 	/**
@@ -3176,8 +3170,8 @@ public class PortletDataContextImpl implements PortletDataContext {
 	private transient List<Layout> _newLayouts;
 	private final Map<String, Map<?, ?>> _newPrimaryKeysMaps = new HashMap<>();
 	private final Set<String> _notUniquePerLayout = new HashSet<>();
-	private long _oldPlid;
 	private Map<String, Object> _objectsMap = new HashMap<>();
+	private long _oldPlid;
 	private Map<String, String[]> _parameterMap;
 	private final Map<String, List<KeyValuePair>> _permissionsMap =
 		new HashMap<>();
