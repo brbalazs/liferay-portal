5c6b591e67a587617e82133a49634b297fa0813a
balazssk <balazs.safrany-kovalik@liferay.com>
2020-07-20 09:33:27 +0200

    LPS-92704 Account for future simplification in size estimation

--- portal-kernel/src/com/liferay/layouts/admin/kernel/util/Sitemap.java
+++ portal-kernel/src/com/liferay/layouts/admin/kernel/util/Sitemap.java
@@ -32,6 +32,8 @@ import java.util.Map;
 @ProviderType
 public interface Sitemap {
 
+	public static final int MAXIMUM_NUMBER_OF_ENTRIES = 50_000;
+
 	public void addURLElement(
 		Element element, String url, UnicodeProperties typeSettingsProperties,
 		Date modifiedDate, String canonicalURL,
