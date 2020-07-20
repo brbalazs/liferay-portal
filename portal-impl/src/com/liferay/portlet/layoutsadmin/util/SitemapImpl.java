ebd64ab96ce6a9fe77dde7cea448c9d4542d261e
balazssk <balazs.safrany-kovalik@liferay.com>
2020-07-20 10:31:56 +0200

    LPS-92704 Count the default link as well

--- portal-impl/src/com/liferay/portlet/layoutsadmin/util/SitemapImpl.java
+++ portal-impl/src/com/liferay/portlet/layoutsadmin/util/SitemapImpl.java
@@ -274,7 +274,7 @@ public class SitemapImpl implements Sitemap {
 
 			int availableLocalesSize = availableLocales.size();
 
-			offset = availableLocalesSize * _XHTML_ATTRIBUTE.length;
+			offset = (availableLocalesSize + 1) * _XHTML_ATTRIBUTE.length;
 
 			offset += _XMLNS_ATTRIBUTE.length;
 		}
