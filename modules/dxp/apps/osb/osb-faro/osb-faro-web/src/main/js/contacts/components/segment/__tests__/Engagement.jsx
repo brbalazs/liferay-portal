c3ae9c85e1cf1359e2b35034b068c6d508a4d8b2
Francisco Dias <francisco.dias@liferay.com>
2020-07-21 12:46:50 -0300

    LRAC-6308 Update test

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/segment/__tests__/Engagement.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/segment/__tests__/Engagement.jsx
@@ -1,47 +1,71 @@
+import Constants from 'shared/util/constants';
 import React from 'react';
 import SegmentEngagementWithList, {SegmentEngagementChart} from '../Engagement';
 import {getTimestamp} from 'test/data';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
 
-const mockEngagementData = [
+const {entityTypes} = Constants;
+
+const MOCK_DATA = [
 	{
+		contributors: 1,
 		intervalInitDate: getTimestamp(-1),
 		scoreAvg: 2
 	},
 	{
+		contributors: 3,
 		intervalInitDate: getTimestamp(),
 		scoreAvg: 7
 	}
 ];
 
+const MOCK_COLUMNS = [
+	{
+		accessor: 'name',
+		label: Liferay.Language.get('name'),
+		sortable: false,
+		title: true
+	},
+	{
+		accessor: 'emailAddress',
+		label: Liferay.Language.get('email'),
+		sortable: false
+	},
+	{
+		accessor: 'score',
+		label: Liferay.Language.get('engagement')
+	}
+];
+
+jest.unmock('react-dom');
+
 describe('SegmentEngagementWithList', () => {
 	it('should render', () => {
-		const component = shallow(
-			<SegmentEngagementWithList
-				data={mockEngagementData}
-				groupId={'23'}
-				id={'3'}
-				previousScore={1}
-				score={7}
-			/>
+		const {container} = render(
+			<StaticRouter>
+				<SegmentEngagementWithList
+					columns={MOCK_COLUMNS}
+					data={MOCK_DATA}
+					entityType={entityTypes.individualsSegment}
+					groupId={'23'}
+					id={'3'}
+					previousScore={1}
+					score={7}
+				/>
+			</StaticRouter>
 		);
 
-		expect(component).toBeTruthy();
+		jest.runAllTimers();
+
+		expect(container).toMatchSnapshot();
 	});
 });
 
 describe('SegmentEngagementChart', () => {
 	it('should render', () => {
-		const component = shallow(
-			<SegmentEngagementChart
-				data={mockEngagementData}
-				groupId={'23'}
-				id={'3'}
-				previousScore={1}
-				score={7}
-			/>
-		);
+		const {container} = render(<SegmentEngagementChart data={MOCK_DATA} />);
 
-		expect(component).toBeTruthy();
+		expect(container).toMatchSnapshot();
 	});
 });
