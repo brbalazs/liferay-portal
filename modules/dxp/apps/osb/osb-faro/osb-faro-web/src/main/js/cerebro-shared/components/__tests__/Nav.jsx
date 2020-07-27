5b373c3493e9a04f6d55c5bb9fd462a7105ffb87
Francisco Dias <francisco.dias@liferay.com>
2020-07-27 09:44:35 -0300

    LRAC-6178 Remove snapshots in Nav

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/cerebro-shared/components/__tests__/Nav.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/cerebro-shared/components/__tests__/Nav.jsx
@@ -1,51 +1,72 @@
 import Nav from '../Nav';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
+
+const MOCK_ITEMS = [
+	<Nav.Item active href='#' key={1}>
+		{'foo'}
+	</Nav.Item>,
+	<Nav.Item key={2}>{'bar'}</Nav.Item>,
+	<Nav.Item key={3}>{'baz'}</Nav.Item>
+];
 
 describe('Nav', () => {
-	const items = [
-		<Nav.Item active href='#' key={1}>
-			{'foo'}
-		</Nav.Item>,
-		<Nav.Item key={2}>{'bar'}</Nav.Item>,
-		<Nav.Item key={3}>{'baz'}</Nav.Item>
-	];
-
 	it('should render', () => {
-		const component = shallow(<Nav />);
-		expect(component).toMatchSnapshot();
+		const {container} = render(<Nav />);
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render vertically', () => {
-		const component = shallow(<Nav vertical />);
+		const {container} = render(<Nav vertical />);
 
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('.flex-column')).toBeTruthy();
 	});
 
 	it('should render with items', () => {
-		const component = shallow(<Nav children={items} />);
+		const {getByText} = render(
+			<StaticRouter>
+				<Nav children={MOCK_ITEMS} />
+			</StaticRouter>
+		);
 
-		expect(component).toMatchSnapshot();
+		MOCK_ITEMS.forEach(({props: {children}}) => {
+			expect(getByText(children)).toBeTruthy();
+		});
 	});
 
 	it('should render with underline class', () => {
-		const component = shallow(<Nav children={items} display='underline' />);
+		const {container} = render(
+			<StaticRouter>
+				<Nav display='underline' />
+			</StaticRouter>
+		);
 
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('.nav-underline')).toBeTruthy();
 	});
 });
 
 describe('Nav.Item', () => {
 	it('should render', () => {
-		const component = shallow(<Nav.Item children='Child' />);
-		expect(component).toMatchSnapshot();
+		const {container} = render(<Nav.Item children='Child' />);
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render as monospaced', () => {
-		const component = shallow(
-			<Nav.Item children='Child' href='www.liferay.com' linkMonospaced />
+		const {container} = render(
+			<StaticRouter>
+				<Nav.Item
+					children='Child'
+					href='www.liferay.com'
+					linkMonospaced
+				/>
+			</StaticRouter>
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('.nav-link-monospaced')).toBeTruthy();
 	});
 });
