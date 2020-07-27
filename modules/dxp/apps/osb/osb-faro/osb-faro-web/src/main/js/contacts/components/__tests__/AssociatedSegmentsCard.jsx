a65d243c1f96545444c70f17bf2cc961953cd3f1
Francisco Dias <francisco.dias@liferay.com>
2020-07-27 12:04:37 -0300

    LRAC-6178 Remove snapshots in AssociatedSegmentsCard

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/__tests__/AssociatedSegmentsCard.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/__tests__/AssociatedSegmentsCard.jsx
@@ -2,68 +2,63 @@ import * as data from 'test/data';
 import AssociatedSegmentsCard from '../AssociatedSegmentsCard';
 import Promise from 'metal-promise';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
+
+const DefaultComponent = props => (
+	<StaticRouter>
+		<AssociatedSegmentsCard
+			dataSourceFn={() =>
+				Promise.resolve(data.mockSearch(data.mockSegment, 2))
+			}
+			groupId='23'
+			id='123'
+			pageUrl='/foo'
+			{...props}
+		/>
+	</StaticRouter>
+);
 
 describe('AssociatedSegmentsCard', () => {
 	it('should render', () => {
-		const component = shallow(
-			<AssociatedSegmentsCard
-				dataSourceFn={() =>
-					Promise.resolve(data.mockSearch(data.mockSegment, 2))
-				}
-				groupId={'23'}
-				id={'123'}
-				pageUrl={'/foo'}
-			/>
-		);
+		const {container} = render(<DefaultComponent />);
 
 		jest.runAllTimers();
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render w/ loading overlay', () => {
-		const component = shallow(
-			<AssociatedSegmentsCard
-				dataSourceFn={() => Promise.resolve({})}
-				groupId={'23'}
-				id={'123'}
-				pageUrl={'/foo'}
-			/>
+		const {container} = render(
+			<DefaultComponent dataSourceFn={() => Promise.resolve({})} />
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('.spinner-overlay')).toBeTruthy();
 	});
 
 	it('should render with an error display', () => {
-		const component = shallow(
-			<AssociatedSegmentsCard
-				dataSourceFn={() => Promise.reject({})}
-				groupId={'23'}
-				id={'123'}
-				pageUrl={'/foo'}
-			/>
+		const {getByText} = render(
+			<DefaultComponent dataSourceFn={() => Promise.reject({})} />
 		);
 
 		jest.runAllTimers();
 
-		expect(component).toMatchSnapshot();
+		expect(getByText('An unexpected error occurred.')).toBeTruthy();
 	});
 
 	it('should render with an no results display', () => {
-		const component = shallow(
-			<AssociatedSegmentsCard
+		const {getByText} = render(
+			<DefaultComponent
 				dataSourceFn={() =>
 					Promise.resolve(data.mockSearch(data.mockSegment, 0))
 				}
-				groupId={'23'}
-				id={'123'}
-				pageUrl={'/foo'}
 			/>
 		);
 
 		jest.runAllTimers();
 
-		expect(component).toMatchSnapshot();
+		expect(getByText('There are no Segments found.')).toBeTruthy();
 	});
 });
