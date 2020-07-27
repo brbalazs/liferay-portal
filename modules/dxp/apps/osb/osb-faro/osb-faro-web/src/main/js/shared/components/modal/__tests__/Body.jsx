ea79f3845a2d683c221b9b1fdd005d899be6e9ec
Masterfoni <joao.lima@liferay.com>
2020-07-27 14:06:00 -0300

    LRAC-6178 Improve modal body tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/modal/__tests__/Body.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/modal/__tests__/Body.jsx
@@ -10,4 +10,20 @@ describe('Modal Body', () => {
 
 		expect(container).toMatchSnapshot();
 	});
+
+	it('should render with scroller', () => {
+		const {container} = render(<Body inlineScroller />);
+
+		expect(container.querySelector('.inline-scroller')).toBeTruthy();
+	});
+
+	it('should render with children', () => {
+		const {queryByText} = render(
+			<Body>
+				<span>{'Body content'}</span>
+			</Body>
+		);
+
+		expect(queryByText('Body content')).toBeTruthy();
+	});
 });
