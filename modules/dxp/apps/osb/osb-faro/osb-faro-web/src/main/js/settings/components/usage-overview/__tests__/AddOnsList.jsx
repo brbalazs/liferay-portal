47358542374b1d7b5ae4d6a4aa4cd55c7a95e840
Masterfoni <joao.lima@liferay.com>
2020-07-28 11:40:43 -0300

    LRAC-6178 Remove snapshots in AddOnsList

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/usage-overview/__tests__/AddOnsList.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/usage-overview/__tests__/AddOnsList.jsx
@@ -7,39 +7,31 @@ import {render} from '@testing-library/react';
 
 jest.unmock('react-dom');
 
+const DefaultComponent = props => (
+	<AddOnsList
+		currentPlan={new Plan(fromJS(mockPlan()))}
+		planType='enterprise'
+		{...props}
+	/>
+);
+
 describe('AddOnsList', () => {
 	it('should render', () => {
-		const {container} = render(
-			<AddOnsList
-				currentPlan={new Plan(fromJS(mockPlan()))}
-				planType={'enterprise'}
-			/>
-		);
+		const {container} = render(<DefaultComponent />);
 
 		expect(container).toMatchSnapshot();
 	});
 
 	it('should render as inactive when the active prop is false', () => {
-		const {container} = render(
-			<AddOnsList
-				active={false}
-				currentPlan={new Plan(fromJS(mockPlan()))}
-				planType={'enterprise'}
-			/>
-		);
+		const {container} = render(<DefaultComponent active={false} />);
 
-		expect(container).toMatchSnapshot();
+		expect(container.querySelector('.inactive')).toBeTruthy();
 	});
 
 	it('should render with the quantity of addons subscribed if any', () => {
-		const {container} = render(
-			<AddOnsList
-				active={false}
-				currentPlan={new Plan(fromJS(mockPlan()))}
-				planType={'enterprise'}
-			/>
-		);
+		const {queryByText} = render(<DefaultComponent />);
 
-		expect(container).toMatchSnapshot();
+		expect(queryByText('2X')).toBeTruthy();
+		expect(queryByText('1X')).toBeTruthy();
 	});
 });
