50503a52eb40872045186bf0d03451cf92bafaf2
Masterfoni <joao.lima@liferay.com>
2020-07-28 15:55:32 -0300

    LRAC-6178 Improve FormNavigation tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/__tests__/FormNavigation.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/__tests__/FormNavigation.jsx
@@ -10,4 +10,22 @@ describe('FormNavigation', () => {
 
 		expect(container).toMatchSnapshot();
 	});
+
+	it('should render with a custom submit text', () => {
+		const submitMessage = 'Submit me';
+
+		const {getByText} = render(
+			<FormNavigation submitMessage={submitMessage} />
+		);
+
+		expect(getByText(submitMessage)).toBeTruthy();
+	});
+
+	it('should render with a previous button', () => {
+		const {getByText} = render(
+			<FormNavigation onPreviousStep={() => jest.fn()} />
+		);
+
+		expect(getByText('Previous')).toBeTruthy();
+	});
 });
