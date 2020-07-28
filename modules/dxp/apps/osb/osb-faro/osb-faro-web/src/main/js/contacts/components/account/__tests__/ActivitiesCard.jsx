e2ef769630300de90f37508e2649b2045a05767c
Francisco Dias <francisco.dias@liferay.com>
2020-07-28 15:34:35 -0300

    LRAC-6178 Remove snapshots in ActivitiesCard

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/account/__tests__/ActivitiesCard.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/account/__tests__/ActivitiesCard.jsx
@@ -4,57 +4,66 @@ import ActivitiesCard from '../ActivitiesCard';
 import Promise from 'metal-promise';
 import React from 'react';
 import {Account} from 'shared/util/records';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
 
 describe('ActivitiesCard', () => {
 	it('should render', () => {
-		const component = shallow(
-			<ActivitiesCard
-				account={data.getImmutableMock(
-					Account,
-					data.mockAccount,
-					'test'
-				)}
-				groupId={'23'}
-			/>
-		);
-
-		expect(component).toMatchSnapshot();
-	});
-
-	it('should render w/o loading', () => {
-		const component = shallow(
-			<ActivitiesCard
-				account={data.getImmutableMock(
-					Account,
-					data.mockAccount,
-					'test'
-				)}
-				groupId={'23'}
-			/>
+		const {container} = render(
+			<StaticRouter>
+				<ActivitiesCard
+					account={data.getImmutableMock(
+						Account,
+						data.mockAccount,
+						'test'
+					)}
+					groupId='23'
+				/>
+			</StaticRouter>
 		);
 
 		jest.runAllTimers();
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
+	});
+
+	it('should render w/ loading', () => {
+		const {container} = render(
+			<StaticRouter>
+				<ActivitiesCard
+					account={data.getImmutableMock(
+						Account,
+						data.mockAccount,
+						'test'
+					)}
+					groupId='23'
+				/>
+			</StaticRouter>
+		);
+
+		expect(container.querySelector('.spinner-root')).toBeTruthy();
 	});
 
 	it('should render w/ ErrorDisplay', () => {
 		API.activities.fetchHistory.mockReturnValueOnce(Promise.reject({}));
 
-		const component = shallow(
-			<ActivitiesCard
-				account={data.getImmutableMock(
-					Account,
-					data.mockAccount,
-					'test'
-				)}
-				groupId={'23'}
-			/>
+		const {getByText} = render(
+			<StaticRouter>
+				<ActivitiesCard
+					account={data.getImmutableMock(
+						Account,
+						data.mockAccount,
+						'test'
+					)}
+					groupId='23'
+				/>
+			</StaticRouter>
 		);
 
 		jest.runAllTimers();
 
-		expect(component).toMatchSnapshot();
+		expect(getByText('An unexpected error occurred.')).toBeTruthy();
 	});
 });
