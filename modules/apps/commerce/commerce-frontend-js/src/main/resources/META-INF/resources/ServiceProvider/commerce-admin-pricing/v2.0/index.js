5f32c2d2f95b207b11c0e0e734613d8ec36f78a2
Alessio Rendina <asilcomando@gmail.com>
2020-07-28 20:28:01 +0200

    COMMERCE-3077 Added service providers

--- modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/ServiceProvider/commerce-admin-pricing/v2.0/index.js
+++ modules/apps/commerce/commerce-frontend-js/src/main/resources/META-INF/resources/ServiceProvider/commerce-admin-pricing/v2.0/index.js
@@ -15,8 +15,15 @@
 export {default as discountAPI} from './Discount';
 export {default as discountAccountAPI} from './DiscountAccount';
 export {default as discountAccountGroupAPI} from './DiscountAccountGroup';
+export {default as discountCategoryAPI} from './DiscountCategory';
 export {default as discountChannelAPI} from './DiscountChannel';
-export {default as discountRuleAPI} from './DiscountRule';
 export {default as discountProductAPI} from './DiscountProduct';
 export {default as discountProductGroupAPI} from './DiscountProductGroup';
-export {default as discountCategoryAPI} from './DiscountCategory';
+export {default as discountRuleAPI} from './DiscountRule';
+export {default as priceEntryAPI} from './PriceEntry';
+export {default as priceListAccountAPI} from './PriceListAccount';
+export {default as priceListAccountGroupAPI} from './PriceListAccountGroup';
+export {default as priceListChannelAPI} from './PriceListChannel';
+export {default as priceModifierCategoryAPI} from './PriceModifierCategory';
+export {default as priceModifierProductAPI} from './PriceModifierProduct';
+export {default as priceModifierProductGroupAPI} from './PriceModifierProductGroup';
