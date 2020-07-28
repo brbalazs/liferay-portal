8acaa8850105019339f5924a35426ef3111cccac
Masterfoni <joao.lima@liferay.com>
2020-07-28 10:36:40 -0300

    LRAC-6178 Remove snapshots in SubscriptionTitle

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/usage-overview/__tests__/SubscriptionTitle.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/usage-overview/__tests__/SubscriptionTitle.jsx
@@ -4,21 +4,25 @@ import {render} from '@testing-library/react';
 
 jest.unmock('react-dom');
 
+const DefaultComponent = props => (
+	<SubscriptionTitle name='Business' price={750} {...props} />
+);
+
 describe('SubscriptionTitle', () => {
 	
 	it('should render', () => {
-		const {container} = render(
-			<SubscriptionTitle name={'Business'} price={750} />
-		);
+		const {container} = render(<DefaultComponent />);
 
 		expect(container).toMatchSnapshot();
 	});
 
 	it('should render with a label', () => {
-		const {container} = render(
-			<SubscriptionTitle labelText={'1x'} name={'Business'} price={750} />
+		const textToMatch = '1x';
+
+		const {getByText} = render(
+			<DefaultComponent labelText={textToMatch} />
 		);
 
-		expect(container).toMatchSnapshot();
+		expect(getByText(textToMatch)).toBeTruthy();
 	});
 });
