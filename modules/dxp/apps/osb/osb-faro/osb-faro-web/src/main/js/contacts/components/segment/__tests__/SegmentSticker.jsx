bcd5bf640622e6bc518cc397eb5cc5f6cacf1da6
Masterfoni <joao.lima@liferay.com>
2020-07-28 09:41:53 -0300

    LRAC-6178 Remove snapshots in SegmentSticker

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/segment/__tests__/SegmentSticker.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/segment/__tests__/SegmentSticker.jsx
@@ -1,32 +1,39 @@
 import FaroConstants from 'shared/util/constants';
 import React from 'react';
 import SegmentSticker from '../SegmentSticker';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
 
 const {segmentStates, segmentTypes} = FaroConstants;
 
 describe('SegmentSticker', () => {
 	it('should render', () => {
-		const component = shallow(
+		const {container} = render(
 			<SegmentSticker segmentType={segmentTypes.static} />
 		);
-
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render with a dynamic segment icon', () => {
-		const component = shallow(
+		const {container} = render(
 			<SegmentSticker segmentType={segmentTypes.dynamic} />
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('use')).toHaveAttribute(
+			'xlink:href',
+			'#individual-dynamic-segment'
+		);
 	});
 
 	it('should render with a disabled segment icon', () => {
-		const component = shallow(
+		const {container} = render(
 			<SegmentSticker state={segmentStates.disabled} />
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('use')).toHaveAttribute(
+			'xlink:href',
+			'#warning'
+		);
 	});
 });
