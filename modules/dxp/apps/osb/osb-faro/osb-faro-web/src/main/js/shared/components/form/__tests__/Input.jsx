5fac2688b62318d3bfd4122b076d96f753a5db4b
Masterfoni <joao.lima@liferay.com>
2020-07-29 19:25:10 -0300

    LRAC-6178 Improve form->input tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/form/__tests__/Input.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/form/__tests__/Input.jsx
@@ -5,21 +5,35 @@ import {render} from '@testing-library/react';
 
 jest.unmock('react-dom');
 
+const DefaultComponent = props => (
+	<Input
+		field={{name: 'foo'}}
+		form={mockForm()}
+		label='Input label'
+		mask={[]}
+		{...props}
+	/>
+);
+
 describe('Input', () => {
 	
 	it('should render', () => {
 		const {container} = render(
-			<Input field={{name: 'foo'}} form={mockForm()} />
+			<Input field={{name: 'foo'}} form={mockForm()} inline />
 		);
 
 		expect(container).toMatchSnapshot();
 	});
 
-	it('should render a masked input', () => {
-		const {container} = render(
-			<Input field={{name: 'foo'}} form={mockForm()} mask={[]} />
-		);
+	it('should render a labelled input', () => {
+		const {queryByText} = render(<DefaultComponent />);
 
-		expect(container).toMatchSnapshot();
+		expect(queryByText('Input label')).toBeTruthy();
+	});
+
+	it('should render a required input', () => {
+		const {queryByText} = render(<DefaultComponent required />);
+
+		expect(queryByText('Input label')).toHaveClass('required');
 	});
 });
