72b9f72d291ebc5501a55597e57a206ed8707eed
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2020-07-29 19:36:31 -0300

    LPS-121803 portal-search-elasticsearch6-impl: Encapsulate divergences in bulkable document translator subsystem
    + (improves source code parity across versions)
    
    https://issues.liferay.com/browse/LPS-121803
    
    Co-authored-by: Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
    Co-authored-by: André de Oliveira <andre.oliveira@liferay.com>
    Co-authored-by: Bryan Engler <bryan.engler@liferay.com>
    Co-authored-by: kevinccruz <kevin.cruz@liferay.com>

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/search/engine/adapter/document/ElasticsearchBulkableDocumentRequestTranslator.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/search/engine/adapter/document/ElasticsearchBulkableDocumentRequestTranslator.java
@@ -14,253 +14,74 @@
 
 package com.liferay.portal.search.elasticsearch6.internal.search.engine.adapter.document;
 
-import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.search.document.Document;
-import com.liferay.portal.search.elasticsearch6.internal.connection.ElasticsearchClientResolver;
-import com.liferay.portal.search.elasticsearch6.internal.document.ElasticsearchDocumentFactory;
 import com.liferay.portal.search.engine.adapter.document.BulkableDocumentRequestTranslator;
 import com.liferay.portal.search.engine.adapter.document.DeleteDocumentRequest;
 import com.liferay.portal.search.engine.adapter.document.GetDocumentRequest;
 import com.liferay.portal.search.engine.adapter.document.IndexDocumentRequest;
 import com.liferay.portal.search.engine.adapter.document.UpdateDocumentRequest;
 
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.delete.DeleteRequestBuilder;
 import org.elasticsearch.action.get.GetRequestBuilder;
 import org.elasticsearch.action.index.IndexRequestBuilder;
-import org.elasticsearch.action.support.WriteRequest;
-import org.elasticsearch.action.support.WriteRequestBuilder;
 import org.elasticsearch.action.update.UpdateRequestBuilder;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.common.xcontent.XContentBuilder;
-import org.elasticsearch.common.xcontent.XContentType;
-
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Michael C. Han
+ * @author Adam Brandizzi
  */
-@Component(
-	property = "search.engine.impl=Elasticsearch",
-	service = BulkableDocumentRequestTranslator.class
-)
-public class ElasticsearchBulkableDocumentRequestTranslator
-	implements BulkableDocumentRequestTranslator {
+public interface ElasticsearchBulkableDocumentRequestTranslator
+	extends BulkableDocumentRequestTranslator
+		<DeleteRequestBuilder, IndexRequestBuilder, UpdateRequestBuilder,
+		 BulkRequestBuilder> {
 
-	@Override
 	public DeleteRequestBuilder translate(
-		DeleteDocumentRequest deleteDocumentRequest) {
+		DeleteDocumentRequest deleteDocumentRequest);
 
-		Client client = _elasticsearchClientResolver.getClient(false);
+	/**
+	 * @deprecated As of Mueller (7.2.x), replaced by {@link
+	 *             #translate(UpdateDocumentRequest)}
+	 */
+	@Deprecated
+	@Override
+	public default DeleteRequestBuilder translate(
+		DeleteDocumentRequest deleteDocumentRequest,
+		BulkRequestBuilder bulkRequestBuilder) {
 
-		DeleteRequestBuilder deleteRequestBuilder = client.prepareDelete();
-
-		_setRefreshPolicy(
-			deleteRequestBuilder, deleteDocumentRequest.isRefresh());
-
-		return deleteRequestBuilder.setId(
-			deleteDocumentRequest.getUid()
-		).setIndex(
-			deleteDocumentRequest.getIndexName()
-		).setType(
-			_getType(deleteDocumentRequest.getType())
-		);
+		return translate(deleteDocumentRequest);
 	}
 
-	@Override
-	public GetRequestBuilder translate(GetDocumentRequest getDocumentRequest) {
-		Client client = _elasticsearchClientResolver.getClient(
-			getDocumentRequest.isPreferLocalCluster());
+	public GetRequestBuilder translate(GetDocumentRequest getDocumentRequest);
 
-		GetRequestBuilder getRequestBuilder = client.prepareGet();
-
-		return getRequestBuilder.setId(
-			getDocumentRequest.getId()
-		).setIndex(
-			getDocumentRequest.getIndexName()
-		).setRefresh(
-			getDocumentRequest.isRefresh()
-		).setFetchSource(
-			getDocumentRequest.getFetchSourceIncludes(),
-			getDocumentRequest.getFetchSourceExcludes()
-		).setStoredFields(
-			getDocumentRequest.getStoredFields()
-		).setType(
-			_getType(getDocumentRequest.getType())
-		);
-	}
-
-	@Override
 	public IndexRequestBuilder translate(
-		IndexDocumentRequest indexDocumentRequest) {
+		IndexDocumentRequest indexDocumentRequest);
 
-		Client client = _elasticsearchClientResolver.getClient(false);
-
-		IndexRequestBuilder indexRequestBuilder = client.prepareIndex();
-
-		_setRefreshPolicy(
-			indexRequestBuilder, indexDocumentRequest.isRefresh());
-		_setSource(indexRequestBuilder, indexDocumentRequest);
-
-		return indexRequestBuilder.setId(
-			_getUid(indexDocumentRequest)
-		).setIndex(
-			indexDocumentRequest.getIndexName()
-		).setType(
-			_getType(indexDocumentRequest.getType())
-		);
-	}
-
+	/**
+	 * @deprecated As of Mueller (7.2.x), replaced by {@link
+	 *             #translate(UpdateDocumentRequest)}
+	 */
+	@Deprecated
 	@Override
+	public default IndexRequestBuilder translate(
+		IndexDocumentRequest indexDocumentRequest,
+		BulkRequestBuilder bulkRequestBuilder) {
+
+		return translate(indexDocumentRequest);
+	}
+
 	public UpdateRequestBuilder translate(
-		UpdateDocumentRequest updateDocumentRequest) {
+		UpdateDocumentRequest updateDocumentRequest);
 
-		Client client = _elasticsearchClientResolver.getClient(false);
+	/**
+	 * @deprecated As of Mueller (7.2.x), replaced by {@link
+	 *             #translate(UpdateDocumentRequest)}
+	 */
+	@Deprecated
+	@Override
+	public default UpdateRequestBuilder translate(
+		UpdateDocumentRequest updateDocumentRequest,
+		BulkRequestBuilder bulkRequestBuilder) {
 
-		UpdateRequestBuilder updateRequestBuilder = client.prepareUpdate();
-
-		_setDoc(updateRequestBuilder, updateDocumentRequest);
-		_setRefreshPolicy(
-			updateRequestBuilder, updateDocumentRequest.isRefresh());
-
-		return updateRequestBuilder.setId(
-			_getUid(updateDocumentRequest)
-		).setIndex(
-			updateDocumentRequest.getIndexName()
-		).setType(
-			_getType(updateDocumentRequest.getType())
-		);
+		return translate(updateDocumentRequest);
 	}
-
-	@Reference(unbind = "-")
-	protected void setElasticsearchClientResolver(
-		ElasticsearchClientResolver elasticsearchClientResolver) {
-
-		_elasticsearchClientResolver = elasticsearchClientResolver;
-	}
-
-	@Reference(unbind = "-")
-	protected void setElasticsearchDocumentFactory(
-		ElasticsearchDocumentFactory elasticsearchDocumentFactory) {
-
-		_elasticsearchDocumentFactory = elasticsearchDocumentFactory;
-	}
-
-	private String _getType(String type) {
-		if (type != null) {
-			return type;
-		}
-
-		return "_doc";
-	}
-
-	private String _getUid(IndexDocumentRequest indexDocumentRequest) {
-		String uid = indexDocumentRequest.getUid();
-
-		if (!Validator.isBlank(uid)) {
-			return uid;
-		}
-
-		if (indexDocumentRequest.getDocument() != null) {
-			Document document = indexDocumentRequest.getDocument();
-
-			return document.getString(Field.UID);
-		}
-
-		@SuppressWarnings("deprecation")
-		com.liferay.portal.kernel.search.Document document =
-			indexDocumentRequest.getDocument71();
-
-		Field field = document.getField(Field.UID);
-
-		if (field != null) {
-			return field.getValue();
-		}
-
-		return uid;
-	}
-
-	private String _getUid(UpdateDocumentRequest updateDocumentRequest) {
-		String uid = updateDocumentRequest.getUid();
-
-		if (!Validator.isBlank(uid)) {
-			return uid;
-		}
-
-		if (updateDocumentRequest.getDocument() != null) {
-			Document document = updateDocumentRequest.getDocument();
-
-			return document.getString(Field.UID);
-		}
-
-		@SuppressWarnings("deprecation")
-		com.liferay.portal.kernel.search.Document document =
-			updateDocumentRequest.getDocument71();
-
-		Field field = document.getField(Field.UID);
-
-		if (field != null) {
-			uid = field.getValue();
-		}
-
-		return uid;
-	}
-
-	private void _setDoc(
-		UpdateRequestBuilder updateRequestBuilder,
-		UpdateDocumentRequest updateDocumentRequest) {
-
-		if (updateDocumentRequest.getDocument() != null) {
-			XContentBuilder xContentBuilder =
-				_elasticsearchDocumentFactory.getElasticsearchDocument(
-					updateDocumentRequest.getDocument());
-
-			updateRequestBuilder.setDoc(xContentBuilder);
-		}
-		else {
-			@SuppressWarnings("deprecation")
-			String elasticsearchDocument =
-				_elasticsearchDocumentFactory.getElasticsearchDocument(
-					updateDocumentRequest.getDocument71());
-
-			updateRequestBuilder.setDoc(
-				elasticsearchDocument, XContentType.JSON);
-		}
-	}
-
-	private void _setRefreshPolicy(
-		WriteRequestBuilder writeRequestBuilder, boolean refresh) {
-
-		if (refresh) {
-			writeRequestBuilder.setRefreshPolicy(
-				WriteRequest.RefreshPolicy.IMMEDIATE);
-		}
-	}
-
-	private void _setSource(
-		IndexRequestBuilder indexRequestBuilder,
-		IndexDocumentRequest indexDocumentRequest) {
-
-		if (indexDocumentRequest.getDocument() != null) {
-			XContentBuilder xContentBuilder =
-				_elasticsearchDocumentFactory.getElasticsearchDocument(
-					indexDocumentRequest.getDocument());
-
-			indexRequestBuilder.setSource(xContentBuilder);
-		}
-		else {
-			@SuppressWarnings("deprecation")
-			String elasticsearchDocument =
-				_elasticsearchDocumentFactory.getElasticsearchDocument(
-					indexDocumentRequest.getDocument71());
-
-			indexRequestBuilder.setSource(
-				elasticsearchDocument, XContentType.JSON);
-		}
-	}
-
-	private ElasticsearchClientResolver _elasticsearchClientResolver;
-	private ElasticsearchDocumentFactory _elasticsearchDocumentFactory;
 
 }
