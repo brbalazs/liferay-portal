e59b32fc9d7bfa6c31e3502a8a794a7d81e144e4
Alessio Rendina <asilcomando@gmail.com>
2020-07-29 12:55:58 +0200

    COMMERCE-3077 Fixed localization issues

--- modules/apps/commerce/commerce-pricing-web/src/main/java/com/liferay/commerce/pricing/web/internal/servlet/taglib/ui/CommercePriceListQualifiersScreenNavigationEntry.java
+++ modules/apps/commerce/commerce-pricing-web/src/main/java/com/liferay/commerce/pricing/web/internal/servlet/taglib/ui/CommercePriceListQualifiersScreenNavigationEntry.java
@@ -30,11 +30,13 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -71,8 +73,11 @@ public class CommercePriceListQualifiersScreenNavigationEntry
 
 	@Override
 	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
 		return LanguageUtil.get(
-			locale,
+			resourceBundle,
 			CommercePriceListScreenNavigationConstants.CATEGORY_KEY_QUALIFIERS);
 	}
 
