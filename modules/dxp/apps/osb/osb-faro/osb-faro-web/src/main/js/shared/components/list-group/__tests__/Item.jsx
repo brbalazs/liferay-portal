851b0763ffd5f211e6fee9dce15747a2b16d98f4
Masterfoni <joao.lima@liferay.com>
2020-07-29 16:50:01 -0300

    LRAC-6178 Remove Snapshot in Item tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/list-group/__tests__/Item.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/list-group/__tests__/Item.jsx
@@ -12,9 +12,32 @@ describe('Item', () => {
 		expect(container).toMatchSnapshot();
 	});
 
+	it('should render with children', () => {
+		const childrenContent = 'Children';
+
+		const {queryByText} = render(<Item children={childrenContent} />);
+
+		expect(queryByText(childrenContent)).toBeTruthy();
+	});
+
+	it('should render a header item', () => {
+		const {container} = render(<Item header />);
+
+		expect(container.querySelector('.list-group-header')).toBeTruthy();
+		expect(container.querySelector('.list-group-item')).toBeFalsy();
+	});
+
+	it('should render as disabled', () => {
+		const {container} = render(<Item disabled />);
+
+		expect(
+			container.querySelector('.list-group-item-disabled')
+		).toBeTruthy();
+	});
+
 	it('should render as active', () => {
 		const {container} = render(<Item active />);
 
-		expect(container).toMatchSnapshot();
+		expect(container.querySelector('.active')).toBeTruthy();
 	});
 });
