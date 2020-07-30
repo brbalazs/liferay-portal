ef0e3e6c306d3b2e75748ebe5a2debb52eeab349
Laszlo Pap <laszlo.pap@liferay.com>
2020-07-30 20:50:52 +0200

    LPS-117578 SF

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/StagingImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/staging/StagingImpl.java
@@ -3910,7 +3910,8 @@ public class StagingImpl implements Staging {
 		if (startCalendar.before(currentCalendar)) {
 			SchedulerException schedulerException = new SchedulerException();
 
-			schedulerException.setType(SchedulerException.TYPE_INVALID_START_DATE);
+			schedulerException.setType(
+				SchedulerException.TYPE_INVALID_START_DATE);
 
 			throw schedulerException;
 		}
