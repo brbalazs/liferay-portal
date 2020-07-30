c6055b46b8c41a16a72ce3f681a835d2b1e2dec7
Francisco Dias <francisco.dias@liferay.com>
2020-07-30 16:52:41 -0300

    LRAC-6178 Update Details snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/pages/account/__tests__/Details.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/pages/account/__tests__/Details.jsx
@@ -3,18 +3,23 @@ import Details from '../Details';
 import React from 'react';
 import {Account} from 'shared/util/records';
 import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
 
 jest.unmock('react-dom');
 
 describe('AccountDetails', () => {
-	it('should render', () => {
+	it('should render', async() => {
 		const {container} = render(
-			<Details
-				account={data.getImmutableMock(Account, data.mockAccount)}
-				groupId={'23'}
-				id={'test'}
-			/>
+			<StaticRouter>
+				<Details
+					account={data.getImmutableMock(Account, data.mockAccount)}
+					groupId='23'
+					id='test'
+					/>
+			</StaticRouter>
 		);
+
+		jest.runAllTimers();
 
 		expect(container).toMatchSnapshot();
 	});
