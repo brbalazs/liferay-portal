aa0ae1242def35d8e165d75694feb873aa75add0
Francisco Dias <francisco.dias@liferay.com>
2020-07-31 12:58:22 -0300

    LRAC-6178 Fix LiferayOld TabRoutes test

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/liferay-old/__tests__/TabRoutes.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/liferay-old/__tests__/TabRoutes.jsx
@@ -1,16 +1,34 @@
+import * as data from 'test/data';
 import LiferayTabRoutes from '../TabRoutes';
+import mockStore from 'test/mock-store';
 import React from 'react';
+import {DataSource} from 'shared/util/records';
+import {Provider} from 'react-redux';
 import {render} from '@testing-library/react';
+import {Routes, toRoute} from 'shared/util/router';
 import {StaticRouter} from 'react-router';
 
 jest.unmock('react-dom');
 
+const defaultProps = {
+	dataSource: data.getImmutableMock(DataSource, data.mockLiferayDataSource),
+	groupId: 'foo',
+	id: 'test'
+};
+
 describe('LiferayTabRoutes', () => {
 	it('should render', () => {
 		const {container} = render(
-			<StaticRouter>
-				<LiferayTabRoutes />
-			</StaticRouter>
+			<Provider store={mockStore()}>
+				<StaticRouter
+					location={toRoute(
+						Routes.SETTINGS_DATA_SOURCE,
+						defaultProps
+					)}
+				>
+					<LiferayTabRoutes {...defaultProps} />
+				</StaticRouter>
+			</Provider>
 		);
 
 		expect(container).toMatchSnapshot();
