3567f4b5425ec9d3874773a3c08deecee27644a1
Masterfoni <joao.lima@liferay.com>
2020-07-31 14:21:07 -0300

    LRAC-6178 Improve Salesforce tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/__tests__/Salesforce.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/__tests__/Salesforce.jsx
@@ -1,12 +1,17 @@
 import * as data from 'test/data';
 import FaroConstants from 'shared/util/constants';
+import mockStore from 'test/mock-store';
 import React from 'react';
 import Salesforce from '../Salesforce';
 import {DataSource, User} from 'shared/util/records';
-import {render} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import {render, waitForElementToBeRemoved} from '@testing-library/react';
+import {Routes, toRoute} from 'shared/util/router';
 import {StaticRouter} from 'react-router';
 
 jest.unmock('react-dom');
+
+jest.useRealTimers();
 
 const {
 	userRoleNames: {administrator}
@@ -23,11 +28,22 @@ const defaultProps = {
 };
 
 describe('Salesforce', () => {
-	it('should render', () => {
+	it('should render', async() => {
 		const {container} = render(
-			<StaticRouter>
-				<Salesforce {...defaultProps} />
-			</StaticRouter>
+			<Provider store={mockStore()}>
+				<StaticRouter
+					location={toRoute(
+						Routes.SETTINGS_DATA_SOURCE,
+						defaultProps
+					)}
+				>
+					<Salesforce {...defaultProps} />
+				</StaticRouter>
+			</Provider>
+		);
+
+		await waitForElementToBeRemoved(() =>
+			container.querySelector('.spinner-root')
 		);
 
 		expect(container).toMatchSnapshot();
