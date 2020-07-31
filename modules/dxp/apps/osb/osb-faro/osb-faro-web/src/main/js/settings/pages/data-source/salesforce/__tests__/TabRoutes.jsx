59748e507e8768af428d24f755b39704916552f6
Francisco Dias <francisco.dias@liferay.com>
2020-07-31 14:37:04 -0300

    LRAC-6178 Fix Salesforce TabRoutes

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/salesforce/__tests__/TabRoutes.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/salesforce/__tests__/TabRoutes.jsx
@@ -1,16 +1,43 @@
+import * as data from 'test/data';
+import Constants from 'shared/util/constants';
+import mockStore from 'test/mock-store';
 import React from 'react';
 import SalesforceTabRoutes from '../TabRoutes';
+import {DataSource, User} from 'shared/util/records';
+import {Provider} from 'react-redux';
 import {render} from '@testing-library/react';
+import {Routes, toRoute} from 'shared/util/router';
 import {StaticRouter} from 'react-router';
 
+const {
+	userRoleNames: {administrator}
+} = Constants;
+
 jest.unmock('react-dom');
+
+const defaultProps = {
+	currentUser: new User({roleName: administrator}),
+	dataSource: data.getImmutableMock(
+		DataSource,
+		data.mockSalesforceDataSource
+	),
+	groupId: '23',
+	id: 'test'
+};
 
 describe('SalesforceTabRoutes', () => {
 	it('should render', () => {
 		const {container} = render(
-			<StaticRouter>
-				<SalesforceTabRoutes />
-			</StaticRouter>
+			<Provider store={mockStore()}>
+				<StaticRouter
+					location={toRoute(
+						Routes.SETTINGS_DATA_SOURCE,
+						defaultProps
+					)}
+				>
+					<SalesforceTabRoutes {...defaultProps} />
+				</StaticRouter>
+			</Provider>
 		);
 
 		expect(container).toMatchSnapshot();
