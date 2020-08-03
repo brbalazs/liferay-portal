183bab0a5c60ce90ddc9451ab8568345b6974b4f
Vendel Toreki <vendel.toreki@liferay.com>
2020-08-03 19:04:15 +0200

    LPS-116519 Use the new schema version

--- modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/portlet/ExportImportPortlet.java
+++ modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/portlet/ExportImportPortlet.java
@@ -71,7 +71,7 @@ public class ExportImportPortlet extends MVCPortlet {
 	}
 
 	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.exportimport.web)(release.schema.version=1.0.0))",
+		target = "(&(release.bundle.symbolic.name=com.liferay.exportimport.web)(release.schema.version=1.0.1))",
 		unbind = "-"
 	)
 	protected void setRelease(Release release) {
