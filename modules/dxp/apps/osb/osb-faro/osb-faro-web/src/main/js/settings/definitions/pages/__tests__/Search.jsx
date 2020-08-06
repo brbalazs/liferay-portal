6caaada382faf312a00de180464cf5352a783daf
Masterfoni <joao.lima@liferay.com>
2020-08-06 10:10:13 -0300

    LRAC-5993 Create Search component tests

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/pages/__tests__/Search.jsx
@@ -0,0 +1,22 @@
+import mockStore from 'test/mock-store';
+import React from 'react';
+import Search from '../Search';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
+
+describe('InterestTopics', () => {
+	it('should render', () => {
+        const {container} = render(
+            <Provider store={mockStore()}>
+                <StaticRouter>
+                    <Search groupId='23' />
+                </StaticRouter>
+            </Provider>
+        );
+
+		expect(container).toMatchSnapshot();
+	});
+});
