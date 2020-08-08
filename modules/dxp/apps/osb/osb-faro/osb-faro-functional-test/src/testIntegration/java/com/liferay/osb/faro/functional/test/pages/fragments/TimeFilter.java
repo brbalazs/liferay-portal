d03ce748948d788a471a7c92587d6dc6325c0fc9
Cheryl Tang <cheryl.tang@liferay.com>
2020-08-07 18:35:11 -0700

    LRAC-6429 Add step to assert invalid date picker Custom Range

--- modules/dxp/apps/osb/osb-faro/osb-faro-functional-test/src/testIntegration/java/com/liferay/osb/faro/functional/test/pages/fragments/TimeFilter.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-functional-test/src/testIntegration/java/com/liferay/osb/faro/functional/test/pages/fragments/TimeFilter.java
@@ -25,10 +25,25 @@ import cucumber.api.java.en.Then;
 public class TimeFilter {
 
 	/**
+	 * Asserts that the Custom Range date picker dropdown shows an error
+	 * message when a date range exceeding 365 days is selected.
+	 *
+	 * @throws Exception
+	 */
+	@Then("^I should see an error saying the range exceeds the maximum range$")
+	public void assertMaximumCustomRange() throws Exception {
+		_faroSelenium.assertText(
+			"//div[@class='range-warning']",
+			"This exceeds the maximum range of 365 days.");
+	}
+
+	/**
 	 * Asserts tht the time filter buttons are disabled.
+	 *
+	 * @throws Exception
 	 */
 	@Then("^I should see (?:that )?the time filter is disabled$")
-	public void iShouldSeeTheTimeFilterIsDisabled() throws Exception {
+	public void assertTimeFilterIsDisabled() throws Exception {
 		_faroSelenium.assertElementPresent("//button[text()='D'][@disabled]");
 		_faroSelenium.assertElementPresent("//button[text()='W'][@disabled]");
 		_faroSelenium.assertElementPresent("//button[text()='M'][@disabled]");
