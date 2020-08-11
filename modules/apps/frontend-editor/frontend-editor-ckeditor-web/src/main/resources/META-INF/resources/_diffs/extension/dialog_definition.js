a3a98fd8cdd64a07556d3592be8a97247c6ff933
Norbert Nemeth <norbert.nemeth@liferay.com>
2020-08-11 11:16:45 +0200

    LPS-118703 Avoid usage of arrow functions

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/extension/dialog_definition.js
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/resources/META-INF/resources/_diffs/extension/dialog_definition.js
@@ -39,10 +39,10 @@ CKEDITOR.on('dialogDefinition', function(event) {
 			dialog.move(x, y, false);
 		};
 
-		AUI().use('aui-debounce', (A) => {
+		AUI().use('aui-debounce', function (A) {
 			boundingWindow.on(
 				'resize',
-				A.debounce(() => {
+				A.debounce(function () {
 					centerDialog();
 				}, 250)
 			);
