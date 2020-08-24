8a88c58475a4dbc9073a84d2cff268bf1d17be9a
Kevin Lee <kevin.lee@liferay.com>
2020-08-24 12:08:05 -0700

    LPS-119672 Partial backport of LPS-105924: The closest polyfill method is a dependency of LPS-112180
    Original commit message: LPS-105924 Add closest polyfill 53f67fa113e5116fbeb7774ffe64ecbcff918463

--- modules/apps/frontend-compatibility/frontend-compatibility-ie/src/main/java/com/liferay/frontend/compatibility/ie/internal/servlet/taglib/IETopHeadDynamicInclude.java
+++ modules/apps/frontend-compatibility/frontend-compatibility-ie/src/main/java/com/liferay/frontend/compatibility/ie/internal/servlet/taglib/IETopHeadDynamicInclude.java
@@ -74,7 +74,7 @@ public class IETopHeadDynamicInclude extends BaseDynamicInclude {
 	}
 
 	private static final String[] _FILE_NAMES = {
-		"core-js-bundle.min.js", "fetch.js", "svg.contains.js"
+		"/closest.js", "core-js-bundle.min.js", "fetch.js", "svg.contains.js"
 	};
 
 	@Reference
