ce1ab4bf4664fed39a4f6cd8d514a9b0fe050bd9
Roland Pákai <roland.pakai@liferay.com>
2020-09-07 15:44:33 +0200

    LPS-120523 Fix object short notation to stay compatible with IE11

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/xml_formatter.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/xml_formatter.js
@@ -29,8 +29,8 @@ AUI.add(
 					var lineIndent = instance.get('lineIndent');
 
 					return Liferay.Util.formatXML(content, {
-						tagIndent,
-						lineIndent
+						tagIndent: tagIndent,
+						lineIndent: lineIndent
 					});
 				}
 			}
