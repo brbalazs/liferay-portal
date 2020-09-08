de638499ccf07907bbe2180f480342af04943ff0
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-09-08 10:05:58 +0200

    LPS-120527 Expire article if article was manually expired.

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/exportimport/data/handler/JournalArticleStagedModelDataHandler.java
@@ -871,6 +871,12 @@ public class JournalArticleStagedModelDataHandler
 				article.setStatus(WorkflowConstants.STATUS_EXPIRED);
 			}
 
+			boolean expired = false;
+
+			if (article.getStatus() == WorkflowConstants.STATUS_EXPIRED) {
+				expired = true;
+			}
+
 			if ((article.getStatus() != WorkflowConstants.STATUS_APPROVED) &&
 				(article.getStatus() != WorkflowConstants.STATUS_SCHEDULED)) {
 
@@ -988,6 +994,13 @@ public class JournalArticleStagedModelDataHandler
 				serviceContext.getAssetTagNames(),
 				serviceContext.getAssetLinkEntryIds(),
 				serviceContext.getAssetPriority());
+
+			if (expired && !importedArticle.isExpired()) {
+				_journalArticleLocalService.expireArticle(
+					userId, importedArticle.getGroupId(),
+					importedArticle.getArticleId(),
+					importedArticle.getVersion(), articleURL, serviceContext);
+			}
 
 			serviceContext.setModifiedDate(importedArticle.getModifiedDate());
 
