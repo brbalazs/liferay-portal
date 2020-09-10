636d0e7058080eb09ebc5b8d9be38a0451103aea
Brian Chan <brian.chan@liferay.com>
2020-09-10 11:37:48 -0700

    LPS-102001 regen

--- modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/serdes/v1_0/ContentSetElementSerDes.java
+++ modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/serdes/v1_0/ContentSetElementSerDes.java
@@ -230,9 +230,8 @@ public class ContentSetElementSerDes {
 						(Object)jsonParserFieldValue);
 				}
 			}
-			else {
-				throw new IllegalArgumentException(
-					"Unsupported field name " + jsonParserFieldName);
+			else if (jsonParserFieldName.equals("status")) {
+				throw new IllegalArgumentException();
 			}
 		}
 
