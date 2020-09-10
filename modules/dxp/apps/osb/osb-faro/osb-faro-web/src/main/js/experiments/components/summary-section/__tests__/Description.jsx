ed3d305d259e24cfcf7e315838101a47ce7c0a56
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-09-10 15:33:02 -0300

    LRAC-6634 Convert to RTL and update snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-section/__tests__/Description.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-section/__tests__/Description.jsx
@@ -1,14 +1,16 @@
 import Description from '../Description';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
 
 describe('SummarySection Description', () => {
 	it('should render component', () => {
-		const component = shallow(<Description value={100} />);
+		const {container, getByText} = render(<Description value={100} />);
 
-		expect(
-			component.hasClass('analytics-summary-section-description')
-		).toBeTruthy();
-		expect(component).toMatchSnapshot();
+		expect(getByText('100')).toHaveClass(
+			'analytics-summary-section-description'
+		);
+		expect(container).toMatchSnapshot();
 	});
 });
