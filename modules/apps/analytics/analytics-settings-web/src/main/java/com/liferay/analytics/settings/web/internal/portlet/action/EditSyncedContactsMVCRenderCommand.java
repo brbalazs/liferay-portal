da7c11aff345909fd97f0ac1762f0eff57355f22
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2020-09-09 22:41:02 -0700

    LRAC-6435 add MVCRenderCommands

--- modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/portlet/action/EditSyncedContactsMVCRenderCommand.java
+++ modules/apps/analytics/analytics-settings-web/src/main/java/com/liferay/analytics/settings/web/internal/portlet/action/EditSyncedContactsMVCRenderCommand.java
@@ -23,7 +23,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Francisco Dias
+ * @author Rachael Koestartyo
  */
 @Component(
 	property = {
