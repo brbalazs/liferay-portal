dd19779f83d7e5726e78d2e0feb540b0f1b213a7
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-09-10 11:32:23 -0300

    LRAC-6633 Convert to RTL and Update snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/pages/account/__tests__/List.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/pages/account/__tests__/List.jsx
@@ -1,18 +1,27 @@
 import * as data from 'test/data';
+import mockStore from 'test/mock-store';
 import React from 'react';
 import {List} from '../List';
-import {shallow} from 'enzyme';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
 import {User} from 'shared/util/records';
+
+jest.unmock('react-dom');
 
 describe('List', () => {
 	it('should render', () => {
-		const component = shallow(
-			<List
-				currentUser={data.getImmutableMock(User, data.mockUser)}
-				groupId='23'
-			/>
+		const {container} = render(
+			<Provider store={mockStore()}>
+				<StaticRouter>
+					<List
+						currentUser={data.getImmutableMock(User, data.mockUser)}
+						groupId='23'
+					/>
+				</StaticRouter>
+			</Provider>
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 });
