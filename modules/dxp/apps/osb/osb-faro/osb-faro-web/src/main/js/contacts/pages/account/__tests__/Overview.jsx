171e1dc2b8979df380e08ff4f09eae3c65015d46
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-09-10 11:25:44 -0300

    LRAC-6632 Convert to RTL and Update snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/pages/account/__tests__/Overview.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/pages/account/__tests__/Overview.jsx
@@ -1,19 +1,31 @@
 import * as data from 'test/data';
+import client from 'shared/apollo/client';
 import Overview from '../Overview';
 import React from 'react';
 import {Account} from 'shared/util/records';
-import {shallow} from 'enzyme';
+import {ApolloProvider} from '@apollo/react-components';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
 
 describe('AccountOverview', () => {
 	it('should render', () => {
-		const component = shallow(
-			<Overview
-				account={data.getImmutableMock(Account, data.mockAccount)}
-				groupId='23'
-				id='test'
-			/>
+		const {container} = render(
+			<ApolloProvider client={client}>
+				<StaticRouter>
+					<Overview
+						account={data.getImmutableMock(
+							Account,
+							data.mockAccount
+						)}
+						groupId='23'
+						id='test'
+					/>
+				</StaticRouter>
+			</ApolloProvider>
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 });
