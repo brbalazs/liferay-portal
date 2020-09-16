7c2cadc210b390e97ca6b19543a6b491a33074c9
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-09-16 12:06:21 -0300

    LRAC-6645 Enhance coverage test of basepage from settings

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/__tests__/BasePage.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/__tests__/BasePage.jsx
@@ -1,13 +1,30 @@
 import mockStore from 'test/mock-store';
 import React from 'react';
 import {SettingsBasePage as BasePage} from '../BasePage';
+import {cleanup, render} from '@testing-library/react';
 import {Provider} from 'react-redux';
-import {render} from '@testing-library/react';
 import {StaticRouter} from 'react-router';
 
 jest.unmock('react-dom');
 
+const mockBreadcrumbItems = [
+	{
+		active: true,
+		href: 'test123',
+		label: 'testLabelBreadcrumbItems'
+	}
+];
+
+const mockPageActions = [
+	{
+		actions: [{label: 'Test Action'}],
+		label: 'testLabelPageActions '
+	}
+];
+
 describe('BasePage', () => {
+	afterEach(cleanup);
+
 	it('should render', () => {
 		const {container} = render(
 			<Provider store={mockStore()}>
@@ -18,5 +35,53 @@ describe('BasePage', () => {
 		);
 
 		expect(container).toMatchSnapshot();
+	});
+
+	it('should render with a description', () => {
+		const {getByText} = render(
+			<Provider store={mockStore()}>
+				<StaticRouter>
+					<BasePage pageDescription='testPageDescription' />
+				</StaticRouter>
+			</Provider>
+		);
+
+		expect(getByText('testPageDescription')).toBeTruthy();
+	});
+
+	it('should render with a breadcrumb', () => {
+		const {getByText} = render(
+			<Provider store={mockStore()}>
+				<StaticRouter>
+					<BasePage breadcrumbItems={mockBreadcrumbItems} />
+				</StaticRouter>
+			</Provider>
+		);
+
+		expect(getByText('testLabelBreadcrumbItems')).toBeTruthy();
+	});
+
+	it('should render with a page action', () => {
+		const {getByText} = render(
+			<Provider store={mockStore()}>
+				<StaticRouter>
+					<BasePage pageActions={mockPageActions} />
+				</StaticRouter>
+			</Provider>
+		);
+
+		expect(getByText('testLabelPageActions')).toBeTruthy();
+	});
+
+	it('should render with a title', () => {
+		const {getByText} = render(
+			<Provider store={mockStore()}>
+				<StaticRouter>
+					<BasePage pageTitle='testPageTitle' />
+				</StaticRouter>
+			</Provider>
+		);
+
+		expect(getByText('testPageTitle')).toBeTruthy();
 	});
 });
