b27efd05a3e3d028664236724acf942c44aafb1d
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-09-16 09:45:43 -0300

    LRAC-6646 Adjust test to put the children element in snapshot and update snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/__tests__/Cell.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/__tests__/Cell.jsx
@@ -6,27 +6,21 @@ jest.unmock('react-dom');
 
 describe('Cell', () => {
 	it('should render', () => {
-		const {container} = render(<Cell />);
-		expect(container).toMatchSnapshot();
-	});
-
-	it('should render a children', () => {
-		const {getByText} = render(
-			<Cell>
-				<span>{'Test'}</span>
-			</Cell>
-		);
-
-		expect(getByText('Test')).toBeTruthy();
-	});
-
-	it('should render as a table title', () => {
 		const {container} = render(
 			<Cell title>
 				<span>{'Test'}</span>
 			</Cell>
 		);
+		expect(container).toMatchSnapshot();
+	});
 
-		expect(container.querySelector('.table-title')).toBeTruthy();
+	it('should render as a normal cell', () => {
+		const {container} = render(
+			<Cell>
+				<span>{'Test'}</span>
+			</Cell>
+		);
+
+		expect(container.querySelector('.table-title')).toBeFalsy();
 	});
 });
