a23f531d9d0c214418ca0d83e9c42b9afb881323
Laszlo Pap <laszlo.pap@liferay.com>
2020-09-18 00:04:21 +0200

    LPS-96270 No needed

--- modules/apps/asset/asset-tags-admin-web/src/main/java/com/liferay/asset/tags/admin/web/internal/display/context/AssetTagsDisplayContext.java
+++ modules/apps/asset/asset-tags-admin-web/src/main/java/com/liferay/asset/tags/admin/web/internal/display/context/AssetTagsDisplayContext.java
@@ -51,9 +51,7 @@ import com.liferay.portlet.asset.util.comparator.AssetTagAssetCountComparator;
 import com.liferay.portlet.asset.util.comparator.AssetTagNameComparator;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 import javax.portlet.ActionRequest;
@@ -282,24 +280,6 @@ public class AssetTagsDisplayContext {
 		_tag = tag;
 
 		return _tag;
-	}
-
-	public Map<Long, Long> getTagCountMap() throws PortalException {
-		if (_tagsSearchContainer == null) {
-			getTagsSearchContainer();
-		}
-
-		Map<Long, Long> result = new HashMap<>();
-
-		List<AssetTag> tags = _tagsSearchContainer.getResults();
-
-		for (AssetTag tag : tags) {
-			long count = getFullTagsCount(tag);
-
-			result.put(tag.getTagId(), count);
-		}
-
-		return result;
 	}
 
 	public Long getTagId() {
