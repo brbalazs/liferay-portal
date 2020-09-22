fd433eea845f70e61df705bdef75b7b5bd5a8220
drewbrokke <drew.brokke@liferay.com>
2020-09-22 09:50:16 -0500

    LPS-115034 portal-upload: includes the size denomination in the error message

--- modules/apps/portal/portal-upload/src/main/java/com/liferay/portal/upload/internal/configuration/UploadServletRequestConfigurationModelListener.java
+++ modules/apps/portal/portal-upload/src/main/java/com/liferay/portal/upload/internal/configuration/UploadServletRequestConfigurationModelListener.java
@@ -17,6 +17,7 @@ package com.liferay.portal.upload.internal.configuration;
 import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
 import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListenerException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.SystemProperties;
@@ -61,11 +62,15 @@ public class UploadServletRequestConfigurationModelListener
 		long maxSize = (long)properties.get("maxSize");
 
 		if (maxSize < _MINIMUM_MAX_SIZE) {
+			ResourceBundle resourceBundle = _getResourceBundle();
+
 			throw new ConfigurationModelListenerException(
 				LanguageUtil.format(
-					_getResourceBundle(),
+					resourceBundle,
 					"the-maximum-upload-request-size-cannot-be-less-than-x",
-					_MINIMUM_MAX_SIZE),
+					LanguageUtil.formatStorageSize(
+						GetterUtil.getDouble(_MINIMUM_MAX_SIZE),
+						resourceBundle.getLocale())),
 				UploadServletRequestConfiguration.class, getClass(),
 				properties);
 		}
