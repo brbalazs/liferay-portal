3d4600246ab90a3b37a48c23945722d8eb009a56
Masterfoni <joao.lima@liferay.com>
2020-09-25 11:15:40 -0300

    LRAC-6697 Fix project fetch call

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/workspaces/workspace-list/ListItem.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/workspaces/workspace-list/ListItem.jsx
@@ -86,7 +86,7 @@ export default class WorkspaceListItem extends React.Component {
 		});
 
 		return API.projects
-			.fetchProject({groupId})
+			.fetch({groupId})
 			.then(({state}) => {
 				this.setState({
 					loading: false,
