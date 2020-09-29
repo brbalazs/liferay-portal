475f2790b391f6735d197159729afb02b694599f
Eric Yan <eric.yan@liferay.com>
2020-09-28 18:00:39 -0700

    LPS-88338 Add tests

--- portal-impl/test/unit/com/liferay/portal/util/HttpImplTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/HttpImplTest.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.test.randomizerbumpers.NumericStringRandomizerB
 import com.liferay.portal.kernel.test.rule.CodeCoverageAssertor;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.URLCodec;
@@ -39,6 +40,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Miguel Pastor
@@ -291,6 +294,64 @@ public class HttpImplTest {
 	}
 
 	@Test
+	}
+
+	@Test
+	public void testGetQueryStringWithHttpServletRequest() {
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setQueryString("a=1");
+
+		mockHttpServletRequest.setAttribute(
+			JavaConstants.JAVAX_SERVLET_FORWARD_QUERY_STRING, "b=2");
+
+		Assert.assertEquals(
+			"a=1", _httpImpl.getQueryString(mockHttpServletRequest));
+	}
+
+	@Test
+	public void testGetQueryStringWithHttpServletRequestForwarded() {
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setQueryString("a=1");
+
+		mockHttpServletRequest.setAttribute(
+			JavaConstants.JAVAX_SERVLET_FORWARD_QUERY_STRING, "b=2");
+
+		// Simulate a forwarded request
+		// attribute: javax.servlet.forward.request_uri is present
+		// attribute: javax.servlet.forward.query_string provides the original
+		//            query string of the request
+
+		mockHttpServletRequest.setAttribute(
+			JavaConstants.JAVAX_SERVLET_FORWARD_REQUEST_URI, "https://foo.com");
+
+		Assert.assertEquals(
+			"b=2", _httpImpl.getQueryString(mockHttpServletRequest));
+	}
+
+	@Test
+	public void testIsForwarded() {
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		Assert.assertFalse(_httpImpl.isForwarded(mockHttpServletRequest));
+	}
+
+	@Test
+	public void testIsForwardedWithHttpServletRequestForwarded() {
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		// Simulate a forwarded request
+		// attribute: javax.servlet.forward.request_uri is present
+
+		mockHttpServletRequest.setAttribute(
+			JavaConstants.JAVAX_SERVLET_FORWARD_REQUEST_URI, "https://foo.com");
+
+		Assert.assertTrue(_httpImpl.isForwarded(mockHttpServletRequest));
 	public void testNormalizePath() {
 		Assert.assertEquals("/api/axis", _httpImpl.normalizePath("/api/axis?"));
 		Assert.assertEquals("/", _httpImpl.normalizePath("/.."));
