d47f68312ba73eff903d99a175320a578d97ce03
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-10-02 14:56:41 -0300

    LRAC-6745 Move AudienceReport component and AudienceReportCard hoc

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/AudienceReportCard.jsx
@@ -0,0 +1,52 @@
+import AudienceReport from 'shared/components/AudienceReport';
+import BaseCard from 'cerebro-shared/components/base-card';
+import Card from 'shared/components/Card';
+import React from 'react';
+import {compose} from 'redux';
+import {HOC_CARD_PROPTYPES} from 'shared/util/proptypes';
+import {withError, withLoading} from 'shared/hoc';
+
+/**
+ * HOC
+ * @description Audience Report Card
+ * @param {function} withAudienceReportCard
+ */
+const withAudienceReportCard = withData => {
+	const AudienceReportWithData = compose(
+		withData(),
+		withLoading({alignCenter: true, page: false}),
+		withError({page: false})
+	)(AudienceReport);
+
+	AudienceReportWithData.propTypes = HOC_CARD_PROPTYPES;
+
+	const defaultProps = {
+		className: 'analytics-audience-report-card'
+	};
+
+	const AudienceReportCard = ({className, label, legacyDropdownRangeKey}) => (
+		<BaseCard
+			className={className}
+			label={label}
+			legacyDropdownRangeKey={legacyDropdownRangeKey}
+			minHeight={536}
+		>
+			{({filters, rangeSelectors, router}) => (
+				<Card.Body>
+					<AudienceReportWithData
+						filters={filters}
+						rangeSelectors={rangeSelectors}
+						router={router}
+					/>
+				</Card.Body>
+			)}
+		</BaseCard>
+	);
+
+	AudienceReportCard.defaultProps = defaultProps;
+
+	return AudienceReportCard;
+};
+
+export {withAudienceReportCard};
+export default withAudienceReportCard;
