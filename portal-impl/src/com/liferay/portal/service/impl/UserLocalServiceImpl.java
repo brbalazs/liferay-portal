e8037afc7623433c3d2697ac304470a5cc6424be
shuyangzhou <shuyang.zhou@liferay.com>
2020-10-02 10:53:27 -0700

    LPS-119805 Push down local variable.

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3572,8 +3572,6 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		Map<Long, Integer> counts = new HashMap<>();
 
-		int count = 0;
-
 		try {
 			for (long groupId : groupIds) {
 				Group group = groupLocalService.fetchGroup(groupId);
@@ -3581,6 +3579,8 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 				if (group == null) {
 					continue;
 				}
+
+				int count = 0;
 
 				if (group.isOrganization()) {
 					count = getOrganizationUsersCount(
