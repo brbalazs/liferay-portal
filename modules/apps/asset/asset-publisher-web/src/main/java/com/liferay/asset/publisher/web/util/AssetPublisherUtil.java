9592869213d7c8f28dfd2699062e263b923c3b9b
Attila Bakay <attila.bakay@liferay.com>
2020-10-07 12:14:54 +0200

    LPS-121757 keeping the consistent behaviour with AssetLists

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/util/AssetPublisherUtil.java
@@ -741,7 +741,7 @@ public class AssetPublisherUtil {
 	public static String[] getAssetTagNames(
 		PortletPreferences portletPreferences) {
 
-		String[] allAssetTagNames = new String[0];
+		List<String> allAssetTagNames = new ArrayList<>();
 
 		for (int i = 0; true; i++) {
 			String[] queryValues = portletPreferences.getValues(
@@ -764,11 +764,11 @@ public class AssetPublisherUtil {
 				queryContains &&
 				(queryAndOperator || (queryValues.length == 1))) {
 
-				allAssetTagNames = queryValues;
+				Collections.addAll(allAssetTagNames, queryValues);
 			}
 		}
 
-		return allAssetTagNames;
+		return allAssetTagNames.toArray(new String[0]);
 	}
 
 	public static String getClassName(
