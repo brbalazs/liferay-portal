f66cfb29ceea757cb2ce6aef2cac9473423d9869
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-10-20 17:34:01 -0300

    LRAC-4004 Adjust import of Legend Component

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/variant-card/MediansChart.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/variant-card/MediansChart.tsx
@@ -1,7 +1,7 @@
 import BarChartHTML from 'cerebro-shared/components/BarChartHTML';
 import BasePage from 'shared/components/base-page';
 import getMedianMapper from 'experiments/hocs/mappers/experiment-variant-median-mapper';
-import Legend from 'cerebro-shared/components/Legend';
+import Legend from 'shared/components/Legend';
 import NoResultsDisplay from 'shared/components/NoResultsDisplay';
 import React, {useContext} from 'react';
 import {EXPERIMENT_QUERY} from 'experiments/queries/ExperimentQuery';
