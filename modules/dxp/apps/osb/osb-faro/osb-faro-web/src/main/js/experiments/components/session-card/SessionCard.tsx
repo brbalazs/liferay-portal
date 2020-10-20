6305dc971c0039e6e9c493a14eddb3cdcc72d3b6
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-10-20 16:02:07 -0300

    LRAC-4004 Create a folder to the SessionCard, put a SafeResults component only in the content o SessionCard and increase coverage test

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/session-card/SessionCard.tsx
@@ -0,0 +1,127 @@
+import BasePage from 'shared/components/base-page';
+import Card from 'shared/components/Card';
+import ClayButton from '@clayui/button';
+import getSessionMapper from 'experiments/hocs/mappers/experiment-session-mapper';
+import getSessionVariantsMapper from 'experiments/hocs/mappers/experiment-session-variants-mapper';
+import Icon from 'shared/components/Icon';
+import LineChart from 'experiments/components/LineChart';
+import React, {useContext} from 'react';
+import {DocumentNode} from 'graphql';
+import {
+	EXPERIMENT_SESSION_HISTOGRAM_QUERY,
+	EXPERIMENT_SESSION_VARIANTS_HISTOGRAM_QUERY
+} from 'experiments/queries/ExperimentQuery';
+import {SafeResults} from 'shared/hoc/util';
+import {useFakeLoading} from 'shared/hooks';
+import {useQuery} from '@apollo/react-hooks';
+import {useStateValue} from 'experiments/state';
+
+const CLASSNAME = 'analytics-session-card';
+const PER_VARIANT = 'per-variant';
+const TOTAL = 'total';
+
+const getSessionView = (sessionView: String) => {
+	let query: DocumentNode = null;
+	let mapper: Function = null;
+
+	if (sessionView === TOTAL) {
+		query = EXPERIMENT_SESSION_HISTOGRAM_QUERY;
+		mapper = getSessionMapper;
+	} else if (sessionView === PER_VARIANT) {
+		query = EXPERIMENT_SESSION_VARIANTS_HISTOGRAM_QUERY;
+		mapper = getSessionVariantsMapper;
+	}
+
+	return {
+		mapper,
+		query
+	};
+};
+
+interface ISessionCardProps extends React.HTMLAttributes<HTMLElement> {
+	label?: string;
+}
+
+const SessionCard: React.FC<ISessionCardProps> = ({label}) => {
+	const [{sessionViewTriggered}, dispatch]: any = useStateValue();
+
+	const {
+		router: {
+			params: {id: experimentId}
+		}
+	} = useContext(BasePage.Context);
+
+	const {mapper, query} = getSessionView(sessionViewTriggered);
+
+	const {data, loading, ...result} = useQuery(query, {
+		variables: {experimentId}
+	});
+
+	const fakeLoading = useFakeLoading(data);
+
+	return (
+		<Card className={CLASSNAME} minHeight={405}>
+			<Card.Header className='align-items-center d-flex justify-content-between'>
+				<Card.Title>{label}</Card.Title>
+
+				<ClayButton.Group>
+					<ClayButton
+						className={
+							sessionViewTriggered === 'total' ? 'active' : ''
+						}
+						displayType='secondary'
+						onClick={() =>
+							dispatch({
+								newAction: 'total',
+								type: 'changeSessionView'
+							})
+						}
+						small
+					>
+						<Icon className='mr-2' symbol='session-single-chart' />{' '}
+						{Liferay.Language.get('total')}
+					</ClayButton>
+					<ClayButton
+						className={
+							sessionViewTriggered === 'per-variant'
+								? 'active'
+								: ''
+						}
+						displayType='secondary'
+						onClick={() =>
+							dispatch({
+								newAction: 'per-variant',
+								type: 'changeSessionView'
+							})
+						}
+						small
+					>
+						<Icon
+							className='mr-2'
+							symbol='session-multiple-chart'
+						/>
+
+						{Liferay.Language.get('per-variant')}
+					</ClayButton>
+				</ClayButton.Group>
+			</Card.Header>
+
+			<Card.Body>
+				{
+					<SafeResults
+						data={data}
+						loading={loading || fakeLoading}
+						{...result}
+					>
+						{props => {
+							const otherProps = mapper(props);
+							return <LineChart {...otherProps} />;
+						}}
+					</SafeResults>
+				}
+			</Card.Body>
+		</Card>
+	);
+};
+
+export default SessionCard;
