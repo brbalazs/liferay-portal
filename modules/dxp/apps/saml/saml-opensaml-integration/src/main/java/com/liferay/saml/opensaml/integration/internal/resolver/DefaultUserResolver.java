73d2d6409a41f1944828c0eafbbc16757b2ede17
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2020-10-22 14:14:28 +0100

    LPS-122195 Remove unused parameter

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/resolver/DefaultUserResolver.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/resolver/DefaultUserResolver.java
@@ -378,7 +378,7 @@ public class DefaultUserResolver implements UserResolver {
 				_log.debug("Found user " + user.toString());
 			}
 
-			user = updateUser(companyId, user, attributesMap, serviceContext);
+			user = updateUser(user, attributesMap, serviceContext);
 		}
 		else {
 			user = addUser(companyId, attributesMap, serviceContext);
@@ -392,8 +392,7 @@ public class DefaultUserResolver implements UserResolver {
 	}
 
 	protected User updateUser(
-			long companyId, User user,
-			Map<String, List<Serializable>> attributesMap,
+			User user, Map<String, List<Serializable>> attributesMap,
 			ServiceContext serviceContext)
 		throws PortalException {
 
