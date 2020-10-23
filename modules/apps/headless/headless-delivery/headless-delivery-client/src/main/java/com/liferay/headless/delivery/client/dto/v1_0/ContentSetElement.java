93da9fc410bf654d59f29937e8c05b7f0a8abcd7
Brian Chan <brian.chan@liferay.com>
2020-10-23 14:00:36 -0700

    LPS-122605 regen

--- modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/dto/v1_0/ContentSetElement.java
+++ modules/apps/headless/headless-delivery/headless-delivery-client/src/main/java/com/liferay/headless/delivery/client/dto/v1_0/ContentSetElement.java
@@ -17,6 +17,8 @@ package com.liferay.headless.delivery.client.dto.v1_0;
 import com.liferay.headless.delivery.client.function.UnsafeSupplier;
 import com.liferay.headless.delivery.client.serdes.v1_0.ContentSetElementSerDes;
 
+import java.io.Serializable;
+
 import java.util.Objects;
 
 import javax.annotation.Generated;
@@ -26,7 +28,7 @@ import javax.annotation.Generated;
  * @generated
  */
 @Generated("")
-public class ContentSetElement implements Cloneable {
+public class ContentSetElement implements Cloneable, Serializable {
 
 	public static ContentSetElement toDTO(String json) {
 		return ContentSetElementSerDes.toDTO(json);
