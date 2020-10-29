5530df2e3069d00a933204bdd76d0a4cbe8f8933
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2020-10-29 10:14:47 -0300

    LRAC-6675 Put a TODO and SF

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/time-zone-selection-modal/TimeZoneAdminModal.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/time-zone-selection-modal/TimeZoneAdminModal.tsx
@@ -7,76 +7,82 @@ import {Formik} from 'formik';
 import {TimeZone} from 'shared/util/records';
 
 interface ITimeZoneSelectionModal {
-    onClose: Function;
-    timeZone: TimeZone;
+	onClose: Function;
+	timeZone: TimeZone;
 }
 
 const TimeZoneSelectionModal: React.FC<ITimeZoneSelectionModal> = ({
-    onClose,
-    timeZone
+	onClose,
+	timeZone
 }) => {
-    const _formRef = useRef<Formik>();
+	const _formRef = useRef<Formik>();
+	// TODO: LRAC-6681 Add the timezone update request in modal
+	const onSubmit = () => {};
 
-    const onSubmit = () => {
-        const {timeZoneId} = _formRef.current.getFormikBag().values;
-    };
+	return (
+		<Modal>
+			<Modal.Header title={Liferay.Language.get('workspace-timezone')} />
 
-    return (
-        <Modal>
-            <Modal.Header
-                title={Liferay.Language.get('workspace-timezone')}
-            />
+			<Form
+				initialValues={{
+					timeZoneId: ''
+				}}
+				onSubmit={onSubmit}
+				ref={_formRef}
+			>
+				{({
+					handleSubmit,
+					isSubmitting,
+					isValid,
+					setFieldTouched,
+					setFieldValue
+				}) => (
+					<Form.Form onSubmit={handleSubmit}>
+						<Modal.Body>
+							<div className='mb-4'>
+								{Liferay.Language.get(
+									'your-workspace-now-supports-custom-timezones-setting-timezones-will-only-impact-future-data-expect-spiked-or-flat-data-for-1-2-days-following-a-change'
+								)}
+							</div>
 
-            <Form
-                initialValues={{
-                    timeZoneId: ''
-                }}
-                onSubmit={onSubmit}
-                ref={_formRef}
-            >
-                {({handleSubmit, isSubmitting, isValid, setFieldTouched, setFieldValue}) => (
-                    <Form.Form onSubmit={handleSubmit}>
-                        <Modal.Body>
-                            <div className='mb-4'>
-                                {Liferay.Language.get('your-workspace-now-supports-custom-timezones-setting-timezones-will-only-impact-future-data-expect-spiked-or-flat-data-for-1-2-days-following-a-change')}
-                            </div>
+							<div className='picker-root-container'>
+								<div className='time-zone-spaced-select'>
+									<TimeZonePicker
+										fieldName='timeZoneId'
+										initialTimeZone={timeZone}
+										setFieldTouched={setFieldTouched}
+										setFieldValue={setFieldValue}
+									/>
+								</div>
 
-                            <div className='picker-root-container'>
-                                <div className='time-zone-spaced-select'>
-                                    <TimeZonePicker
-                                        fieldName='timeZoneId'
-                                        initialTimeZone={timeZone}
-                                        setFieldTouched={setFieldTouched}
-                                        setFieldValue={setFieldValue}
-                                    />
-                                </div>
+								<span className='current-time-display'>
+									{Liferay.Language.get('current-time-colon')}
+								</span>
+								<span className='current-time-value ml-4'>
+									{'09:00 a.m.'}
+								</span>
+							</div>
+						</Modal.Body>
 
-                                <span className='current-time-display'>{Liferay.Language.get('current-time-colon')}</span>
-                                <span className='current-time-value ml-4'>{'09:00 a.m.'}</span>
-                            </div>
-                        </Modal.Body>
+						<Modal.Footer>
+							<Button onClick={() => onClose()}>
+								{Liferay.Language.get('do-this-later')}
+							</Button>
 
-                        <Modal.Footer>
-                            <Button
-                                onClick={() => onClose()}
-                            >
-                                {Liferay.Language.get('do-this-later')}
-                            </Button>
-
-                            <Button
-                                disabled={!isValid}
-                                display='primary'
-                                loading={isSubmitting}
-                                type='submit'
-                            >
-                                {Liferay.Language.get('set-timezone')}
-                            </Button>
-                        </Modal.Footer>
-                    </Form.Form>
-                )}
-            </Form>
-        </Modal>
-    );
+							<Button
+								disabled={!isValid}
+								display='primary'
+								loading={isSubmitting}
+								type='submit'
+							>
+								{Liferay.Language.get('set-timezone')}
+							</Button>
+						</Modal.Footer>
+					</Form.Form>
+				)}
+			</Form>
+		</Modal>
+	);
 };
 
 export default TimeZoneSelectionModal;
