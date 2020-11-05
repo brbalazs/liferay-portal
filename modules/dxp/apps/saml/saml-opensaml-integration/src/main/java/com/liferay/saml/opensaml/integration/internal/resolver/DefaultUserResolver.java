7df72759cf5da27a033bbc222c6b74adda5a3f99
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2020-11-05 12:53:50 +0000

    LPS-122195 Revert "LPS-84540 Use unspecified format if NameID format is not set"
    This reverts commit 91abd2c298cc36d5bb3ac790ee18be351d7f2867.

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/resolver/DefaultUserResolver.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/resolver/DefaultUserResolver.java
@@ -244,10 +244,6 @@ public class DefaultUserResolver implements UserResolver {
 
 		String format = userResolverSAMLContext.resolveSubjectNameFormat();
 
-		if (Validator.isNull(format)) {
-			return _SUBJECT_NAME_TYPE_UNSPECIFIED;
-		}
-
 		if (format.equals(NameIDType.EMAIL)) {
 			return _SUBJECT_NAME_TYPE_EMAIL_ADDRESS;
 		}
@@ -495,8 +491,6 @@ public class DefaultUserResolver implements UserResolver {
 		"emailAddress";
 
 	private static final String _SUBJECT_NAME_TYPE_SCREENNAME = "screenName";
-
-	private static final String _SUBJECT_NAME_TYPE_UNSPECIFIED = "unspecified";
 
 	private static final String _SUBJECT_NAME_TYPE_UUID = "uuid";
 
