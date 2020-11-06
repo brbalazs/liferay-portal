a621af0ea39e8d0b20935f3537d3dfc4f575c892
matthewkong <matthew.kong@liferay.com>
2020-11-06 13:38:25 -0800

    LRAC-6915 Upgrade

--- modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/internal/upgrade/v9_0_0/UpgradeFaroProject.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/internal/upgrade/v9_0_0/UpgradeFaroProject.java
@@ -15,7 +15,14 @@
 package com.liferay.osb.faro.internal.upgrade.v9_0_0;
 
 import com.liferay.osb.faro.model.impl.FaroProjectModelImpl;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.RoleConstants;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * @author Geyson Silva
@@ -26,9 +33,55 @@ public class UpgradeFaroProject extends UpgradeProcess {
 	protected void doUpgrade() throws Exception {
 		alter(
 			FaroProjectModelImpl.class,
+			new AlterTableAddColumn("incidentReportEmailAddresses STRING"),
 			new AlterTableAddColumn("timeZoneId VARCHAR(75)"));
 
 		runSQL("update OSBFaro_FaroProject set timeZoneId = 'UTC'");
+		_updateIncidentReportEmailAddresses();
+	}
+
+	private long _getSiteOwnerRoleId() throws Exception {
+		try (PreparedStatement ps = connection.prepareStatement(
+				"select roleId from Role_ where name = " +
+					StringUtil.quote(
+						RoleConstants.SITE_OWNER, StringPool.APOSTROPHE));
+
+			ResultSet rs = ps.executeQuery()) {
+
+			if (rs.next()) {
+				return rs.getLong(1);
+			}
+
+			throw new Exception("Could not find site owner role ID");
+		}
+	}
+
+	private void _updateIncidentReportEmailAddresses() throws Exception {
+		StringBundler sb = new StringBundler(6);
+
+		sb.append("select OSBFaro_FaroUser.emailAddress,");
+		sb.append("OSBFaro_FaroProject.faroProjectId from ");
+		sb.append("OSBFaro_FaroProject inner join OSBFaro_FaroUser on ");
+		sb.append("OSBFaro_FaroProject.groupId = OSBFaro_FaroUser.groupId ");
+		sb.append("where OSBFaro_FaroUser.roleId = ");
+		sb.append(_getSiteOwnerRoleId());
+
+		try (PreparedStatement ps = connection.prepareStatement(sb.toString());
+			ResultSet rs = ps.executeQuery()) {
+
+			while (rs.next()) {
+				try (PreparedStatement psUpdate = connection.prepareStatement(
+						"update OSBFaro_FaroProject set " +
+							"incidentReportEmailAddresses = ? where " +
+								"faroProjectId = ?")) {
+
+					psUpdate.setString(1, "[\"" + rs.getString(1) + "\"]");
+					psUpdate.setLong(2, rs.getLong(2));
+
+					psUpdate.executeUpdate();
+				}
+			}
+		}
 	}
 
 }
