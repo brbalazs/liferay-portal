07cf0f97b3f077a992a4893c439745057e6a412c
Masterfoni <joao.lima@liferay.com>
2020-11-10 10:44:49 -0300

    LRAC-6934 Migrate Subtitle's test from enzyme to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-base-card/__tests__/Subtitle.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-base-card/__tests__/Subtitle.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import Subtitle from '../Subtitle';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
 
 describe('SummaryBaseCard Subtitle', () => {
-	it('should render component', () => {
-		const component = shallow(<Subtitle label='My Subtitle' />);
+	it('should render', () => {
+		const {container} = render(<Subtitle label='My Subtitle' />);
 
-		expect(component.length).toBe(1);
-		expect(
-			component.hasClass('font-size-sm-1x mb-2 text-uppercase')
-		).toBeTruthy();
-		expect(component.render()).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 });
