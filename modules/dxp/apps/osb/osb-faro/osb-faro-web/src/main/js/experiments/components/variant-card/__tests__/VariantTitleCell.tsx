6333ad8d559bfd7daa0768fc4a592feb87d75716
Masterfoni <joao.lima@liferay.com>
2020-11-10 17:48:52 -0300

    LRAC-6936 Change VariantTitleCell's tests to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/variant-card/__tests__/VariantTitleCell.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/variant-card/__tests__/VariantTitleCell.tsx
@@ -1,39 +1,24 @@
-import Label from 'shared/components/Label';
 import React from 'react';
 import VariantTitle from '../VariantTitleCell';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
+
+const DefaultComponent = props => (
+	<VariantTitle title='Variant Title' {...props} />
+);
 
 describe('VariantTitle', () => {
-	let component;
+	it('should render', () => {
+		const {container} = render(<DefaultComponent />);
 
-	afterEach(() => {
-		if (component) {
-			component.unmount();
-		}
-	});
-
-	it('should render VariantTitle', () => {
-		component = shallow(<VariantTitle title='Variant Title' />);
-
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('.text-truncate')).toBeTruthy();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render label component when a label prop is set', () => {
-		component = shallow(
-			<VariantTitle label='winner' title='Variant Title' />
-		);
+		const {getByText} = render(<DefaultComponent label='winner' />);
 
-		expect(component.exists(Label)).toBe(true);
-	});
-
-	it('should render truncated text when it is a big title', () => {
-		component = shallow(
-			<VariantTitle
-				label='winner'
-				title='Variant Title with a big title should be truncated'
-			/>
-		);
-
-		expect(component.exists('.text-truncate')).toBe(true);
+		expect(getByText('winner')).toBeTruthy();
 	});
 });
