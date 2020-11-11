fa9533ce8d531e2ee9899f0856e7b33cb3fc0a3e
Masterfoni <joao.lima@liferay.com>
2020-11-11 16:11:07 -0300

    LRAC-6949 Migrate PropertyCell component to Typescript

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/cell-components/Property.tsx
@@ -0,0 +1,25 @@
+import getCN from 'classnames';
+import React from 'react';
+import {getSafeDisplayValue} from 'shared/util/util';
+
+interface IPropertyCellProps {
+	className?: string;
+	data: {
+		name: string;
+		value: string;
+	};
+}
+
+const PropertyCell: React.FC<IPropertyCellProps> = ({className, data}) => {
+	const {name, value} = data;
+
+	return (
+		<td className={getCN('property-cell', className)}>
+			<div className='name'>{name}</div>
+
+			<div className='table-title'>{getSafeDisplayValue(value)}</div>
+		</td>
+	);
+};
+
+export default PropertyCell;
