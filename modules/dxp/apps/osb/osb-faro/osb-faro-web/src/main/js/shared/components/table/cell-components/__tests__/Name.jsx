98040dae1bc4185ac9e1da142d08bab7c5b41e43
Masterfoni <joao.lima@liferay.com>
2020-11-13 15:39:58 -0300

    LRAC-6948 Change Name tests to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/cell-components/__tests__/Name.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/cell-components/__tests__/Name.jsx
@@ -1,54 +1,60 @@
 import Name from '../Name';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
+
+const FilledComponent = props => (
+	<Name
+		data={{id: 'test', name: 'foo'}}
+		groupId='23'
+		routeFn={({data: {id}}) => `/foo/${id}`}
+		{...props}
+	/>
+);
 
 describe('Name', () => {
 	it('should render', () => {
-		const component = shallow(
+		const {container} = render(
 			<Name data={{name: 'foo'}} renderSecondaryInfo={() => 'bar'} />
 		);
-		expect(component).toMatchSnapshot();
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render without a link in the name if disabled is true', () => {
-		const component = shallow(
-			<Name
-				data={{id: 'test', name: 'foo'}}
-				disabled
-				groupId='23'
-				routeFn={({data: {id}}) => `/foo/${id}`}
-			/>
-		);
+		const {container, getAllByText} = render(<FilledComponent disabled />);
 
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('a')).toBeFalsy();
+		expect(getAllByText('foo')).toBeTruthy();
 	});
 
 	it('should render the name as a link if a route is passed', () => {
-		const component = shallow(
-			<Name
-				data={{id: 'test', name: 'foo'}}
-				groupId='23'
-				routeFn={({data: {id}}) => `/foo/${id}`}
-			/>
+		const {container} = render(
+			<StaticRouter>
+				<FilledComponent />
+			</StaticRouter>
 		);
-		expect(component).toMatchSnapshot();
+
+		expect(container.querySelector('a')).toHaveAttribute(
+			'href',
+			'/foo/test'
+		);
 	});
 
 	it('should render with secondary info', () => {
-		const component = shallow(
-			<Name
-				data={{id: 'test', name: 'foo'}}
-				groupId='23'
-				renderSecondaryInfo={() => 'bar'}
-				routeFn={({data: {id}}) => `/foo/${id}`}
-			/>
+		const {container} = render(
+			<StaticRouter>
+				<FilledComponent renderSecondaryInfo={() => 'bar'} />
+			</StaticRouter>
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render with an icon', () => {
-		const component = shallow(
+		const {container, getByText} = render(
 			<Name
 				data={{id: 'test', name: 'foo'}}
 				groupId='23'
@@ -56,22 +62,26 @@ describe('Name', () => {
 			/>
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container.querySelector('.icon-container')).toBeTruthy();
+		expect(getByText('foo icon')).toBeTruthy();
 	});
 
 	it('should render using the nameKey', () => {
-		const component = shallow(
+		const {getByText} = render(
 			<Name data={{id: 'test', title: 'foo'}} nameKey='title' />
 		);
 
-		expect(component.render().text()).toEqual('foo');
+		expect(getByText('foo')).toBeTruthy();
 	});
 
 	it('should render the display name in TextTruncate if the tooltip prop is true', () => {
-		const component = shallow(
-			<Name data={{id: 'test', title: 'foo'}} tooltip />
+		const {getByText} = render(
+			<Name
+				data={{id: 'test', name: 'Name in text-truncate', title: 'foo'}}
+				tooltip
+			/>
 		);
 
-		expect(component.find('TextTruncate').exists()).toBe(true);
+		expect(getByText('Name in text-truncate')).toHaveClass('text-truncate');
 	});
 });
