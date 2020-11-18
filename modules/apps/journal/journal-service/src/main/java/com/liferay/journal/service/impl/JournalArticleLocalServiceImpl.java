81c3530990bfdf86c7853f4dbc60024c01b0e7ac
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-11-18 14:39:42 +0100

    LPS-123487 Reorder according to service.xml

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -949,6 +949,18 @@ public class JournalArticleLocalServiceImpl
 		newArticle.setCompanyId(user.getCompanyId());
 		newArticle.setUserId(user.getUserId());
 		newArticle.setUserName(user.getFullName());
+
+		Date modifiedDate = new Date();
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		if (serviceContext != null) {
+			modifiedDate = serviceContext.getModifiedDate(modifiedDate);
+		}
+
+		newArticle.setModifiedDate(modifiedDate);
+
 		newArticle.setFolderId(oldArticle.getFolderId());
 		newArticle.setTreePath(oldArticle.getTreePath());
 		newArticle.setArticleId(newArticleId);
@@ -994,17 +1006,6 @@ public class JournalArticleLocalServiceImpl
 
 		newArticle.setStatusByUserId(user.getUserId());
 		newArticle.setStatusByUserName(user.getFullName());
-
-		Date modifiedDate = new Date();
-
-		ServiceContext serviceContext =
-			ServiceContextThreadLocal.getServiceContext();
-
-		if (serviceContext != null) {
-			modifiedDate = serviceContext.getModifiedDate(modifiedDate);
-		}
-
-		newArticle.setModifiedDate(modifiedDate);
 		newArticle.setStatusDate(modifiedDate);
 
 		ExpandoBridgeUtil.copyExpandoBridgeAttributes(
@@ -5673,13 +5674,12 @@ public class JournalArticleLocalServiceImpl
 			article.setUserId(user.getUserId());
 			article.setUserName(user.getFullName());
 			article.setCreateDate(latestArticle.getCreateDate());
+			article.setModifiedDate(serviceContext.getModifiedDate(now));
 			article.setClassNameId(latestArticle.getClassNameId());
 			article.setClassPK(latestArticle.getClassPK());
 			article.setArticleId(articleId);
 			article.setVersion(version);
 			article.setSmallImageId(latestArticle.getSmallImageId());
-
-			article.setModifiedDate(serviceContext.getModifiedDate(now));
 			article.setStatusByUserId(user.getUserId());
 			article.setStatusByUserName(user.getFullName());
 			article.setStatusDate(serviceContext.getModifiedDate(now));
