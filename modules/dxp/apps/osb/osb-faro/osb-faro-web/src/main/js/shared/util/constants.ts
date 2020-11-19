d85a814a08253452671ca49dc6fa2a770cc233d2
Masterfoni <joao.lima@liferay.com>
2020-11-19 16:08:19 -0300

    LRAC-6958 Add constants entry for AudienceCard viewerMode

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/util/constants.ts
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/util/constants.ts
@@ -192,6 +192,14 @@ export const TIME_RANGE_LABELS = {
 export const spritemap = '/o/osb-faro-web/dist/sprite.svg';
 
 /**
+ * AudienceCard viewer mode
+ */
+export enum AUDIENCE_VIEWER_MODE {
+	PREVIEW = 'PREVIEW',
+	VIEW = 'VIEW'
+}
+
+/**
  * Jobs
  */
 export enum jobRunStatuses {
