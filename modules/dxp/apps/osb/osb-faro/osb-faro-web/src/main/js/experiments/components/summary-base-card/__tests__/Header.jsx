89be8bcfbd06c8955f310a31b91fbabc50359d38
Masterfoni <joao.lima@liferay.com>
2020-11-27 10:26:35 -0300

    LRAC-6985 Change Header tests to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-base-card/__tests__/Header.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-base-card/__tests__/Header.jsx
@@ -1,32 +1,25 @@
 import Header from '../Header';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
 
 describe('SummaryBaseCard Header', () => {
 	it('should render component', () => {
-		const component = shallow(<Header title='My Header' />);
+		const {container} = render(<Header title='My Header' />);
 
-		expect(component.length).toBe(1);
-		expect(
-			component.find('SummaryBaseCardTitle').hasClass('mb-2')
-		).toBeTruthy();
-		expect(component.find('SummaryBaseCardTitle').props().label).toEqual(
-			'My Header'
-		);
-		expect(component.render()).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render component with Description', () => {
-		const component = shallow(
+		const {getByText} = render(
 			<Header
 				Description={() => <div>{'My Description'}</div>}
 				title='My Header'
 			/>
 		);
 
-		expect(
-			component.find('span').hasClass('font-size-sm font-weight-normal')
-		).toBeTruthy();
-		expect(component.find('Description').shallow()).toMatchSnapshot();
+		expect(getByText('My Header'));
+		expect(getByText('My Description'));
 	});
 });
