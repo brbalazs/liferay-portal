439ba0c0af972654c1e2e79e0f6f89320be0871e
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-12-03 11:58:23 +0100

    LPS-123940 Name is indexed differently in 7.1, so change accordingly

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/odata/entity/v1_0/ContentStructureEntityModel.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/odata/entity/v1_0/ContentStructureEntityModel.java
@@ -16,7 +16,6 @@ package com.liferay.headless.delivery.internal.odata.entity.v1_0;
 
 import com.liferay.headless.common.spi.odata.entity.EntityFieldsMapFactory;
 import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.odata.entity.DateTimeEntityField;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
@@ -40,10 +39,7 @@ public class ContentStructureEntityModel implements EntityModel {
 				locale -> Field.getSortableFieldName(Field.MODIFIED_DATE),
 				locale -> Field.MODIFIED_DATE),
 			new StringEntityField(
-				"name",
-				locale -> Field.getSortableFieldName(
-					"localized_name_".concat(
-						LocaleUtil.toLanguageId(locale)))));
+				"name", locale -> Field.getLocalizedName(locale, "name")));
 	}
 
 	@Override
