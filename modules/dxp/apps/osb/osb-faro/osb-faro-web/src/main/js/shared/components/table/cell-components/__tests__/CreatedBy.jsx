29ce97d2d149b9b9ae6b8bcad90afc4a0de7dce2
Francisco Dias <francisco.dias@liferay.com>
2020-12-03 13:56:55 -0300

    LRAC-7004 Convert CreatedBy test to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/cell-components/__tests__/CreatedBy.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/cell-components/__tests__/CreatedBy.jsx
@@ -1,19 +1,24 @@
 import * as data from 'test/data';
 import CreatedByCell from '../CreatedBy';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
+
+const tableRow = document.createElement('tr');
 
 describe('CreatedByCell', () => {
 	it('should render', () => {
-		const component = shallow(
+		const {container} = render(
 			<CreatedByCell
 				data={{
 					dateModified: data.getTimestamp(),
 					userName: 'Test Test'
 				}}
-			/>
+			/>,
+			{container: document.body.appendChild(tableRow)}
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 });
