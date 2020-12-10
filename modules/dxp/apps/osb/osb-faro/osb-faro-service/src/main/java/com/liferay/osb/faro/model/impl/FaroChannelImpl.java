b906ed102cfe5883ba7060823ecf5636e67b275e
Shinn Lok <shinn.lok@liferay.com>
2020-12-10 15:41:50 -0800

    LRAC-7030 Remove generated javadocs

--- modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/model/impl/FaroChannelImpl.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/model/impl/FaroChannelImpl.java
@@ -15,21 +15,10 @@
 package com.liferay.osb.faro.model.impl;
 
 /**
- * The extended model implementation for the FaroChannel service. Represents a row in the &quot;OSBFaro_FaroChannel&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.osb.faro.model.FaroChannel</code> interface.
- * </p>
- *
  * @author Matthew Kong
  */
 public class FaroChannelImpl extends FaroChannelBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a faro channel model instance should use the {@link com.liferay.osb.faro.model.FaroChannel} interface instead.
-	 */
 	public FaroChannelImpl() {
 	}
 
