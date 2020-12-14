518a556059330488cb6c370f7242b05e1cff8b02
Masterfoni <joao.lima@liferay.com>
2020-12-14 18:06:02 -0300

    LRAC-7054 Use RTL on HeaderCell component tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/__tests__/HeaderCell.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/__tests__/HeaderCell.jsx
@@ -1,20 +1,37 @@
 import HeaderCell from '../HeaderCell';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
 
 describe('HeaderCell', () => {
 	it('should render', () => {
-		const component = shallow(<HeaderCell />);
-		expect(component).toMatchSnapshot();
+		const {container} = render(
+			<HeaderCell>
+				<p>{'children'}</p>
+			</HeaderCell>
+		);
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render with sort disabled', () => {
-		const component = shallow(<HeaderCell sortable={false} />);
-		expect(component).toMatchSnapshot();
+		const {container} = render(<HeaderCell sortable={false} />);
+
+		expect(container.querySelector('.button-root')).toBeFalsy();
 	});
 
 	it('should render the header cell as a link if headerLink is true', () => {
-		const component = shallow(<HeaderCell headerLink />);
-		expect(component).toMatchSnapshot();
+		const {container} = render(
+			<StaticRouter>
+				<HeaderCell headerLink />
+			</StaticRouter>
+		);
+
+		expect(container.querySelector('.button-root')).toHaveAttribute(
+			'href',
+			'/?orderBy=asc&orderByField&page=1'
+		);
 	});
 });
