628d6fc5c2b477ae6111019334017af3d79f4050
Masterfoni <joao.lima@liferay.com>
2020-12-15 18:29:50 -0300

    LRAC-7055 Migrate table/Row tests to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/__tests__/Row.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/table/__tests__/Row.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Row from '../Row';
 import {mockIndividual} from 'test/data';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
+
 const INDIVIDUAL = mockIndividual();
 
 const INDIVIDUAL_NO_SALARY = mockIndividual(1, {
@@ -22,24 +25,27 @@ const COLUMNS = [
 
 describe('Row', () => {
 	it('should render', () => {
-		const component = shallow(<Row />);
-		expect(component).toMatchSnapshot();
+		const {container} = render(<Row />);
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render with data', () => {
-		const component = shallow(<Row columns={COLUMNS} data={INDIVIDUAL} />);
-		expect(component).toMatchSnapshot();
+		const {container} = render(<Row columns={COLUMNS} data={INDIVIDUAL} />);
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render with empty data if the column accessor value is null', () => {
-		const component = shallow(
+		const {getByText} = render(
 			<Row columns={COLUMNS} data={INDIVIDUAL_NO_SALARY} />
 		);
-		expect(component).toMatchSnapshot();
+
+		expect(getByText('-')).toBeTruthy();
 	});
 
 	it('should render with empty data if the accessor does not exist at all on the object', () => {
-		const component = shallow(
+		const {getByText} = render(
 			<Row
 				columns={COLUMNS.concat([
 					{accessor: 'nonExistentAccessor', label: 'does not exist'}
@@ -48,13 +54,14 @@ describe('Row', () => {
 			/>
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(getByText('-')).toBeTruthy();
 	});
 
 	it('should render with the header', () => {
-		const component = shallow(
+		const {container} = render(
 			<Row columns={COLUMNS} data={INDIVIDUAL} header />
 		);
-		expect(component).toMatchSnapshot();
+
+		expect(container.querySelector('.table-head-title')).toBeTruthy();
 	});
 });
