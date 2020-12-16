219590dfe29525f585dac40c57eccb70140f15ef
Masterfoni <joao.lima@liferay.com>
2020-12-16 17:08:04 -0300

    LRAC-7098 Use RTL on summary-section/MetricType tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-section/__tests__/MetricType.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-section/__tests__/MetricType.jsx
@@ -1,17 +1,13 @@
 import MetricType from '../MetricType';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
 
 describe('SummarySection MetricType', () => {
 	it('should render component', () => {
-		const component = shallow(<MetricType value='Click rate' />);
+		const {container} = render(<MetricType value='Click rate' />);
 
-		expect(
-			component.hasClass('analytics-summary-section-metric-type')
-		).toBeTruthy();
-		expect(component.find('ClayIcon').props().symbol).toEqual(
-			'web-content'
-		);
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 });
