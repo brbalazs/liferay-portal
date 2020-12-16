eb18f35e2c44537be2e6b794b664f5c618fedcc5
Geyson Silva <geyson.silva@liferay.com>
2020-12-16 11:59:12 -0300

    LRAC-7030 Create service

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-service/src/main/java/com/liferay/osb/faro/service/impl/FaroNotificationLocalServiceImpl.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.faro.service.impl;
+
+import com.liferay.osb.faro.model.FaroNotification;
+import com.liferay.osb.faro.service.base.FaroNotificationLocalServiceBaseImpl;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+
+import java.util.List;
+
+/**
+ * @author Geyson Silva
+ */
+public class FaroNotificationLocalServiceImpl
+	extends FaroNotificationLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public FaroNotification addFaroNotification(
+		long userId, long groupId, String scope, String type, String subType) {
+
+		long faroNotificationId = counterLocalService.increment();
+
+		FaroNotification faroNotification = faroNotificationPersistence.create(
+			faroNotificationId);
+
+		faroNotification.setGroupId(groupId);
+		faroNotification.setUserId(userId);
+
+		long now = System.currentTimeMillis();
+
+		faroNotification.setCreateTime(now);
+		faroNotification.setModifiedTime(now);
+
+		faroNotification.setRead(false);
+		faroNotification.setScope(scope);
+		faroNotification.setType(type);
+		faroNotification.setSubType(subType);
+
+		return faroNotificationPersistence.update(faroNotification);
+	}
+
+	@Override
+	public List<FaroNotification> findFaroNotificationsByG_S_R(
+		long groupId, String scope, boolean read) {
+
+		return faroNotificationPersistence.findByG_S_R(groupId, scope, read);
+	}
+
+	@Override
+	public List<FaroNotification> findFaroNotificationsByU_S_R(
+		long userId, String scope, boolean read) {
+
+		return faroNotificationPersistence.findByU_S_R(userId, scope, read);
+	}
+
+}
