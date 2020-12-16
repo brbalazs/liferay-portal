96a84dab6bbfd9a0765df5359e4fbdc47f48e747
Kien Do <kien.do@liferay.com>
2020-12-16 13:27:23 -0800

    LRAC-7104 Convert data-source Salesforce Authorization page tests to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/salesforce/__tests__/Authorization.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/salesforce/__tests__/Authorization.jsx
@@ -1,40 +1,57 @@
 import * as data from 'test/data';
 import FaroConstants from 'shared/util/constants';
+import mockStore from 'test/mock-store';
 import React from 'react';
 import {DataSource, User} from 'shared/util/records';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
 import {SalesforceAuthorization} from '../Authorization';
-import {shallow} from 'enzyme';
+import {StaticRouter} from 'react-router-dom';
+
+jest.unmock('react-dom');
 
 const {
 	userRoleNames: {member}
 } = FaroConstants;
 
-const defaultProps = {
-	currentUser: data.getImmutableMock(User, data.mockUser),
-	dataSource: data.getImmutableMock(DataSource, data.mockLiferayDataSource),
-	groupId: '23',
-	id: 'test'
-};
+const DefaultComponent = props => (
+	<Provider store={mockStore()}>
+		<StaticRouter>
+			<SalesforceAuthorization
+				currentUser={data.getImmutableMock(User, data.mockUser)}
+				dataSource={data.getImmutableMock(
+					DataSource,
+					data.mockLiferayDataSource
+				)}
+				groupId='23'
+				id='test'
+				{...props}
+			/>
+		</StaticRouter>
+	</Provider>
+);
 
 describe('SalesforceAuthorization', () => {
 	it('should render', () => {
-		const component = shallow(
-			<SalesforceAuthorization {...defaultProps} />
-		);
+		const {container, queryByText} = render(<DefaultComponent />);
 
-		expect(component).toMatchSnapshot();
+		expect(queryByText('Delete Data Source')).toBeTruthy();
+		expect(queryByText('Edit')).toBeTruthy();
+		expect(queryByText('Start Upgrade...')).toBeTruthy();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render as read-only if the user is not authorized to make changes', () => {
-		const component = shallow(
-			<SalesforceAuthorization
-				{...defaultProps}
-				currentUser={data.getImmutableMock(User, data.mockUser, '23', {
+		const {queryByText} = render(
+			<DefaultComponent
+				currentUser={data.getImmutableMock(User, data.mockUser, '24', {
 					roleName: member
 				})}
 			/>
 		);
 
-		expect(component.children().prop('authorized')).toBe(false);
+		expect(queryByText('Delete Data Source')).toBeNull();
+		expect(queryByText('Edit')).toBeNull();
+		expect(queryByText('Start Upgrade...')).toBeNull();
 	});
 });
