eb5efd7b8dc6dcccaf6a11dea8b80d0c9ec5029a
Masterfoni <joao.lima@liferay.com>
2020-12-17 16:18:04 -0300

    LRAC-7100 Use RTL on BaseTabsPage tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/data-source/__tests__/BaseTabsPage.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/data-source/__tests__/BaseTabsPage.jsx
@@ -1,14 +1,19 @@
 import * as data from 'test/data';
 import BaseTabsPage from '../BaseTabsPage';
 import FaroConstants from 'shared/util/constants';
+import mockStore from 'test/mock-store';
 import React from 'react';
 import {DataSource, User} from 'shared/util/records';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
 import {Routes} from 'shared/util/router';
-import {shallow} from 'enzyme';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
 
 const {dataSourceStates} = FaroConstants;
 
-function getMockLiferayDataSource(id, config) {
+function getLiferayDataSourceMock(id, config) {
 	return data.getImmutableMock(
 		DataSource,
 		data.mockLiferayDataSource,
@@ -17,18 +22,27 @@ function getMockLiferayDataSource(id, config) {
 	);
 }
 
-const defaultProps = {
-	addRoute: Routes.SETTINGS_LIFERAY_ADD,
-	configurationRoute: Routes.SETTINGS_LIFERAY_CONFIGURATION_STATUS,
-	currentUser: new User(data.mockUser()),
-	dataSource: getMockLiferayDataSource(1, {
-		provider: {
-			type: 'LIFERAY'
-		}
-	}),
-	groupId: '23',
-	id: 'test'
-};
+const DefaultComponent = props => (
+	<StaticRouter>
+		<Provider store={mockStore()}>
+			<BaseTabsPage
+				addRoute={Routes.SETTINGS_LIFERAY_ADD}
+				configurationRoute={
+					Routes.SETTINGS_LIFERAY_CONFIGURATION_STATUS
+				}
+				currentUser={new User(data.mockUser())}
+				dataSource={getLiferayDataSourceMock(1, {
+					provider: {
+						type: 'LIFERAY'
+					}
+				})}
+				groupId='23'
+				id='test'
+				{...props}
+			/>
+		</Provider>
+	</StaticRouter>
+);
 
 const allConfigured = {
 	provider: {
@@ -40,68 +54,68 @@ const allConfigured = {
 
 describe('BaseTabsPage', () => {
 	it('should render', () => {
-		const component = shallow(<BaseTabsPage {...defaultProps} />);
+		const {container} = render(<DefaultComponent />);
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render without a sticker in the configure datasource tab if all items are configured', () => {
-		const component = shallow(
-			<BaseTabsPage
-				{...defaultProps}
-				dataSource={getMockLiferayDataSource(1, allConfigured)}
+		const {container} = render(
+			<DefaultComponent
+				dataSource={getLiferayDataSourceMock(1, allConfigured)}
 			/>
 		);
 
-		expect(component.find('.badge').length).toBe(0);
+		expect(container.querySelector('.badge')).toBeFalsy();
 	});
 
 	it('should render with the CONFIGURE_DATA_SOURCE tab as active', () => {
-		const component = shallow(
-			<BaseTabsPage {...defaultProps} activeTabId='configureDataSource' />
+		const {getAllByText} = render(
+			<DefaultComponent activeTabId='configureDataSource' />
 		);
 
 		expect(
-			component
-				.find('Item')
-				.at(1)
-				.prop('active')
-		).toBe(true);
+			getAllByText('Configure Data Source')[1].parentElement
+		).toHaveClass('active');
 	});
 
 	it('should render with the CONFIGURE_DATA_SOURCE tab enabled if state is ready', () => {
-		const component = shallow(
-			<BaseTabsPage
-				{...defaultProps}
-				dataSource={getMockLiferayDataSource(0, {
+		const {getAllByText, getByText} = render(
+			<DefaultComponent
+				dataSource={getLiferayDataSourceMock(0, {
 					state: dataSourceStates.ready
 				})}
 			/>
 		);
 
 		expect(
-			component
-				.find('Item')
-				.at(1)
-				.prop('disabled')
-		).toBe(false);
+			getAllByText('Configure Data Source')[1].parentElement
+		).not.toHaveClass('disabled');
+		expect(getByText('Active')).toBeTruthy();
+		expect(
+			getByText(
+				'All data coming from this data source is up to date. There are no errors to report.'
+			)
+		).toBeTruthy();
 	});
 
 	it('should render with the CONFIGURE_DATA_SOURCE tab disabled if state is not valid', () => {
-		const component = shallow(
-			<BaseTabsPage
-				{...defaultProps}
-				dataSource={getMockLiferayDataSource(0, {
+		const {getAllByText, getByText} = render(
+			<DefaultComponent
+				dataSource={getLiferayDataSourceMock(0, {
 					state: dataSourceStates.credentialsInvalid
 				})}
 			/>
 		);
 
 		expect(
-			component
-				.find('Item')
-				.at(1)
-				.prop('disabled')
-		).toBe(true);
+			getAllByText('Configure Data Source')[1].parentElement
+		).toHaveClass('disabled');
+		expect(getByText('Invalid Credentials')).toBeTruthy();
+		expect(
+			getByText(
+				'The authorization for this data source has expired. Please reauthorize the token in the oAuth tab.'
+			)
+		).toBeTruthy();
 	});
 });
