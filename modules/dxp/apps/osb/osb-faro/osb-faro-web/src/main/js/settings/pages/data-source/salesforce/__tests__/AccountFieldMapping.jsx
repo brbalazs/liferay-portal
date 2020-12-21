e27cb8ddbf45d78e9387539fcd2f37112224115b
Kien Do <kien.do@liferay.com>
2020-12-21 13:01:10 -0800

    LRAC-7146 Convert AccountFieldMapping tests to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/salesforce/__tests__/AccountFieldMapping.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/data-source/salesforce/__tests__/AccountFieldMapping.jsx
@@ -1,18 +1,36 @@
 import * as data from 'test/data';
+import mockStore from 'test/mock-store';
 import React from 'react';
 import {AccountFieldMapping} from '../AccountFieldMapping';
-import {shallow} from 'enzyme';
+import {DataSource, User} from 'shared/util/records';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
 
 const defaultProps = {
-	dataSource: data.mockSalesforceDataSource(),
+	currentUser: data.getImmutableMock(User, data.mockUser),
+	dataSource: data.getImmutableMock(
+		DataSource,
+		data.mockSalesforceDataSource
+	),
 	groupId: '23',
 	id: '27'
 };
 
 describe('AccountFieldMapping', () => {
 	it('should render', () => {
-		const component = shallow(<AccountFieldMapping {...defaultProps} />);
+		const {container} = render(
+			<Provider store={mockStore()}>
+				<StaticRouter>
+					<AccountFieldMapping {...defaultProps} />
+				</StaticRouter>
+			</Provider>
+		);
 
-		expect(component.shallow()).toMatchSnapshot();
+		jest.runAllTimers();
+
+		expect(container).toMatchSnapshot();
 	});
 });
