97d09066cee9494a6e32d1ff5be4ece2f2dd6a95
Masterfoni <joao.lima@liferay.com>
2020-12-21 20:12:55 -0300

    LRAC-7139 Add comments clarifying test cases

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/__tests__/InterestPagesList.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/__tests__/InterestPagesList.jsx
@@ -5,6 +5,12 @@ import {StaticRouter} from 'react-router';
 
 jest.unmock('react-dom');
 
+/**
+ * For ActivePagesList (active: true) validation, It's possible to order by
+ * unique visits count, whereas for InactivePagesList (active: false)
+ * it's not, that's why we're validating by the presence
+ * of the order button on those tests.
+ */
 describe('InterestPagesList', () => {
 	it('should render', () => {
 		const {container} = render(
@@ -17,26 +23,30 @@ describe('InterestPagesList', () => {
 	});
 
 	it('should render an activePages component', () => {
-		const {container} = render(
+		const {getByText} = render(
 			<StaticRouter>
 				<InterestPagesList dataSourceParams={{active: true}} />
 			</StaticRouter>
 		);
 
 		expect(
-			container.querySelector('.lexicon-icon-order-arrow-down')
+			getByText('Views').parentElement.querySelector(
+				'.lexicon-icon-order-arrow-down'
+			)
 		).toBeTruthy();
 	});
 
 	it('should render an InactivePages component', () => {
-		const {container} = render(
+		const {getByText} = render(
 			<StaticRouter>
 				<InterestPagesList dataSourceParams={{active: false}} />
 			</StaticRouter>
 		);
 
 		expect(
-			container.querySelector('.lexicon-icon-order-arrow-down')
+			getByText('Views').parentElement.querySelector(
+				'.lexicon-icon-order-arrow-down'
+			)
 		).toBeFalsy();
 	});
 });
