cc241d43ea351d86bf3f1ffa4bdf61c2c57a3fff
Masterfoni <joao.lima@liferay.com>
2020-12-21 17:18:36 -0300

    LRAC-7141 Use RTL on Toolbar tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/toolbar/__tests__/Toolbar.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/toolbar/__tests__/Toolbar.jsx
@@ -1,42 +1,47 @@
 import React from 'react';
 import Toolbar from '../index';
 import {Map, Set} from 'immutable';
-import {render} from 'enzyme';
+import {render} from '@testing-library/react';
 import {withStaticRouter} from 'test/mock-router';
+
+jest.unmock('react-dom');
 
 const DefaultComponent = withStaticRouter(Toolbar);
 
 describe('Toolbar', () => {
 	it('should render', () => {
-		const component = render(<DefaultComponent />);
+		const {container} = render(<DefaultComponent />);
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should NOT render with a search input when alwaysShowSearch is false', () => {
-		const component = render(
+		const {queryByPlaceholderText} = render(
 			<DefaultComponent
 				alwaysShowSearch={false}
 				selectEntirePageIndeterminate
 			/>
 		);
-		expect(component).toMatchSnapshot();
+
+		expect(queryByPlaceholderText('Search')).toBeNull();
 	});
 
 	it('should render with a search input when alwaysShowSearch is true', () => {
-		const component = render(
+		const {getByPlaceholderText} = render(
 			<DefaultComponent alwaysShowSearch selectEntirePageIndeterminate />
 		);
-		expect(component).toMatchSnapshot();
+
+		expect(getByPlaceholderText('Search')).toBeTruthy();
 	});
 
 	it('should render as disabled', () => {
-		const component = render(<DefaultComponent disabled />);
-		expect(component).toMatchSnapshot();
+		const {getByPlaceholderText} = render(<DefaultComponent disabled />);
+
+		expect(getByPlaceholderText('Search')).toBeDisabled();
 	});
 
 	it('should render w/ a search query bar when there is a query', () => {
-		const component = render(
+		const {container, getByText} = render(
 			<DefaultComponent
 				alwaysShowSearch
 				query='Test'
@@ -44,11 +49,13 @@ describe('Toolbar', () => {
 			/>
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(getByText('Test')).toBeTruthy();
+		expect(container.querySelector('.management-bar-primary')).toBeTruthy();
+		expect(container.querySelector('.items-selected')).toBeTruthy();
 	});
 
 	it('should render a list of filter tags when there are active filters', () => {
-		const component = render(
+		const {getByText} = render(
 			<DefaultComponent
 				alwaysShowSearch
 				filterBy={new Map({fooField: new Set(['fooValue'])})}
@@ -62,6 +69,6 @@ describe('Toolbar', () => {
 			/>
 		);
 
-		expect(component).toMatchSnapshot();
+		expect(getByText('fooValue')).toBeTruthy();
 	});
 });
