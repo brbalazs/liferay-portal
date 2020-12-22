fef9fc0b83feb9f6a210dd0cf13cb936a65bf8e8
Eric Yan <eric.yan@liferay.com>
2020-12-22 12:41:50 -0800

    LPS-125083 portal-impl: SF

--- portal-impl/src/com/liferay/portlet/asset/service/impl/AssetCategoryLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/asset/service/impl/AssetCategoryLocalServiceImpl.java
@@ -814,6 +814,7 @@ public class AssetCategoryLocalServiceImpl
 
 	private long _getAssetCategoriesCount(long groupId) {
 		DynamicQuery dynamicQuery = assetCategoryLocalService.dynamicQuery();
+
 		Property groupIdProperty = PropertyFactoryUtil.forName("groupId");
 
 		dynamicQuery.add(groupIdProperty.eq(groupId));
