02760cd536f2a528509ad99ae2f01c7192f7c982
Masterfoni <joao.lima@liferay.com>
2020-12-22 15:41:26 -0300

    LRAC-7153 Use RTL on WithSelectedPoint test

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/__tests__/WithSelectedPoint.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/__tests__/WithSelectedPoint.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import withSelectedPoint from '../WithSelectedPoint';
-import {shallow} from 'enzyme';
+import {fireEvent, render} from '@testing-library/react';
+
+jest.unmock('react-dom');
 
 describe('withSelectedPoint', () => {
 	it('should render the wrapped component', () => {
 		const WrappedComponent = withSelectedPoint(() => <div>{'foo'}</div>);
-		const component = shallow(<WrappedComponent />);
-		expect(component.shallow()).toMatchSnapshot();
+
+		const {container} = render(<WrappedComponent />);
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should pass the selected point to the wrapped component', () => {
-		const WrappedComponent = withSelectedPoint(jest.fn());
-		const component = shallow(<WrappedComponent />);
-		component.props().onPointSelect({index: 2});
-		expect(component.props().selectedPoint).toEqual(2);
-		expect(component.props().hasSelectedPoint).toBe(true);
+		const WrappedComponent = withSelectedPoint(
+			({hasSelectedPoint, onPointSelect, selectedPoint}) => (
+				<div>
+					<span>
+						{hasSelectedPoint
+							? 'There is a selected point'
+							: 'There is no selected point'}
+					</span>
+					<button onClick={() => onPointSelect({index: 1})}>
+						{'bar'}
+						{selectedPoint}
+					</button>
+				</div>
+			)
+		);
+
+		const {getByText} = render(<WrappedComponent hasSelectedPoint />);
+
+		fireEvent.click(getByText('bar'));
+
+		expect(getByText('bar1')).toBeTruthy();
+		expect(getByText('There is a selected point'));
 	});
 });
