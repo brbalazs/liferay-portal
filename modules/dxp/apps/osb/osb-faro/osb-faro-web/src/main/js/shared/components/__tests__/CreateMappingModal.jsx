e6da39316b1ad4f03397e6c5cf2408ca247f292a
Kien Do <kien.do@liferay.com>
2020-12-21 16:11:12 -0800

    LRAC-7149 Convert CreateMappingModal tests to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/__tests__/CreateMappingModal.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/__tests__/CreateMappingModal.jsx
@@ -1,27 +1,16 @@
 import CreateMappingModal from '../CreateMappingModal';
 import React from 'react';
 import {noop} from 'lodash';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
 
 describe('CreateMappingModal', () => {
 	it('should render', () => {
-		const component = shallow(
+		const {container} = render(
 			<CreateMappingModal groupId='23' onClose={noop} />
 		);
-		expect(component).toMatchSnapshot();
-	});
 
-	it('should call onSubmit during handleSubmit', () => {
-		const onSubmit = jest.fn();
-		const component = shallow(
-			<CreateMappingModal
-				groupId='23'
-				onClose={noop}
-				onSubmit={onSubmit}
-			/>
-		);
-		component.instance().handleSubmit({type: 'foo'});
-		jest.runAllTimers();
-		expect(onSubmit).toBeCalled();
+		expect(container).toMatchSnapshot();
 	});
 });
