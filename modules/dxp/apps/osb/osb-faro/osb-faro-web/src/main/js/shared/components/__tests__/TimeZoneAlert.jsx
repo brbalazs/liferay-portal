a736dcc880a117adb44e5ace4287a51ce1fa5a25
Masterfoni <joao.lima@liferay.com>
2020-12-30 13:26:34 -0300

    LRAC-6961 Update snapshots for TimeZoneAlert tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/__tests__/TimeZoneAlert.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/__tests__/TimeZoneAlert.jsx
@@ -1,15 +1,13 @@
 import mockStore from 'test/mock-store';
 import React from 'react';
 import TimeZoneAlert from '../TimeZoneAlert';
-import {cleanup, render} from '@testing-library/react';
 import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
 import {StaticRouter} from 'react-router';
 
 jest.unmock('react-dom');
 
 describe('TimeZoneAlert', () => {
-	afterEach(cleanup);
-	// TODO: Updates the snapshot when add the request of TimeZoneAlert LRAC-6961
 	it('should render', () => {
 		const {container} = render(
 			<Provider store={mockStore()}>
