cb5f4fdfac8f0f9542b49423bc22fbc987e58ef9
Orsolya Dekany <orsolya.dekany@liferay.com>
2021-01-07 11:06:08 +0100

    LPS-125296 Partial backport of LPS-118213: Add stylescombo plugin to extraPlugins
    Original commit message: LPS-118213 Provides new toolbar presets simple, text_advanced and text_simple bda5b60bb89fce0e32c8e031c1caba8584f88cbf

--- modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/internal/editor/configuration/CKEditorConfigContributor.java
+++ modules/apps/frontend-editor/frontend-editor-ckeditor-web/src/main/java/com/liferay/frontend/editor/ckeditor/web/internal/editor/configuration/CKEditorConfigContributor.java
@@ -77,7 +77,7 @@ public class CKEditorConfigContributor extends BaseCKEditorConfigContributor {
 
 		String extraPlugins =
 			"a11yhelpbtn,filebrowser,find,flash,itemselector," +
-				"lfrpopup,media,selectall,smiley,specialchar";
+				"lfrpopup,media,selectall,smiley,specialchar,stylescombo";
 
 		boolean inlineEdit = GetterUtil.getBoolean(
 			(String)inputEditorTaglibAttributes.get(
