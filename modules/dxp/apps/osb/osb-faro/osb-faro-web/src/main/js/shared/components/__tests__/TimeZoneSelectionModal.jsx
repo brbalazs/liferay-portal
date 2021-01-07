6ea28f471ea59aca78cb96c7664b78c806de7a43
Masterfoni <joao.lima@liferay.com>
2021-01-07 12:18:04 -0300

    LRAC-6881 Update timezoneselectionmodal tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/__tests__/TimeZoneSelectionModal.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/__tests__/TimeZoneSelectionModal.jsx
@@ -1,25 +1,24 @@
 import * as data from 'test/data';
+import mockStore from 'test/mock-store';
 import React from 'react';
 import TimeZoneSelectionModal from '../TimeZoneSelectionModal';
-import {cleanup, render} from '@testing-library/react';
 import {mockGetDateNow} from 'test/mock-date';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
 
 jest.unmock('react-dom');
 
-const TIME_ZONE = {
-	timeZoneId: 'UTC'
-};
-
 describe('TimeZoneSelectionModal', () => {
-	beforeAll(() => {
-		mockGetDateNow(data.getTimestamp(0));
-	});
-
-	afterEach(cleanup);
+	mockGetDateNow(data.getTimestamp(0));
 
 	it('should render', () => {
 		const {container} = render(
-			<TimeZoneSelectionModal timeZone={TIME_ZONE} />
+			<Provider store={mockStore()}>
+				<StaticRouter>
+					<TimeZoneSelectionModal groupId={23} />
+				</StaticRouter>
+			</Provider>
 		);
 
 		jest.runAllTimers();
