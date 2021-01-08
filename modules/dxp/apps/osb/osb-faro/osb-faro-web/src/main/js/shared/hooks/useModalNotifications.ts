736573fb0da336357a88f181f64a85f169a8b8c4
Masterfoni <joao.lima@liferay.com>
2021-01-08 11:20:13 -0300

    LRAC-6881 Create useModalNotifications hook

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hooks/useModalNotifications.ts
@@ -0,0 +1,72 @@
+import * as API from 'shared/api';
+import Notification, {
+	NotificationSubtype,
+	NotificationType
+} from 'shared/util/records/Notification';
+import {Modal} from 'shared/types';
+import {modalTypes} from 'shared/actions/modals';
+import {useRequest} from 'shared/hooks';
+
+interface ITimeZoneAdminModalRenderProps {
+	close: Modal.close;
+	groupId: string;
+	notificationId: string;
+	open: Modal.open;
+}
+
+const renderTimeZoneAdminModal = (
+	modalType: Modal.modalTypes,
+	{close, groupId, notificationId, open}
+) =>
+	open(modalType, {
+		groupId,
+		notificationId,
+		onClose: close
+	});
+
+const modalNotificationStrategies = new Map<string, Function>([
+	[
+		NotificationSubtype.TIME_ZONE_ADMIN,
+		(params: ITimeZoneAdminModalRenderProps) =>
+			renderTimeZoneAdminModal(
+				modalTypes.TIME_ZONE_SELECTION_MODAL,
+				params
+			)
+	]
+]);
+
+function useModalNotifications(close: Modal.close, groupId: string, open: Modal.open): void {
+	const {data, loading} = useRequest(API.notifications.fetchNotifications, {
+		groupId,
+		type: NotificationType.MODAL
+	});
+
+	const handleRender = (notificationList: Array<Notification>): void => {
+		const notificationToRender = notificationList.pop();
+
+		if (notificationToRender) {
+			const renderFn = modalNotificationStrategies.get(
+				notificationToRender.subtype
+			);
+
+			const onClose = (notificationList: Array<Notification>) => {
+				handleRender(notificationList);
+				close();
+			};
+
+			renderFn({
+				close: () => onClose(notificationList),
+				groupId,
+				notificationId: notificationToRender.id,
+				open
+			});
+		}
+	};
+
+	if (!loading && data.length) {
+		close();
+		handleRender([...data]);
+	}
+}
+
+export default useModalNotifications;
