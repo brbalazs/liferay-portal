571950ce1d564c0e1579544616c7aa74a60bfe79
Roland Pákai <roland.pakai@liferay.com>
2021-01-14 18:26:16 +0100

    LPS-125881 Fix Liferay.Util.openWindow config title value

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util_window.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util_window.js
@@ -411,7 +411,7 @@ AUI.add(
 					}
 
 					if (!Lang.isValue(config.title)) {
-						config.title = '&nbsp;';
+						config.title = '';
 					}
 
 					modal.titleNode.html(Lang.String.escapeHTML(config.title));
