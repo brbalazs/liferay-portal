ede2d3c78418599fcadce47c63c230e4aa7b2597
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-01-26 18:02:31 -0300

    LRAC-7260 Create Attribute List and put in the Event component

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/pages/events/AttributeList.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {attributesColumns} from 'shared/util/table-columns';
+import {withBaseResults} from 'shared/hoc';
+
+// TODO: LRAC-7330 Use the graphql query instead of mocked data
+const withData = () => WrapperComponent => props => {
+	const items = [
+		{
+			attributeId: 'myid1',
+			defaultDataType: 'TYPE',
+			description: 'mydescription',
+			displayName: 'displayNamehere',
+			name: 'firstTest',
+			sampleValue: '1'
+		},
+		{
+			attributeId: 'myid2',
+			defaultDataType: 'TYPE2',
+			description: 'seconddescription',
+			displayName: 'seconddisplay',
+			name: 'testingtest',
+			sampleValue: '2'
+		},
+		{
+			attributeId: 'myid3',
+			defaultDataType: 'TYPE3',
+			description: 'mydescription',
+			displayName: 'displayNamehere',
+			name: 'anothernamet',
+			sampleValue: '3'
+		}
+	];
+	return <WrapperComponent {...props} items={items}></WrapperComponent>;
+};
+
+const AttributeList = withBaseResults(withData, {
+	getColumns: ({channelId, groupId}) => [
+		attributesColumns.getName({channelId, groupId}),
+		attributesColumns.displayName,
+		attributesColumns.description,
+		attributesColumns.sampleValue,
+		attributesColumns.defaultDataType
+	],
+	rowIdentifier: 'attributeId',
+	showDropdownRangeKey: false
+});
+
+export default AttributeList;
