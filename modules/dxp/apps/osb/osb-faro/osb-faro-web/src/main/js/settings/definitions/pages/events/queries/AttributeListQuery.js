3c57d3b96734b3db76a5d4112c48f3f43fa8c1fd
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-01-26 17:56:27 -0300

    LRAC-7260 Create Attribute List query

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/pages/events/queries/AttributeListQuery.js
@@ -0,0 +1,29 @@
+import {gql} from 'apollo-boost';
+
+export default gql`
+	query AttributeList(
+		$channelId: String
+		$eventId: String
+		$keywords: String
+		$size: Int!
+		$start: Int!
+	) {
+		attributes(
+			channelId: $channelId
+			eventId: $eventId
+			keywords: $keywords
+			size: $size
+			start: $start
+		) {
+			attributes {
+				name
+				attributeId
+				displayName
+				defaultDataType
+				sampleValue
+				description
+			}
+			total
+		}
+	}
+`;
