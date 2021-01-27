de36a573660faad55b73f7943d4e751eb677d0ee
Kien Do <kien.do@liferay.com>
2021-01-27 10:05:19 -0800

    LRAC-7342 Disable product analytics on UAT

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/external-scripts.js
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/external-scripts.js
@@ -16,16 +16,22 @@ if (FARO_ENV === 'prd') {
 	key = 'tcXcIh0trmFf4rxL3hgErPkrLxBBYeZy';
 }
 
-const scripts = [
+let scripts = [
 	/* Google Tag Manager */
 	{
 		innerHTML:
 			"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NHH7QQ7');"
-	},
-	{
-		innerHTML: `(function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error('Segment snippet included twice.');else{analytics.invoked=!0;analytics.methods=['trackSubmit','trackClick','trackLink','trackForm','pageview','identify','reset','group','track','ready','alias','debug','page','once','off','on','addSourceMiddleware','addIntegrationMiddleware','setAnonymousId','addDestinationMiddleware'];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement('script');n.type='text/javascript';n.async=!0;n.src='https://cdn.segment.com/analytics.js/v1/'+t+'/analytics.min.js';var a=document.getElementsByTagName('script')[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION='4.1.0'; analytics.load('${key}');analytics.page({}, {ip: '0'});}})()`
 	}
 ];
+
+if (FARO_ENV !== 'uat') {
+	scripts = [
+		...scripts,
+		{
+			innerHTML: `(function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error('Segment snippet included twice.');else{analytics.invoked=!0;analytics.methods=['trackSubmit','trackClick','trackLink','trackForm','pageview','identify','reset','group','track','ready','alias','debug','page','once','off','on','addSourceMiddleware','addIntegrationMiddleware','setAnonymousId','addDestinationMiddleware'];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement('script');n.type='text/javascript';n.async=!0;n.src='https://cdn.segment.com/analytics.js/v1/'+t+'/analytics.min.js';var a=document.getElementsByTagName('script')[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION='4.1.0'; analytics.load('${key}');analytics.page({}, {ip: '0'});}})()`
+		}
+	];
+}
 
 /**
  * Runtime logic for adding external scripts to the page.
