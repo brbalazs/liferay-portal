81578dfa7ad85ba891fdd74783acc4711500a043
Istvan Sajtos <istvan.sajtos@liferay.com>
2021-02-04 17:52:36 +0100

    LPS-124963 OpenIdConnectSession is now converted to byte array before storing in HttpSession. Add it to HttpSession each time when modified.

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectServiceHandlerImpl.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectServiceHandlerImpl.java
@@ -184,6 +184,9 @@ public class OpenIdConnectServiceHandlerImpl
 
 		openIdConnectSessionImpl.setOpenIdConnectFlowState(
 			OpenIdConnectFlowState.AUTH_COMPLETE);
+
+		OpenIdConnectUtil.setOpenIdConnectSession(
+			httpSession, openIdConnectSessionImpl);
 	}
 
 	@Override
@@ -204,8 +207,8 @@ public class OpenIdConnectServiceHandlerImpl
 			getOpenIdConnectSessionImpl(httpSession, openIdConnectProviderName);
 
 		if (openIdConnectSessionImpl == null) {
-			openIdConnectSessionImpl = createAndSetOpenIdConnectSession(
-				httpSession, openIdConnectProviderName);
+			openIdConnectSessionImpl = new OpenIdConnectSessionImpl(
+				openIdConnectProviderName, new Nonce(), new State());
 		}
 
 		URI authenticationRequestURI = getAuthenticationRequestURI(
@@ -220,6 +223,9 @@ public class OpenIdConnectServiceHandlerImpl
 
 			openIdConnectSessionImpl.setOpenIdConnectFlowState(
 				OpenIdConnectFlowState.AUTH_REQUESTED);
+
+			OpenIdConnectUtil.setOpenIdConnectSession(
+				httpSession, openIdConnectSessionImpl);
 		}
 		catch (IOException ioe) {
 			throw new SystemException(
@@ -229,19 +235,6 @@ public class OpenIdConnectServiceHandlerImpl
 					ioe.getMessage()),
 				ioe);
 		}
-	}
-
-	protected OpenIdConnectSessionImpl createAndSetOpenIdConnectSession(
-		HttpSession httpSession, String openIdConnectProviderName) {
-
-		OpenIdConnectSessionImpl openIdConnectSessionImpl =
-			new OpenIdConnectSessionImpl(
-				openIdConnectProviderName, new Nonce(), new State());
-
-		OpenIdConnectUtil.setOpenIdConnectSession(
-			httpSession, openIdConnectSessionImpl);
-
-		return openIdConnectSessionImpl;
 	}
 
 	protected URI getAuthenticationRequestURI(
