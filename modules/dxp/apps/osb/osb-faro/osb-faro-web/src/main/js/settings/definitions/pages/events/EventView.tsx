d12e000111a06b330fde68defae5269cd6f853af
Masterfoni <joao.lima@liferay.com>
2021-02-05 17:32:41 -0300

    LRAC-6441 Create EventView page

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/pages/events/EventView.tsx
@@ -0,0 +1,56 @@
+import BasePage from 'settings/components/BasePage';
+import React from 'react';
+import {getDefinitions, getEvents} from 'shared/util/breadcrumbs';
+import {HasModal} from 'shared/types';
+import {Routes, setUriQueryValues, toRoute} from 'shared/util/router';
+import {useParams} from 'react-router-dom';
+
+interface IEventViewProps extends React.HTMLAttributes<HTMLElement>, HasModal {
+	groupId: string;
+}
+
+const EventView: React.FC<IEventViewProps> = ({groupId}) => {
+	const event = {
+		description: 'somedescription',
+		displayName: 'View Article',
+		id: 'myid',
+		name: 'viewArticle'
+	};
+
+	const {eventId} = useParams();
+	// TODO: Use useQuery hook and the isEditing to open modal => const isEditing = useQuery('edit');
+
+	const viewEventPageActions = [
+		{
+			href: setUriQueryValues(
+				{edit: true},
+				toRoute(Routes.SETTINGS_DEFINITIONS_EVENTS_VIEW, {
+					eventId,
+					groupId
+				})
+			),
+			label: Liferay.Language.get('edit')
+		}
+	];
+
+	return (
+		<BasePage
+			breadcrumbItems={[
+				getDefinitions({groupId}),
+				getEvents({groupId}),
+				{active: true, label: event.displayName}
+			]}
+			groupId={groupId}
+			pageActions={viewEventPageActions}
+			pageDescription={
+				event.description || Liferay.Language.get('no-description')
+			}
+			pageTitle={event.name}
+			pageTitleSubtext={event.displayName}
+		>
+			<div>{`CARD GOES HERE ${eventId}`}</div>
+		</BasePage>
+	);
+};
+
+export default EventView;
