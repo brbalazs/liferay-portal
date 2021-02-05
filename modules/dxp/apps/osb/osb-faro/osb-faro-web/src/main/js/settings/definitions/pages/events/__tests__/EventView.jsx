f7404922069a44b4c58c60f099acdae64bfb5355
Masterfoni <joao.lima@liferay.com>
2021-02-05 18:05:43 -0300

    LRAC-6441 Create EventView page tests and snapshots

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/pages/events/__tests__/EventView.jsx
@@ -0,0 +1,29 @@
+import EventView from '../EventView';
+import mockStore from 'test/mock-store';
+import React from 'react';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
+
+jest.unmock('react-dom');
+
+jest.mock('react-router-dom', () => ({
+	...jest.requireActual('react-router-dom'),
+	useParams: () => ({
+		eventId: 12345
+	})
+}));
+
+describe('EventView', () => {
+	it('should render', async() => {
+		const {container} = render(
+			<Provider store={mockStore()}>
+				<StaticRouter>
+					<EventView groupId='23' />
+				</StaticRouter>
+			</Provider>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+});
