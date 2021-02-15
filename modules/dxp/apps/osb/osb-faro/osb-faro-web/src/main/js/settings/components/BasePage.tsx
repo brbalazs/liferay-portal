d8659a05bbf8d05cd7a98f7ecedf5bb59f088359
Masterfoni <joao.lima@liferay.com>
2021-02-15 12:36:41 -0300

    LRAC-6441 SF

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/BasePage.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/components/BasePage.tsx
@@ -32,65 +32,60 @@ const SettingsBasePage: React.FC<ISettingsBasePageProps> = ({
 	pageTitle,
 	subTitle,
 	passedChildren
-}) => {
-	const renderPageTitle = () => (
-		<>
-			{pageTitle && (
-				<>
-					<h3
-						className={getCN({
-							['inline-text']: subTitle
-						})}
-					>
-						<TextTruncate title={pageTitle} />
-					</h3>
-					{subTitle && <span className='ml-2'>{subTitle}</span>}
-				</>
-			)}
-		</>
-	);
+}) => (
+	<div className={getCN('settings-base-page-root', className)}>
+		<DocumentTitle
+			title={`${documentTitle || pageTitle} - ${Liferay.Language.get(
+				'settings'
+			)}`}
+		/>
 
-	return (
-		<div className={getCN('settings-base-page-root', className)}>
-			<DocumentTitle
-				title={`${documentTitle || pageTitle} - ${Liferay.Language.get(
-					'settings'
-				)}`}
-			/>
+		<NotificationAlertList groupId={groupId} />
 
-			<NotificationAlertList groupId={groupId} />
+		<MaintenanceAlert />
 
-			<MaintenanceAlert />
+		{breadcrumbItems && <Breadcrumbs items={breadcrumbItems} />}
 
-			{breadcrumbItems && <Breadcrumbs items={breadcrumbItems} />}
+		{(!!pageTitle || !!pageDescription || !!pageActions.length) && (
+			<div
+				className={getCN('content-header', {
+					['has-page-actions']: !!pageActions.length
+				})}
+			>
+				<div className='header-text'>
+					{pageTitle && (
+						<>
+							<h3
+								className={getCN({
+									['inline-text']: subTitle
+								})}
+							>
+								<TextTruncate title={pageTitle} />
+							</h3>
 
-			{(!!pageTitle || !!pageDescription || !!pageActions.length) && (
-				<div
-					className={getCN('content-header', {
-						['has-page-actions']: !!pageActions.length
-					})}
-				>
-					<div className='header-text'>
-						{renderPageTitle()}
+							{subTitle && (
+								<span className='ml-2'>{subTitle}</span>
+							)}
+						</>
+					)}
 
-						{pageDescription && (
-							<div className='description'>{pageDescription}</div>
-						)}
-					</div>
-
-					<div className='page-actions-container'>
-						<PageActions
-							actions={pageActions}
-							actionsDisplayLimit={pageActionsDisplayLimit}
-						/>
-					</div>
+					{pageDescription && (
+						<div className='description'>{pageDescription}</div>
+					)}
 				</div>
-			)}
 
-			<div>{passedChildren}</div>
-		</div>
-	);
-};
+				<div className='page-actions-container'>
+					<PageActions
+						actions={pageActions}
+						actionsDisplayLimit={pageActionsDisplayLimit}
+					/>
+				</div>
+			</div>
+		)}
+
+		<div>{passedChildren}</div>
+	</div>
+);
 
 export default connect((store, ownProps) => ({
 	passedChildren: ownProps.children
