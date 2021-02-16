9739981107120148b704e0abcd9c9c61be8e7765
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2021-02-16 23:45:46 +0000

    LPS-124963 Fits better with the provider pattern

--- modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectServiceHandlerImpl.java
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-impl/src/main/java/com/liferay/portal/security/sso/openid/connect/internal/OpenIdConnectServiceHandlerImpl.java
@@ -31,7 +31,7 @@ import com.liferay.portal.security.sso.openid.connect.OpenIdConnectProviderRegis
 import com.liferay.portal.security.sso.openid.connect.OpenIdConnectServiceException;
 import com.liferay.portal.security.sso.openid.connect.OpenIdConnectServiceHandler;
 import com.liferay.portal.security.sso.openid.connect.constants.OpenIdConnectConstants;
-import com.liferay.portal.security.sso.openid.connect.internal.util.OpenIdConnectSessionHelperImpl;
+import com.liferay.portal.security.sso.openid.connect.internal.provider.OpenIdConnectSessionProviderImpl;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.proc.BadJOSEException;
@@ -185,7 +185,7 @@ public class OpenIdConnectServiceHandlerImpl
 		openIdConnectSessionImpl.setOpenIdConnectFlowState(
 			OpenIdConnectFlowState.AUTH_COMPLETE);
 
-		_openIdConnectSessionHelperImpl.setOpenIdConnectSession(
+		_openIdConnectSessionProviderImpl.setOpenIdConnectSession(
 			httpSession, openIdConnectSessionImpl);
 	}
 
@@ -224,7 +224,7 @@ public class OpenIdConnectServiceHandlerImpl
 			openIdConnectSessionImpl.setOpenIdConnectFlowState(
 				OpenIdConnectFlowState.AUTH_REQUESTED);
 
-			_openIdConnectSessionHelperImpl.setOpenIdConnectSession(
+			_openIdConnectSessionProviderImpl.setOpenIdConnectSession(
 				httpSession, openIdConnectSessionImpl);
 		}
 		catch (IOException ioe) {
@@ -352,7 +352,7 @@ public class OpenIdConnectServiceHandlerImpl
 		HttpSession httpSession, String expectedProviderName) {
 
 		Object openIdConnectSessionObject =
-			_openIdConnectSessionHelperImpl.getOpenIdConnectSession(
+			_openIdConnectSessionProviderImpl.getOpenIdConnectSession(
 				httpSession);
 
 		if (openIdConnectSessionObject instanceof OpenIdConnectSessionImpl) {
@@ -659,7 +659,7 @@ public class OpenIdConnectServiceHandlerImpl
 			_openIdConnectProviderRegistry;
 
 	@Reference
-	private OpenIdConnectSessionHelperImpl _openIdConnectSessionHelperImpl;
+	private OpenIdConnectSessionProviderImpl _openIdConnectSessionProviderImpl;
 
 	@Reference
 	private OpenIdConnectUserInfoProcessor _openIdConnectUserInfoProcessor;
