0e3a034443f60d4fe75ba8a633d3425517712c24
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-02-22 18:11:23 -0300

    LRAC-7226 Adjust test and update snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/pages/events/__tests__/AttributeView.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/pages/events/__tests__/AttributeView.jsx
@@ -1,9 +1,11 @@
 import AttributeView from '../AttributeView';
 import mockStore from 'test/mock-store';
 import React from 'react';
+import {MemoryRouter} from 'react-router';
 import {Provider} from 'react-redux';
 import {render} from '@testing-library/react';
-import {StaticRouter} from 'react-router';
+import {Route} from 'react-router-dom';
+import {Routes} from 'shared/util/router';
 
 jest.unmock('react-dom');
 
@@ -14,13 +16,25 @@ jest.mock('react-router-dom', () => ({
 	})
 }));
 
+const RenderWithRouter = ({children}) => (
+	<MemoryRouter
+		initialEntries={[
+			'/workspace/23/settings/definitions/events/attributes/12345'
+		]}
+	>
+		<Route path={Routes.SETTINGS_DEFINITIONS_ATTRIBUTES_VIEW}>
+			{children}
+		</Route>
+	</MemoryRouter>
+);
+
 describe('AttributeView', () => {
 	it('should render', async() => {
 		const {container} = render(
 			<Provider store={mockStore()}>
-				<StaticRouter>
+				<RenderWithRouter>
 					<AttributeView groupId='23' />
-				</StaticRouter>
+				</RenderWithRouter>
 			</Provider>
 		);
 
