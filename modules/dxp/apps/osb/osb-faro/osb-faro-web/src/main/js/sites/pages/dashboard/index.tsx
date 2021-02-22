4abc0ad58895d50710885c23dcd15db85c2b5202
Kien Do <kien.do@liferay.com>
2021-02-22 11:56:06 -0800

    LRAC-7491 Remove oAuth upgrade warning from Sites dashboard

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/sites/pages/dashboard/index.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/sites/pages/dashboard/index.tsx
@@ -1,77 +1,33 @@
 import * as breadcrumbs from 'shared/util/breadcrumbs';
-import Alert from 'shared/components/Alert';
 import BasePage from 'shared/components/base-page';
 import BundleRouter from 'route-middleware/BundleRouter';
-import Button from 'shared/components/Button';
-import DataSourceQuery from 'shared/queries/DataSourceQuery';
-import EmbeddedAlertList from 'shared/components/EmbeddedAlertList';
-import FaroConstants from 'shared/util/constants';
 import getCN from 'classnames';
 import Loading from 'shared/pages/Loading';
-import React, {lazy, Suspense, useContext, useEffect} from 'react';
+import React, {lazy, Suspense} from 'react';
 import RouteNotFound from 'shared/components/RouteNotFound';
-import {OAuthUpgradeWarningContext} from 'shared/context/oAuthUpgradeWarning';
-import {Routes, toRoute} from 'shared/util/router';
+import {Routes} from 'shared/util/router';
 import {Switch} from 'react-router-dom';
 import {useChannelContext} from 'shared/context/channel';
-import {useQuery} from '@apollo/react-hooks';
-import {User} from 'shared/util/records';
-import {withCurrentUser} from 'shared/hoc';
 
-const {
-	credentialTypes: {oAuth1, oAuth2},
-	dataSourceTypes: {liferay}
-} = FaroConstants;
-
-const InterestDetails = lazy(() =>
-	import(
-		/* webpackChunkName: "SitesDashboardInterestDetails" */ './InterestDetails'
-	)
+const InterestDetails = lazy(
+	() =>
+		import(
+			/* webpackChunkName: "SitesDashboardInterestDetails" */ './InterestDetails'
+		)
 );
-const Interests = lazy(() =>
-	import(/* webpackChunkName: "SitesDashboardInterests" */ './Interests')
+const Interests = lazy(
+	() =>
+		import(/* webpackChunkName: "SitesDashboardInterests" */ './Interests')
 );
-const Overview = lazy(() =>
-	import(/* webpackChunkName: "SitesDashboardOverview" */ './Overview')
+const Overview = lazy(
+	() => import(/* webpackChunkName: "SitesDashboardOverview" */ './Overview')
 );
-const Touchpoints = lazy(() =>
-	import(/* webpackChunkName: "SitesDashboardTouchpoints" */ './Touchpoints')
+const Touchpoints = lazy(
+	() =>
+		import(
+			/* webpackChunkName: "SitesDashboardTouchpoints" */ './Touchpoints'
+		)
 );
-
-const getAlert = (
-	channelId: string,
-	groupId: string,
-	setShowWarning: (value: boolean) => void
-) => [
-	{
-		iconSymbol: 'warning',
-		message: (
-			<>
-				{Liferay.Language.get(
-					'one-or-more-of-your-data-sources-needs-to-upgrade-from-oauth-to-the-new-token-based-connection'
-				)}
-				<div className='mt-3 pl-4'>
-					<Button
-						display='warning'
-						href={toRoute(Routes.SETTINGS_DATA_SOURCE_LIST, {
-							channelId,
-							groupId
-						})}
-						size='sm'
-					>
-						{Liferay.Language.get('go-to-data-sources')}
-					</Button>
-				</div>
-			</>
-		),
-		onClose: () => {
-			setShowWarning(false);
-		},
-		stripe: true,
-		title: 'Warning',
-		type: Alert.TYPES.warning
-	}
-];
 
 const NAV_ITEMS = [
 	{
@@ -102,37 +58,14 @@ type Router = {
 };
 
 interface IDashboardProps extends React.HTMLAttributes<HTMLDivElement> {
-	currentUser: User;
 	router: Router;
 }
 
-export const Dashboard: React.FC<IDashboardProps> = ({currentUser, router}) => {
+export const Dashboard: React.FC<IDashboardProps> = ({router}) => {
 	const {channelId, groupId} = router.params;
 	const {selectedChannel} = useChannelContext();
 
 	const selectedChannelName = selectedChannel && selectedChannel.name;
-
-	const {setShowOAuthUpgradeWarning, showOAuthUpgradeWarning} = useContext(
-		OAuthUpgradeWarningContext
-	);
-
-	const {data: oAuth1Data} = useQuery(DataSourceQuery, {
-		variables: {credentialsType: oAuth1, type: liferay}
-	});
-	const {data: oAuth2Data} = useQuery(DataSourceQuery, {
-		variables: {credentialsType: oAuth2, type: liferay}
-	});
-
-	useEffect(() => {
-		if (
-			showOAuthUpgradeWarning === null &&
-			oAuth1Data &&
-			oAuth2Data &&
-			oAuth1Data.dataSources.length + oAuth2Data.dataSources.length > 0
-		) {
-			setShowOAuthUpgradeWarning(true);
-		}
-	}, [oAuth1Data, oAuth2Data]);
 
 	return (
 		<BasePage
@@ -163,15 +96,6 @@ export const Dashboard: React.FC<IDashboardProps> = ({currentUser, router}) => {
 					routeParams={{channelId, groupId}}
 				/>
 			</BasePage.Header>
-			{showOAuthUpgradeWarning && currentUser.isAdmin() && (
-				<EmbeddedAlertList
-					alerts={getAlert(
-						channelId,
-						groupId,
-						setShowOAuthUpgradeWarning
-					)}
-				/>
-			)}
 
 			<BasePage.Context.Provider
 				value={{
@@ -222,4 +146,4 @@ export const Dashboard: React.FC<IDashboardProps> = ({currentUser, router}) => {
 	);
 };
 
-export default withCurrentUser(Dashboard);
+export default Dashboard;
