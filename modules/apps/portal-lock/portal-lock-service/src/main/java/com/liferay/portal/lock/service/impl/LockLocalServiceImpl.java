6b5d47b3b93c0db2af523272049cac2f078bd362
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2021-03-17 08:34:57 +0100

    LPS-129285 Add addtional information to help understand the log retry mechanism.

--- modules/apps/portal-lock/portal-lock-service/src/main/java/com/liferay/portal/lock/service/impl/LockLocalServiceImpl.java
+++ modules/apps/portal-lock/portal-lock-service/src/main/java/com/liferay/portal/lock/service/impl/LockLocalServiceImpl.java
@@ -21,6 +21,8 @@ import com.liferay.portal.kernel.dao.orm.ORMException;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.lock.LockListener;
 import com.liferay.portal.kernel.lock.LockListenerRegistryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
@@ -298,6 +300,10 @@ public class LockLocalServiceImpl extends LockLocalServiceBaseImpl {
 				if (cause instanceof ConstraintViolationException ||
 					cause instanceof LockAcquisitionException) {
 
+					if (_log.isInfoEnabled()) {
+						_log.info("Failed to acquire lock, will retry again");
+					}
+
 					continue;
 				}
 
@@ -412,6 +418,10 @@ public class LockLocalServiceImpl extends LockLocalServiceBaseImpl {
 				if (cause instanceof ConstraintViolationException ||
 					cause instanceof LockAcquisitionException) {
 
+					if (_log.isInfoEnabled()) {
+						_log.info("Failed to remove lock, will retry again");
+					}
+
 					continue;
 				}
 
@@ -441,6 +451,9 @@ public class LockLocalServiceImpl extends LockLocalServiceBaseImpl {
 			}
 		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LockLocalServiceImpl.class);
 
 	private final TransactionConfig _transactionConfig =
 		TransactionConfig.Factory.create(
