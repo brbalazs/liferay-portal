4a2260c71385312313cd216f87b2b737d5228856
Francisco Dias <francisco.dias@liferay.com>
2021-03-22 12:44:49 -0300

    LRAC-7245 Create PercentOfCell component

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/components/event-analysis-editor/event-analysis-breakdown/__tests__/PercentOfCell.jsx
@@ -0,0 +1,89 @@
+import PercentOfCell from '../PercentOfCell';
+import React from 'react';
+import {mockBreakdownData} from 'test/data';
+import {parserBreakdownData} from 'event-analysis/utils/utils';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
+
+describe('PercentOfCell', () => {
+	it('render', () => {
+		const data = mockBreakdownData();
+		const items = parserBreakdownData(data);
+		const {events} = items[0];
+
+		const {container} = render(
+			<PercentOfCell
+				compareToPrevious={false}
+				events={events}
+				totalEvents={data.totalEvents}
+			/>
+		);
+
+		expect(container).toMatchSnapshot();
+	});
+
+	it('render comparing previous', () => {
+		const data = mockBreakdownData(true);
+		const items = parserBreakdownData(data);
+		const {events} = items[0];
+
+		const {container} = render(
+			<PercentOfCell
+				compareToPrevious
+				events={events}
+				totalEvents={data.totalEvents}
+			/>
+		);
+
+		expect(container.querySelectorAll('ul > li').length).toBe(2);
+	});
+
+	it('render comparing previous and segment', () => {
+		const data = mockBreakdownData(true, true);
+		const items = parserBreakdownData(data);
+		const {events} = items[0];
+
+		const {container} = render(
+			<PercentOfCell
+				compareToPrevious
+				events={events}
+				totalEvents={data.totalEvents}
+			/>
+		);
+
+		expect(container.querySelectorAll('ul > li').length).toBe(4);
+	});
+
+	it('render comparing previous and event', () => {
+		const data = mockBreakdownData(true, false, true);
+		const items = parserBreakdownData(data);
+		const {events} = items[0];
+
+		const {container} = render(
+			<PercentOfCell
+				compareToPrevious
+				events={events}
+				totalEvents={data.totalEvents}
+			/>
+		);
+
+		expect(container.querySelectorAll('ul > li').length).toBe(4);
+	});
+
+	it('render two list when comparing segment and event', () => {
+		const data = mockBreakdownData(false, true, true);
+		const items = parserBreakdownData(data);
+		const {events} = items[0];
+
+		const {container} = render(
+			<PercentOfCell
+				compareToPrevious
+				events={events}
+				totalEvents={data.totalEvents}
+			/>
+		);
+
+		expect(container.querySelectorAll('ul').length).toBe(2);
+	});
+});
