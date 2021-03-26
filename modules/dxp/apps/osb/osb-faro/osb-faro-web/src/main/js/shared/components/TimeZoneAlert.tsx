0adba381a26e4903dd006ccaef8a091b65c6df3d
Kien Do <kien.do@liferay.com>
2021-03-26 15:42:58 -0700

    LRAC-7456 Simplify TimeZoneAlert component

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/TimeZoneAlert.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/TimeZoneAlert.tsx
@@ -1,74 +1,45 @@
-import * as API from 'shared/api';
 import Alert from 'shared/components/Alert';
-import React, {useState} from 'react';
-import {compose} from 'redux';
+import React from 'react';
 import {connect} from 'react-redux';
 import {sub} from 'shared/util/lang';
-import {withRouter} from 'react-router-dom';
 
 const TIME_ZONE_COUNTRY_REGEX = /\([^)]+.*/;
 
 interface ITimeZoneAlertModalProps {
-	groupId: string;
-	notificationId: string;
+	onClose: () => void;
 	stripe: boolean;
 	timeZone: string;
 }
 
 const TimeZoneAlert: React.FC<ITimeZoneAlertModalProps> = ({
-	groupId,
-	notificationId,
+	onClose,
 	stripe,
 	timeZone
-}) => {
-	const [showAlert, setShowAlert] = useState(true);
+}) => (
+	<Alert
+		iconSymbol='exclamation-full'
+		onClose={onClose}
+		stripe={stripe}
+		title={Liferay.Language.get('info')}
+		type={Alert.TYPES.info}
+	>
+		{sub(
+			Liferay.Language.get(
+				'workspace-timezone-has-changed-to-x-as-of-today.-please-allow-1-2-days-for-the-data-to-adjust-to-this-new-setting.'
+			),
+			[timeZone]
+		)}
+	</Alert>
+);
 
-	return showAlert ? (
-		<Alert
-			iconSymbol='exclamation-full'
-			key={notificationId}
-			onClose={() => {
-				API.notifications.readNotification(groupId, notificationId);
-				setShowAlert(false);
-			}}
-			stripe={stripe}
-			title={Liferay.Language.get('info')}
-			type={Alert.TYPES.info}
-		>
-			{sub(
-				Liferay.Language.get(
-					'workspace-timezone-has-changed-to-x-as-of-today.-please-allow-1-2-days-for-the-data-to-adjust-to-this-new-setting.'
-				),
-				[timeZone]
-			)}
-		</Alert>
-	) : null;
-};
-
-export default compose<any>(
-	withRouter,
-	connect(
-		(
-			state,
-			{
-				match: {
-					params: {groupId}
-				}
-			}
-		) => ({
-			timeZone: state
-				.getIn(
-					[
-						'projects',
-						groupId,
-						'data',
-						'timeZone',
-						'displayTimeZone'
-					],
-					''
-				)
-				.replace(TIME_ZONE_COUNTRY_REGEX, '')
-		}),
-		null
-	)
+export default connect(
+	(state, {groupId}) => ({
+		timeZone: state
+			.getIn(
+				['projects', groupId, 'data', 'timeZone', 'displayTimeZone'],
+				''
+			)
+			.replace(TIME_ZONE_COUNTRY_REGEX, '')
+	}),
+	null
 )(TimeZoneAlert);
