f0c27bde6242407566bcdae084403bfdb00d03f6
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-03-29 12:42:27 -0300

    LRAC-7601 Create coverage test to HelpWidget

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/__tests__/HelpWidget.jsx
@@ -0,0 +1,35 @@
+import HelpWidget from '../HelpWidget';
+import React from 'react';
+import {cleanup, render} from '@testing-library/react';
+
+jest.unmock('react-dom');
+
+const wrappedComponentText = () => 'wrapped component text';
+
+describe('HelpWidget', () => {
+	afterEach(cleanup);
+
+	it('should render a wrapped component', () => {
+		const WrappedComponent = HelpWidget(wrappedComponentText);
+
+		const {container} = render(
+			<WrappedComponent
+                groupId='123'
+			/>
+		);
+
+		expect(container.textContent).toBe('wrapped component text');
+	});
+
+	it('should render a helper widget', () => {
+		const WrappedComponent = HelpWidget(wrappedComponentText);
+
+		const {container} = render(
+			<WrappedComponent
+                groupId='123'
+			/>
+		);
+
+		expect(container.querySelector('.helper-widget-wrapper')).toBeTruthy();
+	});
+});
