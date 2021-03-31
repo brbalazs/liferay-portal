2d7cb829dd1610b4e63330c6233c08e8734cfc47
Shinn Lok <shinn.lok@liferay.com>
2021-03-31 14:49:13 -0700

    LRAC-7655 Use ConfigurationPolicy.REQUIRE instead of manual enable/disable

--- modules/apps/analytics/analytics-settings-impl/src/main/java/com/liferay/analytics/settings/internal/configuration/AnalyticsConfigurationTrackerImpl.java
+++ modules/apps/analytics/analytics-settings-impl/src/main/java/com/liferay/analytics/settings/internal/configuration/AnalyticsConfigurationTrackerImpl.java
@@ -22,7 +22,6 @@ import com.liferay.analytics.message.storage.service.AnalyticsMessageLocalServic
 import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
 import com.liferay.analytics.settings.configuration.AnalyticsConfigurationTracker;
 import com.liferay.analytics.settings.internal.model.AnalyticsUserImpl;
-import com.liferay.analytics.settings.internal.security.auth.verifier.AnalyticsSecurityAuthVerifier;
 import com.liferay.analytics.settings.internal.util.EntityModelListenerTracker;
 import com.liferay.analytics.settings.security.constants.AnalyticsSecurityConstants;
 import com.liferay.expando.kernel.model.ExpandoColumn;
@@ -333,19 +332,6 @@ public class AnalyticsConfigurationTrackerImpl
 			AnalyticsConfiguration.class, properties);
 	}
 
-	private void _activate() {
-		if (!_active) {
-			_active = true;
-		}
-
-		if (!_authVerifierEnabled) {
-			_componentContext.enableComponent(
-				AnalyticsSecurityAuthVerifier.class.getName());
-
-			_authVerifierEnabled = true;
-		}
-	}
-
 	private void _addAnalyticsAdmin(long companyId) throws Exception {
 		User user = _userLocalService.fetchUserByScreenName(
 			companyId, AnalyticsSecurityConstants.SCREEN_NAME_ANALYTICS_ADMIN);
@@ -479,19 +465,6 @@ public class AnalyticsConfigurationTrackerImpl
 		_addAnalyticsMessages("update", contacts);
 	}
 
-	private void _deactivate() {
-		if (_active && !_hasConfiguration()) {
-			_active = false;
-		}
-
-		if (_authVerifierEnabled && !_hasConfiguration()) {
-			_componentContext.disableComponent(
-				AnalyticsSecurityAuthVerifier.class.getName());
-
-			_authVerifierEnabled = false;
-		}
-	}
-
 	private void _deleteAnalyticsAdmin(long companyId) throws Exception {
 		User user = _userLocalService.fetchUserByScreenName(
 			companyId, AnalyticsSecurityConstants.SCREEN_NAME_ANALYTICS_ADMIN);
@@ -520,7 +493,9 @@ public class AnalyticsConfigurationTrackerImpl
 				_deleteSAPEntry(companyId);
 			}
 
-			_deactivate();
+			if (_active && !_hasConfiguration()) {
+				_active = false;
+			}
 		}
 		catch (Exception e) {
 			_log.error(e, e);
@@ -529,7 +504,8 @@ public class AnalyticsConfigurationTrackerImpl
 
 	private void _enable(long companyId) {
 		try {
-			_activate();
+			_active = true;
+
 			_addAnalyticsAdmin(companyId);
 			_addSAPEntry(companyId);
 		}
@@ -980,8 +956,6 @@ public class AnalyticsConfigurationTrackerImpl
 
 	@Reference
 	private AnalyticsMessageLocalService _analyticsMessageLocalService;
-
-	private boolean _authVerifierEnabled;
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
