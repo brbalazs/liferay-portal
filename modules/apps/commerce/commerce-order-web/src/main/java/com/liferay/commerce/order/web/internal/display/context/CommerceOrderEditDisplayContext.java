dcb862f0d33b86eb953628005ae559f160443ef3
Riccardo <riccardo.alberti@liferay.com>
2021-04-01 14:10:57 +0200

    COMMERCE-6398 Use orderStatus label if no remapping is available

--- modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/display/context/CommerceOrderEditDisplayContext.java
+++ modules/apps/commerce/commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/display/context/CommerceOrderEditDisplayContext.java
@@ -517,6 +517,11 @@ public class CommerceOrderEditDisplayContext {
 				}
 			}
 
+			if (Validator.isNull(label)) {
+				label = commerceOrderStatus.getLabel(
+					_commerceOrderRequestHelper.getLocale());
+			}
+
 			String buttonCssClass = "btn-primary";
 
 			if (commerceOrderStatus.getPriority() ==
