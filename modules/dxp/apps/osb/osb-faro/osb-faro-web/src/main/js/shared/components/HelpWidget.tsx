6e26ae5b418a79900efb350eed9b4b70c5dc16b7
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-04-05 12:47:55 -0300

    LRAC-7602 Create the dropdown on the HelpWidget component

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/HelpWidget.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/HelpWidget.tsx
@@ -1,18 +1,60 @@
 import Button from 'shared/components/Button';
-import React from 'react';
+import ClayDropDown, {Align} from '@clayui/drop-down';
+import React, {useState} from 'react';
+import urlConstants from 'shared/util/url-constants';
 
-const HelpWidget = () => (
-	<div className='help-widget-wrapper'>
-		<Button
-			aria-label={Liferay.Language.get('help')}
-			borderless
-			className='help-button'
-			display='defaut'
-			icon='ac-question-mark'
-			iconAlignment='right'
-			size='sm'
-		/>
-	</div>
-);
+// TODO: LRAC-7603 Create Help Modal
+const DROPDOWN_ITEMS = [
+	{
+		handleOnClick: () => {},
+		label: Liferay.Language.get('report-an-issue')
+	},
+	{
+		href: urlConstants.HELP_CENTER,
+		label: Liferay.Language.get('help-center')
+	}
+];
+
+const HelpWidget = () => {
+	const [active, setActive] = useState(false);
+
+	return (
+		<div className='help-widget-root'>
+			<ClayDropDown
+				active={active}
+				alignmentPosition={Align.TopLeft}
+				menuElementAttrs={{
+					className: 'help-dropdown-root'
+				}}
+				onActiveChange={setActive}
+				trigger={
+					<Button
+						aria-label={Liferay.Language.get('help')}
+						borderless
+						className='help-button'
+						display='defaut'
+						icon='ac-question-mark'
+						iconAlignment='right'
+						size='sm'
+					/>
+				}
+			>
+				<ClayDropDown.ItemList>
+					<ClayDropDown.Group>
+						{DROPDOWN_ITEMS.map((item, i) => (
+							<ClayDropDown.Item
+								href={item.href}
+								key={i}
+								onClick={item.handleOnClick}
+							>
+								{item.label}
+							</ClayDropDown.Item>
+						))}
+					</ClayDropDown.Group>
+				</ClayDropDown.ItemList>
+			</ClayDropDown>
+		</div>
+	);
+};
 
 export default HelpWidget;
