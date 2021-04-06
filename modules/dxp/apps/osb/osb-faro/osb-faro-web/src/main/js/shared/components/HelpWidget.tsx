e05e2c59711f482522569bdc08d3c62fa035b4ef
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-04-06 12:00:29 -0300

    LRAC-7602 Modify to ClayDropDownWithItems and put a target

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/HelpWidget.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/HelpWidget.tsx
@@ -1,60 +1,42 @@
 import Button from 'shared/components/Button';
-import ClayDropDown, {Align} from '@clayui/drop-down';
-import React, {useState} from 'react';
+import React from 'react';
 import urlConstants from 'shared/util/url-constants';
+import {Align, ClayDropDownWithItems} from '@clayui/drop-down';
 
 // TODO: LRAC-7603 Create Help Modal
 const DROPDOWN_ITEMS = [
 	{
-		handleOnClick: () => {},
-		label: Liferay.Language.get('report-an-issue')
+		label: Liferay.Language.get('report-an-issue'),
+		onClick: () => {}
 	},
 	{
 		href: urlConstants.HELP_CENTER,
-		label: Liferay.Language.get('help-center')
+		label: Liferay.Language.get('help-center'),
+		target: '_blank'
 	}
 ];
 
-const HelpWidget = () => {
-	const [active, setActive] = useState(false);
-
-	return (
-		<div className='help-widget-root'>
-			<ClayDropDown
-				active={active}
-				alignmentPosition={Align.TopLeft}
-				menuElementAttrs={{
-					className: 'help-dropdown-root'
-				}}
-				onActiveChange={setActive}
-				trigger={
-					<Button
-						aria-label={Liferay.Language.get('help')}
-						borderless
-						className='help-button'
-						display='defaut'
-						icon='ac-question-mark'
-						iconAlignment='right'
-						size='sm'
-					/>
-				}
-			>
-				<ClayDropDown.ItemList>
-					<ClayDropDown.Group>
-						{DROPDOWN_ITEMS.map((item, i) => (
-							<ClayDropDown.Item
-								href={item.href}
-								key={i}
-								onClick={item.handleOnClick}
-							>
-								{item.label}
-							</ClayDropDown.Item>
-						))}
-					</ClayDropDown.Group>
-				</ClayDropDown.ItemList>
-			</ClayDropDown>
-		</div>
-	);
-};
+const HelpWidget = () => (
+	<div className='help-widget-root'>
+		<ClayDropDownWithItems
+			alignmentPosition={Align.TopLeft}
+			items={DROPDOWN_ITEMS}
+			menuElementAttrs={{
+				className: 'help-dropdown-root'
+			}}
+			trigger={
+				<Button
+					aria-label={Liferay.Language.get('help')}
+					borderless
+					className='help-button'
+					display='defaut'
+					icon='ac-question-mark'
+					iconAlignment='right'
+					size='sm'
+				/>
+			}
+		/>
+	</div>
+);
 
 export default HelpWidget;
