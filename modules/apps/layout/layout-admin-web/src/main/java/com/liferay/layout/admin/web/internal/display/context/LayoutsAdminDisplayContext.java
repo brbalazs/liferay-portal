72e28aa7dde881aa40d3a2e410d8f6d693dea00f
Brian Chan <brian.chan@liferay.com>
2021-04-14 10:56:43 -0300

    LPS-130377 Fix potential bug

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutsAdminDisplayContext.java
@@ -1022,7 +1022,7 @@ public class LayoutsAdminDisplayContext {
 			return _privateLayout;
 		}
 
-		_privateLayout = false;
+		Boolean privateLayout = false;
 
 		int publicLayoutsCount = LayoutServiceUtil.getLayoutsCount(
 			getSelGroupId(), false, 0);
@@ -1030,8 +1030,10 @@ public class LayoutsAdminDisplayContext {
 			getSelGroupId(), true, 0);
 
 		if ((privateLayoutsCount > 0) && (publicLayoutsCount <= 0)) {
-			_privateLayout = true;
+			privateLayout = true;
 		}
+
+		_privateLayout = privateLayout;
 
 		return _privateLayout;
 	}
