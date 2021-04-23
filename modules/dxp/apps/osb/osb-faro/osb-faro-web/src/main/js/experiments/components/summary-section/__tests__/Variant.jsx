910e8e825dcf54786b59feff635d91e2a9bac8bb
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-04-23 12:40:53 -0300

    LRAC-7773 Change Variant tests to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-section/__tests__/Variant.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/experiments/components/summary-section/__tests__/Variant.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import Variant from '../Variant';
-import {shallow} from 'enzyme';
+import {render} from '@testing-library/react';
+
+jest.unmock('react-dom');
 
 describe('SummarySection Variant', () => {
 	it('should render component', () => {
-		const component = shallow(<Variant lift='50%' status='up' />);
+		const {container, getByText} = render(
+			<Variant lift='50%' status='up' />
+		);
 
-		expect(
-			component.hasClass('analytics-summary-section-variant')
-		).toBeTruthy();
-		expect(component.find('ClayIcon').props().symbol).toEqual('caret-top');
-		expect(component).toMatchSnapshot();
+		expect(getByText('50% lift').parentElement).toHaveClass(
+			'analytics-summary-section-variant'
+		);
+
+		expect(container.querySelector('use')).toHaveAttribute(
+			'xlink:href',
+			'#caret-top'
+		);
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render component with status down', () => {
-		const component = shallow(<Variant lift='50%' status='down' />);
+		const {container} = render(<Variant lift='50%' status='down' />);
 
-		expect(component.find('ClayIcon').props().symbol).toEqual(
-			'caret-bottom'
+		expect(container.querySelector('use')).toHaveAttribute(
+			'xlink:href',
+			'#caret-bottom'
 		);
 	});
 });
