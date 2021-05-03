05fb83f7c5bc997c1fd511b798ab4413bad95da5
Brian Chan <brian.chan@liferay.com>
2021-05-03 09:44:36 -0700

    COMMERCE-6488 rename

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/ProductResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/resource/v1_0/ProductResourceImpl.java
@@ -504,7 +504,7 @@ public class ProductResourceImpl
 		return new ProductTaxConfiguration();
 	}
 
-	private String[] _getTags(CPDefinition cpDefinition) {
+	private String[] _getAssetTags(CPDefinition cpDefinition) {
 		List<AssetTag> assetEntryAssetTags = _assetTagService.getTags(
 			cpDefinition.getModelClassName(), cpDefinition.getCPDefinitionId());
 
@@ -727,7 +727,7 @@ public class ProductResourceImpl
 		String[] assetTags = product.getTags();
 
 		if (product.getTags() == null) {
-			assetTags = _getTags(cpDefinition);
+			assetTags = _getAssetTags(cpDefinition);
 		}
 
 		serviceContext.setAssetTagNames(assetTags);
