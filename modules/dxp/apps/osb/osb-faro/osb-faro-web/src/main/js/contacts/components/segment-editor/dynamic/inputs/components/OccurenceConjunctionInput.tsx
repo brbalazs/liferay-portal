e5142f712742c82e5c2ee3d8938505c6f59555b2
Kien Do <kien.do@liferay.com>
2021-06-01 18:21:57 -0700

    LRAC-7854 Abstract Occurence logic into OccurenceConjunctionInput

---
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/components/segment-editor/dynamic/inputs/components/OccurenceConjunctionInput.tsx
@@ -0,0 +1,93 @@
+import Form from 'shared/components/form';
+import getCN from 'classnames';
+import Input from 'shared/components/Input';
+import React from 'react';
+import {ClaySelectWithOption} from '@clayui/select';
+import {Criterion} from '../../utils/types';
+import {isValid} from '../../utils/utils';
+import {OCCURENCE_OPTIONS} from '../../utils/constants';
+
+const isValidOccurenceCount = occurenceCount =>
+	isValid(occurenceCount) && occurenceCount >= 0;
+
+interface IOccurenceConjunctionInputProps {
+	onChange: (params: {
+		criterion?: Criterion;
+		touched?: boolean;
+		valid?: boolean;
+	}) => void;
+	operatorName: Criterion['operatorName'];
+	touched: boolean;
+	valid: boolean;
+	value: number | string;
+}
+
+const OccurenceConjunctionInput: React.FC<IOccurenceConjunctionInputProps> = ({
+	onChange,
+	operatorName,
+	touched,
+	valid,
+	value
+}) => (
+	<>
+		<Form.GroupItem shrink>
+			<ClaySelectWithOption
+				className='operator-input'
+				onChange={event => {
+					const {value} = event.target;
+
+					onChange({
+						criterion: {
+							operatorName: value as Criterion['operatorName']
+						}
+					});
+				}}
+				options={OCCURENCE_OPTIONS.map(({key, label}) => ({
+					label,
+					value: key
+				}))}
+				value={operatorName}
+			/>
+		</Form.GroupItem>
+
+		<Form.GroupItem
+			className={getCN({
+				'has-error': !valid && touched
+			})}
+			shrink
+		>
+			<Input
+				data-testid='occurence-count-input'
+				min='0'
+				onBlur={() => {
+					onChange({
+						touched: true
+					});
+				}}
+				onChange={event => {
+					const {value} = event.target;
+
+					let numberVal: string | number = '';
+
+					if (isValid(value)) {
+						numberVal = parseInt(value);
+					}
+
+					onChange({
+						criterion: {value: numberVal},
+						touched: true,
+						valid: isValidOccurenceCount(numberVal)
+					});
+				}}
+				type='number'
+				value={value}
+			/>
+		</Form.GroupItem>
+
+		<Form.GroupItem className='unit' label shrink>
+			{Liferay.Language.get('times')}
+		</Form.GroupItem>
+	</>
+);
+
+export default OccurenceConjunctionInput;
