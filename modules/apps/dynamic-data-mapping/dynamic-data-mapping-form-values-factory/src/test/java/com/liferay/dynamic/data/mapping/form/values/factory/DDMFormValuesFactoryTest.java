9eb040e96c9915991fbb8022c06fd7584dd35201
Noor Najjar <noor.najjar@liferay.com>
2021-06-02 15:07:30 -0700

    LPS-133146 Update unit test

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-values-factory/src/test/java/com/liferay/dynamic/data/mapping/form/values/factory/DDMFormValuesFactoryTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-values-factory/src/test/java/com/liferay/dynamic/data/mapping/form/values/factory/DDMFormValuesFactoryTest.java
@@ -1129,6 +1129,12 @@ public class DDMFormValuesFactoryTest extends PowerMockito {
 		).thenReturn(
 			LocaleUtil.US
 		);
+
+		when(
+			LocaleUtil.toLanguageIds(Matchers.anyCollection())
+		).thenReturn(
+			new String[] {"en_US", "pt_BR"}
+		);
 	}
 
 	private final DDMFormValuesFactory _ddmFormValuesFactory =
