74af63ab90128a49e0b3bc0139e6d19e1ec76ae8
Georgel Pop <georgel.pop@liferay.com>
2021-06-08 12:22:33 +0200

    LPS-133314 Fix accessibility point 1.14 Compatibility. Presence of errors in the CSS code that can make it difficult to process it correctly. Unexpected rule '@charset': charset rule in the middle of the file is not allowed! This rule should always be the first one to appear in a CSS file otherwise would not be correctly processed, so we remove it when using ComboServlet. We already use in our portal the header 'Content-Type: text/css;charset=UTF-8' that says to the browser that it should be interpreted as utf-8 content.

--- portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/ComboServlet.java
@@ -378,6 +378,14 @@ public class ComboServlet extends HttpServlet {
 
 					baseURL = PortalUtil.getPathProxy() + baseURL;
 
+					if (StringUtil.contains(
+							stringFileContent, _CSS_CHARSET_UTF_8,
+							StringPool.BLANK)) {
+
+						stringFileContent = StringUtil.removeSubstring(
+							stringFileContent, _CSS_CHARSET_UTF_8);
+					}
+
 					stringFileContent = AggregateUtil.updateRelativeURLs(
 						stringFileContent, baseURL);
 
@@ -493,6 +501,8 @@ public class ComboServlet extends HttpServlet {
 
 		return FileUtil.getExtension(resourcePath);
 	}
+
+	private static final String _CSS_CHARSET_UTF_8 = "@charset \"UTF-8\";";
 
 	private static final String _CSS_EXTENSION = "css";
 
