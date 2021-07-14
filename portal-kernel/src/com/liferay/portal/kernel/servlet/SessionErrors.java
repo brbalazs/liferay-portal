5321e3c93b11590b9c752aa21788bf7d3b1c8b7b
Brian Chan <brian.chan@liferay.com>
2021-07-13 20:05:25 -0700

    LPS-135569 Wordsmith

--- portal-kernel/src/com/liferay/portal/kernel/servlet/SessionErrors.java
+++ portal-kernel/src/com/liferay/portal/kernel/servlet/SessionErrors.java
@@ -75,7 +75,7 @@ public class SessionErrors {
 		}
 
 		if (_log.isDebugEnabled()) {
-			_log.debug("key=" + key);
+			_log.debug("Adding key " + key);
 		}
 
 		map.put(key, key);
@@ -96,7 +96,7 @@ public class SessionErrors {
 			}
 
 			_log.debug(
-				StringBundler.concat("key=", key, ", value=", value),
+				StringBundler.concat("Adding key ", key, " with value ", value),
 				exception);
 		}
 
@@ -123,7 +123,8 @@ public class SessionErrors {
 		if (_log.isDebugEnabled()) {
 			_log.debug(
 				StringBundler.concat(
-					"portletId=", portletRequest.getWindowID(), ", key=", key));
+					"Adding key ", key, " to portlet ",
+					portletRequest.getWindowID()));
 		}
 
 		map.put(key, key);
@@ -147,8 +148,8 @@ public class SessionErrors {
 
 			_log.debug(
 				StringBundler.concat(
-					"portletId=", portletRequest.getWindowID(), ", key=", key,
-					", value=", value),
+					"Adding key ", key, " to portlet ",
+					portletRequest.getWindowID(), " with value ", value),
 				exception);
 		}
 
