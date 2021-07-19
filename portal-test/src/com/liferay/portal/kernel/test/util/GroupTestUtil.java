dceef53d4b158b82f2c14cc03e11028ee41efbb5
Michael Bowerman <michael.bowerman@liferay.com>
2021-07-19 13:45:02 -0700

    LPS-135087 Add new GroupTestUtil method to create virtual hosts on the Group's LayoutSet(s).

--- portal-test/src/com/liferay/portal/kernel/test/util/GroupTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/GroupTestUtil.java
@@ -25,12 +25,14 @@ import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.GroupServiceUtil;
+import com.liferay.portal.kernel.service.LayoutSetLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.randomizerbumpers.NumericStringRandomizerBumper;
 import com.liferay.portal.kernel.test.randomizerbumpers.UniqueStringRandomizerBumper;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 
@@ -167,6 +169,32 @@ public class GroupTestUtil {
 			parentGroupId, GroupConstants.DEFAULT_LIVE_GROUP_ID, nameMap,
 			descriptionMap, type, manualMembership, membershipRestriction,
 			friendlyURL, site, active, serviceContext);
+	}
+
+	public static void addLayoutSetVirtualHost(
+			Group group, boolean privateLayout)
+		throws Exception {
+
+		StringBundler sb = new StringBundler(3);
+
+		sb.append(group.getGroupKey());
+
+		if (privateLayout) {
+			sb.append("-private.");
+		}
+		else {
+			sb.append("-public.");
+		}
+
+		sb.append(RandomTestUtil.randomString(3));
+
+		LayoutSetLocalServiceUtil.updateVirtualHost(
+			group.getGroupId(), privateLayout, sb.toString());
+	}
+
+	public static void addLayoutSetVirtualHosts(Group group) throws Exception {
+		addLayoutSetVirtualHost(group, true);
+		addLayoutSetVirtualHost(group, false);
 	}
 
 	public static Group deleteGroup(Group group) throws Exception {
