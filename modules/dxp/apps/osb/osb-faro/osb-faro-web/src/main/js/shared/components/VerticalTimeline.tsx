0616306b2c202abd4c464b24ce7a3f58abc9a1c6
Gabriel Ramos <gabriel.ramos@liferay.com>
2021-07-22 10:06:51 -0300

    LRAC-7852 Migrate VerticalTimeline to functional components

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/VerticalTimeline.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/VerticalTimeline.tsx
@@ -1,4 +1,3 @@
-import autobind from 'autobind-decorator';
 import getCN from 'classnames';
 import Icon from './Icon';
 import NoResultsDisplay, {getFormattedTitle} from './NoResultsDisplay';
@@ -6,294 +5,231 @@ import React from 'react';
 import Spinner from './Spinner';
 import Sticker from './Sticker';
 import TextTruncate from './TextTruncate';
+import {FC} from 'react';
 import {formatDateToTimeZone} from 'shared/util/date';
 import {get} from 'lodash';
 import {Link} from 'react-router-dom';
-import {onEnter} from 'shared/util/key-constants';
-import {PropTypes} from 'prop-types';
 import {Routes, toRoute} from 'shared/util/router';
+import {useState} from 'react';
 
-const ITEM_SHAPE = {
-	header: PropTypes.bool,
-	individual: PropTypes.object,
-	nestedItems: PropTypes.array,
-	subtitle: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-	symbol: PropTypes.string,
-	time: PropTypes.number,
-	title: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-	url: PropTypes.string
-};
+interface ITEM_SHAPE {
+	header: boolean;
+	individual: object;
+	nestedItems: [];
+	subtitle: string | [];
+	symbol: string;
+	time: number;
+	title: string | string[];
+	url: string;
+}
 
-class TimelineItem extends React.Component {
-	static defaultProps = {
-		initialExpanded: true
-	};
+interface ITimelineItemProps {
+	channelId?: string;
+	className?: string;
+	groupId?: string;
+	initialExpanded?: boolean;
+	item: ITEM_SHAPE;
+	timeZoneId: string;
+}
 
-	static propTypes = {
-		channelId: PropTypes.string,
-		groupId: PropTypes.string,
-		initialExpanded: PropTypes.bool,
-		item: PropTypes.shape(ITEM_SHAPE).isRequired,
-		timeZoneId: PropTypes.string.isRequired
-	};
+const TimelineItem: FC<ITimelineItemProps> = ({
+	channelId,
+	className,
+	groupId,
+	initialExpanded = true,
+	item: {header, individual, nestedItems, subtitle, symbol, time, title, url},
+	timeZoneId
+}) => {
+	const [expanded, setExpanded] = useState(initialExpanded);
 
-	state = {
-		expanded: true
-	};
-
-	constructor(props) {
-		super(props);
-
-		const {initialExpanded} = this.props;
-
-		this.state = {
-			...this.state,
-			expanded: initialExpanded
-		};
-	}
-
-	toggleExpand() {
-		if (this.isExpandable()) {
-			this.setState({
-				expanded: !this.state.expanded
-			});
+	const toggleExpand = () => {
+		if (isExpandable()) {
+			setExpanded(!expanded);
 		}
-	}
+	};
 
-	@autobind
-	handleClick() {
-		this.toggleExpand();
-	}
+	const handleClick = () => {
+		toggleExpand();
+	};
 
-	@autobind
-	@onEnter
-	handleKeyPress() {
-		this.toggleExpand();
-	}
+	const handleKeyPress = () => {
+		toggleExpand();
+	};
 
-	isExpandable() {
-		const {nestedItems} = this.props.item;
+	const isExpandable = () => {
+		return nestedItems?.length;
+	};
 
-		return nestedItems && !!nestedItems.length;
-	}
+	const classes = getCN('timeline-item', className, {
+		header
+	});
 
-	render() {
-		const {
-			props: {
-				channelId,
-				className,
-				groupId,
-				item: {
-					header,
-					individual,
-					nestedItems,
-					subtitle,
-					symbol,
-					time,
-					title,
-					url
-				},
-				timeZoneId
-			},
-			state: {expanded}
-		} = this;
+	const expandable = isExpandable();
 
-		const classes = getCN('timeline-item', className, {
-			header
-		});
+	const bodyClasses = getCN('timeline-panel-body-content', {
+		selectable: expandable
+	});
 
-		const expandable = this.isExpandable();
+	const bodyAttributes = expandable
+		? {
+				onClick: handleClick,
+				onKeyPress: handleKeyPress,
+				role: 'button',
+				tabIndex: 0
+		  }
+		: {};
 
-		const bodyClasses = getCN('timeline-panel-body-content', {
-			selectable: expandable
-		});
+	const individualName = get(individual, 'name');
+	const individualId = get(individual, 'id');
 
-		const bodyAttributes = expandable
-			? {
-					onClick: this.handleClick,
-					onKeyPress: this.handleKeyPress,
-					role: 'button',
-					tabIndex: '0'
-			  }
-			: {};
+	return (
+		<li className={classes}>
+			<div className='timeline-panel'>
+				<div className='timeline-panel-body'>
+					{!header && (
+						<div className='timeline-increment'>
+							<Sticker circle display='point' size='lg' />
+						</div>
+					)}
 
-		const individualName = get(individual, 'name');
-		const individualId = get(individual, 'id');
+					{time && (
+						<div className='timeline-item-label'>
+							{formatDateToTimeZone(time, 'h:mma', timeZoneId)}
+						</div>
+					)}
 
-		return (
-			<li className={classes}>
-				<div className='timeline-panel'>
-					<div className='timeline-panel-body'>
-						{!header && (
-							<div className='timeline-increment'>
-								<Sticker circle display='point' size='lg' />
+					<div className={bodyClasses} {...bodyAttributes}>
+						{symbol && (
+							<div className='sticker-container'>
+								<Sticker display='light' symbol={symbol} />
 							</div>
 						)}
 
-						{time && (
-							<div className='timeline-item-label'>
-								{formatDateToTimeZone(
-									time,
-									'h:mma',
-									timeZoneId
-								)}
-							</div>
-						)}
-
-						<div className={bodyClasses} {...bodyAttributes}>
-							{symbol && (
-								<div className='sticker-container'>
-									<Sticker display='light' symbol={symbol} />
-								</div>
-							)}
-
-							<div className='timeline-panel-body-content-text'>
-								<div>
-									{!!individualName && (
-										<Link
-											className='entity-link'
-											to={toRoute(
-												Routes.CONTACTS_INDIVIDUAL,
-												{
-													channelId,
-													groupId,
-													id: individualId
-												}
-											)}
-										>
-											{individualName}
-										</Link>
-									)}
-
-									<span className='text-truncate'>
-										{url ? (
-											<Link className='title' to={url}>
-												{title}
-											</Link>
-										) : (
-											<span className='title'>
-												{title}
-											</span>
+						<div className='timeline-panel-body-content-text'>
+							<div>
+								{!!individualName && (
+									<Link
+										className='entity-link'
+										to={toRoute(
+											Routes.CONTACTS_INDIVIDUAL,
+											{
+												channelId,
+												groupId,
+												id: individualId
+											}
 										)}
-									</span>
-								</div>
-
-								{subtitle && (
-									<TextTruncate
-										className='subtitle'
-										title={subtitle}
-									/>
+									>
+										{individualName}
+									</Link>
 								)}
+
+								<span className='text-truncate'>
+									{url ? (
+										<Link className='title' to={url}>
+											{title}
+										</Link>
+									) : (
+										<span className='title'>{title}</span>
+									)}
+								</span>
 							</div>
 
-							{this.isExpandable() && (
-								<div className='timeline-panel-body-content-details'>
-									<span className='item-count'>
-										{nestedItems.length}
-									</span>
-
-									<Icon
-										symbol={
-											expanded
-												? 'caret-bottom'
-												: 'caret-top'
-										}
-									/>
-								</div>
+							{subtitle && (
+								<TextTruncate
+									className='subtitle'
+									title={subtitle}
+								/>
 							)}
 						</div>
-					</div>
 
-					{expanded && nestedItems && (
-						<VerticalTimeline
-							items={nestedItems}
-							nested
+						{isExpandable() && (
+							<div className='timeline-panel-body-content-details'>
+								<span className='item-count'>
+									{nestedItems.length}
+								</span>
+
+								<Icon
+									symbol={
+										expanded ? 'caret-bottom' : 'caret-top'
+									}
+								/>
+							</div>
+						)}
+					</div>
+				</div>
+
+				{expanded && nestedItems && (
+					<VerticalTimeline
+						items={nestedItems}
+						nested
+						timeZoneId={timeZoneId}
+					/>
+				)}
+			</div>
+		</li>
+	);
+};
+
+interface HEADER_LABEL_SHAPE {
+	count: string;
+	label: string;
+	title: string;
+}
+
+interface IVerticalTimelineProps {
+	groupId?: string;
+	headerLabels?: HEADER_LABEL_SHAPE;
+	initialExpanded?: boolean;
+	items: ITEM_SHAPE[];
+	loading?: boolean;
+	nested?: boolean;
+	timeZoneId: string;
+}
+
+const VerticalTimeline: FC<IVerticalTimelineProps> = ({
+	groupId,
+	headerLabels,
+	initialExpanded = true,
+	items = [],
+	loading = false,
+	nested = false,
+	timeZoneId
+}) => {
+	const classes = getCN('timeline', 'timeline-center', {
+		'timeline-nested': nested
+	});
+
+	if (loading) {
+		return <Spinner alignCenter={false} className='flex-grow-1' spacer />;
+	} else if (!items.length && !nested) {
+		return <NoResultsDisplay title={getFormattedTitle()} />;
+	} else {
+		return (
+			<div className='vertical-timeline-root'>
+				{headerLabels && (
+					<div className='timeline-header'>
+						<div className='header-label'>{headerLabels.label}</div>
+
+						<div className='header-title'>{headerLabels.title}</div>
+
+						<div className='header-count'>{headerLabels.count}</div>
+					</div>
+				)}
+
+				<ul className={classes}>
+					{items.map((item, i) => (
+						<TimelineItem
+							groupId={groupId}
+							initialExpanded={initialExpanded}
+							item={item}
+							key={i}
 							timeZoneId={timeZoneId}
 						/>
-					)}
-				</div>
-			</li>
+					))}
+				</ul>
+			</div>
 		);
 	}
-}
-
-const HEADER_LABEL_SHAPE = {
-	count: PropTypes.string,
-	label: PropTypes.string,
-	title: PropTypes.string
 };
 
-export default class VerticalTimeline extends React.Component {
-	static defaultProps = {
-		initialExpanded: true,
-		items: [],
-		loading: false,
-		nested: false
-	};
-
-	static propTypes = {
-		groupId: PropTypes.string,
-		headerLabels: PropTypes.shape(HEADER_LABEL_SHAPE),
-		initialExpanded: PropTypes.bool,
-		items: PropTypes.arrayOf(PropTypes.shape(ITEM_SHAPE)),
-		loading: PropTypes.bool,
-		nested: PropTypes.bool,
-		timeZoneId: PropTypes.string
-	};
-
-	render() {
-		const {
-			groupId,
-			headerLabels,
-			initialExpanded,
-			items,
-			loading,
-			nested,
-			timeZoneId
-		} = this.props;
-
-		const classes = getCN('timeline', 'timeline-center', {
-			'timeline-nested': nested
-		});
-
-		if (loading) {
-			return (
-				<Spinner alignCenter={false} className='flex-grow-1' spacer />
-			);
-		} else if (!items.length && !nested) {
-			return <NoResultsDisplay title={getFormattedTitle()} />;
-		} else {
-			return (
-				<div className='vertical-timeline-root'>
-					{headerLabels && (
-						<div className='timeline-header'>
-							<div className='header-label'>
-								{headerLabels.label}
-							</div>
-
-							<div className='header-title'>
-								{headerLabels.title}
-							</div>
-
-							<div className='header-count'>
-								{headerLabels.count}
-							</div>
-						</div>
-					)}
-
-					<ul className={classes}>
-						{items.map((item, i) => (
-							<TimelineItem
-								groupId={groupId}
-								initialExpanded={initialExpanded}
-								item={item}
-								key={i}
-								timeZoneId={timeZoneId}
-							/>
-						))}
-					</ul>
-				</div>
-			);
-		}
-	}
-}
+export default VerticalTimeline;
