f52c67fc8f606796a8fe198f41ac06d15cb3dc40
Brian Chan <brian.chan@liferay.com>
2021-08-13 13:09:27 -0300

    LPS-30938 Sort

--- modules/apps/dynamic-data-lists/dynamic-data-lists-test/src/testIntegration/java/com/liferay/dynamic/data/lists/search/test/DDLRecordSearchTest.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-test/src/testIntegration/java/com/liferay/dynamic/data/lists/search/test/DDLRecordSearchTest.java
@@ -86,8 +86,8 @@ public class DDLRecordSearchTest {
 		DDLRecordSet recordSet = addRecordSet();
 
 		_recordTestHelper = new DDLRecordTestHelper(
-			_user.getUserId(), _group, recordSet);
-		_searchContext = getSearchContext(_group, _user, recordSet);
+			_group, recordSet, _user.getUserId());
+		_searchContext = getSearchContext(_group, recordSet, _user);
 	}
 
 	@Test
@@ -112,7 +112,7 @@ public class DDLRecordSearchTest {
 
 		DDLRecordSet recordSet = recordSetTestHelper.addRecordSet(ddmStructure);
 
-		SearchContext searchContext = getSearchContext(group, user, recordSet);
+		SearchContext searchContext = getSearchContext(group, recordSet, user);
 
 		DDLRecordTestHelper recordTestHelper = new DDLRecordTestHelper(
 			group, recordSet);
@@ -192,17 +192,17 @@ public class DDLRecordSearchTest {
 
 		User user = UserLocalServiceUtil.getDefaultUser(companyId);
 
-		long userId = user.getUserId();
-
 		Group group = GroupTestUtil.addGroup(
-			companyId, userId, GroupConstants.DEFAULT_PARENT_GROUP_ID);
+			companyId, user.getUserId(),
+			GroupConstants.DEFAULT_PARENT_GROUP_ID);
 
 		DDLRecordSetTestHelper recordSetTestHelper = new DDLRecordSetTestHelper(
-			userId, group);
+			group, user.getUserId());
 
 		DDMStructureTestHelper ddmStructureTestHelper =
 			new DDMStructureTestHelper(
-				userId, PortalUtil.getClassNameId(DDLRecordSet.class), group);
+				PortalUtil.getClassNameId(DDLRecordSet.class), group,
+				user.getUserId());
 
 		Set<Locale> locales = DDMFormTestUtil.createAvailableLocales(
 			new Locale[] {LocaleUtil.US, LocaleUtil.JAPAN});
@@ -225,7 +225,7 @@ public class DDLRecordSearchTest {
 
 		DDLRecordSet recordSet = recordSetTestHelper.addRecordSet(ddmStructure);
 
-		SearchContext searchContext = getSearchContext(group, user, recordSet);
+		SearchContext searchContext = getSearchContext(group, recordSet, user);
 
 		searchContext.setLocale(LocaleUtil.JAPAN);
 
@@ -246,7 +246,7 @@ public class DDLRecordSearchTest {
 			ddmFormValues, WorkflowConstants.ACTION_PUBLISH);
 
 		DDLRecordTestHelper recordTestHelper = new DDLRecordTestHelper(
-			userId, group, recordSet);
+			group, recordSet, user.getUserId());
 
 		recordTestHelper.addRecord(
 			ddmFormValues, WorkflowConstants.ACTION_PUBLISH);
@@ -342,7 +342,7 @@ public class DDLRecordSearchTest {
 	}
 
 	protected static SearchContext getSearchContext(
-			Group group, User user, DDLRecordSet recordSet)
+			Group group, DDLRecordSet recordSet, User user)
 		throws Exception {
 
 		SearchContext searchContext = SearchContextTestUtil.getSearchContext(
@@ -413,12 +413,12 @@ public class DDLRecordSearchTest {
 
 	protected DDLRecordSet addRecordSet() throws Exception {
 		DDLRecordSetTestHelper recordSetTestHelper = new DDLRecordSetTestHelper(
-			_user.getUserId(), _group);
+			_group, _user.getUserId());
 
 		DDMStructureTestHelper ddmStructureTestHelper =
 			new DDMStructureTestHelper(
-				_user.getUserId(),
-				PortalUtil.getClassNameId(DDLRecordSet.class), _group);
+				PortalUtil.getClassNameId(DDLRecordSet.class), _group,
+				_user.getUserId());
 
 		DDMStructure ddmStructure = ddmStructureTestHelper.addStructure(
 			createDDMForm(LocaleUtil.US), StorageType.JSON.toString());
