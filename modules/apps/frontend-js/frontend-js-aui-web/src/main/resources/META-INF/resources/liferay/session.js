df89b2fd5316649572e534acb64cfb3ec7a56ee9
Jorge Díaz <jorge.diaz@liferay.com>
2021-08-17 09:28:13 +0200

    LPS-137264 Remove the console.log trace, the final user is already notified with a error popup in case the session cannot be extended

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/session.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/session.js
@@ -396,16 +396,6 @@ AUI.add(
 											instance.extend();
 										}
 										else {
-									if (extend && hasExpired) {
-										// eslint-disable-next-line no-console
-										console.log(
-											'Session was already expired: sessionLength=' +
-												sessionLength +
-												', elapsed time=' +
-												elapsed
-										);
-									}
-
 											instance.expire();
 
 											expirationMoment = true;
