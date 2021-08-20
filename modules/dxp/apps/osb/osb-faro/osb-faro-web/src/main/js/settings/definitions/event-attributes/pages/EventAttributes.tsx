4dac820def84e5221489e1416d133ceed442e7fe
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-08-20 08:44:37 -0300

    LRAC-8761 Adjust event attributes title and update snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/event-attributes/pages/EventAttributes.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/event-attributes/pages/EventAttributes.tsx
@@ -11,13 +11,13 @@ const EventAttributes: React.FC<IEventAttributesProps> = ({groupId}) => (
 	<BasePage
 		breadcrumbItems={[
 			getDefinitions({groupId}),
-			{active: true, label: Liferay.Language.get('attributes')}
+			{active: true, label: Liferay.Language.get('event-attributes')}
 		]}
 		groupId={groupId}
 		pageDescription={Liferay.Language.get(
 			'attributes-provide-additional-context-for-events.-they-are-usually-event-specific-but-can-be-used-by-more-than-one.-global-attributes-will-be-sent-with-all-events-without-needing-to-be-configured'
 		)}
-		pageTitle={Liferay.Language.get('attributes')}
+		pageTitle={Liferay.Language.get('event-attributes')}
 	>
 		<TabsCard groupId={groupId} />
 	</BasePage>
