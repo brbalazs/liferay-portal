e864c2149fc6dbabe7a9364d82b4b349e95d6ea9
Inácio Nery <inacio.nery@liferay.com>
2021-08-27 14:00:45 -0300

    LRAC-0 Update DXP version

--- modules/dxp/apps/osb/osb-faro/wedeploy/uat/script/copy_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp -r /wedeploy-container/resources/log4j $LIFERAY_HOME/osgi/log4j/
-cp -r /wedeploy-container/resources/portal-log4j-ext.xml $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/classes/META-INF
-cp -r /wedeploy-container/resources/system-ext.properties $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/classes/
-cp -r /wedeploy-container/resources/urlrewrite.xml $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/
