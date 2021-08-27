6accdf415fa86bf4e26d38ea9d3c1800f14134cc
Eric Yan <eric.yan@liferay.com>
2021-08-27 12:20:37 -0700

    LPS-128159 export-import-web: Use request parameter if available

--- modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/display/context/ExportImportToolbarDisplayContext.java
+++ modules/apps/export-import/export-import-web/src/main/java/com/liferay/exportimport/web/internal/display/context/ExportImportToolbarDisplayContext.java
@@ -108,7 +108,10 @@ public class ExportImportToolbarDisplayContext {
 							"liveGroupId",
 							String.valueOf(
 								groupDisplayContextHelper.getLiveGroupId()),
-							"privateLayout", Boolean.FALSE.toString(),
+							"privateLayout",
+							ParamUtil.getString(
+								_request, "privateLayout",
+								Boolean.FALSE.toString()),
 							"displayStyle",
 							ParamUtil.getString(
 								_request, "displayStyle", "descriptive"));
