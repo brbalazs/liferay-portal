05129c421b5bf7ff3ea066da545e0ceb0f0fc760
Marcio Fonseca <marcio.fonseca@liferay.com>
2021-10-19 17:54:22 -0300

    LRAC-9515 Create tests for ProfileCard

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/individual/profile/components/__tests__/ProfileCard.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/contacts/individual/profile/components/__tests__/ProfileCard.jsx
@@ -3,11 +3,15 @@ import client from 'shared/apollo/client';
 import IndividualProfileCard from '../ProfileCard';
 import Promise from 'metal-promise';
 import React from 'react';
+import {
+	fireEvent,
+	render,
+	waitForElementToBeRemoved
+} from '@testing-library/react';
 import {Individual} from 'shared/util/records';
 import {MockedProvider} from '@apollo/react-testing';
 import {mockEventMetrics, mockIndividual, mockSessions} from 'test/data';
 import {mockTimeRangeReq} from 'test/graphql-data';
-import {render, waitForElementToBeRemoved} from '@testing-library/react';
 import {StaticRouter} from 'react-router';
 
 jest.unmock('react-dom');
@@ -15,14 +19,6 @@ jest.unmock('react-dom');
 jest.mock('shared/apollo/client', () => ({
 	query: jest.fn()
 }));
-
-client.query
-	.mockResolvedValueOnce({
-		data: mockEventMetrics()
-	})
-	.mockResolvedValue({
-		data: mockSessions()
-	});
 
 const DefaultComponent = props => (
 	<MockedProvider mocks={[mockTimeRangeReq()]}>
@@ -39,8 +35,18 @@ const DefaultComponent = props => (
 	</MockedProvider>
 );
 
+const inputValue = 'add to cart';
+
 describe('IndividualProfileCard', () => {
 	it('should render', async () => {
+		client.query
+			.mockResolvedValueOnce({
+				data: mockEventMetrics()
+			})
+			.mockResolvedValueOnce({
+				data: mockSessions()
+			});
+
 		const {container} = render(<DefaultComponent />);
 
 		jest.runAllTimers();
@@ -50,6 +56,124 @@ describe('IndividualProfileCard', () => {
 		);
 
 		expect(container).toMatchSnapshot();
+	});
+
+	it('should clear search input when clear button is clicked', async () => {
+		client.query
+			.mockResolvedValueOnce({
+				data: mockEventMetrics()
+			})
+			.mockResolvedValueOnce({
+				data: mockSessions()
+			})
+			.mockResolvedValueOnce({
+				data: mockEventMetrics()
+			})
+			.mockResolvedValueOnce({
+				data: mockSessions()
+			})
+			.mockResolvedValueOnce({
+				data: mockEventMetrics()
+			})
+			.mockResolvedValue({
+				data: mockSessions()
+			});
+
+		const {container, getByPlaceholderText, getByText} = render(
+			<DefaultComponent />
+		);
+
+		jest.runAllTimers();
+
+		await waitForElementToBeRemoved(() =>
+			container.querySelector('.spinner-root')
+		);
+
+		const searchInput = getByPlaceholderText('Search');
+
+		fireEvent.change(searchInput, {target: {value: inputValue}});
+
+		fireEvent.keyDown(searchInput, {
+			charCode: 13,
+			code: 'Enter',
+			key: 'Enter'
+		});
+
+		jest.runAllTimers();
+
+		await waitForElementToBeRemoved(() =>
+			container.querySelector('.spinner-root')
+		);
+
+		expect(getByPlaceholderText('Search')).toHaveValue(inputValue);
+
+		fireEvent.click(getByText('Clear'));
+
+		jest.runAllTimers();
+
+		await waitForElementToBeRemoved(() =>
+			container.querySelector('.spinner-root')
+		);
+
+		expect(getByPlaceholderText('Search')).toHaveValue('');
+	});
+
+	it('should clear search input when X clear button is clicked', async () => {
+		client.query
+			.mockResolvedValueOnce({
+				data: mockEventMetrics()
+			})
+			.mockResolvedValueOnce({
+				data: mockSessions()
+			})
+			.mockResolvedValueOnce({
+				data: mockEventMetrics()
+			})
+			.mockResolvedValueOnce({
+				data: mockSessions()
+			})
+			.mockResolvedValueOnce({
+				data: mockEventMetrics()
+			})
+			.mockResolvedValue({
+				data: mockSessions()
+			});
+
+		const {container, getByPlaceholderText} = render(<DefaultComponent />);
+
+		jest.runAllTimers();
+
+		await waitForElementToBeRemoved(() =>
+			container.querySelector('.spinner-root')
+		);
+
+		const searchInput = getByPlaceholderText('Search');
+
+		fireEvent.change(searchInput, {target: {value: inputValue}});
+
+		fireEvent.keyDown(searchInput, {
+			charCode: 13,
+			code: 'Enter',
+			key: 'Enter'
+		});
+
+		jest.runAllTimers();
+
+		await waitForElementToBeRemoved(() =>
+			container.querySelector('.spinner-root')
+		);
+
+		expect(getByPlaceholderText('Search')).toHaveValue(inputValue);
+
+		fireEvent.click(container.querySelector('.lexicon-icon-times'));
+
+		jest.runAllTimers();
+
+		await waitForElementToBeRemoved(() =>
+			container.querySelector('.spinner-root')
+		);
+
+		expect(getByPlaceholderText('Search')).toHaveValue('');
 	});
 
 	xit('should render w/ an error display', () => {
