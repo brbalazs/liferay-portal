946b65b5a966cfa01ed4425ba7b7facc47cae141
Gabriel Ramos <gabriel.ramos@liferay.com>
2021-10-22 15:42:40 -0300

    LRAC-9556 Fix read clientWidth of null error

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/components/event-analysis-editor/event-analysis-breakdown/index.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/components/event-analysis-editor/event-analysis-breakdown/index.tsx
@@ -150,7 +150,7 @@ const BreakdownTable: React.FC<IBreakdownTableProps> = ({
 		setMaxBreakdownLength(maxCharLengthPerColumn);
 	};
 
-	const {clientWidth} = tableRef.current || {};
+	const {clientWidth} = tableRef?.current || {};
 
 	useEffect(() => {
 		const handleResize = debounce(
