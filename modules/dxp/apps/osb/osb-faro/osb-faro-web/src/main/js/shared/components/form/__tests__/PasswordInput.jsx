0cee1be9eabd9443e9861b7f646fb3749fce9e6a
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-11-04 11:49:48 -0300

    LRAC-9682 Convert PasswordInput test to RTL and update snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/form/__tests__/PasswordInput.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/components/form/__tests__/PasswordInput.jsx
@@ -1,29 +1,41 @@
+import Form from 'shared/components/form';
 import PasswordInput from '../PasswordInput';
 import React from 'react';
+import {fireEvent, render} from '@testing-library/react';
 import {mockForm} from 'test/data';
-import {shallow} from 'enzyme';
+
+jest.unmock('react-dom');
+
+const TestComponent = () => (
+	<Form>
+		<PasswordInput field={{name: 'foo'}} form={mockForm()} />
+	</Form>
+);
 
 describe('PasswordInput', () => {
 	it('should render', () => {
-		const component = shallow(
-			<PasswordInput field={{name: 'foo'}} form={mockForm()} />
-		);
-		expect(component).toMatchSnapshot();
+		const {container} = render(<TestComponent />);
+
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render with an input type of "password" if showPassword is false', () => {
-		const component = shallow(
-			<PasswordInput field={{name: 'foo'}} form={mockForm()} />
-		);
-		expect(component.props().type).toEqual('password');
+		const {container} = render(<TestComponent />);
+		const input = container.querySelector('#foo');
+
+		expect(input).toHaveAttribute('type', 'password');
 	});
 
 	it('should render with an input type of "text" if showPassword is true', () => {
-		const component = shallow(
-			<PasswordInput field={{name: 'foo'}} form={mockForm()} />
-		);
-		component.setState({showPassword: true});
+		const {container, getByRole} = render(<TestComponent />);
+		const input = container.querySelector('#foo');
+
+		expect(input).toHaveAttribute('type', 'password');
+
+		fireEvent.click(getByRole('button'));
 		jest.runAllTimers();
-		expect(component.render()).toMatchSnapshot();
+
+		expect(input).toHaveAttribute('type', 'text');
+		expect(container).toMatchSnapshot();
 	});
 });
