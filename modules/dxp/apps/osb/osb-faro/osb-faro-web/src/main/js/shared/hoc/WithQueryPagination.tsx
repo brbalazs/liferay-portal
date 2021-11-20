0321dbc4aa17f0cc1519238ca2ee3a3db11a6434
Kien Do <kien.do@liferay.com>
2021-11-19 18:59:26 -0800

    LRAC-9567 Create withQueryRangeSelectors HOC

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/WithQueryPagination.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/WithQueryPagination.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import {useQueryPagination} from 'shared/hooks';
 
 // TODO: Maybe call this with Query pagination?
-const withQueryPaginationParams = initialParams => WrappedComponent => (
+const withQueryPagination = initialParams => WrappedComponent => (
 	props: any
 ) => {
 	const paginationParams = useQueryPagination(initialParams);
 
-	return <WrappedComponent {...props} paginationParams={paginationParams} />;
+	return <WrappedComponent {...props} {...paginationParams} />;
 };
 
-export default withQueryPaginationParams;
+export default withQueryPagination;
