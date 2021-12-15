9af1714333691aea8722dbb1f65c2d1b859a4c6a
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2021-12-15 15:19:53 -0300

    LRAC-9837 Use the mutation of delete event analysis

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/hocs/EventAnalysisListCard.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/hocs/EventAnalysisListCard.tsx
@@ -1,6 +1,10 @@
 import Button from 'shared/components/Button';
 import Card from 'shared/components/Card';
 import CrossPageSelect from 'shared/hoc/CrossPageSelect';
+import DeleteEventAnalysisMutation, {
+	DeleteEventAnalysisData,
+	DeleteEventAnalysisVariables
+} from '../queries/DeleteEventAnalysisMutation';
 import EventAnalysisListQuery, {
 	EventAnalysisListData,
 	EventAnalysisListVariables
@@ -9,6 +13,8 @@ import Nav from 'shared/components/Nav';
 import React from 'react';
 import RowActions from 'shared/components/RowActions';
 import URLConstants from 'shared/util/url-constants';
+import {addAlert} from 'shared/actions/alerts';
+import {Alert} from 'shared/types';
 import {close, modalTypes, open} from 'shared/actions/modals';
 import {compose} from 'shared/hoc';
 import {connect, ConnectedProps} from 'react-redux';
@@ -22,24 +28,26 @@ import {
 import {getPluralMessage, sub} from 'shared/util/lang';
 import {getSafeRangeSelectors} from 'shared/util/util';
 import {mapListResultsToProps} from 'shared/util/mappers';
-import {Modal} from 'shared/types';
 import {NameCell} from 'shared/components/table/cell-components';
-import {noop} from 'lodash';
 import {Routes, toRoute} from 'shared/util/router';
+import {useMutation, useQuery} from '@apollo/react-hooks';
 import {useParams} from 'react-router-dom';
-import {useQuery} from '@apollo/react-hooks';
 import {useQueryPagination, useQueryRangeSelectors} from 'shared/hooks';
 import {
 	useSelectionContext,
 	withSelectionProvider
 } from 'shared/context/selection';
 
-const connector = connect(null, {close, open});
+const connector = connect(null, {addAlert, close, open});
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-const EventAnalysisListCard: React.FC<PropsFromRedux> = ({close, open}) => {
-	const {selectedItems} = useSelectionContext();
+const EventAnalysisListCard: React.FC<PropsFromRedux> = ({
+	addAlert,
+	close,
+	open
+}) => {
+	const {selectedItems, selectionDispatch} = useSelectionContext();
 
 	const {delta, orderIOMap, page, query} = useQueryPagination({
 		initialOrderIOMap: createOrderIOMap(NAME)
@@ -64,7 +72,14 @@ const EventAnalysisListCard: React.FC<PropsFromRedux> = ({close, open}) => {
 		}
 	});
 
-	const handleDeleteEventAnalysis = ids => {
+	const [deleteEventAnalysis] = useMutation<
+		DeleteEventAnalysisData,
+		DeleteEventAnalysisVariables
+	>(DeleteEventAnalysisMutation);
+
+	const handleDeleteEventAnalysis = (eventAnalysisIds: Array<string>) => {
+		const {refetch} = response;
+
 		open(modalTypes.CONFIRMATION_MODAL, {
 			message: (
 				<div>
@@ -76,7 +91,7 @@ const EventAnalysisListCard: React.FC<PropsFromRedux> = ({close, open}) => {
 							Liferay.Language.get(
 								'are-you-sure-you-want-to-delete-these-analyses'
 							),
-							ids.length
+							eventAnalysisIds.length
 						)}
 					</h4>
 
@@ -88,15 +103,41 @@ const EventAnalysisListCard: React.FC<PropsFromRedux> = ({close, open}) => {
 							Liferay.Language.get(
 								'you-will-lose-all-data-related-to-these-analyses.-you-will-not-be-able-to-undo-this-operation'
 							),
-							ids.length
+							eventAnalysisIds.length
 						)}
 					</p>
 				</div>
 			),
 			modalVariant: 'modal-warning',
 			onClose: close,
-			// TODO: LRAC-9837 Add GraphQL mutation to delete event analysis
-			onSubmit: noop,
+			onSubmit: () => {
+				deleteEventAnalysis({
+					variables: {
+						eventAnalysisIds
+					}
+				})
+					.then(() => {
+						addAlert({
+							alertType: Alert.Types.Success,
+							message: Liferay.Language.get(
+								'the-segment-has-been-deleted'
+							)
+						});
+
+						selectionDispatch({
+							type: 'clear-all'
+						});
+
+						refetch();
+					})
+					.catch(() => {
+						addAlert({
+							alertType: Alert.Types.Error,
+							message: Liferay.Language.get('error'),
+							timeout: false
+						});
+					});
+			},
 			submitButtonDisplay: 'warning',
 			submitMessage: Liferay.Language.get('delete'),
 			title: Liferay.Language.get('deleting-analysis'),
@@ -106,7 +147,7 @@ const EventAnalysisListCard: React.FC<PropsFromRedux> = ({close, open}) => {
 
 	const renderNav = () => {
 		if (!selectedItems.isEmpty()) {
-			const ids = selectedItems.keySeq().toArray();
+			const eventAnalysisIds = selectedItems.keySeq().toArray();
 
 			return (
 				<Nav>
@@ -115,7 +156,9 @@ const EventAnalysisListCard: React.FC<PropsFromRedux> = ({close, open}) => {
 						display='secondary'
 						icon='trash'
 						iconAlignment='left'
-						onClick={() => handleDeleteEventAnalysis(ids)}
+						onClick={() =>
+							handleDeleteEventAnalysis(eventAnalysisIds)
+						}
 						outline
 					/>
 				</Nav>
@@ -125,7 +168,6 @@ const EventAnalysisListCard: React.FC<PropsFromRedux> = ({close, open}) => {
 
 	const renderRowActions = ({data: {id}}) => {
 		if (selectedItems.isEmpty()) {
-			// TODO: LRAC-9837 Add GraphQL mutation to delete event analysis
 			return (
 				<RowActions
 					quickActions={[
--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/queries/DeleteEventAnalysisMutation.ts
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/queries/DeleteEventAnalysisMutation.ts
@@ -1,5 +1,13 @@
 import {gql} from 'apollo-boost';
 
+export interface DeleteEventAnalysisData {
+	null;
+}
+
+export interface DeleteEventAnalysisVariables {
+	eventAnalysisIds: Array<string>;
+}
+
 export default gql`
 	mutation DeleteEventAnalysisMutation($eventAnalysisIds: [String]!) {
 		deleteEventAnalyses(eventAnalysisIds: $eventAnalysisIds)
