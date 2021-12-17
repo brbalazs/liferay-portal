045ce4104bffa3bb503a6f3e2cfb7fe7ae7b8b63
Adriano <adriano.interaminense@gmail.com>
2021-12-16 21:32:57 -0300

    LRAC-9839 Update unit tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/components/event-analysis-editor/__tests__/EventAnalysisEditor.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/event-analysis/components/event-analysis-editor/__tests__/EventAnalysisEditor.jsx
@@ -3,6 +3,7 @@ import EventAnalysisEditor from '../index';
 import mockStore from 'test/mock-store';
 import React from 'react';
 import {ApolloProvider} from '@apollo/react-components';
+import {CalculationTypes} from 'event-analysis/utils/types';
 import {Provider} from 'react-redux';
 import {render} from '@testing-library/react';
 
@@ -13,7 +14,7 @@ describe('Event Analysis Editor', () => {
 		const {container} = render(
 			<ApolloProvider client={client}>
 				<Provider store={mockStore()}>
-					<EventAnalysisEditor />
+					<EventAnalysisEditor type={CalculationTypes.Total} />
 				</Provider>
 			</ApolloProvider>
 		);
