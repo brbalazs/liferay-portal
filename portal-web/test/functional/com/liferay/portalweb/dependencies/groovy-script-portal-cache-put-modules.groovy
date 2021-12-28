0751713eb14e00991ed75aba1d39046975ca798a
Alan Huang <alan.huang@liferay.com>
2021-12-29 01:25:11 +0800

    LPS-144124 Auto SF, clean up files in dependencies directory

--- portal-web/test/functional/com/liferay/portalweb/dependencies/groovy-script-portal-cache-put-modules.groovy
+++ portal-web/test/functional/com/liferay/portalweb/dependencies/groovy-script-portal-cache-put-modules.groovy
@@ -3,7 +3,6 @@ import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.util.ClassLoaderPool;
 import java.lang.reflect.Constructor;
 
-
 try {
 	ClassLoader testModuleClassLoader = ClassLoaderPool.getClassLoader("com.liferay.cluster.test.module_2.0.0");
 
