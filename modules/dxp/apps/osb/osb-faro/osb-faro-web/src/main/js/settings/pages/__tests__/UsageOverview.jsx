1e27671d3aa5fe63a454dd62a74a6b3da5083f01
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2022-01-06 12:24:19 -0300

    LRAC-10348 Convert UsageOverview test to RTL and update snapshot

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/__tests__/UsageOverview.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/__tests__/UsageOverview.jsx
@@ -1,10 +1,15 @@
 import * as data from 'test/data';
+import mockStore from 'test/mock-store';
 import React from 'react';
 import {fromJS} from 'immutable';
 import {Project, User} from 'shared/util/records';
-import {shallow} from 'enzyme';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
+import {StaticRouter} from 'react-router';
 import {SubscriptionStatuses, UserRoleNames} from 'shared/util/constants';
 import {UsageOverview} from '../UsageOverview';
+
+jest.unmock('react-dom');
 
 const defaultProps = {
 	currentUser: new User(data.mockUser()),
@@ -16,11 +21,19 @@ const defaultProps = {
 	)
 };
 
+const WrappedComponent = props => (
+	<Provider store={mockStore()}>
+		<StaticRouter>
+			<UsageOverview {...props} />
+		</StaticRouter>
+	</Provider>
+);
+
 describe('UsageOverview', () => {
 	it('should render', () => {
-		const component = shallow(<UsageOverview {...defaultProps} />);
+		const {container} = render(<WrappedComponent {...defaultProps} />);
 
-		expect(component).toMatchSnapshot();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should render with a warning type and danger type warning if one metric is approaching limit and the other is over', () => {
@@ -35,12 +48,12 @@ describe('UsageOverview', () => {
 			})
 		);
 
-		const component = shallow(
-			<UsageOverview {...defaultProps} project={mockProject} />
+		const {container} = render(
+			<WrappedComponent {...defaultProps} project={mockProject} />
 		);
 
-		expect(component.find('Alert').at(0).prop('type')).toBe('warning');
-		expect(component.find('Alert').at(1).prop('type')).toBe('danger');
+		expect(container.querySelector('.alert-warning')).toBeInTheDocument();
+		expect(container.querySelector('.alert-danger')).toBeInTheDocument();
 	});
 
 	it('should render with an approaching limit warning if a metric is approaching plan limit', () => {
@@ -56,11 +69,11 @@ describe('UsageOverview', () => {
 			})
 		);
 
-		const component = shallow(
-			<UsageOverview {...defaultProps} project={mockProject} />
+		const {container} = render(
+			<WrappedComponent {...defaultProps} project={mockProject} />
 		);
 
-		expect(component.find('Alert').at(0)).toMatchSnapshot();
+		expect(container.querySelector('.alert-warning')).toBeInTheDocument();
 	});
 
 	it('should render with an overage warning if a metric has exceeded the plan limit', () => {
@@ -74,11 +87,11 @@ describe('UsageOverview', () => {
 			})
 		);
 
-		const component = shallow(
-			<UsageOverview {...defaultProps} project={mockProject} />
+		const {container} = render(
+			<WrappedComponent {...defaultProps} project={mockProject} />
 		);
 
-		expect(component.find('Alert').at(0)).toMatchSnapshot();
+		expect(container.querySelector('.alert-danger')).toBeInTheDocument();
 	});
 
 	it('should render with a member-specific message overage warning if a metric is approaching plan limit and the user is a member role', () => {
@@ -92,8 +105,8 @@ describe('UsageOverview', () => {
 			})
 		);
 
-		const component = shallow(
-			<UsageOverview
+		const {container, getByText} = render(
+			<WrappedComponent
 				{...defaultProps}
 				currentUser={
 					new User(data.mockUser(0, {roleName: UserRoleNames.Member}))
@@ -102,7 +115,8 @@ describe('UsageOverview', () => {
 			/>
 		);
 
-		expect(component.find('Alert').at(0)).toMatchSnapshot();
+		expect(container.querySelector('.alert-warning')).toBeInTheDocument();
+		expect(getByText('Usage Limit Approaching:')).toBeInTheDocument();
 	});
 
 	it('should use default addons for basic plans', () => {
@@ -116,12 +130,12 @@ describe('UsageOverview', () => {
 			})
 		);
 
-		const component = shallow(
-			<UsageOverview {...defaultProps} project={mockProject} />
+		const {getAllByText} = render(
+			<WrappedComponent {...defaultProps} project={mockProject} />
 		);
 
 		jest.runAllTimers();
 
-		expect(component).toMatchSnapshot();
+		expect(getAllByText('Add-ons')[0]).toBeInTheDocument();
 	});
 });
