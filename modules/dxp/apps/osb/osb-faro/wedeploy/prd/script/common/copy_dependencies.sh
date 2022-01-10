e5709bf8c01bb0a89e11c8598412af484d445b1a
Inácio Nery <inacio.nery@liferay.com>
2022-01-10 12:53:10 -0300

    LRAC-8225 Update base image to liferaycloud with liferay dxp 7.1.10-ga1-fp18

--- modules/dxp/apps/osb/osb-faro/wedeploy/prd/script/common/copy_dependencies.sh
+++ modules/dxp/apps/osb/osb-faro/wedeploy/prd/script/common/copy_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -r /wedeploy-container/resources/common/log4j $LIFERAY_HOME/osgi/log4j/
-cp -r /wedeploy-container/resources/common/portal-log4j-ext.xml $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/classes/META-INF
-cp -r /wedeploy-container/resources/common/system-ext.properties $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/classes/
-cp -r /wedeploy-container/resources/common/urlrewrite.xml $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/
+cp -r /docker-init.d/resources/common/log4j $LIFERAY_HOME/osgi/log4j/
+cp -r /docker-init.d/resources/common/portal-log4j-ext.xml $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/classes/META-INF
+cp -r /docker-init.d/resources/common/system-ext.properties $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/classes/
+cp -r /docker-init.d/resources/common/urlrewrite.xml $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/
