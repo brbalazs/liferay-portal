9b7d601d6e5e95e9ad5d89dcbd905e22394e5c07
Jhonattas Ferreira <jhonattas.ferreira@liferay.com>
2022-01-11 11:38:12 -0300

    LRAC-10368 Convert WithRangeKey test to RTL

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/__tests__/WithRangeKey.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/__tests__/WithRangeKey.jsx
@@ -1,49 +1,70 @@
 import React from 'react';
 import withRangeKey from '../WithRangeKey';
-import {shallow} from 'enzyme';
+import {RangeKeyTimeRanges} from 'shared/util/constants';
+import {render} from '@testing-library/react';
+
+const RANGE_KEY_180 = RangeKeyTimeRanges.Last180Days;
+
+jest.unmock('react-dom');
 
 describe('WithRangeKey', () => {
-	it('should render the original component', () => {
-		const componentSpy = jest.fn();
-		const WrappedComponent = withRangeKey(componentSpy);
+	it('should render', () => {
+		const WrappedComponent = withRangeKey(({rangeSelectors}) => (
+			<div {...rangeSelectors}>{'Foobar'}</div>
+		));
 
-		shallow(
-			<WrappedComponent
-				rangeSelectors={{
-					rangeEnd: null,
-					rangeKey: '30',
-					rangeStart: null
-				}}
-			/>
-		).shallow();
+		const {container} = render(<WrappedComponent />);
 
-		expect(componentSpy).toHaveBeenCalled();
+		expect(container).toMatchSnapshot();
 	});
 
 	it('should pass rangeSelectors as a prop to the wrapped component', () => {
-		const componentSpy = jest.fn();
-		const WrappedComponent = withRangeKey(componentSpy);
+		const WrappedComponent = withRangeKey(
+			({rangeSelectors: {rangeKey}}) => (
+				<div>
+					{rangeKey === RANGE_KEY_180
+						? 'rangeKey is 180'
+						: 'rangeKey is not 180'}
+				</div>
+			)
+		);
 
-		shallow(
+		const {getByText} = render(
 			<WrappedComponent
 				rangeSelectors={{
 					rangeEnd: null,
-					rangeKey: '30',
+					rangeKey: RANGE_KEY_180,
 					rangeStart: null
 				}}
 			/>
-		).shallow();
-
-		expect(componentSpy).toBeCalledWith(
-			expect.objectContaining({
-				onRangeSelectorsChange: expect.any(Function),
-				rangeSelectors: {
-					rangeEnd: null,
-					rangeKey: '30',
-					rangeStart: null
-				}
-			}),
-			{}
 		);
+
+		expect(getByText('rangeKey is 180'));
+	});
+
+	it('should change the rangeSelectors with the onRangeSelectorsChange function', () => {
+		let handleRangeSelectors;
+
+		const WrappedComponent = withRangeKey(
+			({onRangeSelectorsChange, rangeSelectors: {rangeKey}}) => {
+				handleRangeSelectors = onRangeSelectorsChange;
+
+				return (
+					<div>
+						{rangeKey === RANGE_KEY_180
+							? 'rangeKey is 180'
+							: 'rangeKey is not 180'}
+					</div>
+				);
+			}
+		);
+
+		const {getByText} = render(<WrappedComponent />);
+
+		expect(getByText('rangeKey is not 180'));
+
+		handleRangeSelectors({rangeKey: RANGE_KEY_180});
+
+		expect(getByText('rangeKey is 180'));
 	});
 });
