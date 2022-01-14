d8948f6c1b749c8d0f388fc1167f55ecba09b37b
Adriano <adriano.interaminense@gmail.com>
2022-01-14 10:17:58 -0300

    LRAC-10368 Update unit test for WithRangeKey hoc

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/__tests__/WithRangeKey.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/hoc/__tests__/WithRangeKey.jsx
@@ -3,68 +3,62 @@ import withRangeKey from '../WithRangeKey';
 import {RangeKeyTimeRanges} from 'shared/util/constants';
 import {render} from '@testing-library/react';
 
-const RANGE_KEY_180 = RangeKeyTimeRanges.Last180Days;
-
 jest.unmock('react-dom');
+
+const WrappedComponent = withRangeKey(({rangeSelectors}) => {
+	const {rangeEnd, rangeKey, rangeStart} = rangeSelectors;
+
+	return (
+		<div>
+			<span>{`rangeEnd: ${rangeEnd}`}</span>
+			<span>{`rangeKey: ${rangeKey}`}</span>
+			<span>{`rangeStart: ${rangeStart}`}</span>
+		</div>
+	);
+});
 
 describe('WithRangeKey', () => {
 	it('should render', () => {
-		const WrappedComponent = withRangeKey(({rangeSelectors}) => (
-			<div {...rangeSelectors}>{'Foobar'}</div>
-		));
+		const {getByText} = render(<WrappedComponent />);
 
-		const {container} = render(<WrappedComponent />);
-
-		expect(container).toMatchSnapshot();
+		expect(getByText('rangeEnd: null')).toBeTruthy();
+		expect(
+			getByText(`rangeKey: ${RangeKeyTimeRanges.Last30Days}`)
+		).toBeTruthy();
+		expect(getByText('rangeStart: null')).toBeTruthy();
 	});
 
 	it('should pass rangeSelectors as a prop to the wrapped component', () => {
-		const WrappedComponent = withRangeKey(
-			({rangeSelectors: {rangeKey}}) => (
-				<div>
-					{rangeKey === RANGE_KEY_180
-						? 'rangeKey is 180'
-						: 'rangeKey is not 180'}
-				</div>
-			)
-		);
-
 		const {getByText} = render(
 			<WrappedComponent
 				rangeSelectors={{
-					rangeEnd: null,
-					rangeKey: RANGE_KEY_180,
-					rangeStart: null
+					rangeKey: RangeKeyTimeRanges.Last180Days
 				}}
 			/>
 		);
 
-		expect(getByText('rangeKey is 180'));
+		expect(getByText('rangeEnd: null')).toBeTruthy();
+		expect(
+			getByText(`rangeKey: ${RangeKeyTimeRanges.Last180Days}`)
+		).toBeTruthy();
+		expect(getByText('rangeStart: null')).toBeTruthy();
 	});
 
-	it('should change the rangeSelectors with the onRangeSelectorsChange function', () => {
-		let handleRangeSelectors;
-
-		const WrappedComponent = withRangeKey(
-			({onRangeSelectorsChange, rangeSelectors: {rangeKey}}) => {
-				handleRangeSelectors = onRangeSelectorsChange;
-
-				return (
-					<div>
-						{rangeKey === RANGE_KEY_180
-							? 'rangeKey is 180'
-							: 'rangeKey is not 180'}
-					</div>
-				);
-			}
+	it('should pass custom rangeSelectors as a prop to the wrapped component', () => {
+		const {getByText} = render(
+			<WrappedComponent
+				rangeSelectors={{
+					rangeEnd: '2022-15-01',
+					rangeKey: RangeKeyTimeRanges.CustomRange,
+					rangeStart: '2022-10-01'
+				}}
+			/>
 		);
 
-		const {getByText} = render(<WrappedComponent />);
-
-		expect(getByText('rangeKey is not 180'));
-
-		handleRangeSelectors({rangeKey: RANGE_KEY_180});
-
-		expect(getByText('rangeKey is 180'));
+		expect(getByText('rangeEnd: 2022-15-01')).toBeTruthy();
+		expect(
+			getByText(`rangeKey: ${RangeKeyTimeRanges.CustomRange}`)
+		).toBeTruthy();
+		expect(getByText('rangeStart: 2022-10-01')).toBeTruthy();
 	});
 });
