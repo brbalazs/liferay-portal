77b7f6ffe18494b2545b1917383b73c547c2282f
Marcos Martins <marcos.martins@liferay.com>
2022-01-18 17:57:09 -0300

    LRAC-10393 set value to be accorded to data source provider type column name in asah

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/DataSourceList.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/pages/DataSourceList.tsx
@@ -332,7 +332,7 @@ const DataSourceList: React.FC<IDataSourceListProps> = ({
 							label: Liferay.Language.get('name')
 						},
 						{
-							accessor: 'provider.type',
+							accessor: 'providerType',
 							dataFormatter: typeFormatter,
 							label: Liferay.Language.get('source')
 						},
@@ -347,7 +347,7 @@ const DataSourceList: React.FC<IDataSourceListProps> = ({
 							sortable: false
 						},
 						{
-							accessor: 'dateCreated',
+							accessor: 'createDate',
 							dataFormatter: date =>
 								dateFormatter(date, timeZoneId),
 							label: Liferay.Language.get('date-added')
