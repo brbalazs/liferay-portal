43fa193315be858db6fa74669b5fb5ccc562a63c
Gabriel Ramos <gabriel.ramos@liferay.com>
2022-02-02 13:01:34 -0300

    LRAC-10417 Update Loading and Overview to functional components

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/pages/Loading.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/shared/pages/Loading.tsx
@@ -1,15 +1,15 @@
 import getCN from 'classnames';
-import React from 'react';
+import React, {FC} from 'react';
 import Spinner from '../components/Spinner';
 
-export default class Loading extends React.Component {
-	render() {
-		const {className, fadeIn = true} = this.props;
-
-		return (
-			<div className={getCN('loading-root', className)}>
-				<Spinner fadeIn={fadeIn} />
-			</div>
-		);
-	}
+export interface ILoadingProps extends React.HTMLAttributes<HTMLDivElement> {
+	fadeIn?: boolean;
 }
+
+const Loading: FC<ILoadingProps> = ({className, fadeIn = true}) => (
+	<div className={getCN('loading-root', className)}>
+		<Spinner fadeIn={fadeIn} />
+	</div>
+);
+
+export default Loading;
--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/sites/pages/Overview.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/sites/pages/Overview.tsx
@@ -3,114 +3,102 @@ import CohortAnalysisCard from 'sites/hocs/CohortAnalysisCard';
 import DevicesCard from 'sites/hocs/DevicesCard';
 import InterestsCard from 'sites/hocs/InterestsCard';
 import LocationsCard from 'sites/hocs/LocationsCard';
-import React from 'react';
+import React, {FC} from 'react';
 import SearchTermsCard from 'sites/hocs/SearchTermsCard';
 import SiteMetricsCard from 'sites/hocs/MetricsCard';
 import TopPagesCard from 'sites/hocs/TopPagesCard';
 import VisitorsByTimeCard from 'sites/hocs/VisitorsByTimeCard';
 import {Routes, toRoute} from 'shared/util/router';
 import {sub} from 'shared/util/lang';
+import {useParams} from 'react-router-dom';
 
 interface IOverviewProps extends React.HTMLAttributes<HTMLDivElement> {
 	channelName: string;
-	router: {
-		params: {
-			channelId: string;
-			groupId: string;
-		};
-		query: object;
-	};
+	router: {query: object};
 }
 
-export default class Overview extends React.Component<IOverviewProps> {
-	render() {
-		const {
-			channelName,
-			router: {
-				params: {channelId, groupId}
-			}
-		} = this.props;
+const Overview: FC<IOverviewProps> = ({channelName}) => {
+	const {channelId, groupId} = useParams();
 
-		return (
-			<div className='sites-dashboard-overview-root overview-root'>
-				<div className='row'>
-					<div className='col-xl-12'>
-						<SiteMetricsCard
-							className='site-metrics'
-							label={sub(Liferay.Language.get('x-activities'), [
-								channelName
-							])}
-							legacyDropdownRangeKey={false}
-						/>
-					</div>
-				</div>
-
-				<div className='row'>
-					<div className='col-xl-6'>
-						<TopPagesCard
-							footerHref={toRoute(Routes.SITES_TOUCHPOINTS, {
-								channelId,
-								groupId
-							})}
-							footerLabel={Liferay.Language.get('view-pages')}
-							label={Liferay.Language.get('top-pages')}
-							legacyDropdownRangeKey={false}
-							metricLabel={Liferay.Language.get('pages')}
-						/>
-					</div>
-
-					<div className='col-xl-6'>
-						<AcquisitionsCard
-							className='acquisitions-card-root table-tabs-root'
-							label={Liferay.Language.get('acquisitions')}
-							legacyDropdownRangeKey={false}
-						/>
-					</div>
-				</div>
-
-				<div className='row'>
-					<div className='col-xl-4'>
-						<VisitorsByTimeCard
-							className='visitors-by-time-card'
-							label={Liferay.Language.get(
-								'visitors-by-day-and-time'
-							)}
-						/>
-					</div>
-
-					<div className='col-xl-4'>
-						<SearchTermsCard />
-					</div>
-
-					<div className='col-xl-4'>
-						<InterestsCard />
-					</div>
-				</div>
-
-				<div className='row'>
-					<div className='col-xl-6'>
-						<LocationsCard
-							label={Liferay.Language.get('sessions-by-location')}
-							legacyDropdownRangeKey={false}
-							metricLabel={Liferay.Language.get('sessions')}
-						/>
-					</div>
-
-					<div className='col-xl-6'>
-						<DevicesCard
-							label={Liferay.Language.get('session-technology')}
-							legacyDropdownRangeKey={false}
-							metricLabel={Liferay.Language.get('sessions')}
-						/>
-					</div>
-				</div>
-
-				<div className='row'>
-					<div className='col-xl-12'>
-						<CohortAnalysisCard />
-					</div>
+	return (
+		<div className='sites-dashboard-overview-root overview-root'>
+			<div className='row'>
+				<div className='col-xl-12'>
+					<SiteMetricsCard
+						className='site-metrics'
+						label={sub(Liferay.Language.get('x-activities'), [
+							channelName
+						])}
+						legacyDropdownRangeKey={false}
+					/>
 				</div>
 			</div>
-		);
-	}
-}
+
+			<div className='row'>
+				<div className='col-xl-6'>
+					<TopPagesCard
+						footerHref={toRoute(Routes.SITES_TOUCHPOINTS, {
+							channelId,
+							groupId
+						})}
+						footerLabel={Liferay.Language.get('view-pages')}
+						label={Liferay.Language.get('top-pages')}
+						legacyDropdownRangeKey={false}
+						metricLabel={Liferay.Language.get('pages')}
+					/>
+				</div>
+
+				<div className='col-xl-6'>
+					<AcquisitionsCard
+						className='acquisitions-card-root table-tabs-root'
+						label={Liferay.Language.get('acquisitions')}
+						legacyDropdownRangeKey={false}
+					/>
+				</div>
+			</div>
+
+			<div className='row'>
+				<div className='col-xl-4'>
+					<VisitorsByTimeCard
+						className='visitors-by-time-card'
+						label={Liferay.Language.get('visitors-by-day-and-time')}
+					/>
+				</div>
+
+				<div className='col-xl-4'>
+					<SearchTermsCard />
+				</div>
+
+				<div className='col-xl-4'>
+					<InterestsCard />
+				</div>
+			</div>
+
+			<div className='row'>
+				<div className='col-xl-6'>
+					<LocationsCard
+						label={Liferay.Language.get('sessions-by-location')}
+						legacyDropdownRangeKey={false}
+						metricLabel={Liferay.Language.get('sessions')}
+					/>
+				</div>
+
+				<div className='col-xl-6'>
+					<DevicesCard
+						label={Liferay.Language.get('session-technology')}
+						legacyDropdownRangeKey={false}
+						metricLabel={Liferay.Language.get('sessions')}
+					/>
+				</div>
+			</div>
+
+			<div className='row'>
+				<div className='col-xl-12'>
+					<CohortAnalysisCard />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Overview;
