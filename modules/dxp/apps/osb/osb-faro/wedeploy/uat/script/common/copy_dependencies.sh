2ca07325be574c4f9e0cb2151ab56dfbac9d17e1
Inácio Nery <inacio.nery@liferay.com>
2022-02-14 18:54:22 -0300

    LRAC-10580 Update to liferay cloud 4.3.2

--- modules/dxp/apps/osb/osb-faro/wedeploy/uat/script/common/copy_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp -r /docker-init.d/resources/common/log4j $LIFERAY_HOME/osgi/log4j/
-cp -r /docker-init.d/resources/common/portal-log4j-ext.xml $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/classes/META-INF
-cp -r /docker-init.d/resources/common/system-ext.properties $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/classes/
-cp -r /docker-init.d/resources/common/urlrewrite.xml $LIFERAY_HOME/tomcat/webapps/ROOT/WEB-INF/
