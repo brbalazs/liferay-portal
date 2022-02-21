66d54278e7e86ea1be7b7a1f0b8fd575e993a952
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-02-21 13:28:07 +0100

    LPS-146536 Ensure that no NullPointerException is thrown when using as parameter an undeployed portletId.

--- portal-impl/test/integration/com/liferay/portal/util/PortalImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/util/PortalImplTest.java
@@ -19,7 +19,9 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.upload.UploadServletRequest;
 import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PortletKeys;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.SyntheticBundleRule;
 import com.liferay.portal.upload.LiferayServletRequest;
@@ -113,6 +115,23 @@ public class PortalImplTest {
 			originalRequest.getClass());
 		Assert.assertNotSame(requestWrapper3, originalRequest);
 		Assert.assertSame(request, getWrappedRequest(originalRequest));
+	}
+
+	@Test
+	public void testGetPortletTitleWithDeployedPortletId() {
+		String portletId = PortletKeys.SERVER_ADMIN;
+
+		Assert.assertEquals(
+			"Server Administration",
+			PortalUtil.getPortletTitle(portletId, LocaleUtil.US));
+	}
+
+	@Test
+	public void testGetPortletTitleWithUndeployedPortletId() {
+		String portletId = "TEST_PORTLET_" + RandomTestUtil.randomString();
+
+		Assert.assertEquals(
+			portletId, PortalUtil.getPortletTitle(portletId, LocaleUtil.US));
 	}
 
 	@Test
