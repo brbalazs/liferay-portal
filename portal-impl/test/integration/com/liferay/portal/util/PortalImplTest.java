4fce081847a3f4031dbb0a522ce95eb1b4cfb9a3
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-02-21 13:35:34 +0100

    LPS-146536 Ensure that no NullPointerException is thrown when PortletRequest has an undeployed portletId as attribute.

--- portal-impl/test/integration/com/liferay/portal/util/PortalImplTest.java
+++ portal-impl/test/integration/com/liferay/portal/util/PortalImplTest.java
@@ -14,20 +14,31 @@
 
 package com.liferay.portal.util;
 
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.servlet.PersistentHttpServletRequestWrapper;
+import com.liferay.portal.kernel.servlet.PortletServlet;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadServletRequest;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.SyntheticBundleRule;
 import com.liferay.portal.upload.LiferayServletRequest;
 import com.liferay.portal.upload.UploadServletRequestImpl;
 import com.liferay.portal.util.bundle.portalimpl.TestAlwaysAllowDoAsUser;
 import com.liferay.portal.util.test.AtomicState;
+import com.liferay.portal.util.test.LayoutTestUtil;
 import com.liferay.portal.util.test.PortletContainerTestUtil;
 
 import java.io.InputStream;
@@ -115,6 +126,27 @@ public class PortalImplTest {
 			originalRequest.getClass());
 		Assert.assertNotSame(requestWrapper3, originalRequest);
 		Assert.assertSame(request, getWrappedRequest(originalRequest));
+	}
+
+	@Test
+	public void testGetPortletTitleFromPortletRequestWithDeployedPortletId()
+		throws Exception {
+
+		Assert.assertEquals(
+			"Server Administration",
+			PortalUtil.getPortletTitle(
+				_mockPortletRequest(PortletKeys.SERVER_ADMIN)));
+	}
+
+	@Test
+	public void testGetPortletTitleFromPortletRequestWithUndeployedPortletId()
+		throws Exception {
+
+		String portletId = "TEST_PORTLET_" + RandomTestUtil.randomString();
+
+		Assert.assertEquals(
+			portletId,
+			PortalUtil.getPortletTitle(_mockPortletRequest(portletId)));
 	}
 
 	@Test
@@ -218,7 +250,59 @@ public class PortalImplTest {
 		return (HttpServletRequest)requestWrapper.getRequest();
 	}
 
+	private ThemeDisplay _getThemeDisplay() throws Exception {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		Company company = CompanyLocalServiceUtil.getCompany(
+			TestPropsValues.getCompanyId());
+
+		themeDisplay.setCompany(company);
+
+		_group = GroupTestUtil.addGroup();
+
+		Layout layout = LayoutTestUtil.addLayout(_group);
+
+		themeDisplay.setLayout(layout);
+		themeDisplay.setLayoutSet(layout.getLayoutSet());
+
+		themeDisplay.setLocale(LocaleUtil.US);
+		themeDisplay.setPlid(layout.getPlid());
+		themeDisplay.setPortalURL("http://localhost:8080");
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setUser(TestPropsValues.getUser());
+
+		return themeDisplay;
+	}
+
+	private MockPortletRequest _mockPortletRequest(String portletId)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = _getThemeDisplay();
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest() {
+				{
+					setAttribute(WebKeys.CTX, getServletContext());
+					setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
+				}
+			};
+
+		return new MockPortletRequest() {
+			{
+				setAttribute(WebKeys.PORTLET_ID, portletId);
+				setAttribute(
+					PortletServlet.PORTLET_SERVLET_REQUEST,
+					mockHttpServletRequest);
+				setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
+			}
+		};
+	}
+
 	private static AtomicState _atomicState;
+
+	@DeleteAfterTestRun
+	private Group _group;
 
 	private static class PersistentHttpServletRequestWrapper1
 		extends PersistentHttpServletRequestWrapper {
