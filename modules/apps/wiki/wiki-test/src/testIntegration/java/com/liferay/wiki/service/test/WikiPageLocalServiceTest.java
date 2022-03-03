95afea092185a72eeb6fdba9b16fa5bd5abc0403
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2022-03-03 15:10:36 +0100

    LPS-148306 Add tests

--- modules/apps/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/service/test/WikiPageLocalServiceTest.java
+++ modules/apps/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/service/test/WikiPageLocalServiceTest.java
@@ -37,6 +37,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.AssertUtils;
@@ -46,6 +47,7 @@ import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
@@ -98,6 +100,7 @@ public class WikiPageLocalServiceTest {
 		_group = GroupTestUtil.addGroup();
 
 		_node = WikiTestUtil.addNode(_group.getGroupId());
+		_user = UserTestUtil.addUser(_group.getGroupId());
 	}
 
 	@Test
@@ -1021,6 +1024,21 @@ public class WikiPageLocalServiceTest {
 		testRevertPage(true);
 	}
 
+	@Test
+	public void testUpdatePagePreservesOriginalOwner() throws Exception {
+		WikiPage page = WikiTestUtil.addPage(
+			_group.getGroupId(), _node.getNodeId(), true);
+
+		WikiPage updatedPage = WikiPageLocalServiceUtil.updatePage(
+			_user.getUserId(), _node.getNodeId(), page.getTitle(),
+			page.getVersion(), RandomTestUtil.randomString(),
+			RandomTestUtil.randomString(), false, page.getFormat(), null, null,
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+
+		Assert.assertNotEquals(page.getPageId(), updatedPage.getPageId());
+		Assert.assertEquals(page.getUserId(), updatedPage.getUserId());
+	}
+
 	protected void addExpandoValueToPage(WikiPage page) throws Exception {
 		ExpandoValue value = ExpandoTestUtil.addValue(
 			PortalUtil.getClassNameId(WikiPage.class), page.getPrimaryKey(),
@@ -1210,6 +1228,9 @@ public class WikiPageLocalServiceTest {
 	private Group _group;
 
 	private WikiNode _node;
+
+	@DeleteAfterTestRun
+	private User _user;
 
 	private static class AssetCategoryTestException extends PortalException {
 	}
