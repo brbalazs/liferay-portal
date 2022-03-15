89f0a23b7345d77b2465a71c9c6afe6ed37bff5b
Adriano Interaminense <adriano.interaminense@gmail.com>
2022-03-15 16:59:49 -0300

    LRAC-10361 Create unit test to render alert notification in CustomEventList

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/events/components/__tests__/CustomEventList.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/settings/definitions/events/components/__tests__/CustomEventList.jsx
@@ -1,50 +1,57 @@
 import * as API from 'shared/api';
 import * as data from 'test/data';
+import * as NotificationAlertList from 'shared/components/NotificationAlertList';
 import client from 'shared/apollo/client';
 import CustomEventList from '../CustomEventList';
 import mockStore from 'test/mock-store';
 import React from 'react';
 import {ApolloProvider} from '@apollo/react-components';
+import {cleanup, render} from '@testing-library/react';
 import {MemoryRouter, Route} from 'react-router-dom';
 import {MockedProvider} from '@apollo/react-testing';
 import {mockEventDefinitionsReq} from 'test/graphql-data';
+import {NotificationSubtypes} from 'shared/util/records/Notification';
 import {Provider} from 'react-redux';
-import {render} from '@testing-library/react';
+import {range} from 'lodash';
 import {Routes} from 'shared/util/router';
 import {waitForLoading} from 'test/helpers';
 
 jest.unmock('react-dom');
 
+const mockNotificationAlertList = NotificationAlertList;
+
+const WrappedComponent = props => (
+	<ApolloProvider client={client}>
+		<Provider store={mockStore()}>
+			<MemoryRouter
+				initialEntries={[
+					'/workspace/23/settings/definitions/events/custom?delta=1'
+				]}
+			>
+				<Route path={Routes.SETTINGS_DEFINITIONS_EVENTS_CUSTOM}>
+					<MockedProvider
+						mocks={[
+							mockEventDefinitionsReq(
+								[
+									data.mockEventDefinition(0, {
+										__typename: 'EventDefinition',
+										type: 'CUSTOM'
+									})
+								],
+								{eventType: 'CUSTOM'}
+							)
+						]}
+					>
+						<CustomEventList groupId='23' {...props} />
+					</MockedProvider>
+				</Route>
+			</MemoryRouter>
+		</Provider>
+	</ApolloProvider>
+);
+
 describe('CustomEventList', () => {
-	const WrappedComponent = props => (
-		<ApolloProvider client={client}>
-			<Provider store={mockStore()}>
-				<MemoryRouter
-					initialEntries={[
-						'/workspace/23/settings/definitions/events/custom?delta=1'
-					]}
-				>
-					<Route path={Routes.SETTINGS_DEFINITIONS_EVENTS_CUSTOM}>
-						<MockedProvider
-							mocks={[
-								mockEventDefinitionsReq(
-									[
-										data.mockEventDefinition(0, {
-											__typename: 'EventDefinition',
-											type: 'CUSTOM'
-										})
-									],
-									{eventType: 'CUSTOM'}
-								)
-							]}
-						>
-							<CustomEventList groupId='23' {...props} />
-						</MockedProvider>
-					</Route>
-				</MemoryRouter>
-			</Provider>
-		</ApolloProvider>
-	);
+	afterAll(cleanup);
 
 	it('should render', async () => {
 		const {container, queryByTestId} = render(<WrappedComponent />);
@@ -69,5 +76,25 @@ describe('CustomEventList', () => {
 		jest.runAllTimers();
 
 		expect(queryByTestId('select-all-checkbox')).toBeNull();
+	});
+
+	it('should render alert notification', async () => {
+		mockNotificationAlertList.useNotificationStates = jest.fn(() => ({
+			data: range(1).map(i =>
+				data.mockNotification(i, {
+					subtype: NotificationSubtypes.BlockedEventsLimit
+				})
+			),
+			loading: false,
+			refetch: () => {}
+		}));
+
+		const {container} = render(<WrappedComponent />);
+
+		await waitForLoading(container);
+
+		jest.runAllTimers();
+
+		expect(container).toMatchSnapshot();
 	});
 });
