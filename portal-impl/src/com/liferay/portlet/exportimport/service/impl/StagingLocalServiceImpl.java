354a6912daa2b85fadfb6d802a86fe2ab991a53a
Nikoletta Buza <nikoletta.buza@liferay.com>
2022-03-29 17:18:40 +0200

    LPS-142320 Handle `NoSuchGroupException` after exception information was removed from remote call responses due to a security related change.

--- portal-impl/src/com/liferay/portlet/exportimport/service/impl/StagingLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/exportimport/service/impl/StagingLocalServiceImpl.java
@@ -728,11 +728,6 @@ public class StagingLocalServiceImpl extends StagingLocalServiceBaseImpl {
 		try {
 			GroupServiceHttp.disableStaging(httpPrincipal, remoteGroupId);
 		}
-		catch (NoSuchGroupException nsge) {
-			if (_log.isWarnEnabled()) {
-				_log.warn("Remote live group was already deleted", nsge);
-			}
-		}
 		catch (PrincipalException pe) {
 
 			// LPS-52675
@@ -775,6 +770,24 @@ public class StagingLocalServiceImpl extends StagingLocalServiceBaseImpl {
 				ree.setURL(remoteURL);
 
 				throw ree;
+			}
+
+			if (_log.isWarnEnabled()) {
+				_log.warn("Forcibly disable remote staging");
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			if (!forceDisable) {
+				RemoteExportException remoteExportException =
+					new RemoteExportException(RemoteExportException.NO_GROUP);
+
+				remoteExportException.setGroupId(remoteGroupId);
+
+				throw remoteExportException;
 			}
 
 			if (_log.isWarnEnabled()) {
