fa0b1f11e9c9ddb92b055ee64ca20a3ab5b6c351
Marcio Fonseca <marcio.fonseca@liferay.com>
2022-05-11 17:27:52 -0300

    LRAC-11108 Update empty state message on individual/dashboard/pages/Interests.tsx

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/individual/dashboard/pages/Interests.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/individual/dashboard/pages/Interests.tsx
@@ -1,10 +1,11 @@
+import Button from 'shared/components/Button';
 import Interests from '../hocs/Interests';
-import NoResultsDisplay from 'shared/components/NoResultsDisplay';
 import React from 'react';
 import StatesRenderer from 'shared/components/states-renderer/StatesRenderer';
 import URLConstants from 'shared/util/url-constants';
-import {Sizes} from 'shared/util/constants';
+import {Routes, toRoute} from 'shared/util/router';
 import {useDataSource} from 'shared/hooks/useDataSource';
+import {useParams} from 'react-router-dom';
 import {User} from 'shared/util/records';
 import {withCurrentUser} from 'shared/hoc';
 
@@ -12,51 +13,55 @@ interface IInterestsPageProps extends React.HTMLAttributes<HTMLElement> {
 	currentUser: User;
 }
 
-const InterestsPage: React.FC<IInterestsPageProps> = () => {
+const InterestsPage: React.FC<IInterestsPageProps> = ({currentUser}) => {
+	const {groupId} = useParams();
+	const authorized = currentUser.isAdmin();
+
 	const dataSourceStates = useDataSource();
-
-	const emptyState = {
-		description: (
-			<>
-				{Liferay.Language.get(
-					'check-back-later-to-verify-if-data-has-been-received-from-your-data-sources'
-				)}
-
-				<a
-					className='d-block mb-3'
-					href={
-						URLConstants.IndividualsDashboardInterestsDocumentation
-					}
-					key='DOCUMENTATION'
-					target='_blank'
-				>
-					{Liferay.Language.get('learn-more-about-interests')}
-				</a>
-			</>
-		),
-		title: Liferay.Language.get('there-are-no-interests-found')
-	};
 
 	return (
 		<StatesRenderer {...dataSourceStates}>
-			<StatesRenderer.Empty {...emptyState} displayCard />
+			<StatesRenderer.Empty
+				description={
+					<>
+						{Liferay.Language.get(
+							'connect-a-data-source-with-sites-data'
+						)}
+
+						<a
+							className='d-block mb-3'
+							href={URLConstants.DataSourceConnection}
+							key='DOCUMENTATION'
+							target='_blank'
+						>
+							{Liferay.Language.get(
+								'access-our-documentation-to-learn-more'
+							)}
+						</a>
+
+						{authorized && (
+							<Button
+								display='primary'
+								href={toRoute(Routes.SETTINGS_ADD_DATA_SOURCE, {
+									groupId
+								})}
+							>
+								{Liferay.Language.get('connect-data-source')}
+							</Button>
+						)}
+					</>
+				}
+				displayCard
+				title={Liferay.Language.get(
+					'no-sites-synced-from-data-sources'
+				)}
+			/>
 
 			<StatesRenderer.Success>
 				<div className='individuals-dashboard-interests-root'>
 					<div className='row'>
 						<div className='col-xl-12'>
-							<Interests
-								noResultsRenderer={
-									<NoResultsDisplay
-										{...emptyState}
-										icon={{
-											border: false,
-											size: Sizes.XXXLarge,
-											symbol: 'ac-satellite'
-										}}
-									/>
-								}
-							/>
+							<Interests />
 						</div>
 					</div>
 				</div>
