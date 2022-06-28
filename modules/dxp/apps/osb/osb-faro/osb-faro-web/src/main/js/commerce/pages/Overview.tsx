abfb1ece3b381f474f4a662d6e786ced29df52d3
Adriano Interaminense <adriano.interaminense@gmail.com>
2022-06-28 16:55:06 -0300

    LRAC-11270 Create AverageRevenuePerAccount component

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/commerce/pages/Overview.tsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/commerce/pages/Overview.tsx
@@ -1,4 +1,5 @@
 import AverageOrderValueCard from 'commerce/components/AverageOrderValueCard';
+import AverageRevenuePerAccountCard from 'commerce/components/AverageRevenuePerAccountCard';
 import IncompleteOrdersCard from 'commerce/components/IncompleteOrdersCard';
 import React from 'react';
 import TotalOrderValueCard from 'commerce/components/TotalOrderValueCard';
@@ -16,6 +17,9 @@ const Overview = () => (
 		<div className='row'>
 			<div className='col-xl-6'>
 				<AverageOrderValueCard />
+			</div>
+			<div className='col-xl-6'>
+				<AverageRevenuePerAccountCard />
 			</div>
 		</div>
 	</div>
