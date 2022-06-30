dc025f8beda2a8fb52f031c746c2328e335617b2
Ilza Medeiros <ilza.medeiros@liferay.com>
2022-06-30 11:55:32 -0300

    LRAC-11403 - Convert assets/components/TouchpointsListCard.jsx to functional component

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/assets/components/TouchpointsListCard.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/assets/components/TouchpointsListCard.jsx
@@ -1,11 +1,9 @@
-import BasePage from 'shared/components/base-page';
-import React from 'react';
+import React, {useRef} from 'react';
 import Table from 'shared/components/table';
 import TextTruncate from 'shared/components/TextTruncate';
-import {getUrl} from 'shared/util/urls';
+import {getUrl as getUrlUtil} from 'shared/util/urls';
 import {Link} from 'react-router-dom';
 import {pickBy} from 'lodash';
-import {PropTypes} from 'prop-types';
 import {Routes} from 'shared/util/router';
 import {sub} from 'shared/util/lang';
 import {toFixedPoint} from 'shared/util/numbers';
@@ -13,49 +11,20 @@ import {toFixedPoint} from 'shared/util/numbers';
 const CLASSNAME = 'analytics-touchpoints-list';
 const MAX_PAGES_LIMIT = 1000;
 
-/**
- * Touchpoint List Card
- * @class
- */
-class TouchpointsListCard extends React.Component {
-	static contextType = BasePage.Context;
+const TouchpointsListCard = ({
+	items = [],
+	params,
+	groupId,
+	query,
+	rangeSelectors
+}) => {
+	const elementRef = useRef(null);
 
-	static defaultProps = {
-		items: []
-	};
-
-	static propTypes = {
-		items: PropTypes.arrayOf(
-			PropTypes.shape({
-				title: PropTypes.string,
-				touchpoint: PropTypes.string
-			})
-		)
-	};
-
-	constructor(props) {
-		super(props);
-
-		this._elementRef = React.createRef();
-	}
-
-	/**
-	 * Get Touchpoint URL
-	 * @description Get url to navigate in a dashboard
-	 * @param {string} title
-	 * @param {string} touchpoint
-	 */
-	getUrl(title, touchpoint) {
-		const {
-			context: {
-				router: {params, query}
-			},
-			props: {rangeSelectors}
-		} = this;
-
+	const getUrl = (title, touchpoint) => {
 		const router = {
 			params: {
 				...params,
+				groupId,
 				title,
 				touchpoint: encodeURIComponent(touchpoint)
 			},
@@ -64,33 +33,11 @@ class TouchpointsListCard extends React.Component {
 				...pickBy(rangeSelectors)
 			}
 		};
+		return getUrlUtil(Routes.SITES_TOUCHPOINTS_OVERVIEW, router);
+	};
 
-		return getUrl(Routes.SITES_TOUCHPOINTS_OVERVIEW, router);
-	}
-
-	/**
-	 * When Limit of 1000 pages is reached
-	 */
-	handlePageLimitWarning(items) {
-		if (items.length >= MAX_PAGES_LIMIT) {
-			return (
-				<p>
-					{sub(
-						Liferay.Language.get('x-page-limit-has-been-reached'),
-						[toFixedPoint(MAX_PAGES_LIMIT)]
-					)}
-				</p>
-			);
-		}
-	}
-
-	/**
-	 * Render Title Column
-	 * @param {object} param0
-	 */
-	renderTitleColumn({title, touchpoint}) {
-		const url = this.getUrl(title, touchpoint);
-
+	const renderTitleColumn = ({title, touchpoint}) => {
+		const url = getUrl(title, touchpoint);
 		return (
 			<td className='table-cell-expand'>
 				<Link
@@ -101,15 +48,10 @@ class TouchpointsListCard extends React.Component {
 				</Link>
 			</td>
 		);
-	}
+	};
 
-	/**
-	 * Render Touchpoint Column
-	 * @param {object} param0
-	 */
-	renderTouchpointColumn({title, touchpoint}) {
-		const url = this.getUrl(title, touchpoint);
-
+	const renderTouchpointColumn = ({title, touchpoint}) => {
+		const url = getUrl(title, touchpoint);
 		return (
 			<td className='table-cell-expand'>
 				<Link className='text-secondary text-truncate-inline' to={url}>
@@ -117,42 +59,42 @@ class TouchpointsListCard extends React.Component {
 				</Link>
 			</td>
 		);
-	}
+	};
 
-	/**
-	 * Lifecycle Render - ReactJS
-	 */
-	render() {
-		const {items} = this.props;
+	const tableColumns = [
+		{
+			accessor: 'title',
+			cellRenderer: ({data}) => renderTitleColumn(data),
+			label: Liferay.Language.get('page-name'),
+			sortable: false,
+			title: true
+		},
+		{
+			accessor: 'url',
+			cellRenderer: ({data}) => renderTouchpointColumn(data),
+			label: Liferay.Language.get('canonical-url'),
+			sortable: false
+		}
+	];
 
-		const tableColumns = [
-			{
-				accessor: 'title',
-				cellRenderer: ({data}) => this.renderTitleColumn(data),
-				label: Liferay.Language.get('page-name'),
-				sortable: false,
-				title: true
-			},
-			{
-				accessor: 'url',
-				cellRenderer: ({data}) => this.renderTouchpointColumn(data),
-				label: Liferay.Language.get('canonical-url'),
-				sortable: false
-			}
-		];
-
-		return (
-			<div className={CLASSNAME} ref={this._elementRef}>
-				<Table
-					className='table-hover'
-					columns={tableColumns}
-					items={items}
-					rowIdentifier={['touchpoint', 'title']}
-				/>
-				{this.handlePageLimitWarning(items)}
-			</div>
-		);
-	}
-}
+	return (
+		<div className={CLASSNAME} ref={elementRef}>
+			<Table
+				className='table-hover'
+				columns={tableColumns}
+				items={items}
+				rowIdentifier={['touchpoint', 'title']}
+			/>
+			{items?.length >= MAX_PAGES_LIMIT && (
+				<p>
+					{sub(
+						Liferay.Language.get('x-page-limit-has-been-reached'),
+						[toFixedPoint(MAX_PAGES_LIMIT)]
+					)}
+				</p>
+			)}
+		</div>
+	);
+};
 
 export default TouchpointsListCard;
