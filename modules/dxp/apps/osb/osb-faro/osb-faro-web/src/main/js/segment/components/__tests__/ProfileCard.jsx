f58a0637c4742a322d4e6576e7762e7e3e719e6b
Ilza Medeiros <ilza.medeiros@liferay.com>
2022-07-26 16:43:11 -0300

    LRAC-11546 - [F] Convert segment/components/ProfileCard.jsx to typescript

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/components/__tests__/ProfileCard.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/components/__tests__/ProfileCard.jsx
@@ -12,9 +12,7 @@ const DefaultComponent = props => (
 		<SegmentProfileCard
 			channelId='123'
 			groupId='23'
-			id='3'
-			segment={data.getImmutableMock(Segment, data.mockSegment)}
-			tabId='1'
+			segment={data.getImmutableMock(Segment, data.mockSegment, '3')}
 			{...props}
 		/>
 	</StaticRouter>
