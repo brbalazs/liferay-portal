8eebfe6ebf0615dc77f0bac67883fe3977c7e6c9
Adriano Interaminense <adriano.interaminense@gmail.com>
2022-08-08 18:37:17 -0300

    LRAC-11567 Update unit tests

--- modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/segment-editor/dynamic/__tests__/Toolbar.jsx
+++ modules/dxp/apps/osb/osb-faro/osb-faro-web/src/main/js/segment/segment-editor/dynamic/__tests__/Toolbar.jsx
@@ -1,6 +1,11 @@
+import * as API from 'shared/api';
 import * as data from 'test/data';
 import React from 'react';
-import {cleanup, render} from '@testing-library/react';
+import {
+	cleanup,
+	render,
+	waitForElementToBeRemoved
+} from '@testing-library/react';
 import {Formik} from 'formik';
 import {StaticRouter} from 'react-router';
 import {Toolbar} from '../Toolbar';
@@ -8,7 +13,11 @@ import {Toolbar} from '../Toolbar';
 jest.unmock('react-dom');
 
 describe('Toolbar', () => {
-	afterEach(cleanup);
+	afterEach(() => {
+		jest.clearAllMocks();
+
+		cleanup();
+	});
 
 	it('should render', () => {
 		const {container} = render(
@@ -25,7 +34,23 @@ describe('Toolbar', () => {
 		expect(container).toMatchSnapshot();
 	});
 
-	it('should render w/ preview button disabled', () => {
+	it('should render w/ preview button disabled if criteria is valid', () => {
+		const {getByTestId} = render(
+			<StaticRouter>
+				<Formik>
+					<Toolbar
+						channelId='321'
+						criteria={data.mockNewCriteria(1, {valid: true})}
+						groupId='123'
+					/>
+				</Formik>
+			</StaticRouter>
+		);
+
+		expect(getByTestId('preview-criteria-button')).toBeDisabled();
+	});
+
+	it('should render w/ preview button disabled if total members count is bigger thant 0', () => {
 		const {getByTestId} = render(
 			<StaticRouter>
 				<Formik>
@@ -41,17 +66,24 @@ describe('Toolbar', () => {
 		expect(getByTestId('preview-criteria-button')).toBeDisabled();
 	});
 
-	it('should render w/ preview button enabled', () => {
-		const {getByTestId} = render(
+	it('should render w/ preview button enabled if total members count is bigger than 0 and criteria is valid', async () => {
+		API.individuals.search.mockReturnValue(Promise.resolve({total: 1}));
+
+		const {container, getByTestId} = render(
 			<StaticRouter>
 				<Formik>
 					<Toolbar
 						channelId='321'
 						criteria={data.mockNewCriteria(1, {valid: true})}
 						groupId='123'
+						valid
 					/>
 				</Formik>
 			</StaticRouter>
+		);
+
+		await waitForElementToBeRemoved(() =>
+			container.querySelector('.spinner-root')
 		);
 
 		expect(getByTestId('preview-criteria-button')).toBeEnabled();
