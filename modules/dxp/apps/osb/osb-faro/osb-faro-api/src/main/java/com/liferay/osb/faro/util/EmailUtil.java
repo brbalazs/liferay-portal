a32ae56e348fc47533628631a6dfeae018630d50
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2022-08-24 22:25:43 -0700

    LRAC-11736 EmailUtil: add methods to get icons

--- modules/dxp/apps/osb/osb-faro/osb-faro-api/src/main/java/com/liferay/osb/faro/util/EmailUtil.java
+++ modules/dxp/apps/osb/osb-faro/osb-faro-api/src/main/java/com/liferay/osb/faro/util/EmailUtil.java
@@ -17,10 +17,32 @@ package com.liferay.osb.faro.util;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
 
+import java.util.Objects;
+
 /**
  * @author Matthew Kong
  */
 public class EmailUtil {
+
+	public static String getEmailBannerURL(String frequency) {
+		if (Objects.equals(frequency, "daily")) {
+			return _FARO_URL.concat(
+				"/o/osb-faro-web/images/email/ac-email-banner-daily.png");
+		}
+		else if (Objects.equals(frequency, "monthly")) {
+			return _FARO_URL.concat(
+				"/o/osb-faro-web/images/email/ac-email-banner-monthly.png");
+		}
+		else {
+			return _FARO_URL.concat(
+				"/o/osb-faro-web/images/email/ac-email-banner-weekly.png");
+		}
+	}
+
+	public static String getLiferayLogoIconURL() {
+		return _FARO_URL.concat(
+			"/o/osb-faro-web/images/email/liferay-logo.png");
+	}
 
 	public static String getLogoIconURL() {
 		return _FARO_URL.concat("/o/osb-faro-web/images/email/ac-chart.png");
@@ -28,6 +50,21 @@ public class EmailUtil {
 
 	public static String getTitleIconURL() {
 		return _FARO_URL.concat("/o/osb-faro-web/images/email/ac-title.png");
+	}
+
+	public static String getTrendIconURL(String trend) {
+		if (Objects.equals(trend, "POSITIVE")) {
+			return _FARO_URL.concat(
+				"/o/osb-faro-web/images/email/icon-order-arrow-up.png");
+		}
+		else if (Objects.equals(trend, "NEGATIVE")) {
+			return _FARO_URL.concat(
+				"/o/osb-faro-web/images/email/icon-order-arrow-down.png");
+		}
+		else {
+			return _FARO_URL.concat(
+				"/o/osb-faro-web/images/email/icon-empty.png");
+		}
 	}
 
 	public static String getWorkspaceURL(Group group) {
