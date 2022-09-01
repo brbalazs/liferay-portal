332d6a2491cc1e0106f91014fd4487a6bda56f90
Julius Lee <meng-huan.lee@liferay.com>
2022-09-01 09:34:45 -0700

    LPS-158806 SF, change variable name from Mock to MockedStatic

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
@@ -92,14 +92,15 @@ public class PortalImplUnitTest {
 		ActionRequest actionRequest = _createActionRequestMock(
 			params, enumeration);
 
-		MockedStatic<PortalUtil> portalUtilMock = Mockito.mockStatic(
+		MockedStatic<PortalUtil> portalUtilMockedStatic = Mockito.mockStatic(
 			PortalUtil.class);
 
-		ActionResponse actionResponse = _createActionResponse(portalUtilMock);
+		ActionResponse actionResponse = _createActionResponse(
+			portalUtilMockedStatic);
 
 		_portalImpl.copyRequestParameters(actionRequest, actionResponse);
 
-		portalUtilMock.close();
+		portalUtilMockedStatic.close();
 
 		_assertActionResponse(actionResponse, params);
 	}
@@ -118,14 +119,15 @@ public class PortalImplUnitTest {
 		ActionRequest actionRequestMock = _createActionRequestMock(
 			params, enumeration);
 
-		MockedStatic<PortalUtil> portalUtilMock = Mockito.mockStatic(
+		MockedStatic<PortalUtil> portalUtilMockedStatic = Mockito.mockStatic(
 			PortalUtil.class);
 
-		ActionResponse actionResponse = _createActionResponse(portalUtilMock);
+		ActionResponse actionResponse = _createActionResponse(
+			portalUtilMockedStatic);
 
 		_portalImpl.copyRequestParameters(actionRequestMock, actionResponse);
 
-		portalUtilMock.close();
+		portalUtilMockedStatic.close();
 
 		_assertActionResponse(actionResponse, params);
 	}
@@ -736,7 +738,7 @@ public class PortalImplUnitTest {
 	}
 
 	private ActionResponse _createActionResponse(
-			MockedStatic<PortalUtil> portalUtilMock)
+			MockedStatic<PortalUtil> portalUtilMockedStatic)
 		throws PortletException {
 
 		HttpServletResponse httpServletResponse =
@@ -748,7 +750,7 @@ public class PortalImplUnitTest {
 		layoutTypePortletFactoryUtil.setLayoutTypePortletFactory(
 			new LayoutTypePortletFactoryImpl());
 
-		portalUtilMock.when(
+		portalUtilMockedStatic.when(
 			() -> PortalUtil.updateWindowState(
 				Mockito.anyString(), Mockito.any(UserImpl.class),
 				Mockito.any(LayoutImpl.class), Mockito.any(WindowState.class),
