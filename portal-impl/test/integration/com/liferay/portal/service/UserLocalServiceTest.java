bf043e540c0a5def9bd3b2e614349f4d5f5c6174
Brian Chan <brian.chan@liferay.com>
2022-10-18 18:15:21 +0100

    LPS-162282 Rename

--- portal-impl/test/integration/com/liferay/portal/service/UserLocalServiceTest.java
+++ portal-impl/test/integration/com/liferay/portal/service/UserLocalServiceTest.java
@@ -82,9 +82,7 @@ public class UserLocalServiceTest {
 		new LiferayIntegrationTestRule();
 
 	@Test
-	public void testAuthenticateByEmailAddressWithPasswordPolicy()
-		throws Exception {
-
+	public void testAuthenticateByEmailAddress() throws Exception {
 		User user = UserTestUtil.addUser();
 
 		String password = "password";
