ea87119b864305e336f3f4a95289bc73ad0b6ca9
Istvan Sajtos <istvan.sajtos@liferay.com>
2022-10-31 16:44:32 +0100

    LPS-122640 Rename variables for scope visibility

--- modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
+++ modules/apps/friendly-url/friendly-url-service/src/main/java/com/liferay/friendly/url/internal/servlet/FriendlyURLServlet.java
@@ -96,32 +96,28 @@ public class FriendlyURLServlet extends HttpServlet {
 			return new Redirect();
 		}
 
-		// Group friendly URL
-
-		String friendlyURL = path;
+		String groupFriendlyURL = path;
 
 		int pos = path.indexOf(CharPool.SLASH, 1);
 
 		if (pos != -1) {
-			friendlyURL = path.substring(0, pos);
+			groupFriendlyURL = path.substring(0, pos);
 		}
 
 		long companyId = PortalInstances.getCompanyId(httpServletRequest);
 
-		Group group = _getGroup(path, friendlyURL, companyId);
+		Group group = _getGroup(path, groupFriendlyURL, companyId);
 
 		Locale locale = portal.getLocale(httpServletRequest, null, false);
 
 		SiteFriendlyURL alternativeSiteFriendlyURL =
 			_getAlternativeSiteFriendlyURL(
-				friendlyURL, companyId, group, locale);
+				groupFriendlyURL, companyId, group, locale);
 
-		// Layout friendly URL
-
-		friendlyURL = null;
+		String layoutFriendlyURL = null;
 
 		if ((pos != -1) && ((pos + 1) != path.length())) {
-			friendlyURL = path.substring(pos);
+			layoutFriendlyURL = path.substring(pos);
 		}
 		else {
 			httpServletRequest.setAttribute(
@@ -148,19 +144,19 @@ public class FriendlyURLServlet extends HttpServlet {
 			LayoutFriendlyURLSeparatorComposite
 				layoutFriendlyURLSeparatorComposite =
 					portal.getLayoutFriendlyURLSeparatorComposite(
-						group.getGroupId(), _private, friendlyURL, params,
+						group.getGroupId(), _private, layoutFriendlyURL, params,
 						requestContext);
 
 			Layout layout = layoutFriendlyURLSeparatorComposite.getLayout();
 
 			if (!StringUtil.startsWith(
-					friendlyURL, Portal.FRIENDLY_URL_SEPARATOR) &&
+					layoutFriendlyURL, Portal.FRIENDLY_URL_SEPARATOR) &&
 				_isAssetDisplayPage(layout)) {
 
 				throw new NoSuchLayoutException(
 					StringBundler.concat(
 						"{groupId=", group.getGroupId(), ", privateLayout=",
-						_private, ", friendlyURL=", friendlyURL, "}"));
+						_private, ", friendlyURL=", layoutFriendlyURL, "}"));
 			}
 
 			httpServletRequest.setAttribute(WebKeys.LAYOUT, layout);
@@ -237,7 +233,7 @@ public class FriendlyURLServlet extends HttpServlet {
 					continue;
 				}
 
-				if (layout.matches(httpServletRequest, friendlyURL)) {
+				if (layout.matches(httpServletRequest, layoutFriendlyURL)) {
 					String redirect = portal.getLayoutActualURL(
 						layout, Portal.PATH_MAIN);
 
@@ -249,8 +245,8 @@ public class FriendlyURLServlet extends HttpServlet {
 		}
 
 		String actualURL = portal.getActualURL(
-			group.getGroupId(), _private, Portal.PATH_MAIN, friendlyURL, params,
-			requestContext);
+			group.getGroupId(), _private, Portal.PATH_MAIN, layoutFriendlyURL,
+			params, requestContext);
 		String portalURL = portal.getPortalURL(httpServletRequest);
 
 		if (actualURL.startsWith(portalURL)) {
