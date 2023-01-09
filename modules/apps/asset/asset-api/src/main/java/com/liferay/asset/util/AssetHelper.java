0ff4f15eef65c300b43a6ca75cb35b0c3f3a64ab
Attila Bakay <attila.bakay@liferay.com>
2023-01-09 11:51:41 +0100

    LPS-171484 default to the old method to avoid API break

--- modules/apps/asset/asset-api/src/main/java/com/liferay/asset/util/AssetHelper.java
+++ modules/apps/asset/asset-api/src/main/java/com/liferay/asset/util/AssetHelper.java
@@ -73,8 +73,11 @@ public interface AssetHelper {
 
 	public String getAssetKeywords(String className, long classPK);
 
-	public String getAssetKeywords(
-		String className, long classPK, Locale locale);
+	public default String getAssetKeywords(
+		String className, long classPK, Locale locale) {
+
+		return getAssetKeywords(className, classPK);
+	}
 
 	public List<AssetPublisherAddItemHolder> getAssetPublisherAddItemHolders(
 			LiferayPortletRequest liferayPortletRequest,
