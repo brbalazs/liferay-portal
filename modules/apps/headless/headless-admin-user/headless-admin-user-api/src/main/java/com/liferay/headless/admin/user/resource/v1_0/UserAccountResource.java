a8ab75588692b9e91ee839b8deed5b9f53094fd2
Brian Chan <brian.chan@liferay.com>
2023-03-27 12:04:05 -0700

    LPS-172794 regen

--- modules/apps/headless/headless-admin-user/headless-admin-user-api/src/main/java/com/liferay/headless/admin/user/resource/v1_0/UserAccountResource.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-api/src/main/java/com/liferay/headless/admin/user/resource/v1_0/UserAccountResource.java
@@ -39,6 +39,7 @@ import javax.annotation.Generated;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.osgi.annotation.versioning.ProviderType;
@@ -62,13 +63,28 @@ public interface UserAccountResource {
 			Pagination pagination, Sort[] sorts)
 		throws Exception;
 
+	public Response postOrganizationUserAccountsPageExportBatch(
+			Long organizationId, String search, Filter filter, Sort[] sorts,
+			String callbackURL, String contentType, String fieldNames)
+		throws Exception;
+
 	public Page<UserAccount> getSiteUserAccountsPage(
 			Long siteId, String search, Filter filter, Pagination pagination,
 			Sort[] sorts)
 		throws Exception;
 
+	public Response postSiteUserAccountsPageExportBatch(
+			Long siteId, String search, Filter filter, Sort[] sorts,
+			String callbackURL, String contentType, String fieldNames)
+		throws Exception;
+
 	public Page<UserAccount> getUserAccountsPage(
 			String search, Filter filter, Pagination pagination, Sort[] sorts)
+		throws Exception;
+
+	public Response postUserAccountsPageExportBatch(
+			String search, Filter filter, Sort[] sorts, String callbackURL,
+			String contentType, String fieldNames)
 		throws Exception;
 
 	public UserAccount getUserAccount(Long userAccountId) throws Exception;
--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/graphql/servlet/v1_0/ServletDataImpl.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/graphql/servlet/v1_0/ServletDataImpl.java
@@ -63,8 +63,20 @@ public class ServletDataImpl implements ServletData {
 
 	@Activate
 	public void activate(BundleContext bundleContext) {
+		Mutation.setOrganizationResourceComponentServiceObjects(
+			_organizationResourceComponentServiceObjects);
+		Mutation.setPhoneResourceComponentServiceObjects(
+			_phoneResourceComponentServiceObjects);
+		Mutation.setRoleResourceComponentServiceObjects(
+			_roleResourceComponentServiceObjects);
+		Mutation.setSegmentResourceComponentServiceObjects(
+			_segmentResourceComponentServiceObjects);
 		Mutation.setSubscriptionResourceComponentServiceObjects(
 			_subscriptionResourceComponentServiceObjects);
+		Mutation.setUserAccountResourceComponentServiceObjects(
+			_userAccountResourceComponentServiceObjects);
+		Mutation.setWebUrlResourceComponentServiceObjects(
+			_webUrlResourceComponentServiceObjects);
 
 		Query.setEmailAddressResourceComponentServiceObjects(
 			_emailAddressResourceComponentServiceObjects);
@@ -125,10 +137,60 @@ public class ServletDataImpl implements ServletData {
 			new HashMap<String, ObjectValuePair<Class<?>, String>>() {
 				{
 					put(
+						"mutation#createOrganizationsPageExportBatch",
+						new ObjectValuePair<>(
+							OrganizationResourceImpl.class,
+							"postOrganizationsPageExportBatch"));
+					put(
+						"mutation#createOrganizationPhonesPageExportBatch",
+						new ObjectValuePair<>(
+							PhoneResourceImpl.class,
+							"postOrganizationPhonesPageExportBatch"));
+					put(
+						"mutation#createUserAccountPhonesPageExportBatch",
+						new ObjectValuePair<>(
+							PhoneResourceImpl.class,
+							"postUserAccountPhonesPageExportBatch"));
+					put(
+						"mutation#createRolesPageExportBatch",
+						new ObjectValuePair<>(
+							RoleResourceImpl.class,
+							"postRolesPageExportBatch"));
+					put(
+						"mutation#createSiteSegmentsPageExportBatch",
+						new ObjectValuePair<>(
+							SegmentResourceImpl.class,
+							"postSiteSegmentsPageExportBatch"));
+					put(
 						"mutation#deleteMyUserAccountSubscription",
 						new ObjectValuePair<>(
 							SubscriptionResourceImpl.class,
 							"deleteMyUserAccountSubscription"));
+					put(
+						"mutation#createOrganizationUserAccountsPageExportBatch",
+						new ObjectValuePair<>(
+							UserAccountResourceImpl.class,
+							"postOrganizationUserAccountsPageExportBatch"));
+					put(
+						"mutation#createSiteUserAccountsPageExportBatch",
+						new ObjectValuePair<>(
+							UserAccountResourceImpl.class,
+							"postSiteUserAccountsPageExportBatch"));
+					put(
+						"mutation#createUserAccountsPageExportBatch",
+						new ObjectValuePair<>(
+							UserAccountResourceImpl.class,
+							"postUserAccountsPageExportBatch"));
+					put(
+						"mutation#createOrganizationWebUrlsPageExportBatch",
+						new ObjectValuePair<>(
+							WebUrlResourceImpl.class,
+							"postOrganizationWebUrlsPageExportBatch"));
+					put(
+						"mutation#createUserAccountWebUrlsPageExportBatch",
+						new ObjectValuePair<>(
+							WebUrlResourceImpl.class,
+							"postUserAccountWebUrlsPageExportBatch"));
 
 					put(
 						"query#emailAddress",
@@ -274,24 +336,12 @@ public class ServletDataImpl implements ServletData {
 			};
 
 	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
-	private ComponentServiceObjects<SubscriptionResource>
-		_subscriptionResourceComponentServiceObjects;
-
-	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
-	private ComponentServiceObjects<EmailAddressResource>
-		_emailAddressResourceComponentServiceObjects;
-
-	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
 	private ComponentServiceObjects<OrganizationResource>
 		_organizationResourceComponentServiceObjects;
 
 	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
 	private ComponentServiceObjects<PhoneResource>
 		_phoneResourceComponentServiceObjects;
-
-	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
-	private ComponentServiceObjects<PostalAddressResource>
-		_postalAddressResourceComponentServiceObjects;
 
 	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
 	private ComponentServiceObjects<RoleResource>
@@ -302,12 +352,8 @@ public class ServletDataImpl implements ServletData {
 		_segmentResourceComponentServiceObjects;
 
 	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
-	private ComponentServiceObjects<SegmentUserResource>
-		_segmentUserResourceComponentServiceObjects;
-
-	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
-	private ComponentServiceObjects<SiteResource>
-		_siteResourceComponentServiceObjects;
+	private ComponentServiceObjects<SubscriptionResource>
+		_subscriptionResourceComponentServiceObjects;
 
 	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
 	private ComponentServiceObjects<UserAccountResource>
@@ -316,5 +362,21 @@ public class ServletDataImpl implements ServletData {
 	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
 	private ComponentServiceObjects<WebUrlResource>
 		_webUrlResourceComponentServiceObjects;
+
+	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
+	private ComponentServiceObjects<EmailAddressResource>
+		_emailAddressResourceComponentServiceObjects;
+
+	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
+	private ComponentServiceObjects<PostalAddressResource>
+		_postalAddressResourceComponentServiceObjects;
+
+	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
+	private ComponentServiceObjects<SegmentUserResource>
+		_segmentUserResourceComponentServiceObjects;
+
+	@Reference(scope = ReferenceScope.PROTOTYPE_REQUIRED)
+	private ComponentServiceObjects<SiteResource>
+		_siteResourceComponentServiceObjects;
 
 }
