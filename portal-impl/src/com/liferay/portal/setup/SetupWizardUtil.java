3c41ba3da40829a56e1c10825cb72e0a98905ce8
Jiefeng Wu <jeffwu0724@gmail.com>
2023-05-12 10:36:09 -0700

    LPS-157718 SF

--- portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
+++ portal-impl/src/com/liferay/portal/setup/SetupWizardUtil.java
@@ -306,7 +306,7 @@ public class SetupWizardUtil {
 
 		if (!DriverClassNamesHolder.contains(driverClassName)) {
 			throw new Exception(
-				driverClassName + " is not a valid driver class name!");
+				driverClassName + " is not a valid driver class name");
 		}
 
 		try {
