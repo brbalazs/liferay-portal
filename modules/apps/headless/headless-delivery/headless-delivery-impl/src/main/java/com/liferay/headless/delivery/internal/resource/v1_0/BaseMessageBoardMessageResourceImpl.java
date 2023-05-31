af8d7a84df52dc34e9e7910f556765d2652567af
Brian Chan <brian.chan@liferay.com>
2023-05-31 16:10:58 +0530

    LPS-183728 Regen

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/BaseMessageBoardMessageResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/BaseMessageBoardMessageResourceImpl.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.odata.filter.ExpressionConvert;
@@ -248,6 +249,9 @@ public abstract class BaseMessageBoardMessageResourceImpl
 			existingMessageBoardMessage.setArticleBody(
 				messageBoardMessage.getArticleBody());
 		}
+
+		existingMessageBoardMessage.setCustomFields(
+			messageBoardMessage.getCustomFields());
 
 		if (messageBoardMessage.getHeadline() != null) {
 			existingMessageBoardMessage.setHeadline(
@@ -963,7 +967,7 @@ public abstract class BaseMessageBoardMessageResourceImpl
 		String createStrategy = (String)parameters.getOrDefault(
 			"createStrategy", "INSERT");
 
-		if ("INSERT".equalsIgnoreCase(createStrategy)) {
+		if (StringUtil.equalsIgnoreCase(createStrategy, "INSERT")) {
 			if (parameters.containsKey("messageBoardThreadId")) {
 				messageBoardMessageUnsafeConsumer = messageBoardMessage ->
 					postMessageBoardThreadMessageBoardMessage(
@@ -1091,7 +1095,7 @@ public abstract class BaseMessageBoardMessageResourceImpl
 		String updateStrategy = (String)parameters.getOrDefault(
 			"updateStrategy", "UPDATE");
 
-		if ("PARTIAL_UPDATE".equalsIgnoreCase(updateStrategy)) {
+		if (StringUtil.equalsIgnoreCase(updateStrategy, "PARTIAL_UPDATE")) {
 			messageBoardMessageUnsafeConsumer =
 				messageBoardMessage -> patchMessageBoardMessage(
 					messageBoardMessage.getId() != null ?
@@ -1102,7 +1106,7 @@ public abstract class BaseMessageBoardMessageResourceImpl
 					messageBoardMessage);
 		}
 
-		if ("UPDATE".equalsIgnoreCase(updateStrategy)) {
+		if (StringUtil.equalsIgnoreCase(updateStrategy, "UPDATE")) {
 			messageBoardMessageUnsafeConsumer =
 				messageBoardMessage -> putMessageBoardMessage(
 					messageBoardMessage.getId() != null ?
