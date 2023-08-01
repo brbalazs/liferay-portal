f2ff20c73ee8051d16944a01f4ea22409ba15c5f
Marcos Martins <marcos.martins@liferay.com>
2023-08-01 11:00:08 -0300

    LRAC-14333 update test

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/dog/test/dependencies/osbasahfaroinfo/bq_identity_interest_page.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/testIntegration/resources/com/liferay/osb/asah/common/dog/test/dependencies/osbasahfaroinfo/bq_identity_interest_page.sql
@@ -2,3 +2,4 @@ INSERT INTO IdentityInterestPage (canonicalUrl, channelId, identityId, keyword, 
 INSERT INTO IdentityInterestPage (canonicalUrl, channelId, identityId, keyword, title, views) VALUES ('http://www.liferay.com/what-is-javascript', 1, '374790569167317525', 'javascript', 'What is Javascript', 0);
 INSERT INTO IdentityInterestPage (canonicalUrl, channelId, identityId, keyword, title, views) VALUES ('http://www.liferay.com/is-javascript-good', 2, '374790569167317525', 'javascript', 'Is Javascript Good?', 1);
 INSERT INTO IdentityInterestPage (canonicalUrl, channelId, identityId, keyword, title, views) VALUES ('http://www.liferay.com/java-is-great', 1, '374790569167317525', 'java', 'Java is Great', 1);
+INSERT INTO IdentityInterestPage (canonicalUrl, channelId, identityId, keyword, title, views) VALUES ('http://www.liferay.com/java-is-great', 1, '374790569167317525', 'java', 'Java is Great', 1);
