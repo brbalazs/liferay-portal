758b35f48148ac5105551b528afb7daf93f6b4e6
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2023-08-18 12:47:02 -0700

    LRAC-14443 Enable run log on UpdateMembershipsNanite

--- modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/bot/nanite/UpdateMembershipsNanite.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/bot/nanite/UpdateMembershipsNanite.java
@@ -32,6 +32,11 @@ import org.springframework.stereotype.Component;
 public class UpdateMembershipsNanite extends BaseNanite {
 
 	@Override
+	public boolean isLogRunEnabled() {
+		return true;
+	}
+
+	@Override
 	public void run(JSONObject contextJSONObject) {
 		_updateDynamicSegmentMemberships();
 		_updateStaticSegmentMembershipChanges();
