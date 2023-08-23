debb45a5af501a14219ef21aed4b5c742793e333
Jiefeng Wu <jeffwu0724@gmail.com>
2023-08-23 10:19:28 -0700

    LPS-176787 Make sure only one temp file is created for each FileItem, also this is consistent with the method logic it overrides in super class.

--- portal-impl/src/com/liferay/portal/upload/LiferayFileItem.java
+++ portal-impl/src/com/liferay/portal/upload/LiferayFileItem.java
@@ -221,6 +221,10 @@ public class LiferayFileItem extends DiskFileItem implements FileItem {
 
 	@Override
 	protected File getTempFile() {
+		if (_tempFile != null) {
+			return _tempFile;
+		}
+
 		String tempFileName = "upload_" + _getUniqueId();
 
 		String extension = getFileNameExtension();
@@ -229,13 +233,14 @@ public class LiferayFileItem extends DiskFileItem implements FileItem {
 			tempFileName += "." + extension;
 		}
 
-		File tempFile = new File(_repository, tempFileName);
+		_tempFile = new File(_repository, tempFileName);
 
 		FinalizeManager.register(
-			tempFile, new DeleteFileFinalizeAction(tempFile.getAbsolutePath()),
+			_tempFile,
+			new DeleteFileFinalizeAction(_tempFile.getAbsolutePath()),
 			FinalizeManager.PHANTOM_REFERENCE_FACTORY);
 
-		return tempFile;
+		return _tempFile;
 	}
 
 	private static String _getUniqueId() {
@@ -263,5 +268,6 @@ public class LiferayFileItem extends DiskFileItem implements FileItem {
 	private String _fileName;
 	private final File _repository;
 	private final int _sizeThreshold;
+	private File _tempFile;
 
 }
