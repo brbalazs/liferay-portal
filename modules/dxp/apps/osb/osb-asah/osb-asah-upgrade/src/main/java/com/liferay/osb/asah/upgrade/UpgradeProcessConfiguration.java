40bd27b664730ffb324dcb6a1bc3d52a1795cff4
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-09-06 14:00:08 -0700

    LRAC-14522 Prepare 4.3.0

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/UpgradeProcessConfiguration.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/src/main/java/com/liferay/osb/asah/upgrade/UpgradeProcessConfiguration.java
@@ -5,11 +5,7 @@
 
 package com.liferay.osb.asah.upgrade;
 
-import com.liferay.osb.asah.upgrade.v4_2_0.BQMembershipUpgradeStep;
-import com.liferay.osb.asah.upgrade.v4_2_0.IndividualUpgradeStep;
-import com.liferay.osb.asah.upgrade.v4_2_0.PageUpgradeStep;
-import com.liferay.osb.asah.upgrade.v4_2_0.PostgreSQLUpgradeStep;
-import com.liferay.osb.asah.upgrade.v4_2_0.SuppressionUpgradeStep;
+import com.liferay.osb.asah.upgrade.v4_3_0.ExperimentUpgradeStep;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -26,31 +22,12 @@ public class UpgradeProcessConfiguration {
 		UpgradeProcess upgradeProcess = new UpgradeProcess();
 
 		upgradeProcess.addUpgradeSteps(
-			"4.0.12", "4.0.13", _bqMembershipUpgradeStep);
-		upgradeProcess.addUpgradeSteps(
-			"4.0.13", "4.0.14", _individualUpgradeStep);
-		upgradeProcess.addUpgradeSteps("4.0.14", "4.0.15", _pageUpgradeStep);
-		upgradeProcess.addUpgradeSteps(
-			"4.0.15", "4.0.16", _postgreSQLUpgradeStep);
-		upgradeProcess.addUpgradeSteps(
-			"4.0.16", "4.0.17", _suppressionUpgradeStep);
+			"4.0.18", "4.0.19", _experimentUpgradeStep);
 
 		return upgradeProcess;
 	}
 
 	@Autowired
-	private BQMembershipUpgradeStep _bqMembershipUpgradeStep;
-
-	@Autowired
-	private IndividualUpgradeStep _individualUpgradeStep;
-
-	@Autowired
-	private PageUpgradeStep _pageUpgradeStep;
-
-	@Autowired
-	private PostgreSQLUpgradeStep _postgreSQLUpgradeStep;
-
-	@Autowired
-	private SuppressionUpgradeStep _suppressionUpgradeStep;
+	private ExperimentUpgradeStep _experimentUpgradeStep;
 
 }
