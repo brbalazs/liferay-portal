e4f64fe484f76d9422a647fa092b0a01fb9a97d7
Rachael Koestartyo <rachael.koestartyo@liferay.com>
2023-09-08 10:54:22 -0700

    LRAC-14490 add DeleteChannelsNanite

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-batch-curator/src/main/java/com/liferay/osb/asah/batch/curator/bot/nanite/DeleteChannelsNanite.java
@@ -0,0 +1,45 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.asah.batch.curator.bot.nanite;
+
+import com.liferay.osb.asah.common.dog.ChannelDog;
+import com.liferay.osb.asah.common.json.JSONUtil;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.json.JSONObject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Rachael Koestartyo
+ */
+@Component
+public class DeleteChannelsNanite extends BaseNanite {
+
+	@Override
+	public boolean isLogRunEnabled() {
+		return true;
+	}
+
+	@Override
+	public void run(JSONObject contextJSONObject) throws Exception {
+		_channelDog.deleteChannels(
+			JSONUtil.toLongSet(
+				contextJSONObject.getJSONArray("channelIds"));
+	}
+
+	@Override
+	protected Log getLog() {
+		return LogFactory.getLog(DeleteChannelsNanite.class);
+	}
+
+	@Autowired
+	private ChannelDog _channelDog;
+
+}
