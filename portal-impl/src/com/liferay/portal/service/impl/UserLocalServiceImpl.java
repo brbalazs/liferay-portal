846eab8ce8d1e674a29267039c6fa2e475f23e2c
Stian Sigvartsen <stian.sigvartsen@liferay.com>
2023-09-16 19:21:44 +0100

    LPS-189689 Avoid confusing the semantics of the generic TicketLocalService#update API method with a reset specific logic

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -3931,7 +3931,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 			String passwordResetURL = sb.toString();
 
-			ticketLocalService.updateTicket(ticket);
+			ticket.setKey(PasswordEncryptorUtil.encrypt(ticket.getKey()));
 
 			sendPasswordNotification(
 				user, companyId, null, passwordResetURL, fromName, fromAddress,
