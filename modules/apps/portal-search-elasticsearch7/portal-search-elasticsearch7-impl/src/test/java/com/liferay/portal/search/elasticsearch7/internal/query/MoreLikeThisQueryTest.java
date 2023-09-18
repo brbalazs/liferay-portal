da9da2f48520a29019d18086b45901d8ea647740
gustavolimav <gustavo.lima@liferay.com>
2023-09-18 18:28:04 -0300

    LPS-189226 LPS-193255 portal-search-elasticsearch7-impl: Ignore test because it is causing other tests to fail
    based on master code the test is ignored
    
    https://liferay.atlassian.net/browse/LPS-193255

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/query/MoreLikeThisQueryTest.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/query/MoreLikeThisQueryTest.java
@@ -6,15 +6,10 @@
 package com.liferay.portal.search.elasticsearch7.internal.query;
 
 import com.liferay.portal.search.elasticsearch7.internal.LiferayElasticsearchIndexingFixtureFactory;
-import com.liferay.portal.search.query.MoreLikeThisQuery;
 import com.liferay.portal.search.test.util.indexing.IndexingFixture;
 import com.liferay.portal.search.test.util.query.BaseMoreLikeThisQueryTestCase;
 
-import java.util.Collections;
-
-import org.elasticsearch.action.search.SearchPhaseExecutionException;
-
-import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -22,29 +17,10 @@ import org.junit.Test;
  */
 public class MoreLikeThisQueryTest extends BaseMoreLikeThisQueryTestCase {
 
+	@Ignore
 	@Override
 	@Test
 	public void testMoreLikeThisWithoutFields() throws Exception {
-		addDocuments("java eclipse", "eclipse liferay", "java liferay eclipse");
-
-		MoreLikeThisQuery moreLikeThisQuery = queries.moreLikeThis(
-			Collections.emptyList(), "java");
-
-		try {
-			assertSearch(moreLikeThisQuery, Collections.emptyList());
-
-			Assert.fail();
-		}
-		catch (SearchPhaseExecutionException searchPhaseExecutionException) {
-			Throwable throwable = searchPhaseExecutionException.getRootCause();
-
-			String message = throwable.getMessage();
-
-			Assert.assertTrue(
-				message,
-				message.contains(
-					"[more_like_this] query cannot infer the field"));
-		}
 	}
 
 	@Override
