ac832f72631b7a98723f61ee6b2fde73311f233b
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-09-25 16:08:17 -0700

    LRAC-14631 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/src/main/java/com/liferay/osb/asah/dataflow/ingestion/dxp/transform/CommerceChannelIdMapPTransform.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/src/main/java/com/liferay/osb/asah/dataflow/ingestion/dxp/transform/CommerceChannelIdMapPTransform.java
@@ -6,35 +6,21 @@
 package com.liferay.osb.asah.dataflow.ingestion.dxp.transform;
 
 import com.google.api.services.bigquery.model.TableRow;
-import com.liferay.osb.asah.dataflow.ingestion.dxp.entity.DXPEntityMessageWrapper;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Objects;
-import org.apache.beam.sdk.io.Compression;
-import org.apache.beam.sdk.io.FileIO;
-import org.apache.beam.sdk.io.FileSystems;
-import org.apache.beam.sdk.io.fs.MatchResult;
-import org.apache.beam.sdk.io.fs.ResourceId;
+
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.transforms.Combine;
-import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.PTransform;
-import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.transforms.View;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PBegin;
-import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionView;
 import org.apache.beam.sdk.values.TypeDescriptors;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Marcellus Tavares
@@ -58,14 +44,10 @@ public class CommerceChannelIdMapPTransform
 			).fromQuery(
 				StringUtils.replaceEach(
 					_COMMERCE_CHANNEL_ID_QUERY_TEMPLATE,
-					new String[]{
+					new String[] {
 						"${googleProjectId}", "${projectId}", "${region}"
 					},
-					new String[]{
-						_googleProjectId,
-						_projectId,
-						_region
-					})
+					new String[] {_googleProjectId, _projectId, _region})
 			).usingStandardSql(
 			).withMethod(
 				BigQueryIO.TypedRead.Method.DIRECT_READ
@@ -82,9 +64,9 @@ public class CommerceChannelIdMapPTransform
 
 					@Override
 					public KV<Long, Long> apply(TableRow tableRow) {
-						String commerceChannelId = (String) tableRow.get(
+						String commerceChannelId = (String)tableRow.get(
 							"commercechannelid");
-						String channelId = (String) tableRow.get("id");
+						String channelId = (String)tableRow.get("id");
 
 						return KV.of(
 							Long.parseLong(commerceChannelId),
@@ -113,12 +95,12 @@ public class CommerceChannelIdMapPTransform
 	private static final String _COMMERCE_CHANNEL_ID_QUERY_TEMPLATE =
 		"SELECT * FROM EXTERNAL_QUERY('${googleProjectId}.${region}." +
 			"postgresql', 'SELECT unnest(commercechannelids) AS " +
-			"commercechannelid, id FROM ${projectId}.channel JOIN " +
-			"${projectId}.channeldatasource ON (channel.id = " +
-			"channeldatasource.channelid);')";
+				"commercechannelid, id FROM ${projectId}.channel JOIN " +
+					"${projectId}.channeldatasource ON (channel.id = " +
+						"channeldatasource.channelid);')";
 
-	private String _googleProjectId;
-	private String _projectId;
-	private String _region;
+	private final String _googleProjectId;
+	private final String _projectId;
+	private final String _region;
 
 }
