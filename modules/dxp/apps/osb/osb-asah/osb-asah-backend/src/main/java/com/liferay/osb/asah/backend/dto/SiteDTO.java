c26981a52b961ecc0ea239a6288b0bbe4f0fe079
Marcos Martins <marcos.martins@liferay.com>
2023-09-25 11:42:02 -0300

    LRAC-14584 create SiteDTO

---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dto/SiteDTO.java
@@ -0,0 +1,85 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.asah.backend.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+import com.liferay.osb.asah.common.date.DateUtil;
+import com.liferay.osb.asah.common.model.Site;
+import com.liferay.osb.asah.common.util.SetUtil;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Marcos Martins
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonRootName("recent-sites")
+public class SiteDTO {
+
+	public SiteDTO() {
+	}
+
+	public SiteDTO(List<Site> sites) {
+		_siteDTOs = SetUtil.map(sites, SiteDTO::new);
+	}
+
+	public SiteDTO(Site site) {
+		_count = site.getCounts();
+		_createDate = site.getCreateDate();
+		_groupId = site.getGroupId();
+		_lastModifiedDate = site.getLastModifiedDate();
+	}
+
+	public Long getCount() {
+		return _count;
+	}
+
+	@JsonFormat(
+		pattern = DateUtil.PATTERN_ISO_8601, shape = JsonFormat.Shape.STRING,
+		timezone = "UTC"
+	)
+	public Date getCreateDate() {
+		if (_createDate == null) {
+			return null;
+		}
+
+		return new Date(_createDate.getTime());
+	}
+
+	public String getGroupId() {
+		return _groupId;
+	}
+
+	@JsonFormat(
+		pattern = DateUtil.PATTERN_ISO_8601, shape = JsonFormat.Shape.STRING,
+		timezone = "UTC"
+	)
+	public Date getLastModifiedDate() {
+		if (_lastModifiedDate == null) {
+			return null;
+		}
+
+		return new Date(_lastModifiedDate.getTime());
+	}
+
+	@JsonProperty("recent-sites")
+	public Set<SiteDTO> getSiteDTOs() {
+		return _siteDTOs;
+	}
+
+	private Long _count;
+	private Date _createDate;
+	private String _groupId;
+	private Date _lastModifiedDate;
+	private Set<SiteDTO> _siteDTOs;
+
+}
