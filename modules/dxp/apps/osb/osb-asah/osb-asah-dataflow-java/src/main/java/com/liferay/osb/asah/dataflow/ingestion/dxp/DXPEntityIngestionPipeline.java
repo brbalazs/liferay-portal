68cd82552444b3c1fbd2d72eeb96bff48bad3126
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-10-09 10:47:08 -0700

    LRAC-14695 Deal with User Suppression during ingestion

--- modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/src/main/java/com/liferay/osb/asah/dataflow/ingestion/dxp/DXPEntityIngestionPipeline.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/src/main/java/com/liferay/osb/asah/dataflow/ingestion/dxp/DXPEntityIngestionPipeline.java
@@ -5,16 +5,27 @@
 
 package com.liferay.osb.asah.dataflow.ingestion.dxp;
 
+import com.google.api.services.bigquery.model.TableRow;
+
 import com.liferay.osb.asah.dataflow.ingestion.dxp.entity.DXPEntityMessageWrapper;
 import com.liferay.osb.asah.dataflow.ingestion.dxp.function.DXPEntityParserDoFn;
+import com.liferay.osb.asah.dataflow.ingestion.dxp.function.DXPEntityUserSuppressor;
 import com.liferay.osb.asah.dataflow.ingestion.dxp.transform.BigQueryWriterPTransform;
 import com.liferay.osb.asah.dataflow.ingestion.dxp.transform.DXPEntityMessageWrapperZipReaderPTransform;
 
+import java.util.List;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.transforms.View;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.beam.sdk.values.TypeDescriptors;
 
 /**
  * @author Marcellus Tavares
@@ -41,8 +52,40 @@ public class DXPEntityIngestionPipeline {
 				new DXPEntityMessageWrapperZipReaderPTransform(
 					dxpEntityIngestionPipelineOptions.getZipFilePath()));
 
+		PCollection<String> suppressedEmailsPCollection = pipeline.apply(
+			"Read Suppressed Users from BigQuery",
+			BigQueryIO.readTableRows(
+			).fromQuery(
+				String.format(
+					"SELECT emailAddress FROM %s.%s",
+					dxpEntityIngestionPipelineOptions.getProjectId(),
+					"suppression")
+			).withMethod(
+				BigQueryIO.TypedRead.Method.DIRECT_READ
+			)
+		).apply(
+			"Map Table Row Results",
+			MapElements.into(
+				TypeDescriptors.strings()
+			).via(
+				(SerializableFunction<TableRow, String>)
+					tableRow -> (String)tableRow.get("emailAddress")
+			)
+		);
+
+		PCollectionView<List<String>> suppressedEmailAddressesPCollectionView =
+			suppressedEmailsPCollection.apply(View.asList());
+
 		dxpEntityMessageWrapperPCollection.apply(
 			"Parse DXP Entities", ParDo.of(new DXPEntityParserDoFn())
+		).apply(
+			"Suppress DXP Entity Users",
+			ParDo.of(
+				new DXPEntityUserSuppressor(
+					suppressedEmailAddressesPCollectionView)
+			).withSideInputs(
+				suppressedEmailAddressesPCollectionView
+			)
 		).apply(
 			"Write DXP Entities",
 			new BigQueryWriterPTransform<>(
---
+++ modules/dxp/apps/osb/osb-asah/osb-asah-dataflow-java/src/main/java/com/liferay/osb/asah/dataflow/ingestion/dxp/function/DXPEntityUserSuppressor.java
@@ -0,0 +1,88 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.osb.asah.dataflow.ingestion.dxp.function;
+
+import com.liferay.osb.asah.dataflow.ingestion.dxp.entity.DXPEntity;
+import com.liferay.osb.asah.dataflow.ingestion.dxp.entity.Field;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.commons.lang3.StringUtils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Marcellus Tavares
+ */
+public class DXPEntityUserSuppressor extends DoFn<DXPEntity, DXPEntity> {
+
+	public DXPEntityUserSuppressor(
+		PCollectionView<List<String>> suppressedEmailAddressesPCollectionView) {
+
+		_suppressedEmailAddressesPCollectionView =
+			suppressedEmailAddressesPCollectionView;
+	}
+
+	@ProcessElement
+	public void processElement(ProcessContext processContext) {
+		Set<String> suppressedEmailAddresses = new HashSet<>(
+			processContext.sideInput(_suppressedEmailAddressesPCollectionView));
+
+		DXPEntity dxpEntity = processContext.element();
+
+		if (StringUtils.equals(dxpEntity.type, _DXP_ENTITY_USER_TYPE)) {
+			String emailAddress = _getEmailAddress(dxpEntity.fields);
+
+			if (emailAddress == null) {
+				if (_logger.isWarnEnabled()) {
+					_logger.warn(
+						"Discarding DXP User Entity with null email address");
+				}
+
+				return;
+			}
+
+			if (suppressedEmailAddresses.contains(
+					StringUtils.lowerCase(emailAddress))) {
+
+				if (_logger.isInfoEnabled()) {
+					_logger.info(
+						"Skipping ingestion of suppressed user " +
+							emailAddress);
+				}
+
+				return;
+			}
+		}
+
+		processContext.output(dxpEntity);
+	}
+
+	private String _getEmailAddress(List<Field> fields) {
+		for (Field field : fields) {
+			if (StringUtils.equals(field.name, "emailAddress")) {
+				return field.value;
+			}
+		}
+
+		return null;
+	}
+
+	private static final String _DXP_ENTITY_USER_TYPE =
+		"com.liferay.portal.kernel.model.User";
+
+	private static final Logger _logger = LoggerFactory.getLogger(
+		DXPEntityUserSuppressor.class);
+
+	private final PCollectionView<List<String>>
+		_suppressedEmailAddressesPCollectionView;
+
+}
