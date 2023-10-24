b8ec64bf52300c50ee238f304433d6f64fb7ce2e
Leslie Wong <8095751+lesliewong92@users.noreply.github.com>
2023-10-24 14:30:47 -0700

    LRAC-14769 Supply project time zone as second argument

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_3_0/upgrade_process.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_3_0/upgrade_process.sh
@@ -16,7 +16,7 @@ function upgrade_page_daily_merge_statement {
 
 	sed -e "s/\${asah_project_id}/$asah_project_id/g" -e "s~\${asah_project_time_zone}~$asah_project_timezone~g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" -e "s/\${REGION_ID}/$REGION_ID/g" upgrade_page_daily_merge_statement.sql > new_upgrade_page_daily_merge_statement.sql
 
-	echo "Upgrade Page Daily for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}"
+	echo "Upgrade Page Daily for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}, Asah Project Time Zone: ${asah_project_timezone}"
 
 	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_page_daily_merge_statement.sql
 }
@@ -25,10 +25,10 @@ for i in $(bq ls --datasets=true --max_results=1000 | grep "asah" | grep -v "osb
 do :
 	project_time_zone=$(cat project_time_zones | grep "$i" | awk '{print $2}')
 
-		if [ -n "$project_time_zone" ]
-		then
-			upgrade_page_daily_merge_statement $i
-		else
-			echo "Unable to find time zone for $i. Skipping project.";
-		fi
+	if [ -n "$project_time_zone" ]
+	then
+		upgrade_page_daily_merge_statement $i $project_time_zone
+	else
+		echo "Unable to find time zone for $i. Skipping project.";
+	fi
 done
