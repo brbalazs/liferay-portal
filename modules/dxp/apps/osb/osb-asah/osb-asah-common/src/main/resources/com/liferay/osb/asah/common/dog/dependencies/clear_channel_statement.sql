0cf9396ab1147220a1a84bbadb58f7ddbe21fb25
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-10-30 09:22:09 -0700

    LRAC-14759 Make ClearChannelsNanite fail when transaction fails

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/com/liferay/osb/asah/common/dog/dependencies/clear_channel_statement.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/com/liferay/osb/asah/common/dog/dependencies/clear_channel_statement.sql
@@ -1,30 +1,24 @@
-BEGIN
-	BEGIN TRANSACTION;
+BEGIN TRANSACTION;
 
-	DELETE FROM BlogDaily WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQEvent WHERE channelId IN ( ${channel_ids} ) AND eventDate < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
-	DELETE FROM BQEventProperty WHERE channelId IN ( ${channel_ids} ) AND eventDate < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
-	DELETE FROM BQIdentityActivitySummary WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQIdentityInterestPage WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQIdentityInterestScore WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQMembership WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQMembershipChange WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQMembershipIndividual WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQOrder WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQOrder_Raw WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQProduct WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQProduct_Raw WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQSession WHERE channelId IN ( ${channel_ids} ) AND sessionStart < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
-	DELETE FROM BQSessionInterestScore WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM CustomAssetDaily WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM DocumentLibraryDaily WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM FormDaily WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM JournalDaily WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM PageDaily WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BlogDaily WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQEvent WHERE channelId IN ( ${channel_ids} ) AND eventDate < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
+DELETE FROM BQEventProperty WHERE channelId IN ( ${channel_ids} ) AND eventDate < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
+DELETE FROM BQIdentityActivitySummary WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQIdentityInterestPage WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQIdentityInterestScore WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQMembership WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQMembershipChange WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQMembershipIndividual WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQOrder WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQOrder_Raw WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQProduct WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQProduct_Raw WHERE channelId IN ( ${channel_ids} );
+DELETE FROM BQSession WHERE channelId IN ( ${channel_ids} ) AND sessionStart < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
+DELETE FROM BQSessionInterestScore WHERE channelId IN ( ${channel_ids} );
+DELETE FROM CustomAssetDaily WHERE channelId IN ( ${channel_ids} );
+DELETE FROM DocumentLibraryDaily WHERE channelId IN ( ${channel_ids} );
+DELETE FROM FormDaily WHERE channelId IN ( ${channel_ids} );
+DELETE FROM JournalDaily WHERE channelId IN ( ${channel_ids} );
+DELETE FROM PageDaily WHERE channelId IN ( ${channel_ids} );
 
-	COMMIT TRANSACTION;
-
-EXCEPTION WHEN ERROR THEN
-	SELECT @@error.message;
-	ROLLBACK TRANSACTION;
-END
+COMMIT TRANSACTION;
