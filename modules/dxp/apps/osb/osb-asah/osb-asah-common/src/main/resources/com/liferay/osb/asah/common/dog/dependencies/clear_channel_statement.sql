cf500d5378cafb71c3a5de9d0a9d3b5349daed6c
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-10-30 09:17:14 -0700

    LRAC-14759 Avoid streaming buffer pending data

--- modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/com/liferay/osb/asah/common/dog/dependencies/clear_channel_statement.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-common/src/main/resources/com/liferay/osb/asah/common/dog/dependencies/clear_channel_statement.sql
@@ -2,8 +2,8 @@ BEGIN
 	BEGIN TRANSACTION;
 
 	DELETE FROM BlogDaily WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQEvent WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQEventProperty WHERE channelId IN ( ${channel_ids} );
+	DELETE FROM BQEvent WHERE channelId IN ( ${channel_ids} ) AND eventDate < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
+	DELETE FROM BQEventProperty WHERE channelId IN ( ${channel_ids} ) AND eventDate < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
 	DELETE FROM BQIdentityActivitySummary WHERE channelId IN ( ${channel_ids} );
 	DELETE FROM BQIdentityInterestPage WHERE channelId IN ( ${channel_ids} );
 	DELETE FROM BQIdentityInterestScore WHERE channelId IN ( ${channel_ids} );
@@ -14,7 +14,7 @@ BEGIN
 	DELETE FROM BQOrder_Raw WHERE channelId IN ( ${channel_ids} );
 	DELETE FROM BQProduct WHERE channelId IN ( ${channel_ids} );
 	DELETE FROM BQProduct_Raw WHERE channelId IN ( ${channel_ids} );
-	DELETE FROM BQSession WHERE channelId IN ( ${channel_ids} );
+	DELETE FROM BQSession WHERE channelId IN ( ${channel_ids} ) AND sessionStart < TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 90 MINUTE);
 	DELETE FROM BQSessionInterestScore WHERE channelId IN ( ${channel_ids} );
 	DELETE FROM CustomAssetDaily WHERE channelId IN ( ${channel_ids} );
 	DELETE FROM DocumentLibraryDaily WHERE channelId IN ( ${channel_ids} );
