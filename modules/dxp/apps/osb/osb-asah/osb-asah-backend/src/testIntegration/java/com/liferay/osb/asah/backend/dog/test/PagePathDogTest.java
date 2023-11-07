3524d2233cf3e2c219d48178b2cf8c701c4ffe87
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-11-07 12:53:14 -0800

    LRAC-14846 Add test

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/dog/test/PagePathDogTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/dog/test/PagePathDogTest.java
@@ -7,10 +7,15 @@ package com.liferay.osb.asah.backend.dog.test;
 
 import com.liferay.osb.asah.backend.OSBAsahBackendSpringTestContext;
 import com.liferay.osb.asah.backend.dog.PagePathDog;
+import com.liferay.osb.asah.backend.model.AdjacentPageViewsMetric;
 import com.liferay.osb.asah.common.model.TimeRange;
+import com.liferay.osb.asah.common.util.SetUtil;
 import com.liferay.osb.asah.test.util.annotation.BQSQLResource;
 import com.liferay.osb.asah.test.util.spring.OSBAsahTestExecutionListenersContext;
 
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +27,33 @@ public class PagePathDogTest
 	implements OSBAsahBackendSpringTestContext,
 			   OSBAsahTestExecutionListenersContext {
 
-	@BQSQLResource(resourcePath = "page_referrers_events.sql")
+	@BQSQLResource(resourcePath = "page_path_events.sql")
 	@Test
 	public void testPagePathNode() {
-		System.out.println(
+		Assertions.assertEquals(
+			SetUtil.of(
+				new AdjacentPageViewsMetric(
+					"direct", Boolean.TRUE, "direct", new BigDecimal(3)),
+				new AdjacentPageViewsMetric(
+					"http://google.com", Boolean.TRUE, "http://google.com",
+					new BigDecimal(1)),
+				new AdjacentPageViewsMetric(
+					"http://liferay.com/b", Boolean.FALSE, "B - Liferay DXP",
+					new BigDecimal(2)),
+				new AdjacentPageViewsMetric(
+					"http://liferay.com/c", Boolean.TRUE, "C - Liferay DXP",
+					new BigDecimal(1)),
+				new AdjacentPageViewsMetric(
+					"http://liferay.com/d", Boolean.FALSE, "D - Liferay DXP",
+					new BigDecimal(1)),
+				new AdjacentPageViewsMetric(
+					"http://liferay.com/e", Boolean.TRUE, "E - Liferay DXP",
+					new BigDecimal(2)),
+				new AdjacentPageViewsMetric(
+					"other", Boolean.TRUE, "other", new BigDecimal(1))),
 			_pagePathDog.getAdjacentPagesViewsMetric(
-				"http://liferay.com", 1L, null, TimeRange.LAST_30_DAYS,
-				"Home - Liferay DXP"));
+				"http://liferay.com/a", 1L, null, TimeRange.LAST_30_DAYS,
+				"A - Liferay DXP"));
 	}
 
 	@Autowired
