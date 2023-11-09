0e5933d45bb850aa7b5869e1b56940f2ff8a732d
Marcos Martins <marcos.martins@liferay.com>
2023-11-09 16:28:28 -0300

    LRAC-14937 create asset csv reports

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/ReportDog.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/dog/ReportDog.java
@@ -5,10 +5,17 @@
 
 package com.liferay.osb.asah.backend.dog;
 
+import com.liferay.osb.asah.backend.model.AssetMetric;
 import com.liferay.osb.asah.backend.model.AssetType;
+import com.liferay.osb.asah.backend.model.BlogMetric;
+import com.liferay.osb.asah.backend.model.BlogMetricType;
+import com.liferay.osb.asah.backend.model.DocumentLibraryMetric;
 import com.liferay.osb.asah.backend.model.DocumentLibraryMetricType;
+import com.liferay.osb.asah.backend.model.FormMetric;
 import com.liferay.osb.asah.backend.model.FormMetricType;
+import com.liferay.osb.asah.backend.model.JournalMetric;
 import com.liferay.osb.asah.backend.model.JournalMetricType;
+import com.liferay.osb.asah.backend.model.PageMetric;
 import com.liferay.osb.asah.backend.repository.AssetMetricRepository;
 import com.liferay.osb.asah.common.dog.ChannelDog;
 import com.liferay.osb.asah.common.entity.Channel;
@@ -19,6 +26,7 @@ import com.liferay.osb.asah.common.model.PageMetricType;
 import com.liferay.osb.asah.common.model.Sort;
 import com.liferay.osb.asah.common.model.TimeRange;
 import com.liferay.osb.asah.common.repository.BQIndividualRepository;
+import com.liferay.osb.asah.common.util.SetUtil;
 
 import com.opencsv.CSVWriter;
 
@@ -34,12 +42,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Order;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
@@ -72,19 +82,34 @@ public class ReportDog {
 		List<String[]> rows = null;
 
 		if (StringUtils.equals(type, "blog")) {
-
-			// TODO
-
+			rows = _getAssetBlogRows(
+				channelId, query,
+				SetUtil.of(
+					BlogMetricType.VIEWS.getName(),
+					BlogMetricType.READING_TIME.getName(),
+					BlogMetricType.COMMENTS.getName(),
+					BlogMetricType.RATINGS.getName()),
+				sorts, timeRange, type);
 		}
 		else if (StringUtils.equals(type, "document")) {
-
-			// TODO
-
+			rows = _getAssetDocumentsLibraryRows(
+				channelId, query,
+				SetUtil.of(
+					DocumentLibraryMetricType.DOWNLOADS.getName(),
+					DocumentLibraryMetricType.PREVIEWS.getName(),
+					DocumentLibraryMetricType.COMMENTS.getName(),
+					DocumentLibraryMetricType.RATINGS.getName()),
+				sorts, timeRange, type);
 		}
 		else if (StringUtils.equals(type, "form")) {
-
-			// TODO
-
+			rows = _getAssetFormRows(
+				channelId, query,
+				SetUtil.of(
+					FormMetricType.SUBMISSIONS.getName(),
+					FormMetricType.VIEWS.getName(),
+					FormMetricType.ABANDONMENTS.getName(),
+					FormMetricType.COMPLETION_TIME.getName()),
+				sorts, timeRange, type);
 		}
 		else if (StringUtils.equals(type, "individual") &&
 				 StringUtils.isEmpty(assetId) &&
@@ -99,14 +124,21 @@ public class ReportDog {
 				assetId, assetType, channelId, query, sorts, timeRange);
 		}
 		else if (StringUtils.equals(type, "journal")) {
-
-			// TODO
-
+			rows = _getAssetJournalRows(
+				channelId, query, SetUtil.of(JournalMetricType.VIEWS.getName()),
+				sorts, timeRange, type);
 		}
 		else if (StringUtils.equals(type, "page")) {
-
-			// TODO
-
+			rows = _getAssetPageRows(
+				channelId, query,
+				SetUtil.of(
+					PageMetricType.VISITORS.getName(),
+					PageMetricType.VIEWS.getName(),
+					PageMetricType.TIME_ON_PAGE.getName(),
+					PageMetricType.BOUNCE_RATE.getName(),
+					PageMetricType.ENTRANCES.getName(),
+					PageMetricType.EXIT_RATE.getName()),
+				sorts, timeRange, type);
 		}
 
 		File file = File.createTempFile("report", ".csv");
@@ -127,6 +159,126 @@ public class ReportDog {
 		return file;
 	}
 
+	private List<String[]> _getAssetBlogRows(
+		Long channelId, String keywords, Set<String> selectedMetrics,
+		String[] sorts, TimeRange timeRange, String type) {
+
+		List<String[]> rows = new ArrayList<>();
+
+		rows.add(_columnHeader.get(type));
+
+		Channel channel = _channelDog.getChannel(channelId);
+
+		List<BlogMetric> blogMetrics = (List<BlogMetric>)_getAssetMetrics(
+			channelId, keywords, selectedMetrics,
+			_getSort(sorts, Order.desc(BlogMetricType.VIEWS.getName())),
+			timeRange, type);
+
+		for (BlogMetric blogMetric : blogMetrics) {
+			rows.add(
+				new String[] {
+					blogMetric.getAssetTitle(), blogMetric.getAssetId(),
+					String.valueOf(
+						blogMetric.getViewsMetric(
+						).getValue()),
+					String.valueOf(
+						blogMetric.getReadingTimeMetric(
+						).getValue()),
+					String.valueOf(
+						blogMetric.getCommentsMetric(
+						).getValue()),
+					String.valueOf(
+						blogMetric.getRatingsMetric(
+						).getValue()),
+					channel.getName()
+				});
+		}
+
+		return rows;
+	}
+
+	private List<String[]> _getAssetDocumentsLibraryRows(
+		Long channelId, String keywords, Set<String> selectedMetrics,
+		String[] sorts, TimeRange timeRange, String type) {
+
+		List<String[]> rows = new ArrayList<>();
+
+		rows.add(_columnHeader.get(type));
+
+		Channel channel = _channelDog.getChannel(channelId);
+
+		List<DocumentLibraryMetric> documentLibraryMetrics =
+			(List<DocumentLibraryMetric>)_getAssetMetrics(
+				channelId, keywords, selectedMetrics,
+				_getSort(
+					sorts,
+					Order.desc(DocumentLibraryMetricType.DOWNLOADS.getName())),
+				timeRange, type);
+
+		for (DocumentLibraryMetric documentLibraryMetric :
+				documentLibraryMetrics) {
+
+			rows.add(
+				new String[] {
+					documentLibraryMetric.getAssetTitle(),
+					documentLibraryMetric.getAssetId(),
+					String.valueOf(
+						documentLibraryMetric.getDownloadsMetric(
+						).getValue()),
+					String.valueOf(
+						documentLibraryMetric.getPreviewsMetric(
+						).getValue()),
+					String.valueOf(
+						documentLibraryMetric.getCommentsMetric(
+						).getValue()),
+					String.valueOf(
+						documentLibraryMetric.getRatingsMetric(
+						).getValue()),
+					channel.getName()
+				});
+		}
+
+		return rows;
+	}
+
+	private List<String[]> _getAssetFormRows(
+		Long channelId, String keywords, Set<String> selectedMetrics,
+		String[] sorts, TimeRange timeRange, String type) {
+
+		List<String[]> rows = new ArrayList<>();
+
+		rows.add(_columnHeader.get(type));
+
+		Channel channel = _channelDog.getChannel(channelId);
+
+		List<FormMetric> formMetrics = (List<FormMetric>)_getAssetMetrics(
+			channelId, keywords, selectedMetrics,
+			_getSort(sorts, Order.desc(FormMetricType.SUBMISSIONS.getName())),
+			timeRange, type);
+
+		for (FormMetric formMetric : formMetrics) {
+			rows.add(
+				new String[] {
+					formMetric.getAssetTitle(), formMetric.getAssetId(),
+					String.valueOf(
+						formMetric.getSubmissionsMetric(
+						).getValue()),
+					String.valueOf(
+						formMetric.getViewsMetric(
+						).getValue()),
+					String.valueOf(
+						formMetric.getAbandonmentsMetric(
+						).getValue()),
+					String.valueOf(
+						formMetric.getCompletionTimeMetric(
+						).getValue()),
+					channel.getName()
+				});
+		}
+
+		return rows;
+	}
+
 	private List<String[]> _getAssetIndividualRows(
 		@Nullable String assetId, String assetType, Long channelId,
 		@Nullable String query, @Nullable String[] sorts,
@@ -143,8 +295,8 @@ public class ReportDog {
 			assetMetricRepository.getKnownIndividuals(
 				assetId, null, channelId,
 				_getMetricType(AssetType.of(assetType)),
-				PageRequest.of(0, _MAX_SIZE, _getSort(sorts)), query,
-				timeRange);
+				PageRequest.of(0, _MAX_SIZE, _getSort(sorts, Order.asc("id"))),
+				query, timeRange);
 
 		Channel channel = _channelDog.getChannel(channelId);
 
@@ -154,6 +306,93 @@ public class ReportDog {
 			rows.add(
 				new String[] {
 					individual.getName(), individual.getEmailAddress(),
+					channel.getName()
+				});
+		}
+
+		return rows;
+	}
+
+	private List<String[]> _getAssetJournalRows(
+		Long channelId, String keywords, Set<String> selectedMetrics,
+		String[] sorts, TimeRange timeRange, String type) {
+
+		List<String[]> rows = new ArrayList<>();
+
+		rows.add(_columnHeader.get(type));
+
+		Channel channel = _channelDog.getChannel(channelId);
+
+		List<JournalMetric> journalMetrics =
+			(List<JournalMetric>)_getAssetMetrics(
+				channelId, keywords, selectedMetrics,
+				_getSort(sorts, Order.desc(JournalMetricType.VIEWS.getName())),
+				timeRange, type);
+
+		for (JournalMetric journalMetric : journalMetrics) {
+			rows.add(
+				new String[] {
+					journalMetric.getAssetTitle(), journalMetric.getAssetId(),
+					String.valueOf(
+						journalMetric.getViewsMetric(
+						).getValue()),
+					channel.getName()
+				});
+		}
+
+		return rows;
+	}
+
+	private List<? extends AssetMetric> _getAssetMetrics(
+		Long channelId, String keywords, Set<String> selectedMetrics,
+		org.springframework.data.domain.Sort sorts, TimeRange timeRange,
+		String type) {
+
+		AssetMetricRepository assetMetricRepository =
+			_assetMetricRepositoryMap.get(AssetType.of(type));
+
+		return assetMetricRepository.getAssetMetrics(
+			channelId, keywords, null, PageRequest.of(0, _MAX_SIZE, sorts),
+			selectedMetrics, timeRange);
+	}
+
+	private List<String[]> _getAssetPageRows(
+		Long channelId, String keywords, Set<String> selectedMetrics,
+		String[] sorts, TimeRange timeRange, String type) {
+
+		List<String[]> rows = new ArrayList<>();
+
+		rows.add(_columnHeader.get(type));
+
+		Channel channel = _channelDog.getChannel(channelId);
+
+		List<PageMetric> pageMetrics = (List<PageMetric>)_getAssetMetrics(
+			channelId, keywords, selectedMetrics,
+			_getSort(sorts, Order.desc(PageMetricType.VISITORS.getName())),
+			timeRange, type);
+
+		for (PageMetric pageMetric : pageMetrics) {
+			rows.add(
+				new String[] {
+					pageMetric.getAssetTitle(), pageMetric.getAssetId(),
+					String.valueOf(
+						pageMetric.getVisitorsMetric(
+						).getValue()),
+					String.valueOf(
+						pageMetric.getViewsMetric(
+						).getValue()),
+					String.valueOf(
+						pageMetric.getTimeOnPageMetric(
+						).getValue()),
+					String.valueOf(
+						pageMetric.getBounceRateMetric(
+						).getValue()),
+					String.valueOf(
+						pageMetric.getEntrancesMetric(
+						).getValue()),
+					String.valueOf(
+						pageMetric.getExitRateMetric(
+						).getValue()),
 					channel.getName()
 				});
 		}
@@ -171,7 +410,8 @@ public class ReportDog {
 		List<Individual> individuals =
 			_bqIndividualRepository.searchBQIndividuals(
 				null, channelId, null, null, null,
-				PageRequest.of(0, _MAX_SIZE, _getSort(sorts)), query, null);
+				PageRequest.of(0, _MAX_SIZE, _getSort(sorts, Order.asc("id"))),
+				query, null);
 
 		Channel channel = _channelDog.getChannel(channelId);
 
@@ -228,10 +468,11 @@ public class ReportDog {
 		return null;
 	}
 
-	private org.springframework.data.domain.Sort _getSort(String[] sorts) {
+	private org.springframework.data.domain.Sort _getSort(
+		String[] sorts, Order defaultOrder) {
+
 		if (ArrayUtils.isEmpty(sorts)) {
-			return org.springframework.data.domain.Sort.by(
-				org.springframework.data.domain.Sort.Order.asc("id"));
+			return org.springframework.data.domain.Sort.by(defaultOrder);
 		}
 
 		List<Sort.Order> orders = new ArrayList<>();
@@ -276,12 +517,56 @@ public class ReportDog {
 		_assetMetricRepositoryMap = new HashMap<>();
 	private final BQIndividualRepository _bqIndividualRepository;
 	private final ChannelDog _channelDog;
+
 	private final Map<String, String[]> _columnHeader =
 		new HashMap<String, String[]>() {
 			{
 				put(
+					"blog",
+					new String[] {
+						"Name", "Id", "Views", "Reading Time", "Comments",
+						"Rating", "Property Name"
+					});
+			}
+
+			{
+				put(
+					"document",
+					new String[] {
+						"Name", "Id", "Downloads", "Previews", "Comments",
+						"Rating", "Property Name"
+					});
+			}
+
+			{
+				put(
 					"individual",
 					new String[] {"Name", "Email", "Property Name"});
+			}
+
+			{
+				put(
+					"journal",
+					new String[] {"Name", "Id", "Views", "Property Name"});
+			}
+
+			{
+				put(
+					"form",
+					new String[] {
+						"Name", "id", "Submissions", "Views", "Abandonment",
+						"Completion time", "Property Name"
+					});
+			}
+
+			{
+				put(
+					"page",
+					new String[] {
+						"Page Title", "Canonical URL", "Unique Visitors",
+						"Views", "Time on Page", "Bounce Rate", "Entrances",
+						"Exit %", "Property Name"
+					});
 			}
 		};
 
