52e6a8f065d1b419436918454bf216492472a553
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-11-09 15:11:33 -0800

    LRAC-14937 Sort

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/dog/test/dependencies/test_report_dog.sql
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/resources/com/liferay/osb/asah/backend/dog/test/dependencies/test_report_dog.sql
@@ -1,11 +1,3 @@
-INSERT INTO Identity_Raw (id, individualId) VALUES ('1', '1');
-INSERT INTO Identity_Raw (id, individualId) VALUES ('2', '2');
-INSERT INTO Identity_Raw (id, individualId) VALUES ('3', '3');
-
-INSERT INTO Individual (emailAddress, firstName, id, lastName) VALUES ('test1@liferay.com', 'Test 1', '1', 'Test 1');
-INSERT INTO Individual (emailAddress, firstName, id, lastName) VALUES ('test2@liferay.com', 'Test 2', '2', 'Test 2');
-INSERT INTO Individual (emailAddress, firstName, id, lastName) VALUES ('test3@liferay.com', 'Test 3', '3', 'Test 3');
-
 INSERT INTO BlogDaily (assetId, assetTitle, channelId, clicks, comments, eventDate, ratings, readTime, userId, views) VALUES ('1', 'Blog 1', 1, 1, 1, TIMESTAMP(DATETIME_TRUNC(timestamp '2023-11-04T17:10:00.666Z', HOUR)), 1, 1, '1', 1);
 INSERT INTO BlogDaily (assetId, assetTitle, channelId, clicks, comments, eventDate, ratings, readTime, userId, views) VALUES ('2', 'Blog 2', 1, 2, 2, TIMESTAMP(DATETIME_TRUNC(timestamp '2023-11-04T17:10:00.666Z', HOUR)), 2, 2, '2', 2);
 INSERT INTO BlogDaily (assetId, assetTitle, channelId, clicks, comments, eventDate, ratings, readTime, userId, views) VALUES ('3', 'Blog 3', 1, 3, 3, TIMESTAMP(DATETIME_TRUNC(timestamp '2023-11-04T17:10:00.666Z', HOUR)), 3, 3, '3', 3);
@@ -17,6 +9,14 @@ INSERT INTO DocumentLibraryDaily (assetId, assetTitle, channelId, comments, down
 INSERT INTO FormDaily (abandonments, assetId, assetTitle, channelId, eventDate, submissions, submissionsTime, userId, views) VALUES (1, '1', 'Form 1', 1, TIMESTAMP(DATETIME_TRUNC(timestamp '2023-11-04T17:10:00.666Z', HOUR)), 1, 1, '1', 1);
 INSERT INTO FormDaily (abandonments, assetId, assetTitle, channelId, eventDate, submissions, submissionsTime, userId, views) VALUES (2, '2', 'Form 2', 1, TIMESTAMP(DATETIME_TRUNC(timestamp '2023-11-04T17:10:00.666Z', HOUR)), 2, 2, '2', 2);
 INSERT INTO FormDaily (abandonments, assetId, assetTitle, channelId, eventDate, submissions, submissionsTime, userId, views) VALUES (3, '3', 'Form 3', 1, TIMESTAMP(DATETIME_TRUNC(timestamp '2023-11-04T17:10:00.666Z', HOUR)), 3, 3, '3', 3);
+
+INSERT INTO Identity_Raw (id, individualId) VALUES ('1', '1');
+INSERT INTO Identity_Raw (id, individualId) VALUES ('2', '2');
+INSERT INTO Identity_Raw (id, individualId) VALUES ('3', '3');
+
+INSERT INTO Individual (emailAddress, firstName, id, lastName) VALUES ('test1@liferay.com', 'Test 1', '1', 'Test 1');
+INSERT INTO Individual (emailAddress, firstName, id, lastName) VALUES ('test2@liferay.com', 'Test 2', '2', 'Test 2');
+INSERT INTO Individual (emailAddress, firstName, id, lastName) VALUES ('test3@liferay.com', 'Test 3', '3', 'Test 3');
 
 INSERT INTO JournalDaily (assetId, assetTitle, channelId, eventDate, userId, views) VALUES ('1', 'Journal 1', 1, TIMESTAMP(DATETIME_TRUNC(timestamp '2023-11-04T17:10:00.666Z', HOUR)), '1', 1);
 INSERT INTO JournalDaily (assetId, assetTitle, channelId, eventDate, userId, views) VALUES ('2', 'Journal 2', 1, TIMESTAMP(DATETIME_TRUNC(timestamp '2023-11-04T17:10:00.666Z', HOUR)), '2', 2);
