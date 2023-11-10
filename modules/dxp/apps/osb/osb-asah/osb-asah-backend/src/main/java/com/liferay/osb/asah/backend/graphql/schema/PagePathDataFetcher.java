1712d9a4e91393421df0cae7e38e297249c56324
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-11-10 14:16:36 -0800

    LRAC-14972 Fail request if segmentId is not a number

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/PagePathDataFetcher.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/graphql/schema/PagePathDataFetcher.java
@@ -24,7 +24,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -96,7 +96,7 @@ public class PagePathDataFetcher extends BaseDataFetcher<PagePathNodeDTO> {
 
 		String segmentId = dataFetchingEnvironment.getArgument("segmentId");
 
-		if (NumberUtils.isCreatable(segmentId)) {
+		if (StringUtils.isNotBlank(segmentId)) {
 			return Long.valueOf(segmentId);
 		}
 
