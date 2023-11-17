725e1cbea9d0b843d2e37c37596f50f73c0b934b
Sillas Cavalcanti <sillas.cavalcanti@liferay.com>
2023-11-17 15:31:59 -0300

    LRAC-14997 Test CSV reports Individuals list for a given blog

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/dog/test/ReportDogTest.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/testIntegration/java/com/liferay/osb/asah/backend/dog/test/ReportDogTest.java
@@ -261,6 +261,52 @@ public class ReportDogTest
 
 	@BQSQLResource(resourcePath = "test_report_dog.sql")
 	@Test
+	public void testGetCSVReportAssetIndividualForAGivenBlogFilteredByEmailInQuery()
+		throws Exception {
+
+		ClassPathResource classPathResource = new ClassPathResource(
+			"dependencies/test_get_csv_report_asset_individual_for_a_given_" +
+				"blog_filtered_by_email_in_query_expected.csv",
+			getClass());
+
+		try (InputStream inputStream = new FileInputStream(
+				_reportDog.getCSVReport(
+					"3", "blog", 1L, "test3@liferay.com", null,
+					TimeRange.of(
+						LocalDate.of(2023, 11, 6), LocalDate.of(2023, 11, 3)),
+					"individual"))) {
+
+			Assertions.assertTrue(
+				IOUtils.contentEquals(
+					classPathResource.getInputStream(), inputStream));
+		}
+	}
+
+	@BQSQLResource(resourcePath = "test_report_dog.sql")
+	@Test
+	public void testGetCSVReportAssetIndividualForAGivenBlogFilteredByNameInQuery()
+		throws Exception {
+
+		ClassPathResource classPathResource = new ClassPathResource(
+			"dependencies/test_get_csv_report_asset_individual_for_a_given_" +
+				"blog_filtered_by_name_in_query_expected.csv",
+			getClass());
+
+		try (InputStream inputStream = new FileInputStream(
+				_reportDog.getCSVReport(
+					"1", "blog", 1L, "Test 1", null,
+					TimeRange.of(
+						LocalDate.of(2023, 11, 6), LocalDate.of(2023, 11, 3)),
+					"individual"))) {
+
+			Assertions.assertTrue(
+				IOUtils.contentEquals(
+					classPathResource.getInputStream(), inputStream));
+		}
+	}
+
+	@BQSQLResource(resourcePath = "test_report_dog.sql")
+	@Test
 	public void testGetCSVReportAssetIndividualForAGivenPageFilteredByEmailInQuery()
 		throws Exception {
 
