e335bb942e278108c266d630caa2f92a085af5b9
Marcos Martins <marcos.martins@liferay.com>
2023-11-28 11:42:02 -0300

    LRAC-15069 register filter

--- modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/spring/OSBAsahBackendSpringBootApplication.java
+++ modules/dxp/apps/osb/osb-asah/osb-asah-backend/src/main/java/com/liferay/osb/asah/backend/spring/OSBAsahBackendSpringBootApplication.java
@@ -8,10 +8,12 @@ package com.liferay.osb.asah.backend.spring;
 import com.liferay.osb.asah.common.spring.OSBAsahSpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 /**
  * @author Vishal Reddy
@@ -42,6 +44,18 @@ public class OSBAsahBackendSpringBootApplication
 				"classpath*:/application.properties"));
 
 		return propertySourcesPlaceholderConfigurer;
+	}
+
+	@Bean
+	public FilterRegistrationBean<ForwardedHeaderFilter>
+		forwardedHeaderFilter() {
+
+		FilterRegistrationBean<ForwardedHeaderFilter> bean =
+			new FilterRegistrationBean<>();
+
+		bean.setFilter(new ForwardedHeaderFilter());
+
+		return bean;
 	}
 
 }
