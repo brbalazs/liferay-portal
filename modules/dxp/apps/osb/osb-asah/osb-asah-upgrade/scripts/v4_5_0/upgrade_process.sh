5b4d865b74bd4c6e733f462e7ccce7f3c5de1ca0
Marcellus Tavares <marcellus.tavares@liferay.com>
2023-12-04 08:52:24 -0800

    LRAC-15103 SF

--- modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_5_0/upgrade_process.sh
+++ modules/dxp/apps/osb/osb-asah/osb-asah-upgrade/scripts/v4_5_0/upgrade_process.sh
@@ -3,14 +3,14 @@
 PROJECT_ID=$(gcloud config get-value project)
 REGION_ID=$(gcloud config get-value compute/region)
 
-function upgrade_partitioned_table {
+function upgrade_identityactivitysummary_table {
 	local asah_project_id=${1}
 
-	sed -e "s/\${asah_project_id}/$asah_project_id/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" -e "s/\${REGION_ID}/$REGION_ID/g" upgrade_partitioned_table.sql > new_upgrade_partitioned_table.sql
+	sed -e "s/\${asah_project_id}/$asah_project_id/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" -e "s/\${REGION_ID}/$REGION_ID/g" upgrade_identityactivitysummary_table.sql > new_upgrade_identityactivitysummary_table.sql
 
-	echo "Upgrade Partitioned Table for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}"
+	echo "Upgrade Identity Activity Summary Table for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}"
 
-	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_partitioned_table.sql
+	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_identityactivitysummary_table.sql
 }
 
 function upgrade_partition_expiration {
@@ -18,14 +18,14 @@ function upgrade_partition_expiration {
 
 	sed -e "s/\${asah_project_id}/$asah_project_id/g" -e "s/\${PROJECT_ID}/$PROJECT_ID/g" -e "s/\${REGION_ID}/$REGION_ID/g" upgrade_partition_expiration.sql > new_upgrade_partition_expiration.sql
 
-	echo "Upgrade Partition expiration for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}"
+	echo "Upgrade Partition Expiration for Project ID: ${PROJECT_ID}, Asah Project ID: ${asah_project_id}"
 
 	bq --project_id ${PROJECT_ID} query --use_legacy_sql=false < new_upgrade_partition_expiration.sql
 }
 
 for i in $(bq ls --datasets=true --max_results=1000 | grep "asah" | grep -v "osbasah" | awk '{$1=$1;print}')
 do :
-	upgrade_partitioned_table_script ${i}
+	upgrade_identityactivitysummary_table ${i}
 
 	upgrade_partition_expiration ${i}
 done
