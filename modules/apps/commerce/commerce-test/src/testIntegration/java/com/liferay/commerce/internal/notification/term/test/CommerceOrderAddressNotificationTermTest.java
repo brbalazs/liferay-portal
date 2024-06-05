a6a49959793211affc86b41bfba232cfad1e1a5c
brbalazs <balazs.breier@gmail.com>
2024-06-05 16:11:37 +0200

    LPD-24672 - Integration test

---
+++ modules/apps/commerce/commerce-test/src/testIntegration/java/com/liferay/commerce/internal/notification/term/test/CommerceOrderAddressNotificationTermTest.java
@@ -0,0 +1,172 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.commerce.internal.notification.term.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
+import com.liferay.commerce.model.CommerceAddress;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.commerce.test.util.CommerceTestUtil;
+import com.liferay.notification.context.NotificationContextBuilder;
+import com.liferay.notification.term.evaluator.NotificationTermEvaluatorTracker;
+import com.liferay.notification.type.util.NotificationTypeUtil;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.model.Country;
+import com.liferay.portal.kernel.model.Region;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Balázs Breier
+ */
+@RunWith(Arquillian.class)
+public class CommerceOrderAddressNotificationTermTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		long userId = TestPropsValues.getUserId();
+
+		long groupId = TestPropsValues.getGroupId();
+
+		_commerceCurrency = CommerceCurrencyTestUtil.addCommerceCurrency(
+			TestPropsValues.getCompanyId());
+
+		_commerceChannel = CommerceTestUtil.addCommerceChannel(
+			groupId, _commerceCurrency.getCode());
+
+		_commerceOrder = CommerceTestUtil.addB2CCommerceOrder(
+			userId, _commerceChannel.getGroupId(), _commerceCurrency);
+
+		CommerceAddress shippingAddress =
+			CommerceTestUtil.addUserCommerceAddress(groupId, userId);
+
+		CommerceAddress billingAddress =
+			CommerceTestUtil.addUserCommerceAddress(groupId, userId);
+
+		_commerceOrder.setBillingAddressId(
+			billingAddress.getCommerceAddressId());
+
+		_commerceOrder.setShippingAddressId(
+			shippingAddress.getCommerceAddressId());
+
+		_commerceOrder = _commerceOrderLocalService.updateCommerceOrder(
+			_commerceOrder);
+	}
+
+	@Test
+	public void testCreateNotificationWithCommerceOrderAddressTerms()
+		throws Exception {
+
+		String content = StringBundler.concat(
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_NAME%] | ",
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_PHONE_NUMBER%] | ",
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_STREET1%] | ",
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_STREET2%] | ",
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_STREET3%] | ",
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_COUNTRY%] | ",
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_ZIP%] | ",
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_CITY%] | ",
+			"[%COMMERCEORDER_SHIPPING_ADDRESS_REGION%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_NAME%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_PHONE_NUMBER%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_STREET1%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_STREET2%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_STREET3%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_COUNTRY%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_ZIP%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_CITY%] | ",
+			"[%COMMERCEORDER_BILLING_ADDRESS_REGION%]");
+
+		Map<String, Object> termValues = HashMapBuilder.<String, Object>put(
+			"externalReferenceCode", _commerceOrder.getExternalReferenceCode()
+		).put(
+			"groupId", _commerceOrder.getGroupId()
+		).put(
+			"id", _commerceOrder.getCommerceOrderId()
+		).build();
+
+		String transformedContent = NotificationTypeUtil.evaluateTerms(
+			content,
+			new NotificationContextBuilder(
+			).className(
+				"com.liferay.commerce.model.CommerceOrder"
+			).classPK(
+				GetterUtil.getLong(termValues.get("id"))
+			).externalReferenceCode(
+				GetterUtil.getString(termValues.get("externalReferenceCode"))
+			).groupId(
+				GetterUtil.getLong(termValues.get("groupId"))
+			).termValues(
+				termValues
+			).build(),
+			_notificationTermEvaluatorTracker);
+
+		String expectedContent = _createExpectedContent();
+
+		Assert.assertEquals(expectedContent, transformedContent);
+	}
+
+	private String _createExpectedContent() throws Exception {
+		CommerceAddress shippingAddress = _commerceOrder.getShippingAddress();
+		CommerceAddress billingAddress = _commerceOrder.getBillingAddress();
+		Country shippingCountry = shippingAddress.getCountry();
+		Country billingCountry = billingAddress.getCountry();
+		Region shippingRegion = shippingAddress.getRegion();
+		Region billingRegion = billingAddress.getRegion();
+
+		return StringBundler.concat(
+			shippingAddress.getName(), " | ", shippingAddress.getPhoneNumber(),
+			" | ", shippingAddress.getStreet1(), " | ",
+			shippingAddress.getStreet2(), " | ", shippingAddress.getStreet3(),
+			" | ", shippingCountry.getTitle(), " | ", shippingAddress.getZip(),
+			" | ", shippingAddress.getCity(), " | ", shippingRegion.getTitle(),
+			" | ", billingAddress.getName(), " | ",
+			billingAddress.getPhoneNumber(), " | ", billingAddress.getStreet1(),
+			" | ", billingAddress.getStreet2(), " | ",
+			billingAddress.getStreet3(), " | ", billingCountry.getTitle(),
+			" | ", billingAddress.getZip(), " | ", billingAddress.getCity(),
+			" | ", billingRegion.getTitle());
+	}
+
+	private CommerceChannel _commerceChannel;
+	private CommerceCurrency _commerceCurrency;
+	private CommerceOrder _commerceOrder;
+
+	@Inject
+	private CommerceOrderLocalService _commerceOrderLocalService;
+
+	@Inject
+	private NotificationTermEvaluatorTracker _notificationTermEvaluatorTracker;
+
+	@Inject
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+}
