8fd1289d5089d2dfc099c70186fcaf6faa4f18d7
brbalazs <balazs.breier@liferay.com>
2026-07-30 09:28:21 +0200

    Revert "LPD-97886 Treat depot roles without a subtype as Space roles"
    This reverts commit e1e28cc9c752181df2e64019e8d778412d7e203d.

--- modules/apps/depot/depot-api/src/main/java/com/liferay/depot/util/DepotRoleUtil.java
+++ modules/apps/depot/depot-api/src/main/java/com/liferay/depot/util/DepotRoleUtil.java
@@ -48,16 +48,21 @@ public class DepotRoleUtil {
 			return roles;
 		}
 
+		if (Objects.equals(
+				subtype, DepotRolesConstants.SUBTYPE_DESIGN_LIBRARY) ||
+			Objects.equals(subtype, DepotRolesConstants.SUBTYPE_PROJECT)) {
+
+			return ListUtil.filter(
+				roles, role -> Objects.equals(role.getSubtype(), subtype));
+		}
+
 		return ListUtil.filter(
 			roles,
 			role -> {
 				String roleSubtype = role.getSubtype();
 
-				if (Validator.isNull(roleSubtype)) {
-					roleSubtype = DepotRolesConstants.SUBTYPE_SPACE;
-				}
-
-				return Objects.equals(roleSubtype, subtype);
+				return Validator.isNull(roleSubtype) ||
+					   Objects.equals(roleSubtype, subtype);
 			});
 	}
 
